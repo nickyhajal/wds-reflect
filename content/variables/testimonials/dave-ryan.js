ea684d6ab6f49cd5115baf151e7575aec1fa7502
Jedd <jedidiah475@gmail.com>
2017-01-18 08:59:44 -0600

    Create dave-ryan.js

---
+++ content/variables/testimonials/dave-ryan.js
@@ -0,0 +1,4 @@
+name: "Dave Ryan",
+content: "I was fearless, empowered, and joyful beyond my wildest expectations.That is a feeling I will never forget and it made me realize that I have been settling for so much less for a very long time. I will do so no longer. I will accept nothing less than surrounding myself with people like this throughout my life. I will strive for the impossible and help everyone I can along the way.",
+link: "http://www.reinventure.org/2015/07/17/how-wds-changed-my-life/",
+photo: "https://i0.wp.com/www.reinventure.org/wp-content/uploads/2015/03/david-ryan-reinventure-3.png"
