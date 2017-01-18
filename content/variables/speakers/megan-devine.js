b4e26f898063edeb6d4c4d0f6d39fc7c9c1c8c1f
Jedd <jedidiah475@gmail.com>
2017-01-18 16:25:43 -0600

    Update megan-devine.js

--- content/variables/speakers/megan-devine.js
+++ content/variables/speakers/megan-devine.js
@@ -1,7 +1,10 @@
 name: "Megan Devine",
+year: "2015",
 headerPhotos: "https://farm4.staticflickr.com/3743/9231403243_9fe13f2384_c.jpg"
 photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Megan+Devine.jpg"
-video: "",
-quote: "",
+video: "//player.vimeo.com/video/134620373?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "Pain deserves acknowledgement, not repair. We need to start telling new stories about bravery in the face of pain that can’t be fixed.",
 ---
-**Megan Devine** is the author of the audio book, When *Everything is Not Okay: Practices to Help You Stay in Your Heart & Not Lose Your Mind*. She writes, speaks, and teaches on out-of-order death and how we withstand suffering that cannot be fixed. Megan is known for her practical, no-nonsense approach to grief, and for her fierce love of her readers and students.Megan will share her story of grief, love, and the power of acknowledgment. She writes for the Huffington Post, Jen Pastiloff’s Manifest-Station, Open to Hope, and Modern Loss, where she is also a contributing editor. Megan lives in Portland with her small menagerie of beasts and fruit trees. Find her at [refugeingrief.com](http://refugeingrief.com).
+**Megan Devine** is the author of the audio book, When *Everything is Not Okay: Practices to Help You Stay in Your Heart & Not Lose Your Mind*. She writes, speaks, and teaches on out-of-order death and how we withstand suffering that cannot be fixed. Megan is known for her practical, no-nonsense approach to grief, and for her fierce love of her readers and students.
+
+Megan will share her story of grief, love, and the power of acknowledgment. She writes for the Huffington Post, Jen Pastiloff’s Manifest-Station, Open to Hope, and Modern Loss, where she is also a contributing editor. Megan lives in Portland with her small menagerie of beasts and fruit trees. Find her at [refugeingrief.com](http://refugeingrief.com).
