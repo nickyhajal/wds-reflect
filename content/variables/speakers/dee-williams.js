013bbe62416386df9a5ece90c793e7651f4b4e46
Jedd <jedidiah475@gmail.com>
2017-01-18 14:45:46 -0600

    Create dee-williams.js

---
+++ content/variables/speakers/dee-williams.js
@@ -0,0 +1,9 @@
+name: "Dee Williams",
+headerPhotos: "https://farm4.staticflickr.com/3906/14655149244_7abd99e07f_b.jpg,https://farm4.staticflickr.com/3915/14657005702_79b6c5cde3_b.jpg,https://farm4.staticflickr.com/3853/14470751329_e7a2458147_b.jpg,https://farm3.staticflickr.com/2908/14470767438_e7469415a7_b.jpg,https://farm4.staticflickr.com/3842/14657374965_c978f0ce8a_b.jpg",
+photo: "",
+video: "//player.vimeo.com/video/102690044?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "I started to realize that if I wanted to be courageous, if I wanted to be a superhero in my own life… I had to do it right now.",
+---
+**Dee Williams** is the co-owner of <a href="http://padtinyhouses.com" target="_blank">Portland Alternative Dwellings</a> where she leads workshops focused on tiny houses, green building, and community design. She's the author of a how-to e-book, *Go House Go*, and her memoir, *The Big Tiny* (Blue Rider, Penguin/Random House) was just released in April 2014.
+
+Dee’s story has been featured in numerous broadcasts including Good Morning America, National Public Radio, PBS, MSNBC, CNN, Der Spiegel, CBC, NBC and Univision. She has also been profiled or featured in hundreds of online blogs and articles, and in printed media including *TIME Magazine*, *The New York Times* and *Yes! Magazine*. She lives in an 84-square foot house in Olympia, Washington with an overly-ambitious Australian shepherd, and in the shadow of her dear friends’ homes.
