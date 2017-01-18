24d77f0721cadc69d0b057a7dbc87efa7f36d96e
Jedd <jedidiah475@gmail.com>
2017-01-18 16:34:21 -0600

    Update emily-mcdowell.js

--- content/variables/speakers/emily-mcdowell.js
+++ content/variables/speakers/emily-mcdowell.js
@@ -1,4 +1,5 @@
 name: "Emily McDowell",
+year: "2016",
 headerPhotos: "https://c1.staticflickr.com/9/8302/28906219592_5fca79ee30_b.jpg,https://c1.staticflickr.com/9/8374/28934826731_ac0f922ec1_b.jpg,https://c2.staticflickr.com/8/7616/28725075450_c2cdc28712_b.jpg,https://c1.staticflickr.com/9/8285/28906216292_4f9818f027_b.jpg",
 photo: "",
 video: "//player.vimeo.com/video/183727918?byline=0&amp;portrait=0&amp;color=adbf27",
