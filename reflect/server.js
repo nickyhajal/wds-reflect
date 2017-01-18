5f24a66d5bf39b913d7f669546636d292ecfdc63
Nicky Hajal <nhajal@gmail.com>
2017-01-18 07:29:00 -0600

    server

--- reflect/server.js
+++ reflect/server.js
@@ -234,7 +234,7 @@ const synced = (req, res) => {
 app.get('/test', (req, res) => {
   res.send('works');
 })
-app.post('/webhook81928181994019',  (req, res) => {
+app.all('/webhook81928181994019',  (req, res) => {
   execFile('wds-reflect-sync', (err, stdout, stderr) => {
   });
   res.send('synced');
