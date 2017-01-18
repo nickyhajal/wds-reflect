03834bb64d46262d891ec42745933f11df966d4b
Jedd <jedidiah475@gmail.com>
2017-01-18 15:52:41 -0600

    Create leo-babauta.js

---
+++ content/variables/speakers/leo-babauta.js
@@ -0,0 +1,9 @@
+name: "Leo Babauta",
+headerPhotos: "https://farm6.staticflickr.com/5063/5798081916_4a73b01afe_b.jpg,https://farm3.staticflickr.com/2755/5798082922_624443a258_b.jpg,https://farm3.staticflickr.com/2609/5797527617_75c19cbaa7_b.jpg,https://farm6.staticflickr.com/5225/5797524437_063292171e_b.jpg,https://farm6.staticflickr.com/5182/5798075092_127b461c6a_b.jpg",
+photo: "",
+video: "//player.vimeo.com/video/28562231?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "",
+---
+**Leo Babauta** is a simplicity blogger and author. He created Zen Habits, a Top 25 blog (according to TIME magazine) with 200,000 subscribers, mnmlist.com, and the best-selling books *Focus*, *The Power of Less*, and *Zen To Done*.
+
+Leo is a former journalist of 18 years, a husband, father of six children, and lives in San Francisco, where he leads a simple life.
