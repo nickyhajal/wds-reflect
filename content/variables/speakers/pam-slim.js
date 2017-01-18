0a39af485c24d961c009930220ff348f4c8d4e90
wkorenek <wekorenek@gmail.com>
2017-01-18 12:20:08 -0800

    Create pam-slim.js

---
+++ content/variables/speakers/pam-slim.js
@@ -0,0 +1,9 @@
+name: "Pam Slim",
+headerPhotos: "https://farm6.staticflickr.com/5061/5797510865_5ef4d0ab43_b.jpg,https://farm8.staticflickr.com/7123/7525321168_f1ef6b950d_b.jpg,https://farm9.staticflickr.com/8432/7523497812_dbe1c47257_b.jpg,https://farm6.staticflickr.com/5450/9226362367_fed50eb317_b.jpg,https://farm4.staticflickr.com/3678/9229137186_e50fc92f54_b.jpg"
+photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Pam+Slim.jpg"
+video: "",
+quote: "",
+---
+**Pamela Slim** is a seasoned business coach, author and professional speaker who helps frustrated employees in corporate jobs break out and start their own business. Her blog, Escape from Cubicle Nation, is one of the top career and marketing blogs on the web. Pam teaches new entrepreneurs how to grow their business ethically and effectively. Her book *Escape from Cubicle Nation: From Corporate Prisoner to Thriving Entrepreneur* won Best Small Business Book of 2009 from 800 CEO Read.
+
+Learn more about Pam on <a href="http://pamelaslim.com" target="_blank">her website</a>.
