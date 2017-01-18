80174ebd7d72c7bd1237faea54da9fac79d91ef2
Jedd <jedidiah475@gmail.com>
2017-01-18 08:22:34 -0600

    Create chris-brogran.js

---
+++ content/variables/testimonials/chris-brogran.js
@@ -0,0 +1,4 @@
+name: "Chris Brogan",
+content: "Put this on your calendar. It’s an experience you won’t soon forget. This event is a must-attend event, if you are someone seeking to build a business of your own, especially if you’re seeking an uncompromising solopreneur lifestyle.",
+link: "http://chrisbrogan.com/1000-true-fans/",
+photo: "https://pbs.twimg.com/profile_images/763830398908502016/cGXoBRCd.jpg"
