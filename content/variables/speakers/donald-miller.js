1a50b7667ceea38ca716a7e6dd049d01e1e8cfe4
wkorenek <wekorenek@gmail.com>
2017-01-18 14:21:20 -0800

    Update donald-miller.js

--- content/variables/speakers/donald-miller.js
+++ content/variables/speakers/donald-miller.js
@@ -1,6 +1,6 @@
 name: "Donald Miller",
 headerPhotos: "https://farm6.staticflickr.com/5333/9242326206_960706b048_c.jpg,https://farm6.staticflickr.com/5529/9242330262_e3637372e8_c.jpg,https://farm8.staticflickr.com/7407/9239548891_f38c41c2ef_c.jpg",
-photo: "http://i.imgur.com/ldUtowo.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Donald+Miller.png",
 video: "//player.vimeo.com/video/70277458?byline=0&amp;portrait=0&amp;color=adbf27",
 quote: "",
 ---
