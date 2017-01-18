55dbae88d9755bb5328c02e8ff69f92c3ba4d251
Jedd <jedidiah475@gmail.com>
2017-01-18 08:19:00 -0600

    Create cc-chapman.js

---
+++ content/variables/testimonials/cc-chapman.js
@@ -0,0 +1,4 @@
+name: "CC Chapman",
+content: "Events are started to make money. The World Domination Summit was started to change lives.",
+link: "http://www.cc-chapman.com/2012/the-people-and-magic-of-world-domination-summit-2012/",
+photo: "http://www.cc-chapman.com/wp-content/uploads/2015/07/CC-Chapman-Cubelean.jpg"
