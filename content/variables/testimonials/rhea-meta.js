d6bfeec84891013380a47cea1d1af09aa085daa6
Nicky Hajal <nhajal@gmail.com>
2017-01-18 07:08:54 -0600

    Update rhea-meta.js

--- content/variables/testimonials/rhea-meta.js
+++ content/variables/testimonials/rhea-meta.js
@@ -1,1 +1,4 @@
-name: "bob"
+name: "Karen Walrond",
+content: "both the speakers and the attendees that I met were not only creative and smart, coming from all walks of life, all age groups, all races, all backgrounds, but they were also generous with their thoughts.  They were interested in each other.  And they really, sincerely wanted each other to succeed.",
+link: "http://www.chookooloonks.com/blog/2011/6/6/so-about-that-world-domination-summit.html",
+photo: "https://pbs.twimg.com/profile_images/755907281019998208/rGibIgeF.jpg"
