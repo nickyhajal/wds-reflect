00691db5c0c0f902afedf72e7233b0444b88f867
wkorenek <wekorenek@gmail.com>
2017-01-18 10:42:52 -0800

    Create steve-schalchlin.js

---
+++ content/variables/speakers/steve-schalchlin.js
@@ -0,0 +1,13 @@
+name: "Steve Schalchlin",
+headerPhotos: "https://farm8.staticflickr.com/7398/9239550931_f0ee765d1e_c.jpg,https://farm4.staticflickr.com/3678/9242331528_3f389ec638_c.jpg,https://farm4.staticflickr.com/3680/9239544765_f6f358c6d4_c.jpg,https://farm4.staticflickr.com/3693/9242331036_ecd3436136_c.jpg"
+photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Steve+Schalchlin.jpg"
+video: "//player.vimeo.com/video/70277459?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "",
+---
+**Steve Schalchlin's** journey as an award-winning composer and lyricist began in Arkansas, the son of a preacher and a nurse. Playing in a gospel rock band in Texas, he left to develop a solo act of rock and jazz standards and original songs, touring all over the US and to fifty countries.
+
+After being given a year to live with HIV/AIDS, he wrote Connected about his visit to an ER; how his friends’ love kept him alive. The music brought him strength and gave birth to his Off-Broadway and London hit musical, "The Last Session."
+
+Steve is a pioneering blogger, has lectured at Harvard and Stanford Universities, and was featured in *The New York Times*, *People* and on MSNBC. George Michael honored Steve, including him in a documentary playing his music on John Lennon’s IMAGINE piano.
+
+Learn more about Steve on <a href="http://bonusroundblog.blogspot.com" target="_blank">his blog</a>.
