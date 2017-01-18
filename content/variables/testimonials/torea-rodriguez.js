bf3a0137f3cb6dc98ce3a09985f1c619496fe865
Jedd <jedidiah475@gmail.com>
2017-01-18 08:55:16 -0600

    Create torea-rodriguez.js

---
+++ content/variables/testimonials/torea-rodriguez.js
@@ -0,0 +1,4 @@
+name: "Torea Rodriguez",
+content: "I go each year to the World Domination Summit so that I can be pushed just outside of my comfort zone, to get out of any ruts that I might be in and to kickstart me into thinking differently.",
+link: "",
+photo: "https://pbs.twimg.com/profile_images/763604952207937536/uUQT_rLr.jpg"
