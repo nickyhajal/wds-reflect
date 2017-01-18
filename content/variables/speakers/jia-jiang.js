e86ac0b0a596748b6acbfbe059e7d083662dc8c3
Jedd <jedidiah475@gmail.com>
2017-01-18 15:35:24 -0600

    Create jia-jiang.js

---
+++ content/variables/speakers/jia-jiang.js
@@ -0,0 +1,11 @@
+name: "Jia Jiang",
+headerPhotos: "https://farm3.staticflickr.com/2860/9231321231_644547d47c_c.jpg,https://farm4.staticflickr.com/3686/9231319931_82d41bd4cd_c.jpg,https://farm8.staticflickr.com/7334/9234103568_31970e3523_c.jpg",
+photo: "",
+video: "//player.vimeo.com/video/70167462?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "",
+---
+**Jia Jiang** is an entrepreneur and blogger who lives in Austin, Texas. As a teenager in China, he met Bill Gates, which inspired him to come to the United States to pursue his dream of entrepreneurship.
+
+After spending his twenties in the corporate world, he finally made the leap to start his own company, four days before his first child was born. 
+
+After being rejected with an investment opportunity, he started a project to overcome his fear and pain of rejection. By doing so, he discovered a new side of society and encountered countless opportunities. Jia’s passion is to help people and organizations understand, befriend, and leverage rejection to achieve success.
