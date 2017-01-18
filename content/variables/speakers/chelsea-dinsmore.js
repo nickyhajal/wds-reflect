360a40e6ec6289d6650ae252a43125ff7b1ea4e3
Jedd <jedidiah475@gmail.com>
2017-01-18 11:07:04 -0600

    Create chelsea-dinsmore.js

---
+++ content/variables/speakers/chelsea-dinsmore.js
@@ -0,0 +1,9 @@
+name: "A.J. Jacobs",
+headerPhotos: "https://c1.staticflickr.com/9/8583/28888720661_768be12070_b.jpg,https://c2.staticflickr.com/8/7557/28346239424_b4427e564e_b.jpg,https://c1.staticflickr.com/9/8733/28348610823_1451df442f_b.jpg,https://c1.staticflickr.com/9/8561/28932889106_830e732ae3_b.jpg",
+photo: "https://lh5.googleusercontent.com/-BLC1j91Fa_g/AAAAAAAAAAI/AAAAAAAAJPE/4q7G98qR7ds/photo.jpg",
+video: "//player.vimeo.com/video/182628422?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "You are limitless. The only limits that you have are the limits you put on yourself. And when you shut down those limitations, you open up to possibility.",
+---
+**Chelsea Dinsmore** is the owner and Chief Inspiration Officer of [Live Your Legend](http://liveyourlegend.net/), a community whose mission is to change the world by helping people find their passion and build a career around the work that only they are capable of doing. Live Your Legend hosts monthly in person meet-ups in over 200 cities across nearly 60 countries, focused on creating in-person connections with like-minded living legends. 
+
+Chelsea is a lover of food, adventure, the ocean, and wine country. When she’s not helping others live their legend, you'll find her whipping up healthy vegetarian recipes, chasing sunsets or exploring the world.
