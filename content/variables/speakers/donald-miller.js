8d561db5e731644b0799a4c1645ee6252a4d0cea
Jedd <jedidiah475@gmail.com>
2017-01-18 15:00:41 -0600

    Create donald-miller.js

---
+++ content/variables/speakers/donald-miller.js
@@ -0,0 +1,9 @@
+name: "Donald Miller",
+headerPhotos: "https://farm6.staticflickr.com/5333/9242326206_960706b048_c.jpg,https://farm6.staticflickr.com/5529/9242330262_e3637372e8_c.jpg,https://farm8.staticflickr.com/7407/9239548891_f38c41c2ef_c.jpg",
+photo: "http://i.imgur.com/ldUtowo.jpg",
+video: "//player.vimeo.com/video/70277458?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "",
+---
+**Donald Miller** is an author and the founder of <a href="http://globalfamilyreunion.com" target="_blank">Storyline</a>, an organization that helps people plan their lives using the elements of story.
+
+Don is the author of multiple New York Times Bestsellers including *Blue Like Jazz* and *A Million Miles in a Thousand Years*. He’s also the founder of The Mentoring Project, a non-profit helping to provide mentors for fatherless children. Don currently lives and works in Washington, DC.
