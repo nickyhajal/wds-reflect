8838a56e7eff6c05b63bdc3231c1ba248d71bec3
wkorenek <wekorenek@gmail.com>
2017-01-18 11:04:26 -0800

    Create scott-harrison.js

---
+++ content/variables/speakers/scott-harrison.js
@@ -0,0 +1,13 @@
+name: "Scott Harrison",
+headerPhotos: "https://farm9.staticflickr.com/8025/7523487602_f85c11bac8_c.jpg,https://farm9.staticflickr.com/8282/7523487892_c16fd092bd_c.jpg,https://farm8.staticflickr.com/7109/7523487438_54c13c58c4_c.jpg"
+photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Scott+Harrison.jpg"
+video: "//player.vimeo.com/video/49843996?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "",
+---
+Scott spent ten years as a nightclub promoter in New York City before leaving to volunteer on a hospital ship off the coast of Liberia, Africa as a volunteer photojournalist. Returning home to New York City two years later, he founded the non-profit organization charity: water in 2006. Turning his full attention to the global water crisis and the one billion people without clean water to drink, he created public installations and innovative online fundraising platforms to spread international awareness of the issue.
+
+In five years, with the help of more than 250,000 donors worldwide, charity: water has raised over $60 million and funded 6,185 water projects in 19 developing nations. Those projects will provide over 2.5 million people with clean, safe drinking water.
+
+Scott was recently recognized in *Fortune Magazine*’s 40 under 40 list, and the Forbes Impact 30 list.
+
+Learn more about Scott and his charity, Water, on <a href="http://www.charitywater.org/about/scotts_story.php" target="_blank">his website</a>.
