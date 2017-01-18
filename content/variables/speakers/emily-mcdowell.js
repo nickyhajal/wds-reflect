92fa69673ab2fecf639ae6fed39f66f373a23892
Jedd <jedidiah475@gmail.com>
2017-01-18 15:12:34 -0600

    Create emily-mcdowell.js

---
+++ content/variables/speakers/emily-mcdowell.js
@@ -0,0 +1,10 @@
+name: "Emily McDowell",
+headerPhotos: "https://c1.staticflickr.com/9/8302/28906219592_5fca79ee30_b.jpg,https://c1.staticflickr.com/9/8374/28934826731_ac0f922ec1_b.jpg,https://c2.staticflickr.com/8/7616/28725075450_c2cdc28712_b.jpg,https://c1.staticflickr.com/9/8285/28906216292_4f9818f027_b.jpg",
+photo: "",
+video: "//player.vimeo.com/video/183727918?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: “Do no let not knowing how to do something stand in the way of not doing it. Innovation is learning while doing.”,
+---
+
+**Emily McDowell** is a writer, illustrator, and entrepreneur who specializes in chronicling the human condition. In 2012, she left a successful career in advertising to launch her greeting card line, making cards for the relationships we actually have. Now a multimillion-dollar stationery and gift company, Emily McDowell Studio products are sold online at [emilymcdowell.com](http://emilymcdowell.com) and in nearly 2,000 stores worldwide. 
+
+In 2015, Emily’s Empathy Cards, designed to help people connect around serious illness and loss, struck a nerve around the world. Her first book, *There Is No Good Card For This: What To Say and Do When Life Gets Scary, Awful, and Unfair To People You Love*, launched in January 2017.
