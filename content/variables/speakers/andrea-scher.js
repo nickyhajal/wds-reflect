bdb07444055f9cf3cbe6886fae502be345d66421
Jedd <jedidiah475@gmail.com>
2017-01-18 16:44:54 -0600

    Update andrea-scher.js

--- content/variables/speakers/andrea-scher.js
+++ content/variables/speakers/andrea-scher.js
@@ -1,4 +1,5 @@
 name: "Andrea Scher",
+year: "2011",
 headerPhotos: "https://farm4.staticflickr.com/3276/5806476089_9e60dd3a67_b.jpg,https://farm3.staticflickr.com/2712/5807039164_289f0dc5af_b.jpg,https://farm4.staticflickr.com/3208/5806474317_bc7823e242_b.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Andrea+Scher.jpg",
 video: "",
