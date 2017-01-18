9e0f48bd6ed8dcc3a433ea1528d1c5b7351ded03
Jedd <jedidiah475@gmail.com>
2017-01-18 15:50:39 -0600

    Create kid-president.js

---
+++ content/variables/speakers/kid-president.js
@@ -0,0 +1,10 @@
+name: "Brad Montague & Robby Novak (aka Kid President)",
+headerPhotos: "https://farm4.staticflickr.com/3743/9231403243_9fe13f2384_c.jpg",
+photo: "",
+video: "//player.vimeo.com/video/134968995?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "We are a rebellion that’s fueled by a joyful vision of how things could be.",
+---
+**Brad Montague** is the creator of <a href="https://www.youtube.com/playlist?list=PLzvRx_johoA-YabI6FWcU-jL6nKA1Um-t" target="_blank">Kid President</a>, the popular web series featured on Rainn Wilson's Soulpancake Youtube channel. Together, he and his brother-in-law **Robby Novak** have created a movement online that has been viewed and shared more than 80 million times. 
+
+With a passion for helping foster creativity and compassion in young people he is also the director of GO! Camp, an experience for teenagers who want to change the world, and co-founder of Love In Stereo, a collective of artists striving to create work that matters. Both he and Robby live and work in Tennessee.
+
