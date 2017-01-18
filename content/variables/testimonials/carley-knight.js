ab607942acf9511205124e1e0be09af5722eda48
Jedd <jedidiah475@gmail.com>
2017-01-18 08:27:14 -0600

    Create carley-knight.js

---
+++ content/variables/testimonials/carley-knight.js
@@ -0,0 +1,4 @@
+name: "Carley Knight",
+content: "I have always felt isolated, like I could not connect with other people. WDS showed me that this was a lie, I could connect with other people on a deep level. I am not invisible. WDS has shown me what is possible when a community of like minded people get together.",
+link: "",
+photo: "https://pbs.twimg.com/profile_images/726876042439217152/3xkBnc_l.jpg"
