d408fc3e73080d70137ac96ecbcd871c1c150518
Jedd <jedidiah475@gmail.com>
2017-01-18 10:13:04 -0600

    Update aj-jacobs.js

--- content/variables/speakers/aj-jacobs.js
+++ content/variables/speakers/aj-jacobs.js
@@ -1,6 +1,6 @@
 name: "A.J. Jacobs",
 headerPhotos: "https://farm6.staticflickr.com/5562/14638069811_1a5acbcd9a_b.jpg,https://farm4.staticflickr.com/3871/14454650740_175e7c4551_b.jpg,https://farm4.staticflickr.com/3842/14639135454_ec58f86ac4_b.jpg,https://farm6.staticflickr.com/5557/14641305855_3b98614438_b.jpg",
-photo: "http://i.imgur.com/ldUtowo.jpg"
+photo: "http://i.imgur.com/ldUtowo.jpg",
 video: "//player.vimeo.com/video/102629302?byline=0&amp;portrait=0&amp;color=adbf27",
 quote: "I've been to Disneyworld, and that's supposed to be the happiest place on Earth. It's not. I actually think that Portland, during WDS, might be the happiest place on Earth.",
 ---
