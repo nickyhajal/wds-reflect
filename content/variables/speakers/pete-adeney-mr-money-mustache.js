3ec943a73ea042791313d13e7e201e7c498d2dbf
wkorenek <wekorenek@gmail.com>
2017-01-18 12:10:27 -0800

    Create pete-adeney-mr-money-mustache.js

---
+++ content/variables/speakers/pete-adeney-mr-money-mustache.js
@@ -0,0 +1,9 @@
+name: "Mr. Money Mustache (Pete Adeney)",
+headerPhotos: "https://c1.staticflickr.com/9/8426/28362385233_27b2b601b6_b.jpg,https://c1.staticflickr.com/9/8427/28902773171_d6e1f5a414_b.jpg,https://c1.staticflickr.com/9/8752/28946719926_842e7dd7af_b.jpg"
+photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Pete+Adeney.jpg"
+video: "//player.vimeo.com/video/183016901?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "If you can get yourself financially independent, not only does it give you the power to eliminate most of the bullshit in life, but it frees your life’s work to become more truthful.",
+---
+**Mr Money Mustache** (Pete Adeney in real life) retired with his wife at the age of 30 to start a family. Six years into his new life, he realized his peers were still not only stuck in their well-paying jobs, but barely able to meet their ever-increasing lifestyle inflation bills.
+
+Pete formed the blog [Mr Money Mustache](link: http://www.mrmoneymustache.com/) to teach fellow Americans how to live a slightly less ridiculous life than average in order to amass an incredible surplus of money while still young and able to put it to good use. The blog has reached over 16 million people and become a study on life, happiness, and the joy of being able to focus on work that means something to you.
