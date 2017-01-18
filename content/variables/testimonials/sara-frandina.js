a3b6380fd387adbbb549e62c0d6f1c672580dc7b
Jedd <jedidiah475@gmail.com>
2017-01-18 08:37:34 -0600

    Create sara-frandina.js

---
+++ content/variables/testimonials/sara-frandina.js
@@ -0,0 +1,4 @@
+name: "Sara Frandina",
+content: "WDS is exhilarating. Overwhelming. Authentic. But even more important than what WDS is is what WDS does. It’s power to inspire is underestimated. And it will completely change your perspective, at least on a few things, forever.",
+link: "http://sarafrandina.com/2015/07/25/what-world-domination-summit-will-give-you/",
+photo: "http://sarafrandina.com/wp-content/uploads/2016/11/SaraFrandina_blogsidebar.jpg"
