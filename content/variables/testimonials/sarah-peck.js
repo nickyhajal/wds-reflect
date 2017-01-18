f8145cb7a346657c511189d18d963cb9d3e91520
Nicky Hajal <nhajal@gmail.com>
2017-01-18 07:03:50 -0600

    Update and rename holly-merand.js to sarah-peck.js

---
+++ content/variables/testimonials/sarah-peck.js
@@ -0,0 +1,4 @@
+name: "Sarah Peck",
+content: "Chris pulled together a team of outstanding, energetic people who hustled for an entire year to put together what was arguably one of the best-executed events I've ever attended.",
+link: "http://sarahkpeck.com/2011/06/wds-1/",
+photo: "https://pbs.twimg.com/profile_images/579482586269007872/qqEa3hcj.jpg"
