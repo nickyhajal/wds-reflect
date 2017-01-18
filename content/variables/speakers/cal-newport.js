8c6b19ddba80368f9bcaa940c385f6c12bdeb02c
Jedd <jedidiah475@gmail.com>
2017-01-18 10:16:39 -0600

    Create cal-newport.js

---
+++ content/variables/speakers/cal-newport.js
@@ -0,0 +1,10 @@
+name: "Cal Newport",
+headerPhotos: "https://farm8.staticflickr.com/7118/7532100674_c43a5e961a_c.jpg,https://farm9.staticflickr.com/8024/7532101382_e8741c4674_c.jpg,https://farm8.staticflickr.com/7260/7532100462_c05c7b0246_c.jpg"
+photo: "http://calnewport.com/wp-content/uploads/2013/09/cal-newport-med.jpg",
+video: "//player.vimeo.com/video/48041227?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "The path to a passionate life is often way more complex than the simple advice 'follow your passion' would suggest.",
+---
+**Cal Newport** is a writer and an assistant professor of Computer Science at Georgetown University. In his writing, Newport applies a systematic approach to decoding patterns of success.
+
+His first three books, published by Random House, focused on the world of students, offering unconventional, evidence-based approaches to succeeding in school (hint: do less). His fourth book, which argues that “follow your passion” is bad career advice.
+
