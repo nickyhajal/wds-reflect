8540e84c4ef3d735454b0f39da757a3303c9cc69
Jedd <jedidiah475@gmail.com>
2017-01-18 15:55:40 -0600

    Create lewis-howes.js

---
+++ content/variables/speakers/lewis-howes.js
@@ -0,0 +1,9 @@
+name: "Lewis Howes",
+headerPhotos: "https://farm4.staticflickr.com/3743/9231403243_9fe13f2384_c.jpg",
+photo: "",
+video: "//player.vimeo.com/video/134968997?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "“What is holding you back? What is keeping you from greatness?",
+---
+**Lewis Howes** has achieved many dreams. He has been a professional athlete, represented The USA as a member of the national team for handball, and has built a multi-million dollar online business by helping thousands of people around the world make a full time living doing what they love.
+
+He is the host of The School of Greatness podcast which gets 700,000 downloads a month, publisher of *Greatness Magazine*, advises in many tech start-ups, is an author, and sits on the board of advisors for Pencils of Promise. You can learn more at <a href="http://lewishowes.com" target="_blank">lewishowes.com</a>.
