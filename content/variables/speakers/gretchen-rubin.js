c73b0b99e4a8d8123e268a5ddf4f0a99ed1673fd
wkorenek <wekorenek@gmail.com>
2017-01-18 14:47:18 -0800

    Update gretchen-rubin.js

--- content/variables/speakers/gretchen-rubin.js
+++ content/variables/speakers/gretchen-rubin.js
@@ -1,7 +1,7 @@
 name: "Gretchen Rubin",
 year: "2013",
 headerPhotos: "https://farm8.staticflickr.com/7444/9231360227_ef972e9c3a_c.jpg,https://farm6.staticflickr.com/5519/9234165124_4f1ca905f8_c.jpg,https://farm8.staticflickr.com/7428/9231358917_657d19838f_c.jpg",
-photo: "",
+photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Gretchen+Rubin.jpg",
 video: "//player.vimeo.com/video/70277456?byline=0&amp;portrait=0&amp;color=adbf27",
 quote: "",
 ---
