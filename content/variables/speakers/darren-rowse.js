4e2a9dacb82ff9de8ad201234b290267a13edfef
Jedd <jedidiah475@gmail.com>
2017-01-18 14:35:53 -0600

    Create darren-rowse.js

---
+++ content/variables/speakers/darren-rowse.js
@@ -0,0 +1,13 @@
+name: "Darren Rowse",
+headerPhotos: "https://farm6.staticflickr.com/5527/9226361671_bf39225e1b_c.jpg,https://farm3.staticflickr.com/2813/9229142338_451212eb1f_c.jpg,https://farm4.staticflickr.com/3746/9229141310_9dc114f7f8_c.jpg",
+photo: "",
+video: "//player.vimeo.com/video/70159694?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "",
+---
+**Darren Rowse** started his first ever blog in 2002, not knowing that what would start as a personal hobby site would grow into a full time profession and business. Since that time, Darren has been involved in publishing numerous blogs but now focuses his time editing Digital Photography School and ProBlogger.
+
+At WDS 2013 Darren, the original Problogger, arrived from Australia with his team of Aussie empire builders. He also challenged us to lead an unconventional life and follow a dream… from zero to hero.
+
+Darren’s blogs are read by around five million visitors a month and have given him opportunity to co-author a book, publish 15 eBooks, run an annual training event for bloggers, and found numerous companies including b5media and the Third Tribe.
+
+Darren is passionate about communication, building community and using social media for social good (and good coffee). He lives in Melbourne, Australia with his wife and three young sons.
