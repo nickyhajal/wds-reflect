c65a6409540b5ccf94f0ef5a5c33766479260189
wkorenek <wekorenek@gmail.com>
2017-01-18 08:08:21 -0800

    Create zach-anner.js

---
+++ content/variables/speakers/zach-anner.js
@@ -0,0 +1,9 @@
+name: "Zach Anner",
+headerPhotos: "https://c1.staticflickr.com/9/8587/28912796851_af0f4a7288_b.jpg,https://c1.staticflickr.com/9/8299/28883938652_dbaf105039_b.jpg,https://c1.staticflickr.com/9/8806/28372188003_a42ef22998_b.jpg,https://c1.staticflickr.com/9/8084/28703054730_fc57804e46_b.jpg",
+photo: "https://pbs.twimg.com/profile_images/565844788528693248/nL8H3Ijz_400x400.jpeg"
+video: "//player.vimeo.com/video/183042075?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "We have a very short amount of time on the Earth and it is our responsibility to try and use that time to make a legacy and an impact. I don’t mind if anybody fails, or if anybody screws up, but if you have that voice in your head that’s telling you to do something, at least engage with that, and make it the best effort to make a mark on the world, and do it with compassion and love.",
+---
+**Zach Anner** is the author of the comedic memoir *If at Birth You Don't Succeed*, published by Henry Holt in March 2016. He is an award-winning comedian, show host, and public speaker who won his own travel show on the Oprah Winfrey Network called "Rollin’ With Zach." He’s also hosted several web series on his [YouTube channel](https://www.youtube.com/channel/UCPTVYxUoYWhNa8J7GzIGnyQ), garnering over 13 million hits. 
+
+Zach lives in Buffalo, NY and Austin, TX, and spends most of his spare time in his underwear, thinking about how he can change the world.
