1c53eb810e409d785e374754b1bf823f8ab291df
Jedd <jedidiah475@gmail.com>
2017-01-18 15:37:32 -0600

    Create jodi-ettenberg.js

---
+++ content/variables/speakers/jodi-ettenberg.js
@@ -0,0 +1,10 @@
+name: "Jodi Ettenberg",
+headerPhotos: "https://farm6.staticflickr.com/5240/5816271749_c274c5ceac_b.jpg,https://farm4.staticflickr.com/3305/5816270307_fa8a1db11d_b.jpg,https://farm4.staticflickr.com/3457/5816840534_c02821ffa2_b.jpg,https://farm4.staticflickr.com/3235/5816839582_e1af4eb7b0_b.jpg",
+photo: "",
+video: "//player.vimeo.com/video/285611322?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "",
+---
+**Jodi Ettenberg** knew ever since she watched a PBS documentary about the Trans-Siberian trains in the mid-1990s that she wanted to travel to faraway places. After working as a lawyer for five years to save up money for travel, Jodi quit her job and embarked upon a trip around the world. Three years later, she has traipsed through South America, Siberia, Mongolia, China and a good swath of Southeast Asia, with no plans to stop anytime soon.
+
+Throughout, Jodi has blogged about her misadventures on local transportation, her obsessive love of street food and the politics of the places she visits. She currently works as a freelance writer (while trying to eat as much sticky rice as possible).
+
