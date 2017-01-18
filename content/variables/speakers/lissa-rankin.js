b5cc6754396ff60ff7ca0f9ed1018ec30b5603e8
Jedd <jedidiah475@gmail.com>
2017-01-18 16:00:19 -0600

    Create lissa-rankin.js

---
+++ content/variables/speakers/lissa-rankin.js
@@ -0,0 +1,11 @@
+name: "Lissa Rankin",
+headerPhotos: "https://farm8.staticflickr.com/7438/9242331680_8926700e54_b.jpg",
+photo: "",
+video: "//player.vimeo.com/video/142905975?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "We need you. We need you to stand up and step into your purpose. Find the courage to use your voice, use your gifts, say yes to adventure, and to call upon one another for help.",
+---
+**Lissa Rankin** is a physician, speaker, and the New York Times bestselling author of *Mind Over Medicine* and *The Fear Cure*. Lissa is also the founder of the Whole Health Medicine Institute and a spiritual seeker. Passionate about what makes people optimally healthy and what predisposes them to illness, she's on a mission to merge science and spirituality in a way that not only facilitates the health of the individual, it also heals the collective.
+
+As she became aware of how fear dominates modern culture and how such fear predisposes us not only to unhappiness but to disease, she began researching ways to befriend fear so we can let it heal and liberate us, opening us up to greater compassion, not just for others, but for ourselves.
+
+Lissa has starred in two PBS specials and also leads spirituality workshops, both online, as well as at retreat centers like Esalen, Kripalu, and Omega. When doing what she can to sprinkle pixie dust on a fear-based culture, Lissa loves to hike, ski, and dance. She lives in the San Francisco Bay area with her daughter. Read her blog and learn more at <a href="http://lissarankin.com" target="_blank">LissaRankin.com</a>.
