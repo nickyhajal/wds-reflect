73f630b4103f2099aa02cf3a7efbea5eac809859
Jedd <jedidiah475@gmail.com>
2017-01-18 11:28:31 -0600

    Create chris-brogan.js

---
+++ content/variables/speakers/chris-brogan.js
@@ -0,0 +1,9 @@
+name: "Chris Brogan",
+headerPhotos: "https://farm8.staticflickr.com/7265/7531933544_8607b4d336_c.jpg,https://farm9.staticflickr.com/8014/7531933268_a7f05bd1f7_c.jpg,https://farm8.staticflickr.com/7257/7531930914_c8b9cd4433_c.jpg",
+photo: "https://pbs.twimg.com/profile_images/763830398908502016/cGXoBRCd.jpg",
+video: "//player.vimeo.com/video/48086341?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "The opposite of fear is not courage or bravery; the opposite of fear is surrender...Fear is part of courage.",
+---
+**Chris Brogan** is president of <a href="http://owner.media/about/" target="_blank">Human Business Works</a>, a media and education company. He consults and speaks professionally with Fortune 100 and 500 companies like PepsiCo, General Motors, Microsoft, and more, about the intersection of business, technology and media. He is a New York Times bestselling co-author of *Trust Agents*, and a featured monthly columnist at *Entrepreneur* Magazine.
+
+Chris’s blog is in the Top 5 of the Advertising Age Power 150. He has over 12 years experience in online community, social media, and related technologies.
