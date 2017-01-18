0661a59f926da9469e6e42b743ff931f764bd58d
wkorenek <wekorenek@gmail.com>
2017-01-18 09:36:30 -0800

    Create susan-cain.js

---
+++ content/variables/speakers/susan-cain.js
@@ -0,0 +1,11 @@
+name: "Susan Cain",
+headerPhotos: "https://farm8.staticflickr.com/7251/7523489364_2d57cb687f_c.jpg,https://farm8.staticflickr.com/7115/7523490210_034ece4614_c.jpg,https://farm9.staticflickr.com/8288/7523489534_a9a950fc31_c.jpg"
+photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Susan+Cain.jpg"
+video: "//player.vimeo.com/video/48093792?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "",
+---
+**Susan Cain** is the author of the instant New York Times bestseller *QUIET: The Power of Introverts in A World That Can’t Stop Talking*, which has been translated into more than 20 languages. Her writing on introversion and shyness has appeared in the *The New York Times*; The Dallas Morning News; *O, The Oprah Magazine*; Time.com; and on PsychologyToday.com.
+
+Cain has also spoken at Microsoft, Google, the U.S. Treasury, and at TED 2012. Since her TED talk was posted online, it has been viewed more than one million times. She has appeared on national broadcast television and radio including CBS “This Morning,” NPR’s “All Things Considered,” NPR’s “Diane Rehm,” and her work has been featured on the cover of *Time* magazine, in *The Atlantic*, *Wired*, *Fast Company*, *Real Simple*, *Fortune*, *Forbes*, *USA Today*, *The Washington Post*, CNN, Slate.com, and many other publications.
+
+Learn more about Susan on <a href="http://www.quietrev.com/author/susan-cain/" target="_blank">her website</a>.
