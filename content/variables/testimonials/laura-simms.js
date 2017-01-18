df86dea90bc8a820212c9cb43708bf1f6caa27c0
Jedd <jedidiah475@gmail.com>
2017-01-18 08:13:03 -0600

    Create laura-simms.js

---
+++ content/variables/testimonials/laura-simms.js
@@ -0,0 +1,4 @@
+name: "Laura Simms",
+content: "Belonging. I have a notebook full of quotes and ah-has from the weekend, but what I experienced was belonging. It was someone buying me a donut without me asking. It was dancing like a full-out goober with a couple hundred people. It was knowing shared laughter at things you know other people just wouldn't get. It was happily sharing meals with people who would not otherwise spend time together. It was 1,000 people standing in unison, without hesitation, when asked to donate their birthdays to charity. It was insta-friend. Just because you were there. When was the last time you truly felt like you belonged?",
+link: "http://www.withlaurasimms.com/blog/world-domination-summit-a-moment-you-missed",
+photo: "https://static1.squarespace.com/static/565aee11e4b06b23bf2b35da/t/565d8cd3e4b04b21693479e9/1448971476880/Career+Coach+Laura+Simms?format=500w"
