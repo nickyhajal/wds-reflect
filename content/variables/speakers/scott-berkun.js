2cb2d553867278baec79fa7b1ad589b8a0aaaa70
wkorenek <wekorenek@gmail.com>
2017-01-18 11:10:04 -0800

    Create scott-berkun.js

---
+++ content/variables/speakers/scott-berkun.js
@@ -0,0 +1,9 @@
+name: "Scott Berkun",
+headerPhotos: "https://farm4.staticflickr.com/3857/14471002297_1aee57cfc8_b.jpg,https://farm3.staticflickr.com/2906/14470805059_70d9f22033_b.jpg,https://farm4.staticflickr.com/3910/14657427755_d73f113be5_b.jpg,https://farm3.staticflickr.com/2919/14654205341_6a015b30ec_b.jpg,https://farm4.staticflickr.com/3841/14654206221_fa962203ee_b.jpg"
+photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Scott+Berkun.jpg"
+video: "//player.vimeo.com/video/102690008?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "The whole notion of creativity is all about the personal way you develop ideas. There’s no toolkit that can perfectly work for everybody. The way you find answers to your problems is going to be unique to you.",
+---
+**Scott Berkun** is the bestselling author of *Making Things Happen*, *The Myths of Innovation*, *Confessions of a Public Speaker*, and *The Year Without Pants: WordPress.com & The Future of Work*. His work as a speaker and writer have appeared on CNBC, MSNBC, CNN, NPR, *The New York Times*, *The Washington Post*, *The Wall Street Journal*, *Wired Magazine*, *Forbes*, *The Economist*, and other media. 
+
+Scott runs a popular blog with videos, podcasts, and essays, at <a href="http://scottberkun.com" target="_blank">scottberkun.com</a> and tweets at @berkun.
