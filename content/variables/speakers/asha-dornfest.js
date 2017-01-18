50425568c3e43c90ff197c61c4e84f00005c1d06
Jedd <jedidiah475@gmail.com>
2017-01-18 09:42:48 -0600

    Create asha-dornfest.js

---
+++ content/variables/speakers/asha-dornfest.js
@@ -0,0 +1,11 @@
+name: "Asha Dornfest",
+headerPhotos: "https://farm4.staticflickr.com/3743/9231403243_9fe13f2384_c.jpg",
+photo: "https://pbs.twimg.com/profile_images/560844143476232193/iQcTpq2P.jpeg"
+video: "//player.vimeo.com/video/134968994?byline=0&amp;portrait=0&amp;color=adbf27",
+quote: "Trust the path, even when it goes off the map.",
+---
+**Asha Dornfest** is a writer. For the last 20 years, she’s written books and articles about everything from building websites to raising kids, most recently, *Minimalist Parenting* with co-author Christine Koh. 
+
+In 2005, Asha founded <a href="http://parenthacks.com" target="_blank">Parent Hacks</a>, an award-winning blog that celebrates parents’ unconventional discoveries and clever life hacks. Since then, Asha’s work has been featured in the *Wall Street Journal*, *Real Simple*, *Family Circle*, *Parents*, *Parenting*, and more. She’s been a guest (billed, ironically, as a parenting expert) on local and national television shows and radio broadcasts, including CNN, ABC News Now and the Ronn Owens Show. Asha has also spoken about writing, entrepreneurship, and online community building at SXSW, BlogHer, Mom 2.0 Summit, and TechFest NW. 
+
+Asha’s most recent book, Parent Hacks: 134 Genius Shortcuts for Life with Kids, is an illustrated collection of the best of Parent Hacks.
