2d24f274544a9007794fa61827e4459730e1c478
wkorenek <wekorenek@gmail.com>
2017-01-18 10:19:50 -0800

    Update caroline-webb.js

--- content/variables/speakers/caroline-webb.js
+++ content/variables/speakers/caroline-webb.js
@@ -1,6 +1,6 @@
 name: "Caroline Webb",
 headerPhotos: "https://c1.staticflickr.com/9/8894/28874016412_0f01249b5e_b.jpg,https://c1.staticflickr.com/9/8742/28978946675_dfda22c702_b.jpg,https://c1.staticflickr.com/9/8355/28978940125_b61dcefd28_b.jpg,https://c1.staticflickr.com/9/8808/28902763101_f5bdda93e4_b.jpg",
-photo: "https://pbs.twimg.com/profile_images/663681919297691648/vxLAs1hU.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Caroline+Webb.jpg",
 video: "//player.vimeo.com/video/182628420?byline=0&amp;portrait=0&amp;color=adbf27",
 quote: "It turns out that it is possible to imagine a world where being deliberate about your intentions for where you want to put your attention—in other words being more mindful of what’s top of mind for you when you into any situation—can make your reality 10% better.",
 ---
