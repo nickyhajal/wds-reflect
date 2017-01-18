16b66d88269cbbbd9217126c114be08005a346f2
Jedd <jedidiah475@gmail.com>
2017-01-18 16:35:23 -0600

    Update donald-miller.js

--- content/variables/speakers/donald-miller.js
+++ content/variables/speakers/donald-miller.js
@@ -1,4 +1,5 @@
 name: "Donald Miller",
+year: "2013",
 headerPhotos: "https://farm6.staticflickr.com/5333/9242326206_960706b048_c.jpg,https://farm6.staticflickr.com/5529/9242330262_e3637372e8_c.jpg,https://farm8.staticflickr.com/7407/9239548891_f38c41c2ef_c.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Donald+Miller.png",
 video: "//player.vimeo.com/video/70277458?byline=0&amp;portrait=0&amp;color=adbf27",
