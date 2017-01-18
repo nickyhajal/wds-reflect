533925680f95d15c18c3577c51404e5a394794ef
Jedd <jedidiah475@gmail.com>
2017-01-18 16:18:01 -0600

    Update saki-mafundikwa.js

--- content/variables/speakers/saki-mafundikwa.js
+++ content/variables/speakers/saki-mafundikwa.js
@@ -1,4 +1,5 @@
 name: "Saki Mafundikwa",
+year: "2014",
 headerPhotos: "https://farm4.staticflickr.com/3867/14656885345_d7c40a316a_b.jpg,https://farm6.staticflickr.com/5567/14633924786_737812d5c9_b.jpg,https://farm4.staticflickr.com/3920/14633924916_4f2eb41681_b.jpg,https://farm3.staticflickr.com/2913/14633924186_9c25305a1f_b.jpg,https://farm6.staticflickr.com/5586/14656887425_47ed796953_b.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Saki+Mafundikwa.jpg",
 video: "//player.vimeo.com/video/102690003?byline=0&amp;portrait=0&amp;color=adbf27",
