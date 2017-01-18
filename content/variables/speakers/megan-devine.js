1350f199507b815d01f30639f5516510ce8c9839
wkorenek <wekorenek@gmail.com>
2017-01-18 13:55:35 -0800

    Create megan-devine.js

---
+++ content/variables/speakers/megan-devine.js
@@ -0,0 +1,7 @@
+name: "Megan Devine",
+headerPhotos: "https://farm4.staticflickr.com/3743/9231403243_9fe13f2384_c.jpg"
+photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Megan+Devine.jpg"
+video: "",
+quote: "",
+---
+**Megan Devine** is the author of the audio book, When *Everything is Not Okay: Practices to Help You Stay in Your Heart & Not Lose Your Mind*. She writes, speaks, and teaches on out-of-order death and how we withstand suffering that cannot be fixed. Megan is known for her practical, no-nonsense approach to grief, and for her fierce love of her readers and students.Megan will share her story of grief, love, and the power of acknowledgment. She writes for the Huffington Post, Jen Pastiloff’s Manifest-Station, Open to Hope, and Modern Loss, where she is also a contributing editor. Megan lives in Portland with her small menagerie of beasts and fruit trees. Find her at [refugeingrief.com](http://refugeingrief.com).
