6e5573625f5b388e61efa5ca32a37954ad0dd034
Nicky Hajal <nhajal@gmail.com>
2017-01-19 09:27:21 -0600

    Update academies.js

--- content/pages/academies.js
+++ content/pages/academies.js
@@ -17,12 +17,12 @@
   <Block cols="6" css={{ padding: '50px 0 20px' }}>
    ## Academies will return for WDS 2017 with more amazing topics and Academy leaders!
    The list of WDS 2017 Academies will be made available spring 2017 so stay tuned! Here's a snippet of awesome Academies from previous WDS years:
-<ul>
-<li><b>Pro-Blogger Academy:</b> How to level up your blogging game with Darren Rowse and friends</li>
-<li><b>Rejection Academy:</b> How to overcome fear of failure and turn rejection into opportunities with Jia Jiang</li>
-<li><b>Launchpad to the World:</b> How to use miles and points to achieve your travel dreams with Stephanie Zito</li>
-<li><b>The Science of Happiness:</b> Proven strategies to live happier with Vanessa Van Edwards</li>
-</ul>
+    <ul>
+       <li><b>Pro-Blogger Academy:</b> How to level up your blogging game with Darren Rowse and friends</li>
+       <li><b>Rejection Academy:</b> How to overcome fear of failure and turn rejection into opportunities with Jia Jiang</li>
+       <li><b>Launchpad to the World:</b> How to use miles and points to achieve your travel dreams with Stephanie Zito</li>
+       <li><b>The Science of Happiness:</b> Proven strategies to live happier with Vanessa Van Edwards</li>
+    </ul>
  </Block>
 </Section>
 <Import id="join-us" />
