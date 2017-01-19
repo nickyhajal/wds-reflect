14b02279e350056d49249b504fdd772701ca7309
Jedd <jedidiah475@gmail.com>
2017-01-19 16:16:13 -0600

    Update local.js

--- content/pages/local.js
+++ content/pages/local.js
@@ -5,26 +5,62 @@
   headerSize="thin"
   color="white"
 >
-  <Image src="heading/local.png" width="565px" height="169px" className="scriptHeader" />
+  <Image src="heading/local.png" width="545px" height="149px" className="scriptHeader" />
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     ## Living out our remarkable lives in community
-    WDS Local is a way that alumni can gather regularly through out the year, support each other, and bring WDS home to their local communities. It's a way to continue conversations and go on new adventures together outside the annual WDS gathering in Portland. We know that the best thing about WDS is you, the people who make up this vibrant and wonderful community. And there's nothing better than for awesome people to hang out more often with other awesome people.
-
-We need you.
+    WDS Local is a way that alumni can gather regularly through out the year, support each other, and bring WDS home to their local communities. It's a way to continue conversations and go on new adventures together outside the annual WDS gathering in Portland.
+    
+    WDS Local is for anyone that has previously attended a WDS, or who knows an alum and wants to participate in a great community. To join, find a group in your area and connect on Facebook!
   </Block>
   <Block margin="125px 0 50px 0">
     ## WDS Local Group Directory
     <Block type="image-narrow" src="photos/wdslocal/los.angeles.WDS.local.png">
       ## Los Angeles, California
-      <Button to="/schedule" fitToText>Join Up</Button>
+      <Button to="https://www.facebook.com/groups/WDSLocalLosAngeles/" fitToText>Join Up</Button>
     </Block>
     <Block type="image-narrow" src="photos/wdslocal/minneapolis.st.paul.WDS.local.png">
       ## Minneapolis/St.Paul, Minnesota
-      <Button to="/schedule" fitToText>Join Up</Button>
+      <Button to="https://www.facebook.com/groups/WDSLocalMSP/" fitToText>Join Up</Button>
     </Block>
     <Block type="image-narrow" src="photos/wdslocal/new.york.city.WDS.local.png">
       ## New York City, New York
-      <Button to="/schedule" fitToText>Join Up</Button>
+      <Button to="https://www.facebook.com/groups/NYCWDS/" fitToText>Join Up</Button>
+    </Block>
+ <Block type="image-narrow" src="photos/wdslocal/philadelphia.WDS.local.png">
+      ## Philadelphia, Pennsylvania
+      <Button to="https://www.facebook.com/groups/WDSLocalPhiladelphia/" fitToText>Join Up</Button>
+    </Block>
+    <Block type="image-narrow" src="photos/wdslocal/portland.wds.local.jpg">
+      ## Portland, Oregon
+      <Button to="https://www.facebook.com/groups/pdxwds/" fitToText>Join Up</Button>
+    </Block>
+    <Block type="image-narrow" src="photos/wdslocal/san.diego.WDS.local.jpg">
+      ## San Diego, California
+      <Button to="https://www.facebook.com/groups/362794770457200/" fitToText>Join Up</Button>
+    </Block>
+ <Block type="image-narrow" src="photos/wdslocal/san.francisco.WDS.local.png">
+      ## San Francisco, California
+      <Button to="https://www.facebook.com/groups/BayAreaWDSAlums/" fitToText>Join Up</Button>
+    </Block>
+    <Block type="image-narrow" src="photos/wdslocal/seattle.WDS.local.jpg">
+      ## Seattle, Washington
+      <Button to="https://www.facebook.com/groups/WDSLocalSeattle/" fitToText>Join Up</Button>
+    </Block>
+    <Block type="image-narrow" src="photos/wdslocal/toronto.WDS.local.png">
+      ## Toronto, Canada
+      <Button to="https://www.facebook.com/groups/WDSToronto/" fitToText>Join Up</Button>
+    </Block>
+ <Block type="image-narrow" src="photos/wdslocal/vanouver.WDS.local.png">
+      ## Vancouver, Canada
+      <Button to="https://www.facebook.com/groups/VancouverWDS/" fitToText>Join Up</Button>
+    </Block>
+    <Block type="image-narrow" src="photos/wdslocal/victoria.WDS.local.png">
+      ## Victoria, Canada
+      <Button to="https://www.facebook.com/groups/121832781617165/" fitToText>Join Up</Button>
+    </Block>
+    <Block type="image-narrow" src="photos/washington.dc.WDS.local.png">
+      ## Washington, D.C.
+      <Button to="https://www.facebook.com/groups/263193840458019/" fitToText>Join Up</Button>
     </Block>
   </Block>
    <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
@@ -34,8 +70,8 @@ We need you.
    WDS Local is for anyone that:
    <ul>
    <li>Lives out the WDS mission and core values (community, adventure, and service)</li>
-<li>Has previously attended a WDS, OR</li>
-<li>personally knows a WDS alum and is planning to be an active member of the WDS community</li>
+<li>Has previously attended a WDS or</li>
+<li>personally knows a WDS alum and is planning to attend a future WDS</li>
 <li>Wants to meet and get to know other awesome WDS community members</li>
 <li>Wants to help others live out their remarkable lives</li>
 </ul>
@@ -45,7 +81,7 @@ We need you.
   <Block cols="6" css={{ padding: '50px 0 20px' }}>
    ## Get Connected
 Does this sound like you? Click on the button below and we'll do our best to get you connected to a local group or help you start one in your area if it's possible.
-<Button to="http://bit.ly/WDS-Local-Interest-Form" newWindow={true} styling="white">Connect me to a group</Button>     
+<Button to="http://bit.ly/WDS-Local-Interest-Form" newWindow={true} styling="white" fitToText>Connect me to a group</Button>     
  </Block>
 </Section>
 <Import id="join-us" />
