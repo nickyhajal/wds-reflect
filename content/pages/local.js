07a72112681bee35a83658dc4c1a1113f12dcf59
Jedd <jedidiah475@gmail.com>
2017-01-19 15:48:16 -0600

    Create local.js

---
+++ content/pages/local.js
@@ -0,0 +1,51 @@
+<Section
+  headerImage="hero/local.jpg"
+  headerClip="rl_header"
+  headerPosition="top"
+  headerSize="thin"
+  color="white"
+>
+  <Image src="heading/local.png" width="565px" height="169px" className="scriptHeader" />
+  <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+    ## Living out our remarkable lives in community
+    WDS Local is a way that alumni can gather regularly through out the year, support each other, and bring WDS home to their local communities. It's a way to continue conversations and go on new adventures together outside the annual WDS gathering in Portland. We know that the best thing about WDS is you, the people who make up this vibrant and wonderful community. And there's nothing better than for awesome people to hang out more often with other awesome people.
+
+We need you.
+  </Block>
+  <Block margin="125px 0 50px 0">
+    ## WDS Local Group Directory
+    <Block type="image-narrow" src="photos/wdslocal/los.angeles.WDS.local.png">
+      ## Los Angeles, California
+      <Button to="/schedule" fitToText>Join Up</Button>
+    </Block>
+    <Block type="image-narrow" src="photos/wdslocal/minneapolis.st.paul.WDS.local.png">
+      ## Minneapolis/St.Paul, Minnesota
+      <Button to="/schedule" fitToText>Join Up</Button>
+    </Block>
+    <Block type="image-narrow" src="photos/wdslocal/new.york.city.WDS.local.png">
+      ## New York City, New York
+      <Button to="/schedule" fitToText>Join Up</Button>
+    </Block>
+  </Block>
+   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+   ## How Can I Get Involved?
+   At the moment several groups are already meeting regularly and more are being launched each month. Our goal is to help launch WDS Local groups in areas where there are at least 3 people that are willing to meet on a regular basis. The focus is not about how many people get together, but solely on the commitment that members of these groups have to each other. The success of WDS Local groups is solely dependent on the commitmment and active participation of it's members.
+   
+   WDS Local is for anyone that:
+   <ul>
+   <li>Lives out the WDS mission and core values (community, adventure, and service)</li>
+<li>Has previously attended a WDS, OR</li>
+<li>personally knows a WDS alum and is planning to be an active member of the WDS community</li>
+<li>Wants to meet and get to know other awesome WDS community members</li>
+<li>Wants to help others live out their remarkable lives</li>
+</ul>
+     </Block>
+</Section>
+<Section color="green">
+  <Block cols="6" css={{ padding: '50px 0 20px' }}>
+   ## Get Connected
+Does this sound like you? Click on the button below and we'll do our best to get you connected to a local group or help you start one in your area if it's possible.
+<Button to="http://bit.ly/WDS-Local-Interest-Form" newWindow={true} styling="white">Connect me to a group</Button>     
+ </Block>
+</Section>
+<Import id="join-us" />
