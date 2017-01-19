c46fb67797b6c7b96fb5011c412b503bc3e2f1b2
Jedd <jedidiah475@gmail.com>
2017-01-19 14:13:13 -0600

    Update location.js

--- content/pages/location.js
+++ content/pages/location.js
@@ -63,7 +63,7 @@
     Portland is an affordable destination, with the lowest hotel rates of any major west coast city. Every year we arrange a number of discounts from partner hotels. Other attendees rent through AirBnB or stay at a local hostel. With Portland being a very popular destination in the summertime, it&apos;s important to book your rooms ASAP. This year we&apos;ve secured rooms at these five amazing Portland properties (including the all new WDS Hotel), all within walking distance or a quick ride on public transit to WDS events and activities:
   </Block>
   <Block cols="8" bleed={false} margin="0 0 80px">
-    <Image src="photos/Hotel-Modera-Lobby.png" width="100%" height="302" />
+    <Image src="photos/modera-lobby.png" width="100%" height="302" />
   </Block>
   <Block cols="8" bleed={false} margin="0 0 80px">
     <Block type="caption" src="photos/society-hotel.jpg" label="Coming Soon">
@@ -93,9 +93,9 @@
     If you can&apos;t get a room at one of these hotels or just want to do something else, there are numerous other options. Here are a couple of WDS attendee favorites:
   </Block>
   <Block cols="8" bleed={false} margin="0 0 80px">
-    <Block type="caption" src="photos/Cyrstal.Hotel.jpg">
+    <Block type="caption" src="photos/Crystal.Hotel.jpg">
       ### Crystal Hotel
-      <Link to="http://www.mcmenamins.com/CrystalHotel.jpg">0.6 Mi from Base</Link>
+      <Link to="http://www.mcmenamins.com/CrystalHotel">0.6 Mi from Base</Link>
     </Block>
     <Block type="caption" src="photos/Portland-Hostel.jpg">
      ### Portland NW Hostel
@@ -103,7 +103,7 @@
     </Block>
     <Block type="caption" src="photos/PDX-airbnb.jpg">
       ### Airbnb listings
-      <Link to="https://goo.gl/yOaB4l">.5 Mi from Base</Link>
+      <Link to="https://goo.gl/yOaB4l">0.5 Mi from Base</Link>
     </Block>
   </Block>
   <div className="clear" />
@@ -113,12 +113,14 @@
       <Button to="http://trimet.org/#/planner" newWindow={true} styling="white">TriMet Trip Planner</Button>
     </Block>
     <Block anchor="left" align="none" width="153" margin="0 60px 0 0">
-      <Image src="icon/outline-bike.png" align="center" width="153" height="130" margin="0 0 48px 0" />
+      <Image src="icon/outline-bike.png" align="center" width="130" height="130" margin="0 0 48px 0" />
       <Button to="https://www.biketownpdx.com/" newWindow={true} styling="white">Biketown</Button>
     </Block>
     <Block anchor="left" textAlign="left" width="350" margin="24px 0 0 0">
       ## Transportation
-      Portland has one the best public transit systems in the country - no need to rent a car during your whole time in the city. Of our many options Portland's fast-and-easy light rail service (MAX) goes directly from the airport to downtown for $2.50. Other alternative forms of transportation include the Street Car, Car2Go, Uber, Lyft, and for the cyclists, Portland’s new bikehsare program Biketown.
+      Portland has one the best public transit systems (TriMet) in the country - no need to rent a car during your whole time in the city. Of our many options Portland's fast-and-easy light rail service (MAX) goes directly from the airport to downtown for $2.50. 
+
+Other alternative forms of transportation include the Street Car, Car2Go, Uber, Lyft, and for the cyclists, Portland’s new bikehsare program Biketown.
     </Block>
     <div className="clear" />
   </Block>
@@ -151,7 +153,7 @@
     </Block>
     <Block type="image-overlay" anchor="right" icon="outline-eye" src="photos/powells-books.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
       ## Powell’s Books
-     No trip to Portland is complete without a pilgrimage to Powell’s City of Books, located on an entire city block at <Link to="https://goo.gl/maps/TC3PAN9f3jy" target="_blank">1005 W Burnside St.</Link>.
+     No trip to Portland is complete without a pilgrimage to Powell’s City of Books, located on an entire city block at 1005 W Burnside.
     </Block>
     <Block type="image-overlay" anchor="left" icon="outline-camera" src="photos/waterfront.png" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
       ## Waterfront
