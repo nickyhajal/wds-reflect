92d08085a9ef096612184695cfbef96d6df231e7
Jedd <jedidiah475@gmail.com>
2017-01-19 19:09:09 -0600

    Update academies.js

--- content/pages/academies.js
+++ content/pages/academies.js
@@ -8,13 +8,31 @@
   <Image src="heading/academies.png" width="565px" height="169px" className="scriptHeader" />
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     ## At a WDS Academy
-    At a WDS Academy, you might learn how to travel hack your way to the dream trip you&apos;ve always wanted to take, discover the science behind happiness, or begin building greater community. Academies are half-day workshops where you can learn something new that will help you live your <i>own</i> remarkable life story. We bring in top experts from the WDS community to share their knowledge and teach practical skills. The line up always includes a variety of topics, from business to personal development to building tiny houses. It's a great way to lead into the main events of the WDS weekend and keep the conversations going after.
+    At a WDS Academy, you might learn how to travel hack your way to the dream trip you&apos;ve always wanted to take, discover the science behind happiness, or begin building greater community. Academies are half-day workshops where you can learn something new that will help you to live your *own* remarkable life story. We bring in top experts from the WDS community to share their knowledge and teach practical skills. The line up always includes a variety of topics, from business to personal development to building tiny houses. It's a great way to lead into the main events of the WDS weekend and keep the conversations going after.
   </Block>
   <Video placeholder="photos/academies2.jpg" videoId="121164251" cols="8" />
      <Block cols="6" css={{ padding: '50px 0 20px' }}>
     Academies are the only WDS activity that are also open to the general public, so bring your friends! Tickets are sold a la carte, with special discounts and early access given to WDS ticket holders. 
      ## Awesome Past Academies
      A snippet of some of our most popular Academies from previous years:
+<Grid>
+<Block type="padded" cols="2" anchored="left" clip="tl:5%;" color="sea">
+## Title
+Content
+</Block>
+<Block type="padded" cols="2" anchored="left" clip="tl:5%;" color="orange">
+## Title
+Content
+</Block>
+<Block type="padded" cols="2" anchored="left" clip="tl:5%;" color="green">
+## Title
+Content
+</Block>
+<Block type="padded" cols="2" anchored="left" clip="tl:5%;" color="blue">
+## Title
+Content
+</Block>
+</Grid>
       </Block>
 </Section>
 <Section color="green">
