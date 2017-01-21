d606e3dcd0a23de39e24a6a33cf5537122c0aa04
wkorenek <wekorenek@gmail.com>
2017-01-20 17:18:10 -0800

    Update story.js

--- content/pages/story.js
+++ content/pages/story.js
@@ -8,9 +8,9 @@
   <Image src="heading/story.png" width="429px" height="190px" className="scriptHeader" />
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     ## Why WDS?
-    Since 2011, WDS has brought together more than 10,000 remarkable people from all 50 states and more than 40 countries. A full week of events and an unforgettare than 40 countries. A full week of events and an unforgettable keynote weekend in Portland, Oregon.
+    Since 2011, WDS has brought together more than 10,000 remarkable people from all 50 states and more than 40 countries. And now WDS offers a full week of events and an unforgettable keynote weekend in Portland, Oregon.
 
-    Learn remarkable techniques from unconventional thinkers, aquire powerful new skills, and become inspired to free yourself from the nine to five. A full week of events and an unforgettable keynble keynote weekend in Portland, Oregon. Learn remarkable techniques from unconventional thinkers, aquire powerful new skills, and become inspired to free yourself from the nine to five. A full week of events and an unforgettable keynote weekend in Portland, Oregon. Learn remarkable techniques from unconventional thinkers, aquire powerful new skills, and become inspired to free yourself from the “nine to five."
+    Learn remarkable techniques from unconventional thinkers, aquire powerful new skills, and become inspired to free yourself from the “nine to five."
   </Block>
 </Section>
 <Section color="canvas">
@@ -25,13 +25,13 @@
         </Block>
         <Block type="4col">
           ## Highlights
-          - 6 Days of Community, Service & Adventure
+          - 6 whole days of Community, Service & Adventure
           - Overcoming our fears with Michelle Poler
-          - Dancing Jellyfish!
+          - Dancing Jellyfish at the Closing Party!
         </Block>
         <Block type="4col">
           ## Big Surprise
-          WDS Tattoos on stage!
+          WDS branded tattoos backstage!
         </Block>
         <Block type="4col">
           ## Popular Speakers
@@ -42,12 +42,12 @@
       <div>
         <Block type="image" src="photos/trex.jpg">
           ## 2016 Photos {'\n'}
-          2016 had some great moment, check out the photos!{'\n'}
+          2016 had some great moments, check out the photos!{'\n'}
           <Button to="https://www.flickr.com/photos/chrisguillebeau/albums/72157669269939103">View Them</Button>
         </Block>
          <Block type="image" src="photos/chelsea.jpg">
           ## 2016 Videos{'\n'}
-          2016 had some great moment, check out the videos!{'\n'}
+          2016 had some great moments, check out the videos!{'\n'}
           <Button to="https://vimeo.com/album/4366453">Watch Them</Button>
         </Block>
       </div>
@@ -62,17 +62,17 @@
         </Block>
         <Block type="4col">
           ## Highlights
-          - 6 Days of Community, Service & Adventure
-          - Overcoming our fears with Michelle Poler
-          - Dancing Jellyfish!
+          - Lipstick selfies!
+          - Awesome Q&A with The Kid President
+          - Filler content 
         </Block>
         <Block type="4col">
           ## Big Surprise
-          WDS Tattoos on stage!
+          Breakfast in bed Guinness World Record!
         </Block>
         <Block type="4col">
           ## Popular Speakers
-          "As people who want to commit themselves to ‘world domination,’ it is our responsibility to have some kind of a practice while the world is going to hell in a handbasket, other than buying handbaskets." -Rev. angel Kyodo williams
+          "As people who want to commit themselves to ‘world domination,’ it is our responsibility to have some kind of a practice while the world is going to hell in a handbasket, other than buying handbaskets." -Lewis Howes
         </Block>
       </div>
       <div className="clear" style={{ paddingTop: '80px' }} />
@@ -99,35 +99,69 @@
         </Block>
         <Block type="4col">
           ## Highlights
-          - 6 Days of Community, Service & Adventure
-          - Overcoming our fears with Michelle Poler
-          - Dancing Jellyfish!
+          - The Portland Experience Opening Party & the Portland Spirit Cruise
+          - Hot Air Balloon Rides at the Closing Party!
+          - The Great Namaste World Record
         </Block>
         <Block type="4col">
           ## Big Surprise
-          WDS Tattoos on stage!
+          I don't know, someone please tell me!
         </Block>
         <Block type="4col">
           ## Popular Speakers
-          "As people who want to commit themselves to ‘world domination,’ it is our responsibility to have some kind of a practice while the world is going to hell in a handbasket, other than buying handbaskets." -Rev. angel Kyodo williams
+          "Choosing to take on the content for the page with info about all previous years' highlights and details is a great idea if you've only attended one event!" -Whitney Korenek
         </Block>
       </div>
       <div className="clear" style={{ paddingTop: '80px' }} />
       <div>
         <Block type="image" src="photos/trex.jpg">
-          ## 2015 Photos {'\n'}
+          ## 2014 Photos {'\n'}
           2016 had some great moment, check out the photos!{'\n'}
           <Button to="https://www.flickr.com/photos/chrisguillebeau/albums">View Them</Button>
         </Block>
          <Block type="image" src="photos/chelsea.jpg">
-          ## 2015 Videos{'\n'}
+          ## 2014 Videos{'\n'}
           2016 had some great moment, check out the videos!{'\n'}
-          <Button to="https://vimeo.com/album/4366453">Watch Them</Button>
+          <Button to="https://vimeopro.com/aonc/wds-2014-keynotes">Watch Them</Button>
         </Block>
       </div>
     </Tab>
     <Tab title="2013">
       <Image src="art/legacy-2013.jpg" width="1260" height="514" margin="0 0 0 -142px" preload/>
+           <div>
+        <Block type="4col">
+          ## Year Four
+          July 9-11
+          Portland, OR
+        </Block>
+        <Block type="4col">
+          ## Highlights
+          - The Portland Experience Opening Party & the Portland Spirit Cruise
+          - Hot Air Balloon Rides at the Closing Party!
+          - The Great Namaste World Record
+        </Block>
+        <Block type="4col">
+          ## Big Surprise
+          I don't know, someone please tell me!
+        </Block>
+        <Block type="4col">
+          ## Popular Speakers
+          "Choosing to take on the content for the page with info about all previous years' highlights and details is a great idea if you've only attended one event!" -Whitney Korenek
+        </Block>
+      </div>
+      <div className="clear" style={{ paddingTop: '80px' }} />
+      <div>
+        <Block type="image" src="photos/trex.jpg">
+          ## 2013 Photos {'\n'}
+          2013 had some great moments, check out the photos!{'\n'}
+          <Button to="https://www.flickr.com/photos/chrisguillebeau/albums/72157634510088202">View Them</Button>
+        </Block>
+         <Block type="image" src="photos/chelsea.jpg">
+          ## 2013 Videos{'\n'}
+          2013 had some great moments, check out the videos!{'\n'}
+          <Button to="https://vimeopro.com/aonc/wds2015">Watch Them</Button>
+        </Block>
+      </div>
     </Tab>
     <Tab title="2012">
       <Image src="art/legacy-2012.jpg" width="1260" height="514" margin="0 0 0 -142px" preload/>
