645963b7481f992e20ff10f00551bdde6c595822
193countries <chris.guillebeau@gmail.com>
2017-01-21 20:39:18 +0200

    cg copy

--- content/global/tell-a-friend.reflect.js
+++ content/global/tell-a-friend.reflect.js
@@ -1,7 +1,7 @@
 <Section color="green" clip="top_lr_2.6" margin="-22px 0 0">
   <Block margin="50px 0" width="420px">
-    ## Tell a Buddy
-    Bring a buddy! Great things are better when they’re shared with friends.
+    ## Tell a Friend
+    Spread the word! Great things are even better when they’re shared with friends.
     <Button modal="tellAFriend" width="184px" align="center">Share the Love</Button>
   </Block>
   <Image src="art/friends.png" width="286px" height="201px" margin="-120px 0 -60px" />
