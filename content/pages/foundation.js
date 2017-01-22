6d229a134bdcd0ece6b6f00a20e1b8eab482cc4d
wkorenek <wekorenek@gmail.com>
2017-01-21 23:18:33 -0800

    Update foundation.js

--- content/pages/foundation.js
+++ content/pages/foundation.js
@@ -19,27 +19,39 @@
   <Block>## Featured Project</Block>
   <Tabs style="dots">
     <Tab title="2016">
-      <Image src="photos/foundation2016.jpg" width="904" height="549" margin="0 auto 60px" preload/>
+      <Image src="https://s3.amazonaws.com/el-files-wds/images/Emily+Wray.jpg" width="904" height="549" margin="0 auto 60px" preload/>
       <Block cols="6" textAlign="left">
         ## Emily Wray
 
-        One of the cornerstones of the World Domination Summit is adventure, and WDS attendees certainly know how to celebrate living unconventionally. Many of us are driven by the journey, and enjoy striking out on our own in search of new opportunities and experiences. Yet, we also know and appreciate the value of community—having someone to share the ups and downs of our journey and to help us reflect on what we’ve learned. It is this combination of adventure and community that brings us back each July for the World Domination Summit, and also the motivation for this blog post.opportunity to learn something new to help you live your own remarkable life story. It's a great way to lead into the main events of the WDS weekend and keep the conversations going after.
+        We’ve all experienced moments when we struggling to put into words an experience or emotion. Voices of Petersburg is a three-day digital storytelling workshop for clients of Petersburg Mental Health Services in Petersburg, Alaska. Participants choose an experience they want to share and write a script for a digital story to give voice to their struggles and triumphs. The process of writing, choosing supportive media assets, and bringing the story to the screen provides a platform for healing unique to the mental health therapies.
+
+With the help of the Foundation, Emily and her team will build a mobile curriculum that can travel outside of Petersburg and assist individuals in mental health facilities around the U.S. She is currently looking for assistance with social media, fundraising, and product development. you. If you are interested in contributing to her project, visit voicesofpetersburgak.com or email emilywray@gmail.com.
       </Block>
     </Tab>
     <Tab title="2015">
-      <Image src="http://imgs.wds.fm/foundation_emily.jpg" width="904" height="549" margin="0 auto 60px" preload/>
+      <Image src="https://s3.amazonaws.com/el-files-wds/images/foundation_valerie.jpg" width="904" height="549" margin="0 auto 60px" preload/>
       <Block cols="6" textAlign="left">
-        ## Emily Wray
+        ## Valerie Groth
 
-        One of the cornerstones of the World Domination Summit is adventure, and WDS attendees certainly know how to celebrate living unconventionally. Many of us are driven by the journey, and enjoy striking out on our own in search of new opportunities and experiences. Yet, we also know and appreciate the value of community—having someone to share the ups and downs of our journey and to help us reflect on what we’ve learned. It is this combination of adventure and community that brings us back each July for the World Domination Summit, and also the motivation for this blog post.opportunity to learn something new to help you live your own remarkable life story. It's a great way to lead into the main events of the WDS weekend and keep the conversations going after.
+        School teachers in urban districts face special challenges in promoting their students’ long-term academic success. It is no surprise that so-called “failing” schools are disproportionately situated in urban centers with high rates of poverty and crime. Valerie Groth is a mission to change that for Chicago-area students. In 2014, she founded Ryan Banks Academy, a coeducational boarding school that seeks to impact the lives of students through a rigorous college and career preparatory education, a supportive boarding program, and a focus on personal development in a positive environment. The goal of Ryan Banks Academy is to teach students not only the value of education, but also encourage them to focus on cultivating their own unique strengths, interests, and passions so that they may lead a purpose-filled life. Curriculum focuses not only on academic pursuits, but also on developing their relationships, core values, and mindset.
+
+There are many steps in her journey to build the school, and the Scholarship for Real Life grant will be helping get her fundraising efforts of the ground, as well as help support her burgeoning volunteer program. She is currently looking for assistance with grant writing and donor outreach, as well as anyone with a background in curriculum development. If you are interested in contributing to Ryan Banks Academy, you can email ryanbanksacademy@gmail.com or visit ryanbanksacademy.org
       </Block>
     </Tab>
     <Tab title="2014">
-      <Image src="http://imgs.wds.fm/foundation_valerie.jpg" width="904" height="549" margin="0 auto 60px" preload/>
+      <Image src="https://s3.amazonaws.com/el-files-wds/images/Ben+%26+Amberlynn+Slavin.jpg" width="904" height="549" margin="0 auto 60px" preload/>
       <Block cols="6" textAlign="left">
-        ## Emily Wray
+        ## Ben & Amberlynn Slavin
 
-        One of the cornerstones of the World Domination Summit is adventure, and WDS attendees certainly know how to celebrate living unconventionally. Many of us are driven by the journey, and enjoy striking out on our own in search of new opportunities and experiences. Yet, we also know and appreciate the value of community—having someone to share the ups and downs of our journey and to help us reflect on what we’ve learned. It is this combination of adventure and community that brings us back each July for the World Domination Summit, and also the motivation for this blog post.opportunity to learn something new to help you live your own remarkable life story. It's a great way to lead into the main events of the WDS weekend and keep the conversations going after.
+        SnapType breaks down the barriers to education and helps students survive in a school system that wasn’t designed for them. The app enables students to complete any school worksheet on an iPad.
+
+An estimated 1 in 5 people have difficulty expressing themselves through writing. For students, that can lead to falling behind in class, emotional problems at home, low self-esteem, and a sense of hopelessness. When Amberlynn Slavin was doing her fieldwork as an occupational therapy graduate student, she worked with a child who had dysgraphia - a learning disability that makes handwriting incredibly messy.
+
+Together with her husband, Ben Slavin, they created an app to take a picture of a worksheet and then simply type the answers in on the screen. Feedback from the community has been extremely positive! The challenge they face now is that there are 4 times as many Android devices used around the world than there are Apple. To make SnapType work on Android, they have to re-write the app from the ground up in a completely different programming language.
+
+A WDS Foundation scholarship was granted to help fund the development of SnapType for Android. Soon, students around the world will be given the chance to have more independence, self confidence and the opportunity to keep up with their peers!
+
+To learn more, or to contact Ben and Amberlynn, visit SnapTypeApp.com.
       </Block>
     </Tab>
   </Tabs>
