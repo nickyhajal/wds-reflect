c8a2c636da7c2ca9027f919050990cc75d050dd7
wkorenek <wekorenek@gmail.com>
2017-01-21 22:04:13 -0800

    Update story.js

--- content/pages/story.js
+++ content/pages/story.js
@@ -20,14 +20,14 @@
       <div>
         <Block type="4col">
           ## Year Six
-          August 9-14
+          August 9-14, 2016 
           Portland, OR
         </Block>
         <Block type="4col">
           ## Highlights
-          - 6 whole days of Community, Service & Adventure
-          - Overcoming our fears with Michelle Poler
-          - Dancing Jellyfish at the Closing Party!
+          - 6 Days of Community, Service & Adventure
+          - Overcoming our fears with Michelle Poler  
+          - Dancing Jellyfish!
         </Block>
         <Block type="4col">
           ## Big Surprise
@@ -48,7 +48,7 @@
          <Block type="image" src="photos/chelsea.jpg">
           ## 2016 Videos{'\n'}
           2016 had some great moments, check out the videos!{'\n'}
-          <Button to="https://vimeo.com/album/4366453">Watch Them</Button>
+          <Button to="https://vimeopro.com/aonc/wds-2016-keynotes">Watch Them</Button>
         </Block>
       </div>
     </Tab>
@@ -57,35 +57,35 @@
         <div>
         <Block type="4col">
           ## Year Five
-          July 9-11
+          July 9-13, 2015 
           Portland, OR
         </Block>
         <Block type="4col">
           ## Highlights
-          - Lipstick selfies!
-          - Awesome Q&A with The Kid President
-          - Filler content 
+          - Lipstick selfies
+          - Q&A with The Kid President
+          - Breakfast in Bed Guinness World Record!
         </Block>
         <Block type="4col">
           ## Big Surprise
-          Breakfast in bed Guinness World Record!
+          Giant projector on Portland building
         </Block>
         <Block type="4col">
           ## Popular Speakers
-          "As people who want to commit themselves to ‘world domination,’ it is our responsibility to have some kind of a practice while the world is going to hell in a handbasket, other than buying handbaskets." -Lewis Howes
+          "Your voice is never lost, it’s always waiting to be found." —Jon Acuff
         </Block>
       </div>
       <div className="clear" style={{ paddingTop: '80px' }} />
       <div>
         <Block type="image" src="photos/trex.jpg">
           ## 2015 Photos {'\n'}
-          2016 had some great moment, check out the photos!{'\n'}
+          2015 had some great moment, check out the photos!{'\n'}
           <Button to="https://www.flickr.com/photos/chrisguillebeau/albums">View Them</Button>
         </Block>
          <Block type="image" src="photos/chelsea.jpg">
           ## 2015 Videos{'\n'}
-          2016 had some great moment, check out the videos!{'\n'}
-          <Button to="https://vimeo.com/album/4366453">Watch Them</Button>
+          2015 had some great moment, check out the videos!{'\n'}
+          <Button to="https://vimeopro.com/aonc/wds2015">Watch Them</Button>
         </Block>
       </div>
     </Tab>
@@ -94,34 +94,34 @@
         <div>
         <Block type="4col">
           ## Year Four
-          July 9-11
+          July 10-14, 2014 
           Portland, OR
         </Block>
         <Block type="4col">
           ## Highlights
-          - The Portland Experience Opening Party & the Portland Spirit Cruise
-          - Hot Air Balloon Rides at the Closing Party!
+          - Portland Experience opening party
+          - Hot Air Balloon Rides!
           - The Great Namaste World Record
         </Block>
         <Block type="4col">
           ## Big Surprise
-          I don't know, someone please tell me!
+          Color shower
         </Block>
         <Block type="4col">
           ## Popular Speakers
-          "Choosing to take on the content for the page with info about all previous years' highlights and details is a great idea if you've only attended one event!" -Whitney Korenek
+          "Take imperfect action" —Jadah Sellner
         </Block>
       </div>
       <div className="clear" style={{ paddingTop: '80px' }} />
       <div>
         <Block type="image" src="photos/trex.jpg">
           ## 2014 Photos {'\n'}
-          2016 had some great moment, check out the photos!{'\n'}
-          <Button to="https://www.flickr.com/photos/chrisguillebeau/albums">View Them</Button>
+          2014 had some great moment, check out the photos!{'\n'}
+          <Button to="https://www.flickr.com/photos/chrisguillebeau/albums/72157645692660113">View Them</Button>
         </Block>
          <Block type="image" src="photos/chelsea.jpg">
           ## 2014 Videos{'\n'}
-          2016 had some great moment, check out the videos!{'\n'}
+          2014 had some great moment, check out the videos!{'\n'}
           <Button to="https://vimeopro.com/aonc/wds-2014-keynotes">Watch Them</Button>
         </Block>
       </div>
@@ -130,23 +130,23 @@
       <Image src="art/legacy-2013.jpg" width="1260" height="514" margin="0 0 0 -142px" preload/>
            <div>
         <Block type="4col">
-          ## Year Four
-          July 9-11
+          ## Year Three
+          July 3-5, 2013 
           Portland, OR
         </Block>
         <Block type="4col">
           ## Highlights
-          - The Portland Experience Opening Party & the Portland Spirit Cruise
-          - Hot Air Balloon Rides at the Closing Party!
-          - The Great Namaste World Record
+          - First time at the Arlene Schnitzer Concert Hall
+          - Opening Party at the Oregon Zoo
+          - The World Float Guinness World Record
         </Block>
         <Block type="4col">
           ## Big Surprise
-          I don't know, someone please tell me!
+          Champagne toast
         </Block>
         <Block type="4col">
           ## Popular Speakers
-          "Choosing to take on the content for the page with info about all previous years' highlights and details is a great idea if you've only attended one event!" -Whitney Korenek
+          Brene Brown signing "Don't Stop Believin'"
         </Block>
       </div>
       <div className="clear" style={{ paddingTop: '80px' }} />
@@ -159,16 +159,84 @@
          <Block type="image" src="photos/chelsea.jpg">
           ## 2013 Videos{'\n'}
           2013 had some great moments, check out the videos!{'\n'}
-          <Button to="https://vimeopro.com/aonc/wds2015">Watch Them</Button>
+          <Button to="https://vimeopro.com/aonc/wds1013">Watch Them</Button>
         </Block>
       </div>
     </Tab>
     <Tab title="2012">
       <Image src="art/legacy-2012.jpg" width="1260" height="514" margin="0 0 0 -142px" preload/>
+      <div>
+        <Block type="4col">
+          ## Year Two
+          July 6-8, 2012 
+          Portland, OR
+        </Block>
+        <Block type="4col">
+          ## Highlights
+          - First time at the Antoinette Hatfield Hall
+          - Bungee jumping with Joel Runyon
+          - Carnival themed Opening Party
+        </Block>
+        <Block type="4col">
+          ## Big Surprise
+          The $100 Investment
+        </Block>
+        <Block type="4col">
+          ## Popular Speakers
+          "The path to a passionate life is often way more complex than the simple advice 'follow your passion' would suggest." —Cal Newport
+        </Block>
+      </div>
+      <div className="clear" style={{ paddingTop: '80px' }} />
+      <div>
+        <Block type="image" src="photos/trex.jpg">
+          ## 2012 Photos {'\n'}
+          2012 had some great moments, check out the photos!{'\n'}
+          <Button to="https://www.flickr.com/photos/chrisguillebeau/albums/72157630469839810">View Them</Button>
+        </Block>
+         <Block type="image" src="photos/chelsea.jpg">
+          ## 2012 Videos{'\n'}
+          2012 had some great moments, check out the videos!{'\n'}
+          <Button to="https://vimeopro.com/aonc/wds-2012-keynotes">Watch Them</Button>
+        </Block>
+      </div>
     </Tab>
     <Tab title="2011">
       <Image src="art/legacy-2011.jpg" width="1260" height="514" margin="0 0 0 -142px" preload/>
-    </Tab>
+       <div>
+        <Block type="4col">
+          ## Year One
+          June 3-5, 2011 
+          Portland, OR
+        </Block>
+        <Block type="4col">
+          ## Highlights
+          - A giant ice sculpture
+          - Love notes from every attendee
+          - A dance party
+        </Block>
+        <Block type="4col">
+          ## Big Surprise
+          We actually pulled this off!
+        </Block>
+        <Block type="4col">
+          ## Popular Speakers
+          "Adventuring occurs when we actually embrace possibility." —Jonathan Fields
+        </Block>
+      </div>
+      <div className="clear" style={{ paddingTop: '80px' }} />
+      <div>
+        <Block type="image" src="photos/trex.jpg">
+          ## 2011 Photos {'\n'}
+          2011 had some great moments, check out the photos!{'\n'}
+          <Button to="https://www.flickr.com/photos/chrisguillebeau/albums/72157626854701827">View Them</Button>
+        </Block>
+         <Block type="image" src="photos/chelsea.jpg">
+          ## 2011 Videos{'\n'}
+          2011 had some great moments, check out the videos!{'\n'}
+          <Button to="https://vimeopro.com/aonc/wds-2011-keynotes">Watch Them</Button>
+        </Block>
+      </div>
+        </Tab>
   </Tabs>
 </Section>
 <Import id="join-us" />
