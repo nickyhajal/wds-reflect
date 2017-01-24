05143479780e83e38b4363d7f2a8a02bd7ee6037
wkorenek <wekorenek@gmail.com>
2017-01-23 21:35:17 -0800

    Update kendrick-uy.js

--- content/variables/testimonials/kendrick-uy.js
+++ content/variables/testimonials/kendrick-uy.js
@@ -1,4 +1,4 @@
 name: "Kendrick UY",
-content: "It’s hard to explain, but everyone who’s attended know that it was worth their while. If you’ve never gone to WDS before, definitely sign up for next year. You’ll learn many things that you can use in your everyday life and the connections you make with other people is priceless.",
+content: "It’s hard to explain, but everyone who’s attended knows that it was worth their while. If you’ve never gone to WDS before, definitely sign up for next year. You’ll learn many things that you can use in your everyday life, and the connections you make with other people are priceless.",
 link: "",
 photo: "https://i1.wp.com/kendrickuy.com/wp-content/uploads/2015/05/DSCN0666.jpg"
