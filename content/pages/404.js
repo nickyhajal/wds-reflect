cce7510fad691f623fb3fa2a35a0059921a0f2b3
wkorenek <wekorenek@gmail.com>
2017-02-02 11:55:34 -0800

    Create 404.js

---
+++ content/pages/404.js
@@ -0,0 +1,33 @@
+<Section
+  color="sea"
+  css={{ marginBottom: '-2px' }}
+>
+  <Block width="768px" bleed={true} type="padded" background="white" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## Page destroyed by our kitty overlords!</Block>
+    
+    While we try and calm our furry masters with catnip and treats, here are some existing pages that may help you find what you're looking for:
+    
+    ### The WDS FAQ page
+    
+    <p>Do you have questions? If so, we have answers! Stop off here to see if we cover your questions on our <Link to="https://worlddominationsummit.com/faq" target="blank">FAQ page</Link>.</p>
+    
+    ### What is WDS?
+    
+    <p>Want to learn more about World Domination Summit and what past years have looked like? Visit our <Link to="https://worlddominationsummit.com/story" target="blank">Story page</Link> for more information!</p>
+    
+    ### WDS Schedule
+    
+    <p>Interested in learning more about what our week long event has to offer? Check out the <Link to="https://worlddominationsummit.com/schedule" target="blank">Schedule page</Link> to learn more about all of the events, activities, and meetups we have to offer!</p>
+    
+    ### WDS Local
+    
+    <p>Want more WDS year round? Join one of our many <Link to="https://worlddominationsummit.com/local" target="blank">WDS Local communities</Link> to keep the good times rolling.</p>
+    
+    ###WDS Foundation
+    
+    <p>Interested in being considered for our Scholarships for Real Life? Check out the <Link to="https://worlddominationsummit.com/foundation" target="blank">Foundation page</Link> for more information regarding our selection criteria and how you can be chosen for 2017!</p>
+    
+
+  </Block>
+</Section>
+<Import id="join-us" />
