8a8cb8ef131f7943a9ccca2fcdbab5de279b022c
Jedd <jedidiah475@gmail.com>
2017-02-04 20:57:19 -0800

    Create Apryl-Schlueter.js

---
+++ content/variables/testimonials/Apryl-Schlueter.js
@@ -0,0 +1,6 @@
+
+name: "Apryl Schlueter",
+content: "My first experience at WDS (2016) was unreal. I made great connections with so many top-notch and friendly people from different walks of life, the events and talks are infused with SO much fun, and I left wholeheartedly inspired and ready to take action on everything I learned. I still constantly interact with people I've met there, and I find comfort in the thought that I have a supportive family that spans the globe.",
+link: "",
+avatarPosition: 'center -7px',
+photo: "photos/Apryl.png"
