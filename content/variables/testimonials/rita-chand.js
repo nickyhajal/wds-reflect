2ba703b55f0e6aba87a4929b6cbaeeebe0774dad
Jedd <jedidiah475@gmail.com>
2017-02-05 19:08:06 -0800

    Update rita-chand.js

--- content/variables/testimonials/rita-chand.js
+++ content/variables/testimonials/rita-chand.js
@@ -1,4 +1,4 @@
 name: "Rita Chand",
 content: "Both the speakers and the attendees that I met were not only creative and smart, coming from all walks of life, all age groups, all races, all backgrounds, but they were also generous with their thoughts.  They were interested in each other.  And they really, sincerely wanted each other to succeed.",
 link: "",
-photo: "https://pbs.twimg.com/profile_images/799300713918566401/k0RsdOkM.jpg"
+photo: "https://pbs.twimg.com/profile_images/824310763284070400/G-zWrrIY.jpg"
