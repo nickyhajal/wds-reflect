0818e0145ffb87237b4284816b9fa951e65ffa47
Nicky Hajal <nhajal@gmail.com>
2017-02-15 10:17:43 -0600

    fix

---
+++ content/pages/join-us_waitlist.js
@@ -0,0 +1,66 @@
+<Section
+  headerImage="hero/joinus.jpg"
+  headerClip="rl_header"
+  headerPosition="top"
+  headerSize="thin"
+  color="white"
+  preload
+>
+  <Image src="heading/script-joinUs.png" width="446px" height="196px" className="scriptHeader" align="center" css={{ float: 'none' }} preload/>
+  <Block width="600px" css={{ color: 'orange', marginTop: '80px' }}>
+    <h2 style={{ marginBottom: '4px' }}>World Domination Summit 2017:</h2>
+    ## July 11 - 17, 2017 in Portland, Oregon
+  </Block>
+  <Block cols="6" className="bigger" mobile={{ all:{ padding: '0 50px', marginBottom: '20px' }, phone: { padding: '0 20px' } }}>
+
+    Since 2011, WDS has brought together more than 10,000 people from more than forty countries. As adventurers from all walks of life, our mission is to create a remarkable community that helps each person pursue a big dream. 
+
+    With discussions and activities on topics like how to live a good life, travel hacking your way around the globe, and writing your first book, it’s an experience like no other.
+    
+    We’re doing it again in 2017, and all that’s missing is YOU!
+      
+    All tickets cost $647 and are sold on a first-come, first-served basis.
+    
+    <Button modal="joinUs" fitToText>Join the Wait List!</Button>
+  </Block>
+</Section>
+<Section color="canvas" bound={false}>
+  <Block margin="50px 0 50px 0">
+    ## What's Included in My Ticket
+    <Grid numCols="2" tabletCols="2" block={{ className: 'dropped-button' }} equalize>
+        <Block type="image" src="photos/JoinUs/image-speakers.png">
+        ## Main Stage Speakers
+       Our keynote speakers are bestselling authors, leading activists, and in-demand storytellers. Since our very first year in 2011, the lineup has been inclusive, multicultural, and representative of our whole community.
+      </Block>
+      <Block type="image" src="photos/JoinUs/image-meetups.jpg">
+        ## Meetups
+        Every year attendees help lead over 100+ meetups in and around Portland. Share your expertise. Coordinate an adventure. The best part is you can do both!
+      </Block>
+      <Block type="image" src="photos/JoinUs/image-activities.png">
+        ## Activities
+        From fun social gatherings to focused discussions, starting off the day with yoga to ending the night in Karaoke, choose from a variety of special WDS hosted adventures and activities throughout the week.
+      </Block>
+      <Block type="image" src="photos/JoinUs/image-people.jpg">
+        ## Incredible People
+        Our incredible adventurers come from over 40+ different countries and all 50 states. At WDS there will be an abundance of opportunities to make new, life-long connections.
+      </Block>
+      <Block type="image" src="photos/JoinUs/image-academies.jpg">
+        ## Academies
+        These half-day workshops feature some of our favorite alumni speakers and select WDS community members that are experts in their fields. As a WDS ticket holder you get first access to the Academy of your choice at a huge discount.
+      </Block>
+       <Block type="image" src="photos/JoinUs/image-hotel.jpg">
+        ## Hotel Discounts
+        Each year we partner with iconic Portland properties to offer our attendees special discounted prices when booking your stay in Portland.  
+      </Block>
+      <Block type="image" src="photos/JoinUs/image-parties.jpg">
+        ## Celebrations
+        There's nothing better than celebrating community. Kick-off WDS and meet new friends at the welcome party and dance the night away with them at the closing celebration.
+      </Block>
+      <Block type="image" src="photos/JoinUs/image-communities.jpg">
+        ## Community
+        Once you attend WDS you'll become an alum and part of a global network of over 10,000 adventurers who will continue to support you and your journey. 
+      </Block>
+    </Grid>
+  </Block>
+</Section>
+<Import id="join-us" />
