b2caa5178f0c09cb3daf95ab0b87236c0b30d74e
wkorenek <wekorenek@gmail.com>
2017-02-17 09:59:58 -0800

    Update sara-frandina.js

--- content/variables/testimonials/sara-frandina.js
+++ content/variables/testimonials/sara-frandina.js
@@ -1,4 +1,4 @@
 name: "Sara Frandina",
-content: "WDS is exhilarating. Overwhelming. Authentic. But even more important than what WDS is is what WDS does. It’s power to inspire is underestimated. And it will completely change your perspective, at least on a few things, forever.",
+content: "WDS is exhilarating. Overwhelming. Authentic. But even more important than what WDS is what WDS does. It’s power to inspire is underestimated. And it will completely change your perspective, at least on a few things, forever.",
 link: "http://sarafrandina.com/2015/07/25/what-world-domination-summit-will-give-you/",
 photo: "https://sarafrandina.com/wp-content/uploads/2016/11/SaraFrandina_blogsidebar.jpg"
