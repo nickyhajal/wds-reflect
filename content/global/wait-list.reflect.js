011bd249f790524f81deb85e797d87a29166528c
Jedd <jedidiah475@gmail.com>
2017-02-22 22:36:11 -0500

    Update wait-list.reflect.js

--- content/global/wait-list.reflect.js
+++ content/global/wait-list.reflect.js
@@ -2,7 +2,7 @@
 <Section color="orange">
   <Image src="heading/joinus.png" width="288px" height="88px" align="center" />
   <Block margin="50px 0">## How Can I Get a Ticket for WDS 2017?</Block>
-  <Block margin="50px 0"><p><font color="white">Join the waiting list and you'll be the first to hear when the next round of tickets go on sale!</font></p></Block>
+  <p>Join the waiting list and you'll be the first to hear when the next round of tickets go on sale!</p>
   <Block width="400px" align="center" margin="50px 0 0" css={{ maxWidth: "400px" }}>
     <Form
       list="WDS 2017 Waiting List"
