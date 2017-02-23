7ca676422e9c5f17340576be4e1ab4f638642823
Jedd <jedidiah475@gmail.com>
2017-02-22 22:21:36 -0500

    Create wait-list.reflect.js

---
+++ content/global/wait-list.reflect.js
@@ -0,0 +1,22 @@
+
+<Section color="orange">
+  <Image src="heading/joinus.png" width="288px" height="88px" align="center" />
+  <Block margin="50px 0">## "How Will I Know..."</Block>
+  <p>when WDS 2017 tickets go back on sale? Join the waiting list and you'll be the first to hear when the next round of tickets go on sale!</p>
+  <Block width="400px" align="center" margin="50px 0 0" css={{ maxWidth: "400px" }}>
+    <Form
+      list="WDS 2017 Waiting List"
+      successMessage="Great, we'll let you know when sales start!"
+      buttonStart="Get Notified"
+      buttonProgress="Sending..."
+      buttonSuccess="Success!"
+    >
+      <FormRow>
+        <Input id="full_name" placeholder="Your Full Name" />
+      </FormRow>
+      <FormRow>
+        <Input id="email" placeholder="Your Email Address" />
+      </FormRow>
+    </Form>
+  </Block>
+</Section>
