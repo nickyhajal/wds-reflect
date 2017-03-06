c78ce5f5acf0350b4ba2c01de0ad751cfe89a667
Jedd <jedidiah475@gmail.com>
2017-03-06 06:44:32 -0500

    Update hptest.js

--- content/pages/hptest.js
+++ content/pages/hptest.js
@@ -18,22 +18,22 @@
     <Block type="image" src="photos/society-hotel.jpg">
       ## WDS Hotel™
      The WDS Hotel™ (a.k.a. The Society Hotel) was originally built in 1881 as a boarding house for sailors and is now a modern and charming hotel featuring hostel style rooms, bunk beds, and private suites. Featuring hardwood floors and historical artifacts the hotel is a glimpse into Portland's past. 
-       <p><i>Registration is only available for 4-night packages and through WDS only. Book online using the button below.</i></p> 
-       <p>0.1 Mi from Base<Link to="https://goo.gl/maps/6D6mJGAY7dv">(Google Directions)</Link></p> 
+       <p><i>Registration is only available for 4-night packages and through WDS only.</i></p> 
+       <p>0.1 Mi from Base <Link to="https://goo.gl/maps/6D6mJGAY7dv">(Google Directions)</Link></p> 
       <Button to="http://uspor.webhotel.microsdc.us/bp/search_rooms.jsp?groupCode=WDS17" newWindow={true}>Book Online</Button>
     </Block>
     <Block type="image" src="photos/Paramount-Hotel.jpg">
       ## Paramount Hotel 
       The Paramount Hotel is a casual, intimate boutique hotel boasting oversized guestrooms and genuine hospitality. Each guestroom and suite features relaxing décor, spacious marble finished bathrooms and modern amenities. The Paramount provides easy access to the very best the city has to offer.
       <p><i>Book online using the group code <b>"WDS17"</b> or call 503.223.9900. Be sure to tell them you're with WDS.</i></p>
-      <p>0.1 Mi from Base<Link to="https://goo.gl/maps/6D6mJGAY7dv">(Google Directions)</Link></p> 
+      <p>0.1 Mi from Base <Link to="https://goo.gl/maps/6D6mJGAY7dv">(Google Directions)</Link></p> 
       <Button to="http://uspor.webhotel.microsdc.us/bp/search_rooms.jsp?groupCode=WDS17" newWindow={true}>Book Online</Button>
     </Block>
     <Block type="image" src="photos/Hotel-Modera-Exterior.jpg">
       ## Hotel Modera
       A stylish luxury boutique hotel in downtown Portland, Hotel Modera is located in the center of the city's theater district, surrounded by fine restaurants, art galleries, museums and entertaining nightlife. The hotel's elegant atmosphere reflects the vibrancy and unique beauty of Portland itself. 
       <p><i>Book online using the group code <b>"WDS2017"</b> or call 877.484.1084. Be sure to tell them you're with WDS.</i></p> 
-      <p>0.1 Mi from Base<Link to="https://goo.gl/maps/T3BnDWTY9oq">(Google Directions)</Link></p> 
+      <p>0.1 Mi from Base <Link to="https://goo.gl/maps/T3BnDWTY9oq">(Google Directions)</Link></p> 
 <Button to="https://gc.synxis.com/rez.aspx?Chain=5173&Hotel=53439&group=WDS2017" newWindow={true}>Book Online</Button>    
 </Block>
   </Grid>
