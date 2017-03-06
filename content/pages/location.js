2e923f3aef3e26a8759e9df6f53132eb1d29d9e8
Jedd <jedidiah475@gmail.com>
2017-03-06 11:35:32 -0500

    Update location.js

--- content/pages/location.js
+++ content/pages/location.js
@@ -77,32 +77,46 @@
   <Block cols="8" bleed={false} margin="0 0 80px">
     <Image src="photos/modera-lobby.png" width="100%" height="302" />
   </Block>
-  <Block cols="8" bleed={false} margin="0 0 80px">
-    <Grid numCols="3" tabletCols="2">
-      <Block type="caption" src="photos/society-hotel.jpg" label="Coming Soon">
-        ### The WDS Hotel
-        **0.8 Mi** from Base
-      </Block>
-      <Block type="caption" src="photos/Paramount-Hotel.jpg" label="Coming Soon">
-        ### Paramount Hotel
-        **0.1 Mi** from Base
-      </Block>
-      <Block type="caption" src="photos/Hotel-Modera-Exterior.jpg" label="Coming Soon">
-        ### Hotel Modera
-        **0.3 Mi** from Base
-      </Block>
-      <Block type="caption" src="photos/hilton.png" label="Coming Soon">
-        ### Hilton
-        **0.5 Mi** from Base
-      </Block>
-      <Block type="caption" src="photos/Jupiter-Hotel-exterior.jpg" label="Coming Soon">
-        ### Jupiter Hotel
-        **1.5 Mi** from Base
-      </Block>
-    </Grid>
-  </Block>
-  <div className="clear" />
-  <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+  
+ <Grid margin="60px 0 -20px 0">
+    <Block type="image" src="photos/society-hotel.jpg">
+      ## WDS Hotel™
+     The WDS Hotel™ (a.k.a. The Society Hotel) was originally built in 1881 as a boarding house for sailors and is now a modern and charming hotel featuring hostel style rooms, bunk beds, and private suites. Featuring hardwood floors and historical artifacts the hotel is a glimpse into Portland's past. 
+       <p><i>Registration is only available for 4-night packages and through WDS only.</i></p> 
+       <p>0.1 Mi from Base <Link to="https://goo.gl/maps/6D6mJGAY7dv">(Google Directions)</Link></p> 
+      <Button to="" newWindow={true}>Learn More</Button>
+    </Block>
+    <Block type="image" src="photos/Paramount-Hotel.jpg">
+      ## Paramount Hotel 
+      The Paramount Hotel is a casual, intimate boutique hotel boasting oversized guestrooms and genuine hospitality. Each guestroom and suite features relaxing décor, spacious marble finished bathrooms and modern amenities. The Paramount provides easy access to the very best the city has to offer.
+      <p><i>Book online using the group code <b>"WDS17"</b> or call 503.223.9900. Be sure to tell them you're with WDS.</i></p>
+      <p>0.1 Mi from Base <Link to="https://goo.gl/maps/6D6mJGAY7dv">(Google Directions)</Link></p> 
+      <Button to="http://uspor.webhotel.microsdc.us/bp/search_rooms.jsp?groupCode=WDS17" newWindow={true}>Book Online</Button>
+    </Block>
+    <Block type="image" src="photos/Hotel-Modera-Exterior.jpg">
+      ## Hotel Modera
+      A stylish luxury boutique hotel in downtown Portland, Hotel Modera is located in the center of the city's theater district, surrounded by fine restaurants, art galleries, museums and entertaining nightlife. The hotel's elegant atmosphere reflects the vibrancy and unique beauty of Portland itself. 
+      <p><i>Book online using the group code <b>"WDS2017"</b> or call 877.484.1084. Be sure to tell them you're with WDS.</i></p> 
+      <p>0.1 Mi from Base <Link to="https://goo.gl/maps/T3BnDWTY9oq">(Google Directions)</Link></p> 
+<Button to="https://gc.synxis.com/rez.aspx?Chain=5173&Hotel=53439&group=WDS2017" newWindow={true}>Book Online</Button>    
+</Block>
+  <Block type="image" src="photos/hilton.png">
+      ## Hilton Portland
+      The modern Hilton Portland & Executive Tower is conveniently located in the heart of downtown next to great restaurants and venues. The Hilton Portland comprises of two buildings: the landmark Hilton Portland and the upscale Executive Tower with spacious guest rooms and executive level amenities.     
+  <p><i>More information about booking online or via phone is coming soon.</i></p> 
+      <p>0.1 Mi from Base <Link to="https://goo.gl/maps/UYkeGdPMfqJ2">(Google Directions)</Link></p> 
+<Button to="http://www3.hilton.com/en/hotels/oregon/hilton-portland-and-executive-tower-PDXPHHH/index.html" newWindow={true}>Coming Soon!</Button>    
+</Block>
+ <Block type="image" src="photos/Jupiter-Hotel-exterior.jpg">
+      ## Jupiter Hotel
+      The Jupiter Hotel is a mid-century, renovated motor inn-turned-boutique hotel located just across the river from downtown in the heart of one of Portland's most vibrant neighborhoods. The Jupiter hotel offers modern guestrooms that surprise and connect guests to the vibrancy of Portland at affordable rates.  
+  <p><i>To reserve a room please email or call the Jupiter staff at <b>sales@jupiterhotel.com</b> or call <b>503.230.9200</b> and ask for the Sales Department. Don't forget to mention WDS.</i></p> 
+      <p>1.5 Mi from Base <Link to="https://goo.gl/maps/zqv2uKd4GKG2">(Google Directions)</Link></p> 
+<Button to="tel:503.230.9200" newWindow={true}>Call to Book</Button>    
+</Block>
+  </Grid>
+  
+    <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     ## Alternative Lodging Options
     If you can&apos;t get a room at one of these hotels or just want to do something else, there are numerous other options. Here are a couple of WDS attendee favorites:
   </Block>
