1f64b1057168ce5277abe1eebc698831d2342e06
Jedd <jedidiah475@gmail.com>
2017-03-06 11:43:36 -0500

    Update location.js

--- content/pages/location.js
+++ content/pages/location.js
@@ -81,8 +81,8 @@
     <Block type="image" src="photos/society-hotel.jpg">
       ## WDS Hotel™
      The WDS Hotel™ (a.k.a. The Society Hotel) was originally built in 1881 as a boarding house for sailors and is now a modern and charming hotel featuring hostel style rooms, bunk beds, and private suites. Featuring hardwood floors and historical artifacts the hotel is a glimpse into Portland's past. 
-       <p><i>Registration is only available for 4-night packages and through WDS only.</i></p> 
-       <p>0.1 Mi from Base <Link to="https://goo.gl/maps/6D6mJGAY7dv">(Google Directions)</Link></p> 
+       <p><i>Registration is only available for 4-night packages and through WDS.</i></p> 
+       <p>0.8 Mi from Base <Link to="https://goo.gl/maps/6D6mJGAY7dv">(Google Directions)</Link></p> 
       <Button to="http://wds.fm/hotel" newWindow={true}>Learn More</Button>
     </Block>
     <Block type="image" src="photos/Paramount-Hotel.jpg">
