e979b0bf99817e07279394dea6735407cf62a24d
Jedd <jedidiah475@gmail.com>
2017-03-07 06:05:22 -0500

    Update hotel.js

--- content/pages/hotel.js
+++ content/pages/hotel.js
@@ -159,15 +159,15 @@
     </Block>
     <Block cols="8" bleed={false} margin="0 0 80px">
     <Grid numCols="3" tabletCols="2" width="968">
-      <Block type="caption" src="wds-hotel/suite-room1.jpg" label="06 Left">
+      <Block type="caption" src="wds-hotel/suite-room1.jpg" label="05 Left">
         ### Private Suites ($947)
         Private bathroom
       </Block>
-      <Block type="caption" src="wds-hotel/standard-room1.jpg" label="20 Left">
+      <Block type="caption" src="wds-hotel/standard-room1.jpg" label="19 Left">
       ### Standard Room ($697)
       Shared bathroom
       </Block>
-      <Block type="caption" src="wds-hotel/Bunk-room1.jpg" label="10 Left">
+      <Block type="caption" src="wds-hotel/Bunk-room1.jpg" label="08 Left">
         ### Bunks ($297)
       Shared bathroom
       </Block>
