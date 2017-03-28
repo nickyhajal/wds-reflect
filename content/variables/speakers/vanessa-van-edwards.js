e8516303bd38807f5432adbe23ed27c72a525bac
Jedd <jedidiah475@gmail.com>
2017-03-27 21:17:06 -1000

    Create vanessa-van-edwards.js

---
+++ content/variables/speakers/vanessa-van-edwards.js
@@ -0,0 +1,10 @@
+name: "Vanessa Van Edwards",
+year: "2017",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/vanessa.van.edwards.WDS.2017.speaker.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/vanessa.van.edwards.WDS.speaker.thumbnail.jpg",
+video: "",
+quote: "",
+---
+<p>Vanessa Van Edwards is a behavioral investigator and published author. She runs a human behavior research lab where she uncovers the hidden forces that drive our behavior called the <Link to="http://www.scienceofpeople.com/">Science of People.</Link>.</p>
+
+<p>Vanessa is fascinated by body language, leadership and charisma and writes about these topics for CNN, <i>Fast Company</i> and <i>Forbes</i>. She also writes a monthly column for <i>Entrepreneur Magazine</i> and the <i>Huffington Post</i>. Her innovative work has been featured on NPR, <i>Business Week</i> and <i>USA Today</i>. More importantly she's addicted to sour patch kids, airplane coffee and puppies. Vanessa's new book, <i><Link to="http://www.scienceofpeople.com/captivate/">Captivate</Link></i>, launches this April.</p>
