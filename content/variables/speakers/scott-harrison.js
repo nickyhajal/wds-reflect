3694686f653404e2e07479d43f5f4e6ccd98ad9e
Jedd <jedidiah475@gmail.com>
2017-03-27 21:21:55 -1000

    Update scott-harrison.js

--- content/variables/speakers/scott-harrison.js
+++ content/variables/speakers/scott-harrison.js
@@ -1,14 +1,10 @@
 name: "Scott Harrison",
-year: "2012",
-headerPhotos: "https://farm9.staticflickr.com/8025/7523487602_f85c11bac8_c.jpg,https://farm9.staticflickr.com/8282/7523487892_c16fd092bd_c.jpg,https://farm8.staticflickr.com/7109/7523487438_54c13c58c4_c.jpg",
-photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Scott+Harrison.jpg",
+year: "2012, 2017",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Scott.Harrison.WDS.2017.speaker.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Scott.Harrison.WDS.speaker.thumbnail.jpg",
 video: "//player.vimeo.com/video/49843996?byline=0&amp;portrait=0&amp;color=adbf27",
 quote: "",
 ---
-Scott spent ten years as a nightclub promoter in New York City before leaving to volunteer on a hospital ship off the coast of Liberia, Africa as a volunteer photojournalist. Returning home to New York City two years later, he founded the non-profit organization charity: water in 2006. Turning his full attention to the global water crisis and the one billion people without clean water to drink, he created public installations and innovative online fundraising platforms to spread international awareness of the issue.
-
-In five years, with the help of more than 250,000 donors worldwide, charity: water has raised over $60 million and funded 6,185 water projects in 19 developing nations. Those projects will provide over 2.5 million people with clean, safe drinking water.
-
-Scott was recently recognized in *Fortune Magazine*’s 40 under 40 list, and the Forbes Impact 30 list.
-
-Learn more about Scott and his charity, Water, on <a href="http://www.charitywater.org/about/scotts_story.php" target="_blank">his website</a>.
+<p>Scott Harrison is a former nightclub promoter, volunteer photojournalist, and in 2006 founded the non-profit organization <Link to="https://www.charitywater.org/">charity: water</Link>. In ten years, with the help of more than one million donors worldwide, charity: water has raised over $225 million and funded over 21,000 water projects in 24 countries. When completed, those projects will provide over 6.4 million people with clean, safe drinking water.</p> 
+        
+<p>Scott has been recognized in Fortune Magazine’s 40 under 40 list, the Forbes Magazine Impact 30 list and was recently #10 in Fast Company’s 100 Most Creative People in Business issue. Currently he serves as a World Economic Forum Young Global Leader. Five years ago, Scott received a standing ovation at WDS 2012, and more than half the community volunteered to give up their birthdays for clean water. He's returning this year with a new challenge.</p> 
