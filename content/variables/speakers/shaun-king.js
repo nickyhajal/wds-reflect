771798dc7bb616382cb74c2f37e02bc5c9f2e792
Jedd <jedidiah475@gmail.com>
2017-03-27 21:14:18 -1000

    Create shaun-king.js

---
+++ content/variables/speakers/shaun-king.js
@@ -0,0 +1,10 @@
+name: "Shaun King",
+year: "2017",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Shaun.King.WDS.2017.speaker.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Shaun.King.WDS.speaker.thumbnail.jpg",
+video: "",
+quote: "",
+---
+<p><b>Shaun King</b> is a journalist, a humanitarian, an activist, and the Senior Justice Writer for the <i>New York Daily News</i>. Today, there are crucial conversations rippling across North America—conversations happening on social media, on campuses, in the streets and around dinner tables. In greater numbers, people are talking about real empowerment and liberation for historically disadvantaged groups.</p>
+
+<p>When it comes to the Black Lives Matter movement, they’re talking specifically about human dignity for African Americans. And for this movement, Shaun is amongst the most compelling voices: a humane and passionate advocate for justice and families, and an extremely visible fundraiser for victims of brutality and discrimination. His latest project is the <Link to="http://www.injusticeboycott.com/">Injustice Boycott.</Link></p>
