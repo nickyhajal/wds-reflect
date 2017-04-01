03cb26ccc87b19ef4bf892fe2fce3fde2373f858
Jedd <jedidiah475@gmail.com>
2017-03-31 15:09:37 -1000

    Update benny-lewis.js

--- content/variables/speakers/benny-lewis.js
+++ content/variables/speakers/benny-lewis.js
@@ -5,6 +5,6 @@ photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Benny.Lewis.WDS.sp
 video: "",
 quote: "",
 ---
-Benny Lewis is the language hacker, _National Geographic_ "Traveler of the Year", author, YouTuber and charming Irish lad behind the hugely successful <Link to="http://www.fluentin3months.com/">Fluent in 3 Months</Link> blog and international best selling book. For a decade and a half he has traveled the world, immersing himself in local cultures and learning local languages. He can speak 7 languages fluently, and can get by in many more, despite the fact that he only spoke English at the age of 21. 
+Benny Lewis is the language hacker, _National Geographic_ "Traveler of the Year", author, YouTuber and charming Irish lad behind the hugely successful [Fluent in 3 Months](http://www.fluentin3months.com/) blog and international best selling book. For a decade and a half he has traveled the world, immersing himself in local cultures and learning local languages. He can speak 7 languages fluently, and can get by in many more, despite the fact that he only spoke English at the age of 21. 
 
 Although he mostly writes about language learning, his background is in electronic engineering and he applies this engineering mindset to his business, his language learning projects and his lifestyle. He is a passionate "imperfectionist" and loves learning new things that involve pushing his limits, and finding the right balance between insane random experimental wonder and strict scientific logic. Imagine Jack Sparrow meets Spock with an Irish accent!
