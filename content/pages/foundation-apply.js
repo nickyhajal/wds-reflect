4a5f34e5eb3a7786b698581f37bbbb9365205ff0
Jedd <jedidiah475@gmail.com>
2017-03-31 22:33:02 -1000

    Update foundation-apply.js

--- content/pages/foundation-apply.js
+++ content/pages/foundation-apply.js
@@ -7,7 +7,7 @@
   preload
 >
  <Image src="heading/foundation.png" width="470px" height="165px" className="scriptHeader" preload/>
-   <Block><center><Image src="https://s3.amazonaws.com/el-files-wds/images/photos/WDS-Foundation-Scholarship.png" width="640px" height="212px" /></center></Block>
+   <Block><center><Image src="https://s3.amazonaws.com/el-files-wds/images/photos/WDS-Foundation-Scholarship.png" width="840px" height="212px" /></center></Block>
   <Block cols="6">
     <iframe height="4300" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/embed/zq9n4iq0s4da50/"><a href="https://worlddominationsummit.wufoo.com/forms/zq9n4iq0s4da50/">Fill out my Wufoo form!</a></iframe>
   </Block>
