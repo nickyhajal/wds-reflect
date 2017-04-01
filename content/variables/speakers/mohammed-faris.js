a7fa316049bb6d8a5518052ae542df09d04068d9
Jedd <jedidiah475@gmail.com>
2017-03-31 15:24:13 -1000

    Update mohammed-faris.js

--- content/variables/speakers/mohammed-faris.js
+++ content/variables/speakers/mohammed-faris.js
@@ -5,6 +5,6 @@ photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/mohammed.faris.WDS
 video: "",
 quote: "",
 ---
-<p>Mohammed Faris** is an international author, speaker and leader of a community with more than one million people. He started his journey in 2007 when he became obsessed with the question: "Can faith boost productivity?" Mohammed has since spent the last decade trying to answer this question - from his own Islamic faith tradition - his popular blog,, <Link to="http://ProductiveMuslim.com">ProductiveMuslim.com</Link>, and his new book "The Productive Muslim: Where Faith Meets Productivity".</p> 
+**Mohammed Faris** is an international author, speaker and leader of a community with more than one million people. He started his journey in 2007 when he became obsessed with the question: "Can faith boost productivity?" Mohammed has since spent the last decade trying to answer this question - from his own Islamic faith tradition - his popular blog, [ProductiveMuslim.com](http://ProductiveMuslim.com), and his new book "The Productive Muslim: Where Faith Meets Productivity". 
 
-<p>Mohammed has delivered talks and trainings at numerous events and seminars across the world. He's also been featured in international media and on the TEDx stage. In 2014, the Royal Islamic Strategic Studies Centre added him to the World’s 500 Most Influential Muslims list</p>  
+Mohammed has delivered talks and trainings at numerous events and seminars across the world. He's also been featured in international media and on the TEDx stage. In 2014, the Royal Islamic Strategic Studies Centre added him to the World’s 500 Most Influential Muslims list.  
