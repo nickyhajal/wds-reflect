2bb4ad383c5f02bd95616576e153d1227582daaf
Jedd <jedidiah475@gmail.com>
2017-03-31 16:55:55 -1000

    Update vanessa-van-edwards.js

--- content/variables/speakers/vanessa-van-edwards.js
+++ content/variables/speakers/vanessa-van-edwards.js
@@ -5,6 +5,6 @@ photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/vanessa.van.edward
 video: "",
 quote: "",
 ---
-<p>Vanessa Van Edwards is a behavioral investigator and published author. She runs a human behavior research lab where she uncovers the hidden forces that drive our behavior called the <Link to="http://www.scienceofpeople.com/">Science of People.</Link>.</p>
+Vanessa Van Edwards is a behavioral investigator and published author. She runs a human behavior research lab where she uncovers the hidden forces that drive our behavior called the [Science of People](http://www.scienceofpeople.com/).
 
-<p>Vanessa is fascinated by body language, leadership and charisma and writes about these topics for CNN, <i>Fast Company</i> and <i>Forbes</i>. She also writes a monthly column for <i>Entrepreneur Magazine</i> and the <i>Huffington Post</i>. Her innovative work has been featured on NPR, <i>Business Week</i> and <i>USA Today</i>. More importantly she's addicted to sour patch kids, airplane coffee and puppies. Vanessa's new book, <i><Link to="http://www.scienceofpeople.com/captivate/">Captivate</Link></i>, launches this April.</p>
+Vanessa is fascinated by body language, leadership and charisma and writes about these topics for CNN, _Fast Company_ and _Forbes_. She also writes a monthly column for _Entrepreneur Magazine_ and the _Huffington Post_. Her innovative work has been featured on NPR, _Business Week_ and _USA Today_. More importantly she's addicted to sour patch kids, airplane coffee and puppies. Vanessa's new book, _[Captivate](http://www.scienceofpeople.com/captivate/)_, launches this April.
