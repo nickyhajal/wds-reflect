ae470eaf15c3f6283c48c49e8968a8aba086ff6d
Jedd <jedidiah475@gmail.com>
2017-04-04 11:02:49 -1000

    Update faq.js

--- content/pages/faq.js
+++ content/pages/faq.js
@@ -27,11 +27,11 @@
     <p>Tickets for WDS are only offered a couple of times a year, and they go quickly. There is no fast-track or secret process to get a ticket; they are all offered on a first-come, first-served basis. The best way to be notified when tickets go on sale is to join the 2017 <Link to="/join-us">waiting list.</Link></p>
     Please note: Once tickets are gone, they're gone. We do not hold any tickets back for higher-priced sales later, nor do we sell tickets at the door.
 
-    ### What is the refund/exchange policy on WDS tickets?
-    All WDS tickets are non-refundable.    
+    ### What is the refund/cancellation policy for WDS tickets?
+    All WDS tickets are non-refundable. Tickets can be transferred to another person for a fee up until June 10th. 
 
     ### How can I transfer a 360 ticket?
-    Transfers and name changes are available for a $100 fee from the end of March until May 10th (60 days prior to WDS) and a $200 fee from May 11th until June 10th (30 days prior to WDS). Starting **June 11th, 2017**, no further transfers or name changes will be possible.
+    Transfers and name changes are available for a $100 fee until May 10th (60 days prior to WDS) and a $200 fee from May 11th until June 10th (30 days prior to WDS). Starting **June 11th, 2017**, no further transfers or name changes will be possible.
     
     <p>You will need the name, email address, and mailing address of the new ticket holder. After entering that information and paying the transfer fee your ticket will be officially transferred to the new person</p>
 
