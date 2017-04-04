3bcb0de2219f405fb8fb394ee0b9f7be636be3bc
Jedd <jedidiah475@gmail.com>
2017-04-03 20:10:06 -1000

    Update ambassadors.js

--- content/pages/ambassadors.js
+++ content/pages/ambassadors.js
@@ -6,10 +6,10 @@
   color="white"
   preload
 >
- <Image src="heading/ambassadors.png" width="610px" height="165px" className="scriptHeader" preload/>
+ <Image src="heading/ambassadors.png" width="700px" height="165px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-    ## We need you!
-    Our entire team is composed of volunteers and has been working for a year to prepare. For the big weekend, we’ll be recruiting additional help. You’ll work directly with the WDS organizers in serving our attendees by making them feel welcome and ensuring that everything runs smoothly. Rewards include fame, pizza, high fives, unforgettable memories, and your very own coveted Ambassador t-shirt! 
+    ## Be a part of the magic of WDS!
+    We're recruiting additional help to pull off one of the world's most amazing events -- and we need you! You’ll work directly with the WDS organizers in serving our attendees with the only goal being to help attendees have the best experience possible (while also having the best experience possible). Rewards include fame, pizza, high fives, unforgettable memories, and your very own coveted Ambassador t-shirt! 
 
     Please note WDS 2017 is taking place from Tuesday July 11th - Monday July 17th. Though we don't expect Ambassadors to be available and serving for all 7 days, we do ask that you:
   - be available for an orientation the evening of Monday, July 10th
@@ -20,6 +20,6 @@ So, you want to apply to be a WDS 2017 Ambassador? Awesome!
 Just fill out this brief form and tell us a little bit about yourself (please apply by May 10th).
   </Block>
   <Block cols="6">
-    <iframe height="3000" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/embed/k1qb9rtw1jhaxfo/"><a href="https://worlddominationsummit.wufoo.com/forms/k1qb9rtw1jhaxfo/">Fill out my Wufoo form!</a></iframe>
+    <iframe height="2500" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/embed/k1qb9rtw1jhaxfo/"><a href="https://worlddominationsummit.wufoo.com/forms/k1qb9rtw1jhaxfo/">Fill out my Wufoo form!</a></iframe>
   </Block>
 </Section>
