7e4c75435880a1c527a90e2030919440280b45bc
Jedd <jedidiah475@gmail.com>
2017-04-04 09:06:11 -1000

    Update ambassadors.js

--- content/pages/ambassadors.js
+++ content/pages/ambassadors.js
@@ -9,12 +9,14 @@
  <Image src="heading/ambassadors.png" width="700px" height="165px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     ## Be a part of the magic of WDS!
-    We're recruiting additional help to pull off one of the world's most amazing events -- and we need you! You’ll work directly with the WDS organizers in serving our attendees with the only goal being to help attendees have the best experience possible (while also having your best experience possible). Rewards include new friends, fame, pizza, high fives, experiencing WDS for yourself, unforgettable memories, and your very own coveted Ambassador t-shirt! 
+    We're recruiting additional help to pull off one of the world's most amazing events -- and we need you! You’ll work directly with the WDS organizers in serving our attendees with the only goal being to help attendees have the best experience possible (while also having your best experience possible). 
+    
+    Rewards include new friends, fame, pizza, high fives, experiencing WDS for yourself, unforgettable memories, and your very own coveted Ambassador t-shirt! 
     <Image src="photos/2016-Ambassadors.jpg" width="100%" height="541" /> 
 
-    Please note WDS 2017 is taking place from Tuesday July 11th - Monday July 17th. Though we don't expect Ambassadors to be available and serving for all 7 days, we do ask that you:
-  - be available for an orientation the evening of Monday, July 10th
-  - be available the entire weekend of WDS main events from Friday, July 14th to Sunday, July 16th
+    **Please note WDS 2017 is taking place from Tuesday July 11th - Monday July 17th.** Though we don't expect Ambassadors to be available and serving for all 7 days, we do ask that you:
+  -> be available for an orientation the evening of Monday, July 10th
+  -> be available the entire weekend of WDS main events from Friday, July 14th to Sunday, July 16th
 
 So, you want to apply to be a WDS 2017 Ambassador? Awesome! 
 
