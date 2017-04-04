7ff95645925553fe6c8e1d9643c031ccfbae45b8
Jedd <jedidiah475@gmail.com>
2017-04-03 19:45:30 -1000

    Update ambassadors.js

--- content/pages/ambassadors.js
+++ content/pages/ambassadors.js
@@ -11,9 +11,9 @@
     ## We need you!
     Our entire team is composed of volunteers and has been working for a year to prepare. For the big weekend, we’ll be recruiting additional help. You’ll work directly with the WDS organizers in serving our attendees by making them feel welcome and ensuring that everything runs smoothly. Rewards include fame, pizza, high fives, unforgettable memories, and your very own coveted Ambassador t-shirt! 
 
-    <p>Please note WDS 2017 is taking place from Tuesday July 11th - Monday July 17th. Though we don't expect Ambassadors to be available and serving for all 7 days, we do ask that you:<ul>
-  <li>be available for an orientation the evening of Monday, August 8th</li>
-  <li>be available the entire weekend of WDS main events from Friday, July 14th to Sunday, July 16th
+    <p>Please note WDS 2017 is taking place from Tuesday July 11th - Monday July 17th. Though we don't expect Ambassadors to be available and serving for all 7 days, we do ask that you:
+  <ul><li>be available for an orientation the evening of Monday, August 8th</li>
+  <li>be available the entire weekend of WDS main events from Friday, July 14th to Sunday, July 16th</li>
 </ul></p>
 
 So, you want to apply to be a WDS 2017 Ambassador? Awesome! 
