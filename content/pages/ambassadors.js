0431a9a3c51178c3e6b2e9ea1c45ee44a6376e5f
Jedd <jedidiah475@gmail.com>
2017-04-03 19:48:35 -1000

    Update ambassadors.js

--- content/pages/ambassadors.js
+++ content/pages/ambassadors.js
@@ -11,14 +11,13 @@
     ## We need you!
     Our entire team is composed of volunteers and has been working for a year to prepare. For the big weekend, we’ll be recruiting additional help. You’ll work directly with the WDS organizers in serving our attendees by making them feel welcome and ensuring that everything runs smoothly. Rewards include fame, pizza, high fives, unforgettable memories, and your very own coveted Ambassador t-shirt! 
 
-    <p>Please note WDS 2017 is taking place from Tuesday July 11th - Monday July 17th. Though we don't expect Ambassadors to be available and serving for all 7 days, we do ask that you:
-  <ul><li>be available for an orientation the evening of Monday, August 8th</li>
-  <li>be available the entire weekend of WDS main events from Friday, July 14th to Sunday, July 16th</li>
-</ul></p>
+    Please note WDS 2017 is taking place from Tuesday July 11th - Monday July 17th. Though we don't expect Ambassadors to be available and serving for all 7 days, we do ask that you:
+  - be available for an orientation the evening of Monday, July 10th
+  - be available the entire weekend of WDS main events from Friday, July 14th to Sunday, July 16th
 
 So, you want to apply to be a WDS 2017 Ambassador? Awesome! 
 
-<p>Just fill out this brief form and tell us a little bit about yourself <b>(please apply by May 10th)</b>.</p> 
+Just fill out this brief form and tell us a little bit about yourself (please apply by May 10th).
   </Block>
   <Block cols="6">
     <iframe height="4300" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/embed/k1qb9rtw1jhaxfo/"><a href="https://worlddominationsummit.wufoo.com/forms/k1qb9rtw1jhaxfo/">Fill out my Wufoo form!</a></iframe>
