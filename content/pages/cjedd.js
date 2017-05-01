c6fab3e21a3690ea69a0a084514f3b87c52ed72c
Jedd <jedidiah475@gmail.com>
2017-04-30 14:29:34 -1000

    Update cjedd.js

--- content/pages/cjedd.js
+++ content/pages/cjedd.js
@@ -6,14 +6,41 @@
   color="white"
   preload
 >
-  <Image src="heading/league.of.local.leaders.header.png" width="429px" height="190px" className="scriptHeader" preload/>
+  <Image src="heading/league.of.local.leaders.header.png" width="100%" height="212px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     ## Greeting extraordinary leaders! 
     <p>We're thrilled to invite you to join us at the 7th annual <i>World Domination Summit</i> taking place July 14-16th in Portland, OR. Best of all, we want you to attend as our complimentary and honored guest!</p>
 
 <p>Since 2011, WDS has brought together more than 10,000 people from over forty countries together around the core values of <b>community, adventure, and service</b> to discuss this central question, <i>"how can we live a remarkable life in a conventional world</i>.</p>
 
-
+ <Image src="photos/local.leaders.collage.jpg" width="100%" height="576" />
+  </Block>
+</Section>
+<Section color="sea" bound={false}>
+  <Block margin="50px 0 50px 0">
+    ## What Happens at WDS
+    <Grid numCols="2" tabletCols="2" block={{ className: 'dropped-button' }} equalize>
+        <Block type="image" src="photos/JoinUs/image-speakers.png">
+        ## Main Stage Speakers
+       Our keynote speakers are bestselling authors, leading activists, and in-demand storytellers. Since our very first year in 2011, the lineup has been inclusive, multicultural, and representative of our whole community.
+      </Block>
+      <Block type="image" src="photos/JoinUs/image-meetups.jpg">
+        ## Meetups
+        Every year attendees help lead over 100+ meetups in and around Portland. Share your expertise. Coordinate an adventure. The best part is you can do both!
+      </Block>
+      <Block type="image" src="photos/JoinUs/image-activities.png">
+        ## Activities
+        From fun social gatherings to focused discussions, starting off the day with yoga to ending the night in Karaoke, choose from a variety of special WDS hosted adventures and activities throughout the week.
+      </Block>
+      <Block type="image" src="photos/JoinUs/image-people.jpg">
+        ## Incredible People
+        Our incredible adventurers come from over 40+ different countries and all 50 states. At WDS there will be an abundance of opportunities to make new, life-long connections.
+      </Block>
+      <Block type="image" src="photos/JoinUs/image-academies.jpg">
+        ## Academies
+        These half-day workshops feature some of our favorite alumni speakers and select WDS community members that are experts in their fields. As a WDS ticket holder you get first access to the Academy of your choice at a huge discount.
+      </Block>
+    </Grid>
   </Block>
 </Section>
 <Import id="join-us" />
