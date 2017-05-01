7dde2ebd725c70e7878d8a778115679bbb07e202
Jedd <jedidiah475@gmail.com>
2017-04-30 14:16:04 -1000

    Update cjedd.js

--- content/pages/cjedd.js
+++ content/pages/cjedd.js
@@ -1,13 +1,19 @@
 <Section
-  headerImage="hero/location.jpg"
+  headerImage="hero/story.jpg"
   headerClip="rl_header"
   headerPosition="top"
   headerSize="thin"
   color="white"
   preload
 >
-    <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-      ## Nothing to See Here Anymore
-    </Block>
+  <Image src="heading/league.of.local.leaders.header.png" width="429px" height="190px" className="scriptHeader" preload/>
+  <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+    ## Greeting extraordinary leaders! 
+    <p>We're thrilled to invite you to join us at the 7th annual <i>World Domination Summit</i> taking place July 14-16th in Portland, OR. Best of all, we want you to attend as our complimentary and honored guest!</p>
+
+<p>Since 2011, WDS has brought together more than 10,000 people from over forty countries together around the core values of <b>community, adventure, and service</b> to discuss this central question, <i>"how can we live a remarkable life in a conventional world</i>.</p>
+
+
+  </Block>
 </Section>
-<Import id="wait-list" />
+<Import id="join-us" />
