a41ff455fdb44ed54114b22b50a1f897ee6d7afb
Jedd <jedidiah475@gmail.com>
2017-04-30 17:55:29 -1000

    Update cjedd.js

--- content/pages/cjedd.js
+++ content/pages/cjedd.js
@@ -8,22 +8,22 @@
 >
   <Image src="heading/league.of.local.leaders.header.png" width="100%" height="106px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-    ## Greeting Extraordinary Leaders! 
-    <p>We're thrilled to invite you to join us at the 7th annual <i>World Domination Summit</i> taking place July 14-16th in Portland. Best of all, we want you to attend as our honored guest (at no charge) as part of our <b>League of Local Leaders Program</b>.</p>
+    ## Greetings Extraordinary Leaders! 
+    <p>We're thrilled to invite you to join us at the 7th annual <i>World Domination Summit</i> taking place July 14-16th in Portland. Best of all, we want you to attend as our honored guest (at no charge) as part of our <b>League of Local Leaders</b> program.</p>
 
 <p>Since 2011, WDS has brought together more than 10,000 people from over forty countries together around the core values of <b>community, adventure, and service</b> to discuss this central question, <i>"how can we live a remarkable life in a conventional world</i>.</p>
 
-## WDS + Portland + You!
+#### WDS + Portland + You!
 Though We love that people travel great distances to be here, but we also know that Portland (and our surrounding communities) boast some incredibly talented world-changers. Our goal is to create opportunities to meet with other folks who are doing amazing things from around the country and the world, and give them the opportunity to meet you.
 
-We've seen the power of bringing together remarkable people together. This will be a time to think through social challenges in creative and unconventional ways. To brainstorm new ideas for solving complex issues you’re working on and to meet people that may become key allies and partners on projects you're working on. But we're also looking forward to what you will bring to our community. What experience, knowledge, and stories can you impart to help our community grow. That's what makes WDS an event unlike any other. It's a gathering of amazing individuals where we learn from each other and then take what we've learned (supported by this remarkable community) to make a postive imapact in our local communities, and ultimately, our world. 
+We've seen the power of bringing together remarkable people together. This will be a time to think through social challenges in creative and unconventional ways. To brainstorm new ideas for solving complex issues you’re working on and to meet people that may become key allies and partners on projects you're working on. But we're also looking forward to what you will bring to our community. What experience, knowledge, and stories can you impart to help our community grow. That's what makes WDS an event unlike any other. It's a gathering of amazing individuals where we learn from each other and then take what we've learned to make a postive imapact in our local communities, and ultimately, our world. 
   
-#### Join Us? (details below) 
+<p><b>Join Us? (details below)</b></p> 
   </Block>
 </Section>
 <Section color="sea" bound={false}>
   <Block margin="50px 0 50px 0">
-    ## What Happens at WDS
+    ## A Gathering of the World's Most Remarkable People
     <Grid numCols="2" tabletCols="2" block={{ className: 'dropped-button' }} equalize>
         <Block type="image" src="photos/JoinUs/image-speakers.png">
         ## Main Stage Speakers
@@ -52,6 +52,11 @@ We've seen the power of bringing together remarkable people together. This will 
  - You will have access to register for WDS Academies at a hugely discounted rate of $29 per Academy ($59 each for the general public)
  - You will be able to propose or attend as many attendee-led meetups as you like 
  - You will receive access to the WDS mobile-app to help maximize your WDS experience
+ 
+  ## Helpful Links
+ - To learn more about how WDS got started see our story page.
+ - To watch videos from previous key note speakers and WDS years go here.
+ - To read about how WDS has impacted our attendees, checkout their own personal reviews here.
      
 ## I'm In! How do I Apply?
 Apply below to  join us in learning from unconventional thinkers, acquiring powerful new skills, and gaining an all-new community of friends and supporters. If you still have questions or want to know more email Tina at tinahartpdx@gmail.com. 
