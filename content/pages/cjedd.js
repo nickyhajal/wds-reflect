7464bb7ecf3719941d8c9ecd21229c90dfd00088
Jedd <jedidiah475@gmail.com>
2017-04-30 15:36:06 -1000

    Update cjedd.js

--- content/pages/cjedd.js
+++ content/pages/cjedd.js
@@ -6,14 +6,12 @@
   color="white"
   preload
 >
-  <Image src="heading/league.of.local.leaders.header.png" width="100%" height="212px" className="scriptHeader" preload/>
+  <Image src="heading/league.of.local.leaders.header.png" width="100%" height="106px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-    ## Greeting extraordinary leaders! 
-    <p>We're thrilled to invite you to join us at the 7th annual <i>World Domination Summit</i> taking place July 14-16th in Portland, OR. Best of all, we want you to attend as our complimentary and honored guest!</p>
+    ## Greeting Extraordinary Leaders! 
+    <p>We're thrilled to invite you to join us at the 7th annual <i>World Domination Summit</i> taking place July 14-16th in Portland, OR. Best of all, we want you to attend as our honored guest!</p>
 
 <p>Since 2011, WDS has brought together more than 10,000 people from over forty countries together around the core values of <b>community, adventure, and service</b> to discuss this central question, <i>"how can we live a remarkable life in a conventional world</i>.</p>
-
- <Image src="photos/local.leaders.collage.jpg" width="100%" height="576" />
   </Block>
 </Section>
 <Section color="sea" bound={false}>
@@ -26,21 +24,26 @@
       </Block>
       <Block type="image" src="photos/JoinUs/image-meetups.jpg">
         ## Meetups
-        Every year attendees help lead over 100+ meetups in and around Portland. Share your expertise. Coordinate an adventure. The best part is you can do both!
+        Every year attendees help lead over 100+ meetups in and around Portland. Share your expertise. Coordinate an adventure. Meet others that share your passions and interests. The best part is you can do all three!
       </Block>
       <Block type="image" src="photos/JoinUs/image-activities.png">
-        ## Activities
-        From fun social gatherings to focused discussions, starting off the day with yoga to ending the night in Karaoke, choose from a variety of special WDS hosted adventures and activities throughout the week.
-      </Block>
+        ## Celebrations
+       There's nothing better than celebrating community. Kick-off WDS and meet new friends at the welcome party and dance the night away with them at the closing celebration.      </Block>
       <Block type="image" src="photos/JoinUs/image-people.jpg">
         ## Incredible People
         Our incredible adventurers come from over 40+ different countries and all 50 states. At WDS there will be an abundance of opportunities to make new, life-long connections.
       </Block>
-      <Block type="image" src="photos/JoinUs/image-academies.jpg">
-        ## Academies
-        These half-day workshops feature some of our favorite alumni speakers and select WDS community members that are experts in their fields. As a WDS ticket holder you get first access to the Academy of your choice at a huge discount.
-      </Block>
     </Grid>
   </Block>
+</Section>
+<Section color="white">
+<Block cols="6" css={{ padding: '50px 0 20px' }} textAlign="left">
+    ## WDS + Portland
+    ##### Connect with Other Leaders and Gamechangers?
+Since our first year we’ve recognize how special our hometown of Portland is and have been trying to make stronger connections with our local community. We love that people travel great distances to be here, but we also know that Portland (and our surrounding communities) boast some incredibly talented world-changers. We’d like to give you the opportunity to meet with other folks who are doing amazing things from around the country and the world, and give them the opportunity to meet you.
+
+Specifically, we want to give local leaders like you the chance to think through your challenges in an unconventional way, to brainstorm new ideas for solving issues you’re working on, and to meet people that may be able to help you along your way. It’s a way to help support individuals in our community and as well as to more broadly lift up the area that our entire team also calls home. By waiving the standard ticket price of $647 for you, we hope that you’ll considering being a part of our community. 
+Apply below to  join us in learning from unconventional thinkers, acquiring powerful new skills, and gaining an all-new community of friends and supporters. If you still have questions or want to know more email Tina at tinahartpdx@gmail.com. 
+</Block>
 </Section>
 <Import id="join-us" />
