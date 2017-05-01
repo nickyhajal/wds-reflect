8253673feaffad4a463ccc645681941ba6580337
Jedd <jedidiah475@gmail.com>
2017-04-30 17:43:48 -1000

    Update cjedd.js

--- content/pages/cjedd.js
+++ content/pages/cjedd.js
@@ -9,9 +9,15 @@
   <Image src="heading/league.of.local.leaders.header.png" width="100%" height="106px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     ## Greeting Extraordinary Leaders! 
-    <p>We're thrilled to invite you to join us at the 7th annual <i>World Domination Summit</i> taking place July 14-16th in Portland, OR. Best of all, we want you to attend as our honored guest!</p>
+    <p>We're thrilled to invite you to join us at the 7th annual <i>World Domination Summit</i> taking place July 14-16th in Portland. Best of all, we want you to attend as our honored guest (at no charge) as part of our <b>League of Local Leaders Program</b>.</p>
 
 <p>Since 2011, WDS has brought together more than 10,000 people from over forty countries together around the core values of <b>community, adventure, and service</b> to discuss this central question, <i>"how can we live a remarkable life in a conventional world</i>.</p>
+
+Though We love that people travel great distances to be here, but we also know that Portland (and our surrounding communities) boast some incredibly talented world-changers. Our goal is to create opportunities to meet with other folks who are doing amazing things from around the country and the world, and give them the opportunity to meet you.
+
+We've seen the power of bringing together remarkable people together. This will be a time to think through social challenges in creative and unconventional ways. To brainstorm new ideas for solving complex issues you’re working on and to meet people that may become key allies and partners on projects you're working on. But we're also looking forward to what you will bring to our community. What experience, knowledge, and stories can you impart to help our community grow. That's what makes WDS an event unlike any other. It's a gathering of amazing individuals where we learn from each other and then take what we've learned (supported by this remarkable community) to make a postive imapact in our local communities, and ultimately, our world. 
+  
+Will you join us? (details below) 
   </Block>
 </Section>
 <Section color="sea" bound={false}>
@@ -26,7 +32,7 @@
         ## Meetups
         Every year attendees help lead over 100+ meetups in and around Portland. Share your expertise. Coordinate an adventure. Meet others that share your passions and interests. The best part is you can do all three!
       </Block>
-      <Block type="image" src="photos/JoinUs/image-activities.png">
+      <Block type="image" src="photos/JoinUs/image-parties.jpg">
         ## Celebrations
        There's nothing better than celebrating community. Kick-off WDS and meet new friends at the welcome party and dance the night away with them at the closing celebration.      </Block>
       <Block type="image" src="photos/JoinUs/image-people.jpg">
@@ -38,11 +44,15 @@
 </Section>
 <Section color="white">
 <Block cols="6" css={{ padding: '50px 0 20px' }} textAlign="left">
-    ## WDS + Portland
-    ##### Connect with Other Leaders and Gamechangers?
-Since our first year we’ve recognize how special our hometown of Portland is and have been trying to make stronger connections with our local community. We love that people travel great distances to be here, but we also know that Portland (and our surrounding communities) boast some incredibly talented world-changers. We’d like to give you the opportunity to meet with other folks who are doing amazing things from around the country and the world, and give them the opportunity to meet you.
-
-Specifically, we want to give local leaders like you the chance to think through your challenges in an unconventional way, to brainstorm new ideas for solving issues you’re working on, and to meet people that may be able to help you along your way. It’s a way to help support individuals in our community and as well as to more broadly lift up the area that our entire team also calls home. By waiving the standard ticket price of $647 for you, we hope that you’ll considering being a part of our community. 
+    ## The Details
+ - As our honored guest, WDS will waive the standard ticket price of $647 for you. 
+ - You'll be given a ticket to attend the main events of the WDS weekend from Friday July 14th - Sunday July 16th
+ - These tickets are non-transferrable and cannot be sold to another person
+ - You will have access to register for WDS Academies at a hugely discounted rate of $29 per Academy ($59 each for the general public)
+ - You will be able to propose or attend as many attendee-led meetups as you like 
+ - You will receive access to the WDS mobile-app to help maximize your WDS experience
+     
+## I'm In! How do I Apply?
 Apply below to  join us in learning from unconventional thinkers, acquiring powerful new skills, and gaining an all-new community of friends and supporters. If you still have questions or want to know more email Tina at tinahartpdx@gmail.com. 
 </Block>
 </Section>
