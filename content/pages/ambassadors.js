9eb1375c4841cd55c51821a8cfc57f03ef2d249c
193countries <chris.guillebeau@gmail.com>
2017-05-02 15:06:42 -0700

    update copy

--- content/pages/ambassadors.js
+++ content/pages/ambassadors.js
@@ -8,7 +8,7 @@
 >
  <Image src="heading/ambassadors.png" width="700px" height="165px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-    ## Be a part of the magic of WDS!
+    ## Be part of the magic of WDS!
     We're recruiting additional help to pull off one of the world's most amazing events -- and we need you! You’ll work directly with the WDS organizers in serving our attendees with the only goal being to help attendees have the best experience possible (while also having your best experience possible). 
     
     Rewards include new friends, fame, pizza, high fives, experiencing WDS for yourself, unforgettable memories, and your very own coveted Ambassador t-shirt! 
