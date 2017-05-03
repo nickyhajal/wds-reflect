7c01da413edd3c061c594e529c47a852c90ade28
Jedd <jedidiah475@gmail.com>
2017-05-03 13:46:52 -1000

    Update cjedd-apply.js

--- content/pages/cjedd-apply.js
+++ content/pages/cjedd-apply.js
@@ -6,7 +6,7 @@
   color="white"
   preload
 >
-  <Image src="heading/league.of.local.leaders.header.png" width="100%" height="106px" className="scriptHeader" preload/>
+  <Image src="heading/league.of.local.leaders.header.png" width="100%" height="156px" className="scriptHeader" preload/>
   <Block cols="6">
   <Image src="photos/2016-Ambassadors.jpg" width="100%" height="541" />
     <iframe height="2050" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/embed/zn8tpsb0rzdb6x/"><a href="https://worlddominationsummit.wufoo.com/forms/zn8tpsb0rzdb6x/">Fill out my Wufoo form!</a></iframe>
