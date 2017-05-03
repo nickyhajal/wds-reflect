d04d4a46e60602c7966ef04fb849743c9cc460e0
Jedd <jedidiah475@gmail.com>
2017-05-03 12:18:11 -1000

    Update cjedd.js

--- content/pages/cjedd.js
+++ content/pages/cjedd.js
@@ -11,14 +11,30 @@
     ## Greetings Extraordinary Leaders! 
     <p>We're thrilled to invite you to join us at the 7th annual <i>World Domination Summit</i> taking place July 14-16th in Portland. Best of all, we want you to attend as our honored guest (at no charge) as part of our <b>League of Local Leaders</b> program.</p>
 
-<p>Since 2011, WDS has brought together more than 10,000 people from over forty countries together around the core values of <b>community, adventure, and service</b> to discuss this central question, <i>"how can we live a remarkable life in a conventional world</i>.</p>
+<p>Since 2011, WDS has brought together more than 10,000 people from over forty countries together around the core values of <b>community, adventure, and service</b> to discuss this central question, <i>"how can we live a remarkable life in a conventional world"</i>.</p>
 
 #### WDS + Portland + You!
-Though We love that people travel great distances to be here, but we also know that Portland (and our surrounding communities) boast some incredibly talented world-changers. Our goal is to create opportunities to meet with other folks who are doing amazing things from around the country and the world, and give them the opportunity to meet you.
+We love that people travel great distances to be here, but we also know that Portland (and our surrounding communities) boast some incredibly talented world-changers. Our goal is to give you the opportunity to meet with other folks who are doing amazing things from around the country and the world, and give them the opportunity to meet you.
 
-We've seen the power of bringing together remarkable people together. This will be a time to think through social challenges in creative and unconventional ways. To brainstorm new ideas for solving complex issues you’re working on and to meet people that may become key allies and partners on projects you're working on. But we're also looking forward to what you will bring to our community. What experience, knowledge, and stories can you impart to help our community grow. That's what makes WDS an event unlike any other. It's a gathering of amazing individuals where we learn from each other and then take what we've learned to make a postive imapact in our local communities, and ultimately, our world. 
-  
-<p><b>Join Us? (details below)</b></p> 
+We've seen the power of bringing together remarkable people together, and we want to give local leaders like you the chance to think through your challenges in an unconventional way, to brainstorm new ideas for solving issues you’re working on, and to meet new allies and partners that may be able to help you along your way. We're also looking forward to the great experience, knowledge, and stories you can bring to our community. That's what makes WDS an event unlike any other. It's a gathering of amazing individuals who learn from each other and then take what we've learned to make a postive imapact in our local communities, and ultimately, our world.  
+
+## The Details
+ - As our honored guest, WDS will waive the standard ticket price of $647 for you. 
+ - You'll be given a ticket to attend the main events of the WDS weekend from Friday July 14th - Sunday July 16th
+ - These tickets are non-transferrable and cannot be sold to another person
+ - You will have access to register for WDS Academies at a hugely discounted rate of $29 per Academy ($59 each for the general public)
+ - You will be able to propose or attend as many attendee-led meetups as you like 
+ - You will receive access to the WDS mobile-app to help maximize your WDS experience
+ 
+<p><b>Helpful Links</b><p>
+ - To learn more about how WDS got started see our story page.
+ - To watch videos from previous key note speakers and WDS years go here.
+ - To read about how WDS has impacted our attendees, checkout their own personal reviews here.
+
+<p><b>Join Us? (apply below)</b></p> 
+   <Block cols="6">
+    <iframe height="2750" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/embed/zn8tpsb0rzdb6x/"><a href="https://worlddominationsummit.wufoo.com/forms/zn8tpsb0rzdb6x/">Fill out my Wufoo form!</a></iframe>
+  </Block>
   </Block>
 </Section>
 <Section color="sea" bound={false}>
@@ -42,24 +58,5 @@ We've seen the power of bringing together remarkable people together. This will 
       </Block>
     </Grid>
   </Block>
-</Section>
-<Section color="white">
-<Block cols="6" css={{ padding: '50px 0 20px' }} textAlign="left">
-    ## The Details
- - As our honored guest, WDS will waive the standard ticket price of $647 for you. 
- - You'll be given a ticket to attend the main events of the WDS weekend from Friday July 14th - Sunday July 16th
- - These tickets are non-transferrable and cannot be sold to another person
- - You will have access to register for WDS Academies at a hugely discounted rate of $29 per Academy ($59 each for the general public)
- - You will be able to propose or attend as many attendee-led meetups as you like 
- - You will receive access to the WDS mobile-app to help maximize your WDS experience
- 
-  #### Helpful Links
- - To learn more about how WDS got started see our story page.
- - To watch videos from previous key note speakers and WDS years go here.
- - To read about how WDS has impacted our attendees, checkout their own personal reviews here.
-     
-#### I'm In! How do I Apply?
-Apply below to  join us in learning from unconventional thinkers, acquiring powerful new skills, and gaining an all-new community of friends and supporters. If you still have questions or want to know more email Tina at tinahartpdx@gmail.com. 
-</Block>
 </Section>
 <Import id="join-us" />
