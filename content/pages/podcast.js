5ec08aacbad9f2355760c882ff8ebab14c704ba3
Jedd <jedidiah475@gmail.com>
2017-05-07 23:26:21 -1000

    Update podcast.js

--- content/pages/podcast.js
+++ content/pages/podcast.js
@@ -9,7 +9,7 @@
   <Image src="heading/podcast.png" width="565px" height="169px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     ## Building a Better World
-    How can we live a remarkable life in a conventional world? **_Building a Better World_** revisits talks from the main stage of the annual World Domination Summit to highlight ways that we can work together—through community, adventure, and service—to help others, live remarkable lives, and build the world we want to live in.  
+    How can we live a remarkable life in a conventional world? **_Building a Better World_** is a new podcast that revisits talks from the main stage of the annual World Domination Summit to highlight ways that we can work together—through community, adventure, and service—to help others, live remarkable lives, and build the world we want to live in.  
   <Image src="photos/league.of.leaders.banner.jpg" width="100%" height="405" />
       </Block>
   
@@ -19,16 +19,15 @@
       So what does it mean to Build a Better World? Well, to answer that question, this new series will highlight some of the most popular main-stage WDS talks.    
      </Block>
       <Grid numCols="3" tabletCols="2" width="968">
-      <Block type="image-overlay" anchor="right" icon="outline-beers" src="photos/wds.podcast.jonathan.fields.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+      <Block type="image-overlay" anchor="right" src="photos/wds.podcast.jonathan.fields.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Episode 1: Jonathan Fields
-       #### The 5 BEs for Living A Good Life
        In this recent keynote, Jonathan looked back at all the interviews he’s done to find a common thread for living a good life. He calls them: The Five BEs     
        </Block>      
-        <Block type="image-overlay" anchor="left" icon="outline-bike" src="photos/wds.podcast.tess.vigeland.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+        <Block type="image-overlay" anchor="left" src="photos/wds.podcast.tess.vigeland.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Episode 2: Tess Vigeland
         Tess' first book, titled “Leap: Leaving A Job With No Plan B to Find the Career and Life You Really Want,” was published as a direct result of her heartfelt, soul-bearing experience from the main stage of the World Domination Summit in 2013.      
         </Block>      
-        <Block type="image-overlay" anchor="right" icon="outline-eye" src="photos/wds.podcast.john.francis.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+        <Block type="image-overlay" anchor="right" src="photos/wds.podcast.john.francis.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Episode 3: John Francis
         John shares with us how he discovered his concern for the environment, why he decided to stop talking, and what he learned about the power of listening and observing in the process.
       </Block>
