86e6c949d5c85d69670e7e95a483029931a80c12
Jedd <jedidiah475@gmail.com>
2017-05-07 23:41:34 -1000

    Update podcast.js

--- content/pages/podcast.js
+++ content/pages/podcast.js
@@ -23,17 +23,17 @@
       <Block type="image-overlay" anchor="right" src="photos/wds.podcast.jonathan.fields.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Episode 1: Jonathan Fields
        In this recent keynote, Jonathan looked back at all the interviews he’s done to find a common thread for living a good life. He calls them: The Five BEs     
-       [Download Episode 1 Here](https://www.youtube.com/watch?v=dQw4w9WgXcQ)
+       ####[>> Download Episode 1](https://www.youtube.com/watch?v=dQw4w9WgXcQ)
          </Block>      
         <Block type="image-overlay" anchor="left" src="photos/wds.podcast.tess.vigeland.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Episode 2: Tess Vigeland
-        Tess' first book, titled “Leap: Leaving A Job With No Plan B to Find the Career and Life You Really Want,” was published as a direct result of her heartfelt, soul-bearing experience from the main stage of the World Domination Summit in 2013.      
-        [Download Episode 2 Here](https://www.youtube.com/watch?v=dQw4w9WgXcQ)
+        At WDS 2013 Tess shared her heartfelt, soul-bearing experience that led to her first book “Leap: Leaving A Job With No Plan B to Find the Career and Life You Really Want.”
+       ####[>> Download Episode 2](https://www.youtube.com/watch?v=dQw4w9WgXcQ)
         </Block>      
         <Block type="image-overlay" anchor="right" src="photos/wds.podcast.john.francis.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Episode #3: John Francis
         John shares with us how he discovered his concern for the environment, why he decided to stop talking, and what he learned about the power of listening and observing in the process.
-        [Download Episode 3 Here](https://www.youtube.com/watch?v=dQw4w9WgXcQ)
+      ####[>> Download Episode 3](https://www.youtube.com/watch?v=dQw4w9WgXcQ)
      </Block>
     </Grid>
   </Block>
