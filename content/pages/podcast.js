07b5c56bd7e293d8c1ad263f51e25f22f01c0d52
Jedd <jedidiah475@gmail.com>
2017-05-07 23:35:57 -1000

    Update podcast.js

--- content/pages/podcast.js
+++ content/pages/podcast.js
@@ -9,7 +9,8 @@
   <Image src="heading/podcast.png" width="565px" height="169px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     ## Building a Better World
-    How can we live a remarkable life in a conventional world? **_Building a Better World_** is a new podcast that revisits talks from the main stage of the annual World Domination Summit to highlight ways that we can work together—through community, adventure, and service—to help others, live remarkable lives, and build the world we want to live in.  
+    How can we live a remarkable life in a conventional world? 
+    **_Building a Better World_** is a new podcast that revisits talks from the main stage of the annual World Domination Summit to highlight ways that we can work together—through community, adventure, and service—to help others, live remarkable lives, and build the world we want to live in.  
   <Image src="photos/league.of.leaders.banner.jpg" width="100%" height="405" />
       </Block>
   
@@ -22,15 +23,18 @@
       <Block type="image-overlay" anchor="right" src="photos/wds.podcast.jonathan.fields.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Episode 1: Jonathan Fields
        In this recent keynote, Jonathan looked back at all the interviews he’s done to find a common thread for living a good life. He calls them: The Five BEs     
-       </Block>      
+       [Download Episode 1 Here](https://www.youtube.com/watch?v=dQw4w9WgXcQ)
+         </Block>      
         <Block type="image-overlay" anchor="left" src="photos/wds.podcast.tess.vigeland.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Episode 2: Tess Vigeland
         Tess' first book, titled “Leap: Leaving A Job With No Plan B to Find the Career and Life You Really Want,” was published as a direct result of her heartfelt, soul-bearing experience from the main stage of the World Domination Summit in 2013.      
+        [Download Episode 2 Here](https://www.youtube.com/watch?v=dQw4w9WgXcQ)
         </Block>      
         <Block type="image-overlay" anchor="right" src="photos/wds.podcast.john.francis.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
-        ## Episode 3: John Francis
+        ## Episode #3: John Francis
         John shares with us how he discovered his concern for the environment, why he decided to stop talking, and what he learned about the power of listening and observing in the process.
-      </Block>
+        [Download Episode 3 Here](https://www.youtube.com/watch?v=dQw4w9WgXcQ)
+     </Block>
     </Grid>
   </Block>
 </Section>
