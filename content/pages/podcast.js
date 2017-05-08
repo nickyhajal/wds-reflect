6e374d94c106338a304e6abf2d1fa8b09bded68a
Jedd <jedidiah475@gmail.com>
2017-05-07 22:51:06 -1000

    Update podcast.js

--- content/pages/podcast.js
+++ content/pages/podcast.js
@@ -12,20 +12,21 @@
     How can we live a remarkable life in a conventional world? **_Building a Better World_** revisits talks from the main stage of the annual World Domination Summit to highlight ways that we can work together—through community, adventure, and service—to help others, live remarkable lives, and build the world we want to live in.  
   <Image src="photos/league.of.leaders.banner.jpg" width="100%" height="405" />
       </Block>
-  <Block margin="125px 0 50px 0">
+  
+ <Block margin="125px 0 50px 0">
     <Block cols="6">
       ## Episodes
-      So what does it mean to Build a Better World? Well, to answer that question, this new series will highlight some of the most popular main-stage WDS talks.    </Block>
+      So what does it mean to Build a Better World? Well, to answer that question, this new series will highlight some of the most popular main-stage WDS talks.    
      </Block>
       <Grid numCols="3" tabletCols="2" width="968">
       <Block type="image-overlay" anchor="right" icon="outline-beers" src="photos/portland-sign.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Episode 1: Jonathan Fields
        #### The 5 BEs for Living A Good Life
-       In this recent keynote, Jonathan looked back at all the interviews he’s done to find a common thread for living a good life. He calls them: The Five BEs      </Block>
+       In this recent keynote, Jonathan looked back at all the interviews he’s done to find a common thread for living a good life. He calls them: The Five BEs     
        </Block>      
         <Block type="image-overlay" anchor="left" icon="outline-bike" src="photos/pearl-district-portland.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Episode 2: Tess Vigeland
-         Tess Vigeland is a veteran journalist, and a well-known voice to millions of American radio listeners. Her first book, titled “Leap: Leaving A Job With No Plan B to Find the Career and Life You Really Want,” was published in August 2015 and was a direct result of her heartfelt, soul-bearing experience from the main stage of the World Domination Summit in 2013.      </Block>
+         Tess Vigeland is a veteran journalist, and a well-known voice to millions of American radio listeners. Her first book, titled “Leap: Leaving A Job With No Plan B to Find the Career and Life You Really Want,” was published in August 2015 and was a direct result of her heartfelt, soul-bearing experience from the main stage of the World Domination Summit in 2013.      
         </Block>      
         <Block type="image-overlay" anchor="right" icon="outline-eye" src="photos/powells-books.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Episode 3: John Francis
