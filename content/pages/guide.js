4558abe80e7ed7afbb0ebe416eee83d8d7f63073
Jedd <jedidiah475@gmail.com>
2017-05-08 23:04:51 -1000

    Update guide.js

--- content/pages/guide.js
+++ content/pages/guide.js
@@ -32,44 +32,66 @@
   </Block>
   <Image src="photos/jellyfish.png" width="426" height="263" margin="-50px -60px 100px 0" css={{ float: 'right' }} mobile={{ all: { margin: '-50px -15px 100px 0' } }} />
   <div className="clear" />
+    </Section> 
+    <Section color="canvas">
 <Block margin="125px 0 50px 0">
     <Block cols="6">
       ## You're Guide to WDS 2017
-      Use this helpful steps to help you prepare for WDS 2017.    
      </Block>
       <Grid numCols="3" tabletCols="2" width="968">
       <Block type="image-overlay" anchor="right" src="photos/JoinUs/image-speakers.png" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 1: Understand the WDS Schedule of Events
-       In this recent keynote, Jonathan looked back at all the interviews he’s done to find a common thread for living a good life. He calls them: The Five BEs     
-          <Button to="https://www.youtube.com/watch?v=dQw4w9WgXcQ" newWindow={true} styling="blue" fitToText>Download Episode 1</Button> 
+        WDS is unique in that you create your own personalized schedule from the weeks events.      
+          <Button to="https://madmimi.com/p/7db00a/" newWindow={true} styling="orange" fitToText>Learn More</Button> 
          </Block>      
         <Block type="image-overlay" anchor="left" src="photos/society-hotel.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 2: When to Arrive and Lodging Information
-        At WDS 2013 Tess shared her heartfelt, soul-bearing experience about leaving a job with no plan b. 
-       <Button to="https://www.youtube.com/watch?v=dQw4w9WgXcQ" newWindow={true} styling="blue" fitToText>Coming Soon</Button>
+        Love logistics and travel planning? Even if you don't this information will be helpful.
+       <Button to="https://madmimi.com/p/4504d9/" newWindow={true} styling="blue" fitToText>Learn more</Button>
         </Block>      
         <Block type="image-overlay" anchor="right" src="photos/JoinUs/image-academies.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 3: Register for Academies
-        John shares with us how he discovered his concern for the environment, why he decided to stop talking, and the power of listening and observing in the process.
-        <Button to="https://www.youtube.com/watch?v=dQw4w9WgXcQ" newWindow={true} styling="blue" fitToText>Coming Soon</Button>     
+        Our super popular mini-events hosted by some of our favorite WDS community members are back for WDS 2017.
+        <Button to="" newWindow={true} styling="blue" fitToText>Coming Soon</Button>     
         </Block>
          <Block type="image-overlay" anchor="left" src="photos/wds.mobile.app.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 4: Download the Mobile App + Propose a Meetup
-        At WDS 2013 Tess shared her heartfelt, soul-bearing experience about leaving a job with no plan b. 
-       <Button to="https://www.youtube.com/watch?v=dQw4w9WgXcQ" newWindow={true} styling="blue" fitToText>Coming Soon</Button>
+        RSVP for activities. Check your personalized schedule. Message other attendees. The WDS app can do all of this and more. 
+       <Button to="" newWindow={true} styling="blue" fitToText>Coming Soon</Button>
         </Block>      
         <Block type="image-overlay" anchor="right" src="photos/image-meetups.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">   
         ## Step #5: RSVP for Meetups and Events
-        John shares with us how he discovered his concern for the environment, why he decided to stop talking, and the power of listening and observing in the process.
-        <Button to="https://www.youtube.com/watch?v=dQw4w9WgXcQ" newWindow={true} styling="blue" fitToText>Coming Soon</Button>     
+        Using the online Hub or the mobile app, browse and RSVP for hundreds of WDS hosted activities and attendee-led meetups.
+        <Button to="" newWindow={true} styling="blue" fitToText>Coming Soon</Button>     
+        </Block>
+        <Block type="image-overlay" anchor="left" src="photos/wds.packing.list" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+        ## Step 6: Packing List + Tips for WDS
+        Want to pack like an expert scout and maximize your WDS experience? Want helpful suggestions from WDS alum? 
+       <Button to="" newWindow={true} styling="blue" fitToText>Coming Soon</Button>
+        </Block>   
+         <Block type="image-overlay" anchor="right" src="photos/image-meetups.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">   
+        ## Step #7: Check-in at WDS Registration
+        WDS 2017 starts here. Learn what to do when you arrive to Portland.
+        <Button to="" newWindow={true} styling="blue" fitToText>Coming Soon</Button>     
+        </Block>
+          <Block type="image-overlay" anchor="left" src="photos/JoinUs/image-parties.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+        ## Step 8: Have an Amazing Experience
+        We can't to meet you. Let's do this!
         </Block>
     </Grid>
-  </Block>
-</Section>  
+  </Block> 
 <Section
   color="white"
 >
-  <Grid margin="60px 0 -20px 0">
+ <Block type="padded" background="sea">
+    <Image src="icon/circle-map-green.png" width="101" height="78"  align="center" margin="0 0 40px 0" />
+    <Block cols="5">
+      ## We're Here to Help
+      Have questions about WDS? Trying to find the best donut in Portland? Looking for a high-five? Let our concierge team know how we can help make your WDS experience great.
+           <Button to="mailto:concierge@wds.fm" newWindow={true} styling="orange" fitToText>Email Concierge Team</Button> 
+    </Block>
+  </Block> 
+  <Grid margin="60px 0 20px 0">
   ## Helpful Links
     <Block type="image" src="photos/Portland-Oregon.png" label="Portlandia">
       ## Portland
