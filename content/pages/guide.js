3a65b9493d6738a5891239b45a5a2b7cc237a88a
Jedd <jedidiah475@gmail.com>
2017-05-08 14:39:21 -1000

    Create guide.js

---
+++ content/pages/guide.js
@@ -0,0 +1,86 @@
+<Section
+  headerImage="hero/schedule.jpg"
+  headerClip="rl_header"
+  headerPosition="top"
+  headerSize="thin"
+  color="white"
+  preload
+>
+  <Image src="heading/schedule.png" width="421" height="151" className="scriptHeader" preload/>
+  <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+    ## Building a Better World
+    How can we live a remarkable life in a conventional world? 
+    **_Building a Better World_** is a new podcast that revisits talks from the main stage of the annual World Domination Summit to highlight ways that we can work together—through community, adventure, and service—to help others, live remarkable lives, and build the world we want to live in.  
+  <Image src="photos/league.of.leaders.banner.jpg" width="100%" height="405" />
+      </Block>
+  <Image src="photos/audience.png"
+    width="426" height="263"
+    margin="0 0 -100px -50px"
+    mobile={{ phone: { margin: '0 0 -100px -20px', width: '100%' } }}
+    preload
+  />
+  <Block type="padded" cols="6" bleed={false} background="sea" className="table-list" css={{ padding: '60px 40px', position: 'relative', zIndex:'4' }} clip="top_lr_1.6">
+    ## A Week of WDS: July 11th - 17th, 2017
+
+    - **Tuesday** Small Group Activities and Attendee-Led Meetups
+    - **Wednesday** Academies, Small Group Activities and Attendee-Led Meetups
+    - **Thursday** Academies, Small Group Activities, and Attendee-Led Meetups
+    <li><b className="highlight highlight-2">Friday</b> Big Adventure, Academies, Opening Party</li>
+    <li><b className="highlight highlight-2">Saturday</b> Main Stage Keynotes, Evening Activities</li>
+    <li><b className="highlight highlight-3">Sunday</b> Main Stage Keynotes, Closing Party</li>
+    - **Monday** Academies and Attendee-Led Meetups
+      <i>Can't come for the whole time? No problem, but you don't want to miss the main events taking place on Friday, Saturday, and Sunday. For the best possible WDS experience we recommend arriving no later than Thursday evening, and leaving no earlier than Monday morning.</i>
+  </Block>
+  <Image src="photos/jellyfish.png" width="426" height="263" margin="-50px -60px 100px 0" css={{ float: 'right' }} mobile={{ all: { margin: '-50px -15px 100px 0' } }} />
+  <div className="clear" />
+</Section>
+<Section
+  color="white"
+>
+<Block margin="125px 0 50px 0">
+    <Block cols="6">
+      ## Steps to WDS 2017
+      Keep checking back for new episodes from more of our favorite alumni speakers.   
+     </Block>
+      <Grid numCols="3" tabletCols="2" width="968">
+      <Block type="image-overlay" anchor="right" src="photos/wds.podcast.jonathan.fields.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+        ## Step 1: Understand the WDS Schedule of Events
+       In this recent keynote, Jonathan looked back at all the interviews he’s done to find a common thread for living a good life. He calls them: The Five BEs     
+          <Button to="https://www.youtube.com/watch?v=dQw4w9WgXcQ" newWindow={true} styling="blue" fitToText>Download Episode 1</Button> 
+         </Block>      
+        <Block type="image-overlay" anchor="left" src="photos/wds.podcast.tess.vigeland.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+        ## Step 2: When to Arrive and Book Lodging
+        At WDS 2013 Tess shared her heartfelt, soul-bearing experience about leaving a job with no plan b. 
+       <Button to="https://www.youtube.com/watch?v=dQw4w9WgXcQ" newWindow={true} styling="blue" fitToText>Coming Soon</Button>
+        </Block>      
+        <Block type="image-overlay" anchor="right" src="photos/wds.podcast.john.francis.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+        ## Step 3: Register for Academies
+        John shares with us how he discovered his concern for the environment, why he decided to stop talking, and the power of listening and observing in the process.
+        <Button to="https://www.youtube.com/watch?v=dQw4w9WgXcQ" newWindow={true} styling="blue" fitToText>Coming Soon</Button>     
+        </Block>
+        <Block type="image-overlay" anchor="right" src="photos/wds.podcast.john.francis.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+        ## Step 4: Download the Mobile App + Submit a Meetup
+        John shares with us how he discovered his concern for the environment, why he decided to stop talking, and the power of listening and observing in the process.
+        <Button to="https://www.youtube.com/watch?v=dQw4w9WgXcQ" newWindow={true} styling="blue" fitToText>Coming Soon</Button>     
+        </Block>
+        ## Step #5: RSVP for Meetups and Events
+        John shares with us how he discovered his concern for the environment, why he decided to stop talking, and the power of listening and observing in the process.
+        <Button to="https://www.youtube.com/watch?v=dQw4w9WgXcQ" newWindow={true} styling="blue" fitToText>Coming Soon</Button>     
+        </Block>
+    </Grid>
+  </Block>
+  <Grid margin="60px 0 -20px 0">
+  ## Helpful Links
+    <Block type="image" src="photos/Portland-Oregon.png" label="Portlandia">
+      ## Portland
+      Our home base since year one. Learn more about our beloved city and confirm whether or not all the fun things you've heard is true (they probably are).
+      <Button to="/location">Learn More</Button>
+    </Block>
+    <Block type="image" src="photos/JoinUs/image-academies.jpg" label="Coming Soon">
+      ## Academies
+      These half-day workshops feature some of our favorite alumni speakers and select WDS community members that are experts in their fields.
+      <Button to="/academies">Learn More</Button>
+    </Block>
+  </Grid>
+</Section>
+<Import id="join-us" />
