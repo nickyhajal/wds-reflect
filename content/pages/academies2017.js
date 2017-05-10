f1b8957f17100ac8f7a9321f15938b289dd7ce5e
Jedd <jedidiah475@gmail.com>
2017-05-10 07:09:39 -1000

    Update academies2017.js

--- content/pages/academies2017.js
+++ content/pages/academies2017.js
@@ -8,8 +8,21 @@
 >
   <Image src="heading/academies.png" width="565px" height="169px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-    At a WDS Academy, you might learn how to travel hack your way to the dream trip you&apos;ve always wanted to take, discover the science behind happiness, or begin building greater community. Academies are half-day workshops where you can learn something new that will help you to live your *own* remarkable life story. We bring in top experts from the WDS community to share their knowledge and teach practical skills. The line up always includes a variety of topics, from business to personal development to building tiny houses. It's a great way to lead into the main events of the WDS weekend and keep the conversations going after.
+At a WDS Academy, you might learn how to travel hack your way to a life-changing travel experience, master the art of telling compelling stories, or develop a winning branding strategy for your business. 
+You'll have the opportunity to learn something new to help live your _own_ remarkable life story. It's a great way to lead into the main events of the WDS weekend and keep the conversations going afterward.
+  This short video shows more of what it's all about:
   </Block>
+<Video placeholder="photos/academies2.jpg" videoId="121164251" cols="8" /> 
+  <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+    ## WDS Academies are back in 2017! 
+Registration for Academies is separate from WDS itself and is available to WDS attendees at a heavily discounted rate of $29 each. Academies are also available to the general public for $59 per Academy. (*Please note:* if you are attending WDS, [log in to your WDS account](http://worlddominationsummit.com/login) before making your Academy selection)
+
+Each Academy has a limited number of free "Insider Access" seats only available to early-bird alumni ticket holders (folks who purchased at WDS last August) on a first-come, first-serve basis. If you have access to these seats, you'll see it as an option on each Academy page when you're signed in to your WDS account.
+
+There is no limit to how many Academies you can register for, but please note that you can only purchase Academies for yourself. No physical ticket or confirmation will be required upon check-in, just a valid photo ID. Academy tickets are capacity controlled and offered on a first-come, first-served basis. All tickets are *non-refundable* and *non-transferrable*.
+
+If you have any questions or need any assistance, please contact our concierge team at *[concierge@wds.fm](mailto:concierge@wds.fm)*.
+    </Block>
   <EventList year="17" type="academy" title="WDS 2017 Academies" />
 </Section>
 <Import id="join-us" />
