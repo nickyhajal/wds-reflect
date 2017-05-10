623de5b52bbf54c8bf6c97b27b84f10e1bead820
Jedd <jedidiah475@gmail.com>
2017-05-10 07:13:52 -1000

    Update academies2017.js

--- content/pages/academies2017.js
+++ content/pages/academies2017.js
@@ -8,13 +8,14 @@
 >
   <Image src="heading/academies.png" width="565px" height="169px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+    ## WDS Academies are back in 2017! 
 At a WDS Academy, you might learn how to travel hack your way to a life-changing travel experience, master the art of telling compelling stories, or develop a winning branding strategy for your business. 
 You'll have the opportunity to learn something new to help live your _own_ remarkable life story. It's a great way to lead into the main events of the WDS weekend and keep the conversations going afterward.
   This short video shows more of what it's all about:
   </Block>
 <Video placeholder="photos/academies2.jpg" videoId="121164251" cols="8" /> 
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-    ## WDS Academies are back in 2017! 
+    ## The Details 
 Registration for Academies is separate from WDS itself and is available to WDS attendees at a heavily discounted rate of $29 each. Academies are also available to the general public for $59 per Academy. (*Please note:* if you are attending WDS, [log in to your WDS account](http://worlddominationsummit.com/login) before making your Academy selection)
 
 Each Academy has a limited number of free "Insider Access" seats only available to early-bird alumni ticket holders (folks who purchased at WDS last August) on a first-come, first-serve basis. If you have access to these seats, you'll see it as an option on each Academy page when you're signed in to your WDS account.
