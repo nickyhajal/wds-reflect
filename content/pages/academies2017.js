f36f05fe64a26990f85f1df7b075f5df0c5fd529
Jedd <jedidiah475@gmail.com>
2017-05-10 07:20:10 -1000

    Update academies2017.js

--- content/pages/academies2017.js
+++ content/pages/academies2017.js
@@ -16,13 +16,14 @@ You'll have the opportunity to learn something new to help live your _own_ remar
 <Video placeholder="photos/academies2.jpg" videoId="121164251" cols="8" /> 
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     ## The Details 
-Registration for Academies is separate from WDS itself and is available to WDS attendees at a heavily discounted rate of $29 each. Academies are also available to the general public for $59 per Academy. (*Please note:* if you are attending WDS, [log in to your WDS account](http://worlddominationsummit.com/login) before making your Academy selection)
+Registration for Academies is separate from WDS itself and is available to WDS attendees at a heavily discounted rate of **$29 per Academy**.(**Please note:** if you are attending WDS, [log in to your WDS account](http://worlddominationsummit.com/login) before making your Academy selection)
+                                                                                                                                              Academies are also available to the general public for $59 per Academy.
 
 Each Academy has a limited number of free "Insider Access" seats only available to early-bird alumni ticket holders (folks who purchased at WDS last August) on a first-come, first-serve basis. If you have access to these seats, you'll see it as an option on each Academy page when you're signed in to your WDS account.
 
-There is no limit to how many Academies you can register for, but please note that you can only purchase Academies for yourself. No physical ticket or confirmation will be required upon check-in, just a valid photo ID. Academy tickets are capacity controlled and offered on a first-come, first-served basis. All tickets are *non-refundable* and *non-transferrable*.
+There is no limit to how many Academies you can register for, but please note that you can only purchase Academies for yourself. No physical ticket or confirmation will be required upon check-in, just a valid photo ID. Academy tickets are capacity controlled and offered on a first-come, first-served basis. All tickets are **_non-refundable_** and **_non-transferrable_**.
 
-If you have any questions or need any assistance, please contact our concierge team at *[concierge@wds.fm](mailto:concierge@wds.fm)*.
+If you have any questions or need any assistance, please contact our concierge team at **[concierge@wds.fm](mailto:concierge@wds.fm)**.
     </Block>
   <EventList year="17" type="academy" title="WDS 2017 Academies" />
 </Section>
