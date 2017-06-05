619db160c20c2735828cc284f0009f8a4f458e4b
Jedd <jedidiah475@gmail.com>
2017-06-05 09:35:32 -0700

    Update guide.js

--- content/pages/guide.js
+++ content/pages/guide.js
@@ -28,7 +28,7 @@
     - **Tuesday (11th)** Small Group Activities and Attendee-Led Meetups
     - **Wednesday (12th)** Small Group Activities and Attendee-Led Meetups
     - **Thursday (13th)** Academies, Small Group Activities, and Attendee-Led Meetups
-    <li><b className="highlight highlight-2">Friday (14th)</b> Big Adventure, Academies, Opening Party</li>
+    <li><b className="highlight highlight-2">Friday (14th)</b> Academies, Small Group Activities, Attendee-Led Meetups, and Opening Party</li>
     <li><b className="highlight highlight-2">Saturday (15th)</b> Main Stage Keynotes, Evening Activities</li>
     <li><b className="highlight highlight-3">Sunday (16th)</b> Main Stage Keynotes, Closing Party</li>
     - **Monday (17th)** Academies and Attendee-Led Meetups
