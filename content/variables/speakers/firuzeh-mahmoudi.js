9a1475133768c5701d2a916c0f6f0d51e95deaa6
Jedd <jedidiah475@gmail.com>
2017-06-16 10:51:18 -0700

    Create firuzeh-mahmoudi.js

---
+++ content/variables/speakers/firuzeh-mahmoudi.js
@@ -0,0 +1,10 @@
+name: "Firuzeh Mahmoudi",
+year: "2017",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Firuzeh.Mahmoudi.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Firuzeh.Mahmoudi.thumbnail.png",
+video: "",
+quote: "",
+---
+**Firuzeh Mahmoudi** is the Director of United for Iran, a Bay Area NGO working to improve civil liberties in Iran. After witnessing the 2009 uprising in Iran, Mahmoudi organized a global rally in 110 cities. The day turned to be the largest day of global support for Iran in history. Shortly after, Mahmoudi started United for Iran. Seven years later, United for Iran works to improve the capacity of civil society and improve human rights conditions — all through the use of technology.
+
+From 2005 to 2009, Mahmoudi was the international coordinator of a $13 million United Nations project aiming to demonstrate best environmental health practices in the global south. Previous to that, Mahmoudi served as the international co-coordinator of the environmental health coalition Health Care Without Harm, supporting communities in dozens of countries as they advocated for environmental justice and health. 
