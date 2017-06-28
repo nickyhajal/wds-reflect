a9174d94c3b7a351f0a40397bebf1fd98b8c753b
Nicky Hajal <nhajal@gmail.com>
2017-06-28 00:51:17 -0700

    event purchased

---
+++ content/pages/event-purchased.js
@@ -0,0 +1,18 @@
+<Section color="sea" css={{ marginBottom: '-18px' }} fillScreen>
+  <Block
+    width="768px"
+    bleed={true}
+    type="padded"
+    background="white"
+    textAlign="left"
+    margin="60px 0 80px"
+    clip="tr:0,1%;"
+  >
+    ## Woohoo WDS Event Purchase Complete!
+
+    We're excited you'll be joining us for a a special WDS event! You should
+    receive an email shortly with all the details.
+
+    <Button to="/events">Back to Events</Button>
+  </Block>
+</Section>;
