c5761b7ad43e25d68219b9e13874b16d3a1e5aaf
wkorenek <wekorenek@gmail.com>
2017-06-30 13:24:41 -0700

    Update conduct.js

--- content/pages/conduct.js
+++ content/pages/conduct.js
@@ -7,7 +7,7 @@
 
     <Image src="https://s3.amazonaws.com/el-files-wds/images/photos/7537795550_09638d7745_z.jpg" width="640" height="427" mobile={{ all: { width: '100%' }}} margin="0 auto 60px" preload/>
       
-      <p><b>WDS is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, or religion (or lack thereof).</b></p>
+### WDS is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, or religion (or lack thereof).
 
 We do not tolerate harassment of event participants in any form. Sexual language and imagery is not appropriate for any event venue, including talks, workshops, parties, and online media. Attendees violating these rules may be sanctioned or expelled from WDS without a refund at the discretion of the organizers.
 
