d9e8b150c1ce52bbfde524910a090029bcfc2060
Jedd <jedidiah475@gmail.com>
2017-07-09 09:43:22 -0700

    Create Harris.Zafar.js

---
+++ content/variables/speakers/Harris.Zafar.js
@@ -0,0 +1,12 @@
+name: "Harris Zafar",
+year: "2017",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Harris.Zafar.WDS2017.speaker.png",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Harris.Zafar.WDS2017.speaker.thumbnail.png",
+video: "",
+quote: "",
+---
+**Harris Zafar** is National Spokesperson for Ahmadiyya Muslim Community USA – among the oldest Muslim organizations in America – and the author of “Demystifying Islam: Tackling the Tough Questions.” Harris addresses issues facing Islam and the Muslim world. A staunch advocate for universal human rights, Harris was praised in a March 2012 motion raised on the floor of the House of Commons in Great Britain for his work defending religious freedom.
+
+From college campuses like Harvard, Yale and Carnegie Mellon to interfaith services, churches and other public events, Harris has spoken across the United States and internationally. From Fox News and CNN to syndicated radio programs, he regularly appears on national and local news in print, TV and radio to provide insight on current issues from an Islamic perspective and to condemn intolerance and human rights abuses, especially when done in the name of religion.
+
+The son of Pakistani parents, Harris was born and raised in the United States and maintains his identity as a Muslim and an American by following Islam’s commandment to be a loyal citizen of the country in which one resides. He is married with three beautiful children and currently resides in Portland, Oregon.
