540f0e34baae9873e5b04e37b97ca6f551f1ce49
wkorenek <wekorenek@gmail.com>
2017-07-16 18:44:15 -0700

    Update everybody.js

--- content/pages/everybody.js
+++ content/pages/everybody.js
@@ -1,5 +1,6 @@
 <Section color="sea" css={{ marginBottom: '-2px' }} >
   <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
+    <Image src="<Image src="https://s3.amazonaws.com/el-files-wds/images/photos/everybody.jpg" width="700px" height="165px" className="scriptHeader" preload/>
     <Block width="100%" margin="40px 0 0 0" textAlign="center">## Let's keep this going!</Block>
     We'll have more information soon, so fill out this form and keep an eye out on your inbox for more information.
     <iframe height="497" allowTransparency="true" frameborder="0" scrolling="no" style={{width:'100%', border:'none'}}  src="https://worlddominationsummit.wufoo.com/embed/mfc5ns40aeiur6/" />
