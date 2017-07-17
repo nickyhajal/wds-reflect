6ef6843041b4246515835421508160c8ae91465e
Nicky Hajal <nhajal@gmail.com>
2017-07-16 18:05:41 -0700

    errbdy

--- content/pages/everybody.js
+++ content/pages/everybody.js
@@ -1,7 +1,4 @@
-<Section
-  color="sea"
-  css={{ marginBottom: '-2px' }}
->
+<Section color="sea" css={{ marginBottom: '-2px' }}>
   <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
     <Block width="100%" margin="40px 0 0 0" textAlign="center">## Coming Soon!</Block>
 </Section>
