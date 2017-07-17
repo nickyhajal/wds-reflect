765a0d6e7622f6c44308f755ca5096a4245cc478
wkorenek <wekorenek@gmail.com>
2017-07-16 18:22:52 -0700

    Update everybody.js

--- content/pages/everybody.js
+++ content/pages/everybody.js
@@ -1,5 +1,13 @@
-<Section color="sea" css={{ marginBottom: '-2px' }}>
+<Section
+  color="sea"
+  css={{ marginBottom: '-2px' }}
+>
   <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
-    <Block width="100%" margin="40px 0 0 0" textAlign="center">## Coming Soon!</Block>
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## Let's keep this going!</Block>
+    We'll have more information soon, so fill out this form and keep an eye out on your inbox for more information.
+    <iframe height="497" allowTransparency="true" frameborder="0" scrolling="no" style={{width:'100%', border:'none'}}  src="https://worlddominationsummit.wufoo.com/embed/mfc5ns40aeiur6/" />
+      <iframe height="647" allowTransparency="true" frameborder="0" scrolling="no" style="width:100%;border:none"  src="https://aonc.wufoo.com/embed/qrj1w5v0u0kjjp/"></iframe>
+  </Block>
 </Section>
 <Import id="join-us" />
+
