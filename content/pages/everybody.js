5bf5195f08c876fb2b0c95144983fd9456633105
wkorenek <wekorenek@gmail.com>
2017-07-18 18:19:30 -0700

    Update everybody.js

--- content/pages/everybody.js
+++ content/pages/everybody.js
@@ -6,7 +6,7 @@
     <Block width="100%" margin="40px 0 0 0" textAlign="center">## Let's keep this going!</Block>
       <Image src="https://s3.amazonaws.com/el-files-wds/images/photos/35130494774_df5f3d840a_z.jpg" width="640" height="427" mobile={{ all: { width: '100%' }}} margin="0 auto 60px" preload/>
         We'll have more information soon, so fill out this form and keep an eye out on your inbox.
-    <iframe height="647" allowTransparency="true" frameborder="0" scrolling="no" style={{width:'100%', border:'none'}}  src="https://aonc.wufoo.com/embed/qrj1w5v0u0kjjp/"></iframe>
+    <iframe height="647" allowTransparency="true" frameborder="0" scrolling="no" style={{width:'100%', border:'none'}}  src="https://worlddominationsummit.wufoo.com/embed/zg6r8ee1qhiu5i"></iframe>
   </Block>
 </Section>
 <Import id="join-us" />      
