d653eba99f3559234516e9d4641366a8b269a9f9
Jedd <jedidiah475@gmail.com>
2017-08-01 19:23:51 -1000

    Update home.js

--- content/pages/home.js
+++ content/pages/home.js
@@ -13,7 +13,7 @@
 <Section color="white" css={{ padding: '90px 0 230px 0' }} mobile={{ phone: { padding: '20px 0 230px 0' } }}>
   <Block width="600px" css={{ color: 'orange' }}>
     <h2 style={{ marginBottom: '4px' }}>World Domination Summit 2018:</h2>
-    ## June 26 - July 2, 2017 in Portland, Oregon
+    ## June 26 - July 2, 2018 in Portland, Oregon
   </Block>
   <Block cols="6" className="bigger" mobile={{ all:{ padding: '0 50px', marginBottom: '20px' }, phone: { padding: '0 20px' } }}>
 
