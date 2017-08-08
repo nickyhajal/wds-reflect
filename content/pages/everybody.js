96239dbf4447ebd90ed7eb7c761d5da3ca849e60
wkorenek <wekorenek@gmail.com>
2017-08-07 22:59:17 -0700

    Update everybody.js

--- content/pages/everybody.js
+++ content/pages/everybody.js
@@ -1,12 +1,77 @@
 <Section
+  headerClip="br:0%,-10%;"
+  headerImage="photos/35130494774_4d9152beac_o.jpg"
   color="sea"
   css={{ marginBottom: '-2px' }}
 >
-  <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
-    <Block width="100%" margin="40px 0 0 0" textAlign="center">## Let's keep this going!</Block>
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/photos/35130494774_df5f3d840a_z.jpg" width="640" height="427" mobile={{ all: { width: '100%' }}} margin="0 auto 60px" preload/>
-        We'll have more information soon, so fill out this form and keep an eye out on your inbox.
-    <iframe height="647" allowTransparency="true" frameborder="0" scrolling="no" style={{width:'100%', border:'none'}}  src="https://worlddominationsummit.wufoo.com/embed/zg6r8ee1qhiu5i"></iframe>
+  <Block
+    cols="8"
+    bleed={false}
+    textAlign="left"
+    margin="80px 0 80px"
+    background="grey"
+    type="padded"
+    padding="80px 120px"
+    className="bigger"
+  >
+      <Block type="padded" background="green">
+    <Block cols="5">
+      ## Join us for our Refugee Restart Kits Collection Day!
+      ###September 16th, 2017
+      
+        <p>Join us on <strong>September 16</strong> for a fun and meaningful WDS community reunion. We're gathering and packaging supplies for family <Link to="http://refugeecarecollective.org/restart-kits/">Restart Kits</Link>. These will supply <em>dozens</em> of refugee families who resettle in Portland over the next year!</p>
+
+**Sign up to get more info:**
+  
+  <iframe height="647" allowTransparency="true" frameborder="0" scrolling="no" style={{width:'100%', border:'none'}}  src="https://worlddominationsummit.wufoo.com/embed/zg6r8ee1qhiu5i"></iframe>
+
+Contact **[concierge@wds.fm](mailto:concierge@wds.fm)** for questions and offers.
+</Block>
+
+<Image src="photos/35970449155_18eeb8a811_z.jpg" width="640" height="427" mobile={{ all: { width: '100%' }}} margin="0 auto 60px" preload/>
+    
+##About our Everybody efforts
+
+From the beginning, WDS has been an event focused on community, adventure, and service.
+
+We have always been an affirming and inclusive community. However, we do _not_ have tolerance for intolerance. When groups of people are threatened or victimized, we will speak out, and we will take action.
+
+With the unprecedented rhetoric against immigrants, Muslims, minorities, and other historically disadvantaged groups unfolding in our world, WDS is taking a more active stand for social justice.
+
+<Grid numCols="3" tabletCols="2" width="968">
+      <Block type="image-overlay" anchor="right" src="photos/Team+Everybody.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+        ## Team Everybody
+        We’ve formed “Team Everybody”, a task force to make sure that, well, everybody’s perspective is heard as we plan WDS.
+      </Block>
+      <Block type="image-overlay" anchor="left" src="photos/Charles+Hunt.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+        ## Speaker Diversity
+        Our main-stage speaker lineup has always been diverse and inclusive, but we're making sure to invite activists who’ve taken real risks to advance equality and civil rights
+      </Block>
+      <Block type="image-overlay" anchor="right" src="photos/Local+Leaders.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+        ## Powell’s Books
+        We’re meeting with local activists and community leaders in Portland and inviting more of them to be part of WDS.
+      </Block>
+      <Block type="image-overlay" anchor="left" src="photos/Welcome+Kit.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+        ## Refugee Care Collective
+        <p>We partnered with the Portland non-profit, **<Link to="http://refugeecarecollective.org/">Refugee Care Collective</Link>**, to help our community learn how we can be more welcoming to our new neighbors coming from places of great hardship.</p>
+      </Block>
+    </Grid>
+
+<Image src="photos/Supper+Club.jpg" width="640" height="427" mobile={{ all: { width: '100%' }}} margin="0 auto 60px" preload/>
+          
+          **Remember:** we’re in this together! Thank you for joining with us to make the world a better and more welcoming place.
+
+To learn more about refugees in the U.S. and refugee services, visit the Refugee Care Collective site:
+<p>- **<Link to="http://refugeecarecollective.org/get-involved/">Get Involved</Link>**</p>
+<p>- **<Link to="http://refugeecarecollective.org/frequently-asked-questions/">FAQs</Link>**</p>
+<p>- **<Link to="http://refugeecarecollective.org/awareness/">Awareness articles and resources</Link>**</p> 
+  
+  <Block type="padded" background="green">
+  
+  <p>**If you're not an Oregon resident, <Link to="https://static1.squarespace.com/static/580e4274e58c624696efadc6/t/595a92019de4bbb650b10ec0/1499107841341/Public+Affiliate+Directory+7-3-17.pdf">check these listings</Link> and <Link to="https://static1.squarespace.com/static/580e4274e58c624696efadc6/t/583c76e7440243877aff59ef/1480357955543/PRM-RPP+Affilaite+Sites+2016+-+LG">this map</Link> for refugee resettlement service organizations in your State**</p> 
+
+</Block>
+  
   </Block>
 </Section>
 <Import id="join-us" />      
