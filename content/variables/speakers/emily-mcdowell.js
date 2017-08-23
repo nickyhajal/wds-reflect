e620413003b78bfc82d2a3cde06787de625b9ced
Jedd <jedidiah475@gmail.com>
2017-08-23 10:16:15 -0700

    Update emily-mcdowell.js

--- content/variables/speakers/emily-mcdowell.js
+++ content/variables/speakers/emily-mcdowell.js
@@ -3,7 +3,7 @@ year: "2016",
 headerPhotos: "https://c1.staticflickr.com/9/8302/28906219592_5fca79ee30_b.jpg,https://c1.staticflickr.com/9/8374/28934826731_ac0f922ec1_b.jpg,https://c2.staticflickr.com/8/7616/28725075450_c2cdc28712_b.jpg,https://c1.staticflickr.com/9/8285/28906216292_4f9818f027_b.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Emily+McDowell.jpg",
 video: "//player.vimeo.com/video/183727918?byline=0&amp;portrait=0&amp;color=adbf27",
-quote: "Do no let not knowing how to do something stand in the way of not doing it. Innovation is learning while doing.",
+quote: "Do not let not knowing how to do something stand in the way of not doing it. Innovation is learning while doing.",
 ---
 
 **Emily McDowell** is a writer, illustrator, and entrepreneur who specializes in chronicling the human condition. In 2012, she left a successful career in advertising to launch her greeting card line, making cards for the relationships we actually have. Now a multimillion-dollar stationery and gift company, Emily McDowell Studio products are sold online at [emilymcdowell.com](http://emilymcdowell.com) and in nearly 2,000 stores worldwide. 
