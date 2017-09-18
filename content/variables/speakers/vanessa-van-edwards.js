7295b152c5dc521ba20988c3df714511e62e3f97
Jedd <jedidiah475@gmail.com>
2017-09-18 13:40:43 -0700

    Update vanessa-van-edwards.js

--- content/variables/speakers/vanessa-van-edwards.js
+++ content/variables/speakers/vanessa-van-edwards.js
@@ -1,10 +1,12 @@
 name: "Vanessa Van Edwards",
 year: "2017",
-headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/vanessa.van.edwards.WDS.2017.speaker.jpg",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/vanessa.van.edwards.WDS.2017.speaker.png",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/vanessa.van.edwards.WDS.speaker.thumbnail.jpg",
-video: "",
-quote: "",
+video: "https://vimeo.com/229493630",
+quote: "Behind every business, every pretty dress, everyone is wearing a pair of Spanx to hold in the wobbly stuff, my friends! Just because you can't see it, doesn't mean it's not there.",
 ---
 Vanessa Van Edwards is a behavioral investigator and published author. She runs a human behavior research lab where she uncovers the hidden forces that drive our behavior called the [Science of People](http://www.scienceofpeople.com/).
 
-Vanessa is fascinated by body language, leadership and charisma and writes about these topics for CNN, _Fast Company_ and _Forbes_. She also writes a monthly column for _Entrepreneur Magazine_ and the _Huffington Post_. Her innovative work has been featured on NPR, _Business Week_ and _USA Today_. More importantly she's addicted to sour patch kids, airplane coffee and puppies. Vanessa's new book, _[Captivate](http://www.scienceofpeople.com/captivate/)_, launches this April.
+Vanessa is fascinated by body language, leadership and charisma and writes about these topics for CNN, _Fast Company_ and _Forbes_. She also writes a monthly column for _Entrepreneur Magazine_ and the _Huffington Post_. Her innovative work has been featured on NPR, _Business Week_ and _USA Today_. More importantly she's addicted to sour patch kids, airplane coffee and puppies. Vanessa's new book, _[Captivate](http://www.scienceofpeople.com/captivate/)_, recently launched this past April.
+
+Sitting in the audience 4 years ago, Vanessa vowed that if she ever got to the WDS stage, she would share the full story behind her success. Behind the scenes is years and years of hustle, failure, finding the right people, and building a team. 
