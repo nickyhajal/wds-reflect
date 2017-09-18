cb141e1a83680105cacf9b7d4568d6603db08951
Jedd <jedidiah475@gmail.com>
2017-09-18 14:41:27 -0700

    Update mohammed-faris.js

--- content/variables/speakers/mohammed-faris.js
+++ content/variables/speakers/mohammed-faris.js
@@ -1,10 +1,11 @@
 name: "Mohammed Faris",
 year: "2017",
-headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/mohammed.faris.WDS.2017.speaker.jpg",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/mohammed.faris.WDS.2017.speaker.png",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/mohammed.faris.WDS.2017.speaker.thumbnail.png",
-video: "",
-quote: "",
+video: "https://vimeo.com/229735837/",
+quote: "It's not about the dollars and the followers. It's about long-lasting impact that truly brings benefit to yourself and those around you and society at large".
 ---
 **Mohammed Faris** is an international author, speaker and leader of a community with more than one million people. He started his journey in 2007 when he became obsessed with the question: "Can faith boost productivity?" Mohammed has since spent the last decade trying to answer this question - from his own Islamic faith tradition - his popular blog, [ProductiveMuslim.com](http://ProductiveMuslim.com), and his new book "The Productive Muslim: Where Faith Meets Productivity"
 
-Mohammed has delivered talks and trainings at numerous events and seminars across the world. He's also been featured in international media and on the TEDx stage. In 2014, the Royal Islamic Strategic Studies Centre added him to the World’s 500 Most Influential Muslims list  
+At WDS 2017 Mohammed taught us about the missing soul of productivity - leveraging spirituality to boost our productivity. There's a hidden, spiritual element of success that, in the Islamic tradition, is called "Barakah." This means that Divine goodness attaches to a thing (time, money, space) so you can do more with less, beyond your expectations through mindset, values, and rituatls with sincerity.
+
