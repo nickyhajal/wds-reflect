0bcef677b55280f8d69e98d3c41ea85069905fe5
Jedd <jedidiah475@gmail.com>
2017-10-12 07:25:48 -0700

    Update scott-harrison.js

--- content/variables/speakers/scott-harrison.js
+++ content/variables/speakers/scott-harrison.js
@@ -2,9 +2,11 @@ name: "Scott Harrison",
 year: "2017",
 headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Scott.Harrison.WDS.2017.speaker.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Scott.Harrison.WDS.speaker.thumbnail.jpg",
-video: "//player.vimeo.com/video/49843996?byline=0&amp;portrait=0&amp;color=adbf27",
-quote: "",
+video: "https://vimeo.com/230032497",
+quote: "If your work is in the service of others, if you are working to end inequality, injustice, the suffering of others, it will never end. And that’s okay! Do not be afraid of work that has no end.",
 ---
+In 2017, there is absolutely no reason why millions of people should be without clean water. Scott Harrison urges us to create a culture of generosity by giving generously and joyfully to those who lack some of the basic necessities that we take for granted. 
+        
 Scott Harrison is a former nightclub promoter, volunteer photojournalist, and in 2006 founded the non-profit organization [charity: water](https://www.charitywater.org/). In ten years, with the help of more than one million donors worldwide, charity: water has raised over $225 million and funded over 21,000 water projects in 24 countries. When completed, those projects will provide over 6.4 million people with clean, safe drinking water.
         
 Scott has been recognized in Fortune Magazine’s 40 under 40 list, the Forbes Magazine Impact 30 list and was recently #10 in Fast Company’s 100 Most Creative People in Business issue. Currently he serves as a World Economic Forum Young Global Leader. Five years ago, Scott received a standing ovation at WDS 2012, and more than half the community volunteered to give up their birthdays for clean water. He's returning this year with a new challenge.
