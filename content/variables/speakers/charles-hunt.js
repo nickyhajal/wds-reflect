9a073934a99bf599a86162118cc25e1d8b22e42b
Jedd <jedidiah475@gmail.com>
2017-10-12 07:16:23 -0700

    Update charles-hunt.js

--- content/variables/speakers/charles-hunt.js
+++ content/variables/speakers/charles-hunt.js
@@ -1,10 +1,13 @@
 name: "Charles Hunt",
 year: "2017",
-headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Charles.Hunt.WDS.2017.speaker.jpg",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Charles.Hunt.WDS.2017.speaker.png",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Charles.Hunt.WDS.speaker.thumbnail.png",
-video: "",
-quote: "",
+video: "https://vimeo.com/229861221",
+quote: "We all have the capacity and the ability and the power to make instrumental changes in our lives. And the only time we relinquish that power is when we simply stop believing that we have it.",
 ---
-**Charles Hunt** is a resilience expert, adjunct professor, speaker and trainer.  Adept at leveraging transparency to inspire and get results, this former college recruiting, talent management, diversity and supply chain leader left the corporate world to fulfill his vision of building unbreakable spirits and cultivating resiliency for those who like him, have [The Audacity](http://www.audacityfirm.com/) to succeed despite adversity. 
+
+Charles Hunt knows a thing or two about resilenance. At WDS 2017, Charles shared his powerful story and reminded us that society does not dictate who we become because we have more power than we realize—no matter who we are, where we come from, or what our circumstances are, we have the power of choice and the opportunity to become more resilient in the face of adversity. 
+  
+**Charles Hunt** is a resilience expert, adjunct professor, speaker and trainer. Adept at leveraging transparency to inspire and get results, this former college recruiting, talent management, diversity and supply chain leader left the corporate world to fulfill his vision of building unbreakable spirits and cultivating resiliency for those who like him, have [The Audacity](http://www.audacityfirm.com/) to succeed despite adversity. 
 
 With a mantra of “you’re as resilient as you THINK you are”, he helps students, young professionals, business and thought leaders build resilience while harnessing our most important resource in resilience…our minds. Charles’ TEDx Talk ["What Trauma Taught Me About Resilience"](https://www.youtube.com/watch?v=3qELiw_1Ddg&feature=youtu.be) has attracted international acclaim. A lover of hip-hop, Charles is a firm believer that there’s a Jay-Z lyric for every situation and finds joy in weaving lines into everyday conversation!
