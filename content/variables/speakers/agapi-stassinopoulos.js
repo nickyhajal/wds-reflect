98a8db86303cd4715b8d60b43510f75a0289ed8b
Jedd <jedidiah475@gmail.com>
2017-10-12 06:56:03 -0700

    Update agapi-stassinopoulos.js

--- content/variables/speakers/agapi-stassinopoulos.js
+++ content/variables/speakers/agapi-stassinopoulos.js
@@ -1,10 +1,12 @@
 name: "Agapi Stassinopoulos",
 year: "2017",
-headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Agapi.Stassinopoulos.WDS.2017.speaker.jpg",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Agapi.Stassinopoulos.WDS.2017.speaker.png",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Agapi.Stassinopoulos.WDS.speaker.thumbnail.png",
-video: "",
-quote: "",
+video: "https://vimeo.com/229780096",
+quote: "We are not supposed to fit in! We are supposed to stand out.",
 ---
+Beginning and ending her WDS 2017 keynote with joy and dancing, Agapi challenged us to claim everything we want with 'hutspa' because we're good people and we deserve it. Agapi shared her belief that we carry the light that the earth needs right now. 
+ 
 **Agapi Stassinopoulos** is a best-selling author and speaker who inspires audiences around the world. In her previous book, _Unbinding the Heart: A Dose of Greek Wisdom, Generosity, and Unconditional Love_, she shares the wisdom from her life’s adventures and experiences. In her new book, _Wake Up to the Joy of You: 52 Meditations and Practices for a Calmer, Happier Life,_ she takes readers on a journey and inspires them to let go of what doesn’t work and instead create the lives they really want. 
 
 She is currently conducting workshops for Thrive Global, a company founded by her sister, Arianna Huffington, to help change the way we work and live. Agapi has spoken and conducted meditations at many corporations including PepsiCo, Google, Nike and Starbucks. She divides her time between New York and Los Angeles and was born and raised in Athens, Greece. 
