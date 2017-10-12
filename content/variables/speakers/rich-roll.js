dc57d5128bd0cf092b3f456a1f4d3b420cf51360
Jedd <jedidiah475@gmail.com>
2017-10-12 14:24:27 -0700

    Update rich-roll.js

--- content/variables/speakers/rich-roll.js
+++ content/variables/speakers/rich-roll.js
@@ -1,10 +1,12 @@
 name: "Rich Roll",
 year: "2017",
-headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/rich.roll.jpg",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Rich.Roll.WDS.2017.speaker.png",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Rich.Roll.WDS.speaker.thumbnail.jpg",
-video: "",
-quote: "",
+video: "https://vimeo.com/230075337",
+quote: "The teachable moment ocurs when things don't go your way and you're forced to make a decision, and that decision reveals character.",
 ---
+"We live in the most prosperous nation on earth, and yet we've never been more sick as a society." As an elite athlete Rich Roll's big change to a plant-based lifestyle has become more than about fueling the body. Rich shared with the WDS community that by being more concious of what we eat we are not only creating postive change for ourselves, but also for the world. 
+  
 **Rich Roll** is one of the world's most popular world-class ultra-endurance triathletes, a widely regarded plant-based nutrition & wellness advocate, writer, public speaker, podcast host, husband and father of 4. In 2009 he was recognized as one of the “25 Fittest Men in the World” by *Men's Fitness Magazine* and has been featured on CNN and contributed to publications such as *Outside*, *Los Angeles Times Sunday Magazine*, *VegNews*, *Huffington Post*, *Triathlete*, *3/GO* and *Men’s Fitness*.
 
 After a struggle with alcoholism and 20 year respite from competitive sports, Rich adopted a plant-based diet, shed 50 pounds and launched a middle-aged assault on the Ultraman World Championships, one of the most daunting endurance races on in the world. His inspirational memoir *[Finding Ultra](http://www.richroll.com/shop/books/finding-ultra-signed/)* is a #1 bestseller on Amazon and he also hosts the popular, Rich Roll Podcast. Learn more about Rich's story at [RichRoll.com](http://www.richroll.com/).
