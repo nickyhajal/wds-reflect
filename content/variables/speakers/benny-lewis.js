68abb04fd4d6247a940280fab234eeb6d2530874
Jedd <jedidiah475@gmail.com>
2017-10-12 06:46:48 -0700

    Update benny-lewis.js

--- content/variables/speakers/benny-lewis.js
+++ content/variables/speakers/benny-lewis.js
@@ -1,10 +1,12 @@
 name: "Benny Lewis",
 year: "2017",
-headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Benny.Lewis.WDS.2017.speaker.jpg",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Benny.Lewis.WDS.2017.speaker.png",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Benny.Lewis.WDS.speaker.thumbnail.jpg",
-video: "",
-quote: "",
+video: "https://vimeo.com/229128147",
+quote: "Ask yourself, 'should I try something different' instead of 'should I give up'?",
 ---
+In his highly practical opening keynote for WDS 2017, **Benny Lewis** emphasizes that logic trumps luck. Luck is actually beating the odds, seeing the whole picture, and having an open mind. Benny encourages people to make their own luck, to gravitate to situations with unpredictability, and to use math and reasoning to understand situations, rather than listening to the universe or relying on destiny.
+  
 **Benny Lewis** is the language hacker, *National Geographic* Traveler of the year, author, YouTuber and charming Irish lad behind the hugely successful [Fluent in 3 Months](http://www.fluentin3months.com) blog and international best selling book. For a decade and a half he has traveled the world, immersing himself in local cultures and learning local languages. He can speak 7 languages fluently, and can get by in many more, despite the fact that he only spoke English at the age of 21.
 
 Although he mostly writes about language learning, his background is in electronic engineering and he applies this engineering mindset to his business, his language learning projects and his lifestyle. He is a passionate "imperfectionist" and loves learning new things that involve pushing his limits, and finding the right balance between insane random experimental wonder and strict scientific logic. Imagine Jack Sparrow meets Spock with an Irish accent!
