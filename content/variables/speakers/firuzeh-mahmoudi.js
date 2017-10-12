f9341316f8b3947917f7eb5aa4a45cc8b9b6afe7
Jedd <jedidiah475@gmail.com>
2017-10-12 07:35:41 -0700

    Update firuzeh-mahmoudi.js

--- content/variables/speakers/firuzeh-mahmoudi.js
+++ content/variables/speakers/firuzeh-mahmoudi.js
@@ -1,10 +1,12 @@
 name: "Firuzeh Mahmoudi",
 year: "2017",
-headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Firuzeh.Mahmoudi.jpg",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Firuzeh.Mahmoudi.WDS.2017.speaker.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Firuzeh.Mahmoudi.thumbnail.png",
-video: "",
-quote: "",
+video: "https://vimeo.com/229394466",
+quote: "I have been called an anti-revolutionary fugitive by the Isalmic Republic, which I think is a pretty bad-ass title, so I put it on my business card.",
 ---
+**Firuzeh Mahmoudi** has been labeled and activist and revolutionist, both which she proudly embraces. It's not hard to become an activist; it can be accidental. In her 2017 keynote, Firuzeh shared her incredible story and spoke about the "7 truths about activism". 
+  
 **Firuzeh Mahmoudi** is the Director of United for Iran, a Bay Area NGO working to improve civil liberties in Iran. After witnessing the 2009 uprising in Iran, Mahmoudi organized a global rally in 110 cities. The day turned to be the largest day of global support for Iran in history. Shortly after, Mahmoudi started United for Iran. Seven years later, United for Iran works to improve the capacity of civil society and improve human rights conditions — all through the use of technology.
 
 From 2005 to 2009, Mahmoudi was the international coordinator of a $13 million United Nations project aiming to demonstrate best environmental health practices in the global south. Previous to that, Mahmoudi served as the international co-coordinator of the environmental health coalition Health Care Without Harm, supporting communities in dozens of countries as they advocated for environmental justice and health. 
