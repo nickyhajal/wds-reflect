6300720381b0f18831682a9438fa4922f6615dc9
Jedd <jedidiah475@gmail.com>
2017-10-15 08:16:42 -0700

    Update jolie-guillebeau.js

--- content/variables/speakers/jolie-guillebeau.js
+++ content/variables/speakers/jolie-guillebeau.js
@@ -5,8 +5,8 @@ photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Jolie.Guillebeau.t
 video: "https://vimeo.com/229787785",
 quote: "Use your heart. Find that empathy and feel what it feels like for those who are suffering and struggling.",
 ---
-In her 2017 keynote, Jolie challenged us to not turn away from injustices just because they're shameful and it's easier for you to do so. We have to use our eyes, our heads, our voice, and our heart to be aware of what is going on around us and do something about it. Being kind is not enough—we need to have empathy for our fellow humans in order to make positive social change.
-  
+In her 2017 keynote, Jolie challenged the WDS community not to turn away from injustices, even though they're shameful and it's easier to do so. She encouraged the community to use our eyes, our heads, our voice, and our heart to be aware of what is going on around us and do something about it. Jolie reminded us that being kind is not enough—we need to have empathy for our fellow humans in order to make positive social change.
+
 **Jolie Guillebeau** is an artist, teacher, and perpetual learner. She recently completed an M.A. in Art Therapy Counseling and is beginning a new career understanding and serving people impacted with developmental trauma. 
 
 A founding team member of WDS, she serves as President of the WDS Foundation, Team Fixer, and our longstanding Voice of Reason. She also makes magical ice cream sundaes and can quote every lyric from Lin-Manuel Miranda's Hamilton.
