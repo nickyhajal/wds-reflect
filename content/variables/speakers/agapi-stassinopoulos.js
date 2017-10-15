037394749a49a90a2c9866d912a149ab4ede3067
Jedd <jedidiah475@gmail.com>
2017-10-15 05:33:37 -0700

    Update agapi-stassinopoulos.js

--- content/variables/speakers/agapi-stassinopoulos.js
+++ content/variables/speakers/agapi-stassinopoulos.js
@@ -5,7 +5,7 @@ photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Agapi.Stassinopoul
 video: "https://vimeo.com/229780096",
 quote: "We are not supposed to fit in! We are supposed to stand out.",
 ---
-Beginning and ending her WDS 2017 keynote with joy and dancing, Agapi challenged us to claim everything we want with 'hutspa' because we're good people and we deserve it. Agapi shared her belief that we carry the light that the earth needs right now. 
+Beginning and ending her WDS 2017 keynote with joy and dancing, Agapi challenged us to claim everything we want with 'chutzpah' because we're good people and we deserve it. Agapi shared her belief that we carry the light that the earth needs right now. 
  
 **Agapi Stassinopoulos** is a best-selling author and speaker who inspires audiences around the world. In her previous book, _Unbinding the Heart: A Dose of Greek Wisdom, Generosity, and Unconditional Love_, she shares the wisdom from her life’s adventures and experiences. In her new book, _Wake Up to the Joy of You: 52 Meditations and Practices for a Calmer, Happier Life,_ she takes readers on a journey and inspires them to let go of what doesn’t work and instead create the lives they really want. 
 
