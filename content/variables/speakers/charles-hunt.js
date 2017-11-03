348a3118e4aa155cbfe6bb5e422b7f00d6b26a71
Jedd <jedidiah475@gmail.com>
2017-11-03 15:14:03 -0700

    Update charles-hunt.js

--- content/variables/speakers/charles-hunt.js
+++ content/variables/speakers/charles-hunt.js
@@ -6,7 +6,7 @@ video: "https://vimeo.com/229861221",
 quote: "We all have the capacity and the ability and the power to make instrumental changes in our lives. And the only time we relinquish that power is when we simply stop believing that we have it.",
 ---
 
-**Charles Hunt** knows a thing or two about resilenance. At WDS 2017, Charles shared his powerful story and reminded us that society does not dictate who we become because we have more power than we realize—no matter who we are, where we come from, or what our circumstances are, we have the power of choice and the opportunity to become more resilient in the face of adversity. 
+**Charles Hunt** knows a thing or two about resilience. At WDS 2017, Charles shared his powerful story and reminded us that society does not dictate who we become because we have more power than we realize—no matter who we are, where we come from, or what our circumstances are, we have the power of choice and the opportunity to become more resilient in the face of adversity. 
   
 **Charles Hunt** is a resilience expert, adjunct professor, speaker and trainer. Adept at leveraging transparency to inspire and get results, this former college recruiting, talent management, diversity and supply chain leader left the corporate world to fulfill his vision of building unbreakable spirits and cultivating resiliency for those who like him, have [The Audacity](http://www.audacityfirm.com/) to succeed despite adversity. 
 
