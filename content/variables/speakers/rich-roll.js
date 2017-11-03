2329de2511a207eeb94d3b713349e10d5d7361a0
Jedd <jedidiah475@gmail.com>
2017-11-03 14:59:09 -0700

    Update rich-roll.js

--- content/variables/speakers/rich-roll.js
+++ content/variables/speakers/rich-roll.js
@@ -5,7 +5,7 @@ photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Rich.Roll.WDS.spea
 video: "https://vimeo.com/230075337",
 quote: "The teachable moment ocurs when things don't go your way and you're forced to make a decision, and that decision reveals character.",
 ---
-"We live in the most prosperous nation on earth, and yet we've never been more sick as a society." As an elite athlete Rich Roll's big change to a plant-based lifestyle has become more than about fueling the body. Rich shared with the WDS community that by being more concious of what we eat we are not only creating postive change for ourselves, but also for the world. 
+"We live in the most prosperous nation on earth, and yet we've never been more sick as a society." As an elite athlete Rich Roll's big change to a plant-based lifestyle has become more than about fueling the body. Rich shared with the WDS community that by being more conscious of what we eat we are not only creating positive change for ourselves, but also for the world. 
   
 **Rich Roll** is one of the world's most popular world-class ultra-endurance triathletes, a widely regarded plant-based nutrition & wellness advocate, writer, public speaker, podcast host, husband and father of 4. In 2009 he was recognized as one of the “25 Fittest Men in the World” by *Men's Fitness Magazine* and has been featured on CNN and contributed to publications such as *Outside*, *Los Angeles Times Sunday Magazine*, *VegNews*, *Huffington Post*, *Triathlete*, *3/GO* and *Men’s Fitness*.
 
