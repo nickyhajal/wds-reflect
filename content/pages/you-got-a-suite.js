7fb10133b20066a2436705b0465f67dd886dd53d
Jedd <jedidiah475@gmail.com>
2018-02-08 18:11:57 -0800

    Create you-got-a-suite.js

---
+++ content/pages/you-got-a-suite.js
@@ -0,0 +1,19 @@
+
+<Section
+  color="blue"
+  css={{ marginBottom: '-2px' }}
+>
+  <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## Aw Yeah! Your Booking is Confirmed.</Block>
+    
+       ###  You Got a Bunk! 
+    
+    <Block><Image src="wds-hotel/suite-room1.jpg" width="100%" /></Block>
+
+   <p>You should have received an email confirmation of your reservation along with some helpful information about WDS Hotel™ packages.</p>  
+   
+   <p>If you haven't, please contact our year-round concierge team at <Link to="mailto:concierge@wds.fm">concierge@wds.fm</Link> and they'll be able to assist you.</p>
+
+ <p>That's it for now. Keep checking your inbox for more information about WDS and the WDS Hotel™ as we get closer to the event.</p> 
+  </Block>
+</Section>
