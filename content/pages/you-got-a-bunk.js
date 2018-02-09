edd7028f2f3d22e03afb64fa2a703a5419dd64f3
Jedd <jedidiah475@gmail.com>
2018-02-08 17:26:24 -0800

    Create you-got-a-bunk.js

---
+++ content/pages/you-got-a-bunk.js
@@ -0,0 +1,15 @@
+<Section
+  color="blue"
+  css={{ marginBottom: '-2px' }}
+>
+  <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## Aw Yeah! Your WDS Hotel™ Package is Reserved!</Block>
+    
+    <Image src="wds-hotel/Bunk-room1.jpg" width="100%" />
+    
+
+    ###  You Got a Bunk! 
+
+   <p>You should have received an email confirmation of your reservation along with some helpful information about WDS Hotel™ packages. If you haven't, please contact our year-round concierge team at <Link to="mailto:concierge@wds.fm">concierge@wds.fm</Link> and they'll be able to assist you.</p> 
+  </Block>
+</Section>
