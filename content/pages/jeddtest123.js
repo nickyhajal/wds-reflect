6b02b81f18a2e154fa43b95921bbe42ac6c1afcf
Nicky Hajal <nhajal@gmail.com>
2018-02-08 18:53:37 -0600

    Update jeddtest123.js

--- content/pages/jeddtest123.js
+++ content/pages/jeddtest123.js
@@ -32,7 +32,7 @@
 >
   <Image src="heading/location.png" width="446px" height="156px" className="scriptHeader" preload/>
     <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-    ## Your Base in Portland, OR
+    ## Your Base in Portland, OR BALLER
     Our beloved city is one of the most electic and beautiful cities in the US. Known for our amazing food and drink, our love for the arts, the bold and creative, the outdoors -- Portland is the perfect gathering spot for remarkable adventurers.  For 2018 we'll be based out of the Newmark Theatre in downtown Portland, located two blocks from a light-rail stop and directly across from the south park blocks. 
   </Block>
     
