852a80b9041b7e6e74c8e6fb62b29db51789f698
Jedd <jedidiah475@gmail.com>
2018-02-10 23:55:20 -0800

    Update foundation.js

--- content/pages/foundation.js
+++ content/pages/foundation.js
@@ -170,7 +170,7 @@ Part of this training will be developed internationally, including a teaching tr
 The "Scholarships for Real Life" program is open to all alumni who have attended one of our past seven WDS events AND those that have purchased a ticket for WDS 2018 even if this is your first time attending.
 
 ##### 2018 Applications - Coming Soon!
-<p>If you would like to be notified when applications re-open for 2018, please email <Link to="mailto:foundation@wds.fm">foundation@wds.fm</Link> with your contact information and you'll be notified when applications become available. </p>
+<p>If you would like to be notified when applications re-open for 2018, please <Link to="http://madmimi.com/signups/150899f15087481aa41434436a6e3729/join">sign-up here with your email</Link> and you'll be notified when applications become available.</p>
 </Block>
 </Section>
 <Section color="green">
