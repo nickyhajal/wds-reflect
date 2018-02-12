faaf88a91404c795afc950fd4fcc63038f68faec
Jedd <jedidiah475@gmail.com>
2018-02-12 07:12:59 -0800

    Update ambassadors.js

--- content/pages/ambassadors.js
+++ content/pages/ambassadors.js
@@ -16,16 +16,18 @@
        </Block>
       <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
  ## How to Apply
-**Applications to be a 2018 Ambassador are now open!** 
+
+### Applications to be a 2018 Ambassador are now open! 
   
  We are searching for 75 amazing people ready to support our attendees throughout WDS. To be an Ambassador, we ask that you:
 + Commit to helping our attendees have the best experience possible! 
 + Live in or near Portland, Oregon (or are very familiar with the city)
-+ Are generally available during WDS week from Tuesday, June 26th through Monday, July 2nd
-+ Are **fully available** Friday, June 29th - Sunday, July 1st (no conflicts from morning to night all three days)
++ Are available some of the time during WDS week from Tuesday, June 26th through Monday, July 2nd
++ Are **_fully available_** Friday, June 29th - Sunday, July 1st (no conflicts from morning to night all three days)
 + Can attend Ambassador orientation on the evening of Monday, June 25th. 
 + Are ready to work hard and play hard to help make WDS 2018 our best event yet!
 
+  
   _Special consideration will be given to those that are available for the entire WDS week from Monday, June 25th until Monday, July 2nd._
   
   Please fill out the application by clicking on the link below and we’ll get back to you within about 3 weeks with next steps. Questions? Email Tina at concierge@wds.fm for help!
