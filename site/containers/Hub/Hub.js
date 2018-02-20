438d84b847282f647d28f103857584196b617ae3
Nicky Hajal <nhajal@gmail.com>
2018-02-20 01:25:46 -0600

    updates

--- site/containers/Hub/Hub.js
+++ site/containers/Hub/Hub.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import CSSModules from 'react-css-modules';
 import autoBind from 'react-autobind';
+import styled from 'styled-components';
 import actions from '../../actions';
 import types from '../../utils/types';
 import styles from './styles.css';
@@ -10,8 +11,19 @@ import Dispatch from '../../components/Dispatch/Dispatch';
 import angler from '../../utils/angler';
 import Section from '../../components/Section/Section';
 import Block from '../../components/Block/Block';
+import Link from '../../components/Link/Link';
+
 // import Auth from '~/containers/Auth';
 // import Dashboard from '~/containers/Dashboard';
+
+
+const SideBtn = styled(Link)`
+  width: 280px;
+  height: 187px;
+  background-size: cover;
+  display: block;
+  margin-bottom: 20px;
+`
 
 export class Hub extends Component {
 
@@ -32,7 +44,10 @@ export class Hub extends Component {
       <div styleName="shell">
         <Section headerSize="thin" headerClip="br:0%,-10%;" headerImage="hero/green.jpg" color="canvas">
           <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <div style={{ width: '280px', background: 'blue', marginRight: '20px' }} />
+            <div style={{ width: '280px', background: 'transparent', marginRight: '20px', marginTop: '-35px' }}>
+              <SideBtn style={{ backgroundImage: 'url(https://s3.amazonaws.com/el-files-wds/images/wds-portlandia.png)'}} to="/a-wds-guide-of-portlandia" />
+              <SideBtn style={{ backgroundImage: 'url(https://files.wds.fm/images/where-to-stay.png)'}} to="/a-wds-guide-of-portlandia" />
+            </div>
               <Block cols="6" background="transparent" styleName="contentShell">
                 <h2 style={headcss} >The Dispatch</h2>
                 <Block width="100%" background="white" type="padded">
