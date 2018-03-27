d809fe56f33434949290994a7ea0d02191c7ff71
Jedd <jedidiah475@gmail.com>
2018-03-27 02:21:13 -0700

    Update c2.js

--- content/pages/c2.js
+++ content/pages/c2.js
@@ -8,39 +8,40 @@ preload
 >
 <Image src="heading/script-joinUs.png" width="446px" height="196px" className="scriptHeader" align="center" css={{ float: 'none' }} preload/>
 <Block width="600px" css={{ color: 'orange', marginTop: '80px' }}>
-  <h2 style={{ marginBottom: '4px' }}>World Domination Summit 2018:</h2><p></p>
-  ## June 26 - July 2, 2018 in Portland, Oregon
+  <h2 style={{ marginBottom: '4px' }}>World Domination Summit 2018</h2><p></p>
+    ## June 26 - July 2, 2018 in Portland, Oregon
 
 </Block>
 <Block cols="6" className="bigger" mobile={{ all:{ padding: '0 50px', marginBottom: '20px' }, phone: { padding: '0 20px' } }}>
 
-For 2018 we are returning to the beautiful and intimate Newmark Theater, bringing back Expeditions, and as always, we'll have a few suprises for our time together.
+For 2018 we are returning to the beautiful and intimate Newmark Theater, bringing back the WDS Connect experience, and as always, we'll have a few suprises for our time together.
 
 All that’s missing is YOU!
   
-<p>All tickets cost <b>$697</b> and are sold on a first-come, first-served basis. <i>(For all ticket policies, please refer to our <a href="https://worlddominationsummit.com/faq" target="_blank">FAQ page</a>)</i></p>
+<p>All-access WDS 360 tickets cost <b>$697</b> and are sold on a first-come, first-served basis. <i></p>
+<p>(For all ticket policies, please refer to our <a href="https://worlddominationsummit.com/faq" target="_blank">FAQ page</a>)</i></p>
 
-## Tickets are on sale now!
+## WDS 360 Tickets are on sale now!
 
-<Button to="be-there" style={{ fontSize: '22px', padding: '18px 100px', marginTop: '10px', marginBottom: '30px' }} fitToText>Get Your Ticket!</Button>
+<Button to="be-there" style={{ fontSize: '22px', padding: '18px 100px', marginTop: '10px', marginBottom: '30px' }} fitToText>Get Your 360 Ticket!</Button>
 
   </Block>
 </Section>
 <Section color="canvas" bound={false}>
 <Block margin="50px 0 50px 0">
-  ## What's Included in My Ticket
+  ## What's Included in My 360 Ticket
   <Grid numCols="2" tabletCols="2" block={{ className: 'dropped-button' }} equalize>
       <Block type="image" src="photos/JoinUs/image-speakers.png">
       ## Main Stage Speakers
      Our keynote speakers are bestselling authors, leading activists, and in-demand storytellers. Since our very first year in 2011, the lineup has been inclusive, multicultural, and representative of our whole community.
     </Block>
+    <Block type="image" src="photos/JoinUs/image-activities.jpg">
+      ## Connect
+      From fun social gatherings to focused discussions, choose from a variety of special WDS hosted adventures and activities throughout the week with the purpose of meeting and connecting with other attendees.
+    </Block>
     <Block type="image" src="photos/JoinUs/image-meetups.jpg">
       ## Meetups
       Every year attendees help lead over 100+ meetups in and around Portland. Share your expertise. Coordinate an adventure. Meet others that share your passions and interests. The best part is you can do all three!
-    </Block>
-    <Block type="image" src="photos/JoinUs/image-activities.jpg">
-      ## Activities
-      From fun social gatherings to focused discussions, starting off the day with yoga to ending the night in Karaoke, choose from a variety of special WDS hosted adventures and activities throughout the week.
     </Block>
     <Block type="image" src="photos/JoinUs/image-people.jpg">
       ## Incredible People
