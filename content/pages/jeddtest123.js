9575336fe24d85e93cf12cc69ca53274b72a27c5
Jedd <jedidiah475@gmail.com>
2018-03-27 20:00:28 -0700

    Update jeddtest123.js

--- content/pages/jeddtest123.js
+++ content/pages/jeddtest123.js
@@ -8,7 +8,7 @@
 >
   <Image src="heading/connect.png" width="446px" height="156px" className="scriptHeader" preload/>
   <Block cols="8" bleed={false} margin="0 0 80px">
-    <Image src="images/photos/Connect/WDS-Connect-Header.jpg" width="100%" height="302" /></Block>
+    <Image src="photos/Connect/WDS-Connect-Header.jpg" width="100%" height="302" /></Block>
     <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
      <p style={{textAlign: 'center'}}><h2 style={{ marginBottom: '4px' }}>WDS Connect: Join a Remarkable Community</h2></p>
    <p style={{textAlign: 'center'}}><h4 style={{ marginBottom: '4px' }}>June 26 - July 2, 2018 in Portland, Oregon</h4></p>
@@ -18,7 +18,9 @@
 
       Are you one of them?
 
-      Join us for WDS CONNECT, a collection of events during WDS week that allows you to sample the spirit of WDS and meet other amazing adventurers. Where WDS 360 is the whole pizza, Connect is a giant slice of the experience.  
+      Join us for WDS CONNECT, a collection of events during WDS week that allows you to sample the spirit of WDS and meet other amazing adventurers. Whether you've been to WDS before or heard about the event but not quite ready for the whole shebang, WDS 360 is the whole pizza whereas Connect is a giant slice of the experience.  
+      
+      <p style={{textAlign: 'center'}}>Connect tickets cost <b>$179</b> and are non-refundable and non-transferrable.</p>
       
        <Button to="be-there" style={{ fontSize: '22px', padding: '18px 100px', marginTop: '10px', marginBottom: '30px' }} fitToText>Get Your Connect Ticket</Button>
     </Block>
@@ -27,11 +29,11 @@
       ## Here’s some things we are cooking up for CONNECT                                                                                   
     </Block>
     <Grid numCols="3" tabletCols="2" width="968">
-      <Block type="image-overlay" anchor="right" icon="outline-beers" src="images/photos/Connect/Assembly.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+      <Block type="image-overlay" anchor="right" icon="outline-beers" src="photos/Connect/Assembly.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Assemblies
         Kick off your morning with a little inspiration to take you through the day. Also find out what’s happening for the day, ask questions of WDS core team members, and meet new friends.
       </Block>
-      <Block type="image-overlay" anchor="left" icon="outline-bike" src="images/photos/Connect/SparkSessions.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
+      <Block type="image-overlay" anchor="left" icon="outline-bike" src="photos/Connect/SparkSessions.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Spark Sessions
         Explore a market of personal skills.  Learn how to captivate strangers. Next level your networking.
       </Block>
@@ -72,28 +74,16 @@
   >
         ## Connect™ FAQs
         <Block className="bigger" width="100%" margin="30px 0 0 0" css={{ textAlign: 'left' }}>You might have some questions. We've got some answers.</Block>
-        ### Can I book a WDS Hotel™ package through the Society Hotel? 
-        Because of our arrangement with the Society Hotel, WDS is handling reservations for all rooms during the WDS weekend (arriving Thursday, June 28th, checking out Monday, July 2nd). All rooms will be sold as 4-night packages.   
-        ### What is your refund/transfer policy for WDS Hotel™ packages?
-        <p>All WDS Hotel packages are <b>non-refundable</b>. You can, however, transfer your package to another attendee for a $50 fee until June 27th by contacting the WDS concierge team (not the Society Hotel) at <b>concierge@wds.fm</b>.</p>  
-        ### How do I book additional nights before or after the WDS package dates?
-        <p>Once you've reserved your WDS Hotel™ package, please contact the Society Hotel by phone (avoid their online reservation system) to set up any additional dates by calling their front desk at <b>503-445-0444</b>. You may be able to stay in the same room type as your WDS Hotel™ package, but that will depend on the hotel's room availability.</p>
-        ### When will my credit card be charged?
-        Your card will be immediately charged for the full amount when you book a WDS Hotel™ package. You'll also need to provide a credit card upon check-in for an incidental hold. This will also allow you to charge any hotel services, like buying a coffee or cocktail, directly to your room.
-        ### Can I book a room with another WDS attendee?
-        <p>Yes! Similar to any hotel reservation, one person is responsible for the reservation itself and only that person will be able to make any changes. <i>Bunks require individual reservations.</i></p>
-        ### Can I book a partial stay or split days with a friend?
-        Once you've booked a package, you get the room or bunk for the entire 4 nights regardless of how many days you plan to use. Like any hotel reservation, you are ultimately responsible for any other guests that you let stay in your room. For example, if your friend charges 20 craft microbrews and one bag of Skittles to the room, you'd be responsible for paying the bill.
-        ### Can I book a private room with another person who isn’t attending WDS?
-        <p>We'd love for everyone staying at the WDS Hotel™ to be part of WDS. However, we understand if you’ve booked a private room and you have a significant other or dependent staying with you who is not attending WDS. <i>For the bunk section of the hotel, all guests need to be registered WDS attendees.</i></p>
-        ### What kinds of facilities are in the hotel?
-        <p>There is an excellent cafe that serves light fare for breakfast and lunch, which you'll be able to charge to your room/bunk. Additionally, as a registered guest you'll have access to the amazing rooftop lounge. Please note there is not a fitness center onsite.</p>    
-        ### How do I get to the hotel from the airport? 
-        <p>For just $2.50, you can take the MAX red line train from the airport towards "City Center/Beaverton" and get off at the "Old Town/Chinatown" stop. Walk two blocks west on Davis St. and you'll see the hotel at the corner of Davis and 3rd Ave. <Link to="https://goo.gl/maps/93LythVvQEG2">(Google directions).</Link> Taxis and ride share options are also available.</p>
-        ### How far is the hotel from the WDS Main Stage?
-        <p>The hotel is less than a mile (0.8) walk through downtown <Link to="https://goo.gl/maps/v6kK2y4xsN82">(Google directions)</Link>. You can also make the trip by bike, light rail (MAX) system, or ride share.</p>
-        ### It looks like you're sold out but I'd really like to stay at the WDS Hotel™. What are my options?
-        <p>Reach out to our concierge team at <b>concierge@wds.fm</b> to see if there are any cancellations and openings. <i>Do not contact the Society Hotel regarding WDS packages.</i></p>
+        ### What is the difference between 360 and Connect tickets? 
+        WDS 360 is an all-access pass that includes the Connect experience and main-events (the big adventure, 360 opening party, and all main stage sessions). Connect is a huge part of the WDS but is not the full experience.   
+        ### When does WDS Connect take place?
+        WDS Connect takes place starting from Tuesday, June 26 until Monday, July 2. Using the mobile-app you'll be able to create your own personalized schedule of events and activities that you want to participate in (except for 360 designated events).
+        ### What is your refund/transfer policy for WDS Connect Tickets?
+        <p>All WDS tickets are <b>non-refundable</b> and Connect tickets are <b>non-transferrable.</b></p>  
+        ### Can I buy more than one Connect ticket?
+        Yes! You can purchase up to 3 Connect tickets per transaction but you will need to tell us who will be attending at the time of purchase. 
+        ### Can I Updgrade my Connect ticket to a 360 ticket?
+        <p>Yes! As long as we still have 360 tickets available you can upgrade your ticket by paying the difference. Contact our concierge team at <b>concierge@wds.fm</b> and they’ll walk you through the process.</p>
         ### Not finding the answer to your question?
         <p>Contact our year-round concierge team at <b>concierge@wds.fm</b>. They’ll get back to you at lightning speed!</p>
  </Block>
