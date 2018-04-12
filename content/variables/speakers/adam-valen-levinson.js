6b6f6aaa5b994720af4ebccc7a777cefea1c7ffb
Jedd <jedidiah475@gmail.com>
2018-04-12 14:39:27 -0700

    Update adam-valen-levinson.js

--- content/variables/speakers/adam-valen-levinson.js
+++ content/variables/speakers/adam-valen-levinson.js
@@ -5,8 +5,8 @@ photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Adam.Valenn.Levins
 video: "",
 quote: "",
 ---
-*Adam Valen Levinson* is an affiliate of the Middle East Institute, and a Fellow at the Center for Cultural Sociology at Yale University, where he studies senses of humor as a key to cross cultural understanding.  
+**Adam Valen Levinson** is an affiliate of the Middle East Institute, and a Fellow at the Center for Cultural Sociology at Yale University, where he studies senses of humor as a key to cross cultural understanding.  
 
 He has written, filmed, and photographed for _Al Jazeera, The Paris Review, Haaretz, Neue Zürcher Zeitung,_ and _VICE_, and did college stints at the Meccas of real fake news: _The Colbert Report_ and _The Onion_.  All of his stories are true.
 
-He usually lives in America.
+He usually lives in America. Learn more about Adam and his stories at [adamvalenlevinson.com](https://www.adamvalenlevinson.com/). 
