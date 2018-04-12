046f6a87dec5afcd420b49432aece382dadd27cf
Jedd <jedidiah475@gmail.com>
2018-04-12 14:45:21 -0700

    Update sch3dul3-update.js

--- content/pages/sch3dul3-update.js
+++ content/pages/sch3dul3-update.js
@@ -94,7 +94,7 @@ He usually lives in America. Learn more about Adam and his stories at [adamvalen
                                                                                                                                               
         **Ryan Holiday** is a writer and media strategist. When he was 19 years old, he dropped out of college to apprentice under Robert Greene, author of The 48 Laws of Power and went on to become the director of marketing for American Apparel. He has written for the New York Observer, Thought Catalog, Entrepreneur, 99U, Fast Company, The Huffington Post, Medium, Boing Boing, Forbes, Columbia Journalism Review and multiple other outlets.
 
-Ryan is the author of six books, including The Obstacle Is the Way, Ego Is the Enemy, The Daily Stoic and Perennial Seller. Now days Ryan can be found on a ranch outside Austin, Texas where he continues to write and work in between raising cattle, donkeys and goats. You can learn more about Ryan from his blog, [RyanHoliday.net}(https://ryanholiday.net/).
+Ryan is the author of six books, including The Obstacle Is the Way, Ego Is the Enemy, The Daily Stoic and Perennial Seller. Now days Ryan can be found on a ranch outside Austin, Texas where he continues to write and work in between raising cattle, donkeys and goats. You can learn more about Ryan from his blog, [RyanHoliday.net](https://ryanholiday.net/).
       </Block>
     </Tab>                                                                
   </Tabs>
