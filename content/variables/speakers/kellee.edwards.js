9e31ea28c243a6415797312831182be96d4e54ce
Jedd <jedidiah475@gmail.com>
2018-04-14 14:03:20 -0700

    Create kellee.edwards.js

---
+++ content/variables/speakers/kellee.edwards.js
@@ -0,0 +1,10 @@
+name: "Kellee Edwards",
+year: "2018",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Kellee.Edwards.WDS.2018.Speaker.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Kellee.Edwards.WDS.speaker.thumbnail.png",
+video: "",
+quote: "",
+---
+**Kellee Edwards** had big dreams but never imagined that she would become a Pilot, Scuba Diver, world traveler and the ultimate adrenaline junkie. She began her travels as a child, taking road trips with her parents. This definitely ignited her passion and curiosity for the next step, traveling internationally. 
+
+Exploring over 40 countries thus far, including parts of North and West Africa, Asia, South and Central America, Europe and the UAE, her love for travel has allowed Kellee to also take her enthusiasm for outdoor adventure globally. She is the Host of Mysterious Islands and the co-host of The Trip 2018 on Travel Channel. In addition to Travel Channel she is also the travel expert for FOX 5 San Diego's "Live for Travel" segment, writer and editor for _Pilot Getaways_ magazine and advocate for Women in Adventure Travel. 
