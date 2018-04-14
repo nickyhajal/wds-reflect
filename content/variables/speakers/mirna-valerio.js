1134466e34abcd469c5c7630eeceb7f5a5bf9222
Jedd <jedidiah475@gmail.com>
2018-04-14 13:59:15 -0700

    Create mirna-valerio.js

---
+++ content/variables/speakers/mirna-valerio.js
@@ -0,0 +1,10 @@
+name: "Mirna Valerio",
+year: "2018",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Mirna.Valerio.WDS.2018.Speaker.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Mirna.WDS.speaker.thumbnail.png",
+video: "",
+quote: "",
+---
+**Mirna Valerio** is a runner. In 2017 a WSJ reporter contacted Mirna inquiring about her blog, [FATGIRLRUNNING](http://fatgirlrunning-fatrunner.blogspot.com), subsequently writing a piece in the newspaper about being fat and fit. Mirna has been featured in _Runner’s World_ and NBC along with other publications such as _Women's Running Magazine_, _People_, _Buzzfeed_, _Upworthy_, and the _Huffington Post_ and continues to feel grateful to share her story and inspire others to be active with whatever body they have. 
+
+In addition to being an avid trail runner and writer, Mirna is also a Spanish teacher, choral director, diversity practitioner, cross country running coach, who believes that many of life's lessons can be learned by simply engaging oneself in the pursuit of wisdom gained through simply moving your body in nature. 
