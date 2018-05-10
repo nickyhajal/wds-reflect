f8ec030819c7559f3f7027fab5ee49e815366543
Jedd <jedidiah475@gmail.com>
2018-05-09 17:06:50 -0700

    Update buy-box.js

--- content/global/buy-box.js
+++ content/global/buy-box.js
@@ -5,7 +5,7 @@
   </Block>
   <Button styling="white" align="center" to={(
     window.location.pathname.indexOf('join-us') > -1 ? '/be-there' : '/join-us'
-  )} style={{ width: '260px', margin: '60px auto 110px', padding: '20px 0', fontSize: '21px', fontFamily: 'VitesseBold' }}>Get Your Ticket!</Button>
+  )} style={{ width: '260px', margin: '60px auto 110px', padding: '20px 0', fontSize: '21px', fontFamily: 'VitesseBold' }}>Get Your 360 Ticket!</Button>
   <Block width="400px" align="center" margin="50px 0 90px" css={{ maxWidth: "400px" }}>
     <TicketCounter sale="wds2018" type="countup" />
   </Block>
