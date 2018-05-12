ab3a5ecd56f49c85f8b563ee6a4e9e6aa57cb121
Jedd <jedidiah475@gmail.com>
2018-05-11 18:37:50 -0700

    Update transfer.js

--- content/pages/transfer.js
+++ content/pages/transfer.js
@@ -6,7 +6,7 @@
       <Transfer title="So sorry you can't make it for the adventure!">
         <p>Can't make it to the World Domination Summit after all? We're sorry we won't see you there! (there's always next year).</p>
 
-        <p>Transfers and name changes are available for a $100 fee from now until April 15th (10 weeks prior to WDS) and a $200 fee from April 16th until May 15th (6 weeks prior to WDS).</p> 
+        <p>Transfers and name changes are available for a $100 fee from now until May 15th (6 weeks prior to WDS).</p> 
         
         <p><i>Starting May 16th, 2018, no further transfers or name changes will be possible.</i></p>
 
