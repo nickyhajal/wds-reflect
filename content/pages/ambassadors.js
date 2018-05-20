d9ea3b2b10930747497f6b15b9d8516930525ec3
Jedd <jedidiah475@gmail.com>
2018-05-19 21:53:27 -0700

    Update ambassadors.js

--- content/pages/ambassadors.js
+++ content/pages/ambassadors.js
@@ -17,22 +17,9 @@
       <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
  ## How to Apply
 
-### Applications to be a 2018 Ambassador are now open! (until April 15) 
+### Oh darn. Applications to be a 2018 Ambassador are now closed! 
   
- We are searching for 75 amazing people ready to support our attendees throughout WDS. To be an Ambassador, we ask that you:
-+ Commit to helping our attendees have the best experience possible! 
-+ Live in or near Portland, Oregon (or are very familiar with the city)
-+ Are available some of the time during WDS week from Tuesday, June 26th through Monday, July 2nd
-+ Are **_fully available_** Friday, June 29th - Sunday, July 1st (no conflicts from morning to night all three days)
-+ Can attend Ambassador orientation on the evening of Monday, June 25th. 
-+ Are ready to work hard and play hard to help make WDS 2018 our best event yet!
-
+  Thank you for your interest in wanting to help make WDS 2018 awesome and we're sorry we missed you. There's always next year. If you have any questions email our Viceroy of Volunteers, Tina, at concierge@wds.fm.
   
-  <p><i>Special consideration will be given to those that are available for the entire WDS week from Monday, June 25th until Monday, July 2nd.</i></p>
-  
-  Please fill out the application by clicking on the link below and we’ll get back to you within about 3 weeks with next steps. Questions? Email Tina at concierge@wds.fm for help!
-  
- <iframe height="3800" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/embed/k1qb9rtw1jhaxfo/"><a href="https://worlddominationsummit.wufoo.com/forms/k1qb9rtw1jhaxfo/">Fill out my Wufoo form!</a></iframe>
-
    </Block>
 </Section>
