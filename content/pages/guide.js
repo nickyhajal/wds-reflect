96e8fe359e5275364befa456cfbe4a10751c1d3c
Jedd <jedidiah475@gmail.com>
2018-05-21 23:47:05 -0700

    Update guide.js

--- content/pages/guide.js
+++ content/pages/guide.js
@@ -66,7 +66,7 @@
        <Block type="image-overlay" anchor="right" src="photos/wds.mobile.app.png" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 5: Mobile App 
         Download our very own mobile app to RSVP for activities, check your personalized schedule, and so much more (coming soon).
-       <Button to="https://madmimi.com/p/d8ad3c/preview" newWindow={true} styling="blue" fitToText>Download Now</Button>
+       <Button to="https://madmimi.com/p/d8ad3c/" newWindow={true} styling="blue" fitToText>Download Now</Button>
         </Block>      
         <Block type="image-overlay" anchor="left" src="photos/wds.meetups.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">   
         ## Step #6: RSVP for Activities
