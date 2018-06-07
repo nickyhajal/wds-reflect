26642f70768b8480f1ef6a2f86b0c2be3e3294bc
Jedd <jedidiah475@gmail.com>
2018-06-07 13:41:12 -0700

    Create submit-your-story.js

---
+++ content/pages/submit-your-story.js
@@ -0,0 +1,36 @@
+<Section
+  headerClip="br:0%,-10%;"
+  headerImage="photos/propose-a-meetup.jpg"
+  color="canvas"
+>
+  <Block
+    cols="8"
+    bleed={false}
+    textAlign="left"
+    margin="80px 0 80px"
+    background="white"
+    type="padded"
+    padding="80px 120px"
+    className="bigger"
+  >
+    <h2 style={{ fontSize: '32px', textAlign: 'center' }}>
+      Submit Your Story: the best part of WDS is you!
+    </h2>
+    Over the past 8 years 10,000+ amazing adventurers have taken part in WDS. As we reflect over the many memories and connections we've made together, we want to know <b>how has WDS impacted your life?</b>
+    {' '}
+
+    ## Here's what we're looking for:
+<ul>
+  <li>How has WDS inspired you and what has happened since attending?</li>
+  <li>What has WDS confirmed or challenged you to think about in your life?</li>
+  <li>What are the most valuable things you learned from WDS?</li>
+  <li>Do you have any awesome WDS stories the world should know about?</li>
+  <li>What connections have you made within the WDS community that have helped you in your work or personal life?</li>
+  <li>Was there a speaker, fellow attendee, Academy, or meet up that gave you an "a-ha" moment or changed your perspective about something?</li>
+</ul>
+
+Do you have (or know of another attendee that has) an awesome story that you think the WDS Community should hear about? Tell us using the form below:
+ <iframe height="3800" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/embed/k1qb9rtw1jhaxfo/"><a href="https://worlddominationsummit.wufoo.com/forms/k1qb9rtw1jhaxfo/">Fill out my Wufoo form!</a></iframe>
+
+  </Block>
+</Section>;
