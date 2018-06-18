e9ac5cc9056fe6823dbd5064067c23925deba6ab
Jedd <jedidiah475@gmail.com>
2018-06-18 10:58:14 -0700

    Create cassie-de-pecol.js

---
+++ content/variables/speakers/cassie-de-pecol.js
@@ -0,0 +1,8 @@
+name: "Cassie De Pecol",
+year: "2018",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/cassie.de.pecol.WDS.2018.Speaker.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/cassie.thumbnail.png",
+video: "",
+quote: "",
+---
+**Cassie De Pecol** is a traveler, activist, entreprenuer, philanthropist, and speaker best known for being the fastest person and first documented woman to travel to every sovereign nation in the world. She officially set Guinness World Records in two categories: "Fastest time to visit all sovereign countries" and "Fastest time to visit all sovereign countries - Female". When Cassie isn't traveling she enjoys competing in the Ironman and Ironman 70.3 triathlon races and is currently working on her next Guinness World Record attempt.
