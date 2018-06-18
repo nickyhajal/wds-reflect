72b90caaab4025cb66929c92be1bb5343e75a1ae
Jedd <jedidiah475@gmail.com>
2018-06-18 10:36:02 -0700

    Create lindsey.murphy.js

---
+++ content/variables/speakers/lindsey.murphy.js
@@ -0,0 +1,8 @@
+name: "Lindsey Murphy",
+year: "2018",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/lindsey.murphey.WDS.2018.Speaker.png",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/lindsey.thumbnail.png",
+video: "",
+quote: "",
+---
+**Lindsey Murphy** stars in and produces _Crazy Aunt Lindsey and The Fab Lab_, an amazing and fresh online web science series for children. Crazy Aunt Lindsey is bringing a fresh, exciting, and needed face to the world of STEM education. She looks at the world in a different way and always has something interesting in her cupboard, on her mind, up her sleeve — Imagination! Creation! Exploration!
