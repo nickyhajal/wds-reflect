6597a64e1905aeda1bf75cb570204c1d44e673d1
Jedd <jedidiah475@gmail.com>
2018-06-18 10:31:23 -0700

    Create dj.prashant.js

---
+++ content/variables/speakers/dj.prashant.js
@@ -0,0 +1,8 @@
+name: "DJ Prashant",
+year: "2018",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/DJ.Prashant.WDS.2018.Speaker.jpg",
+photo: "dj.prashant.thumbnail.png",
+video: "",
+quote: "",
+---
+A one-of-a-kind Bollywood entertainer, ** DJ Prashant** is a singer, choreographer, MC & DJ with one simple agenda—to make people dance non-stop to an irresistible blend of world’s greatest dance beats. Prashant’s effervescent personality & charisma instantly strikes a chord with audiences of all ages & backgrounds. Through his energetic performances, Prashant has moved masses across both coasts of America, Costa Rica, Panama & India.
