ed0ad0fd9fb41af2b3d71ac2f5480617ffbcd549
Nicky Hajal <nhajal@gmail.com>
2018-06-21 19:04:07 -0500

    number of notes in register

--- content/global/join-us.js
+++ content/global/join-us.js
@@ -4,8 +4,8 @@
     We release WDS tickets for a few weeks each year and now is your chance! Are you ready to be part of an experience that will change your life forever?
   </Block>
   <Button styling="white" align="center" to={(
-    window.location.pathname.indexOf('join-us') > -1 ? '/be-there' : '/join-us'
-  )} style={{ width: '260px', margin: '60px auto 110px', padding: '20px 0', fontSize: '21px', fontFamily: 'VitesseBold' }}>Get Your 360 Ticket!</Button>
+    window.location.pathname.indexOf('join-us') > -1 ? '/connect' : '/connect'
+  )} style={{ width: '260px', margin: '60px auto 110px', padding: '20px 0', fontSize: '21px', fontFamily: 'VitesseBold' }}>Get Your Connect Ticket!</Button>
   <Block width="400px" align="center" margin="50px 0 90px" css={{ maxWidth: "400px" }}>
     <TicketCounter type="countup" />
   </Block>
