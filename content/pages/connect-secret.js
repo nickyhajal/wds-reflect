c7a7be85a4a6d30e5a189780cc5a80f44a2376a2
Nicky Hajal <nhajal@gmail.com>
2018-06-29 00:52:26 +0200

    Create connect-secret.js

---
+++ content/pages/connect-secret.js
@@ -0,0 +1,127 @@
+<Section
+  headerImage="hero/squiggle.png"
+  headerClip="rl_header"
+  headerPosition="top"
+  headerSize="thin"
+  color="white"
+  preload
+>
+  <Image src="heading/wds-connect.png" width="615px" height="202px" className="scriptHeader" preload/>
+  <Block cols="8" bleed={false} margin="0 0 80px">
+    <Image src="photos/Connect/WDS-Connect-Header-2.jpg" width="100%" height="302" /></Block>
+    <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+     <p style={{textAlign: 'center'}}><h2 style={{ marginBottom: '4px' }}>WDS Connect: Join a Remarkable Community</h2></p>
+   <p style={{textAlign: 'center'}}><h4 style={{ marginBottom: '4px' }}>June 26 - July 2, 2018 in Portland, Oregon</h4></p>
+      
+      Back in 2011, we invited 440 remarkable adventurers to Portland, Oregon for a two-day event we called the World Domination Summit. Our common curiosity brought us together to explore one central question: *How do we live a remarkable life in a conventional world?* 
+        
+        Seven years later, WDS is an annual gathering that brings 1000+ attendees from 30+ countries to learn, get inspired, think outside of the box, break out of what’s holding them back, and most importantly- connect. Armed with an incredible community of over 10,000 alumni, our quest continues to find more remarkable people. 
+      
+      Are you one of them?
+        
+       Join us for **WDS Connect** -- a collection of activities during WDS week that allows you to sample the spirit of WDS and meet other amazing adventurers. While WDS 360 is the all-access pass, Connect is a social, interactive part of the WDS experience.
+   
+      <p style={{textAlign: 'center'}}>Connect tickets cost <b>$179</b> and are <i>non-refundable and non-transferrable.</i></p>
+<p style={{textAlign: 'center'}}><i>(For all ticket policies, please refer to our <a href="https://worlddominationsummit.com/faq" target="_blank">FAQ page</a>)</i></p>
+      
+  <ConnectPurchaseButton style={{ fontSize: '22px', padding: '18px 100px', marginTop: '10px', marginBottom: '30px', marginLeft: 'auto', marginRight: 'auto' }}>Get Your Connect Ticket</ConnectPurchaseButton>
+    </Block>
+  <Block margin="125px 0 50px 0">
+    <Block cols="6">
+      ## Here are some things we're cooking up for Connect.                                                                                
+    </Block>
+    <Grid numCols="3" tabletCols="2" width="968">
+      <Block type="image-overlay" anchor="right" icon="outline-eye" src="photos/Connect/Assembly.jpg" imageClip="tr:0,+10%;">
+        ## Assemblies
+      The best part of starting each day of WDS is together in community.  Get some inspiration with your coffee, learn what’s going on for the day, chat with the core team members and meet other attendees. 
+      </Block>
+      <Block type="image-overlay" anchor="left" icon="outline-bike" src="photos/Connect/spark-sessions.jpg" imageClip="tr:0,+10%;" >
+        ## Spark Sessions
+        Find and engage with fellow WDSers in some wild and extraordinary ways. For example:
+- Speed Networking with Meaning, hosted by the Science of People team
+- The Ask/Offer Market—what happens when you ask a community to tell you what passions, skills, resources, and connections they have and what they need? Matchmaking interests!
+- The "Yes Mingler"—improv-based networking
+
+
+      </Block>
+      <Block type="image-overlay" anchor="right" icon="outline-beers" src="photos/Connect/Gathers.jpg" imageClip="tr:0,+10%;">
+        ## Gatherings
+        Unstructured but themed time to meet new people while exploring and sharing tips and ideas of remarkable living. 
+- Vision boarding session
+- Hot Travel Tips
+- And more
+
+      </Block>
+      <Block type="image-overlay" anchor="left" icon="outline-eye" src="photos/Connect/ServiceProject.jpg" imageClip="tr:0,+10%;" >
+        ## Service Project
+        Making a positive impact on our world is a core value of WDS. Working in conjunction with local agencies, we'll be creating kits filled with homewares and other essential supplies as a small gesture to welcome refugees who are relocating to the greater Portland area. 
+      </Block>
+      <Block type="image-overlay" anchor="right" icon="outline-camera" src="photos/powells-books.jpg" imageClip="tr:0,+10%;" >
+        ## Tours
+        Bikes, coffee, beer, hikes, makers—PDX has it all and we can’t wait to have some of our WDS Ambassadors show you what they love about WDS and their city. Among other highlights, this year's tours include:
+- City Art Tour
+- Goat Yoga
+- Food Cart Exploration
+- And more! 
+
+      </Block>
+       <Block type="image-overlay" anchor="left" icon="outline-bike" src="photos/Connect/Meetups.jpg" imageClip="tr:0,+10%;" >
+        ## Attendee-led Meetups
+        Each year WDS attendees lead over 100 different kinds of meet ups throughout the city of Portland. Attend or organize your own meetups using the WDS mobile app: 
+- Share your wisdom and expertise. 
+- Create an opportunity for people with similar passions to meet. 
+- Organize something fun to create memories.
+      </Block>
+   <Block type="image-overlay" anchor="right" icon="outline-camera" src="photos/Connect/WDS-Closing-Party.jpg" imageClip="tr:0,+10%;" >
+        ## Epic Closing Party
+        Join your fellow WDS attendees for an unforgettable celebration of community. Prepare yourself to be transported to a whimsical world and dance the night away with new and old friends. A major highlight of each WDS that you won't want to miss.
+- Pure, unadulterated fun and joy. 
+- Magic. 
+- Food, drink, and dancing.
+      </Block>
+    </Grid>
+<ConnectPurchaseButton style={{ fontSize: '22px', padding: '18px 100px', marginTop: '10px', marginBottom: '30px', marginLeft: 'auto', marginRight: 'auto' }}>Get Your Connect Ticket</ConnectPurchaseButton>
+       </Block>
+  </Section>
+<Section color="canvas" css={{ padding: '90px 0 45px 0' }} mobile={{ phone: { padding: '20px 0 40px 0' } }}>
+    <Block bleed={false} textAlign="center" margin="0px 0 80px">  
+## Choose Your Adventure: 360 or Connect
+    **WDS 360** is the all-access pass. **Connect** is a social, interactive part of the WDS experience. 
+</Block>
+<ComparisonTable />
+      <Block>
+  <ConnectPurchaseButton style={{ fontSize: '22px', padding: '18px 100px', marginTop: '10px', marginBottom: '30px', marginLeft: 'auto', marginRight: 'auto' }}>Get Your Connect Ticket</ConnectPurchaseButton>
+</Block>
+</Section>
+<Section color="sea" css={{ padding: '90px 0 45px 0' }} mobile={{ phone: { padding: '20px 0 40px 0' } }}>
+  <div className="clear"></div>
+    <Block
+    type="padded"
+    cols="6"
+    textAlign="left"
+    bleed={false}
+    background="canvas"
+    className="has-checklist"
+    css={{ padding: '60px 40px' }}
+    mobile={{ phone: {
+      padding: '60px 25px 60px 10px'
+    }}}
+    clip="top_lr_1.6"
+  >
+        ## WDS Connect™ FAQs
+        <Block className="bigger" width="100%" margin="30px 0 0 0" css={{ textAlign: 'left' }}>You might have some questions. We've got some answers.</Block>
+        ### What's the difference between 360 and Connect tickets? 
+        WDS 360 is an all-access pass that includes the Connect experience and all main-stage events (a special adventure on Friday morning, 360 opening kickoff, and all theatre sessions on Saturday and Sunday). Connect is an important part of WDS, but is not the full experience.   
+        ### When does WDS Connect take place?
+        WDS Connect takes place starting from *Tuesday, June 26 until Monday, July 2*. Using the mobile app, you'll be able to create your own personalized schedule of events and activities that you want to participate in (except for 360 designated events).
+       
+        ### Can I buy more than one Connect ticket?
+        Yes! You can purchase up to three tickets per transaction. Please note you will need to assign each ticket by submitting a unique name and email for each ticket at the time of purchase.  
+        ### Can I Upgrade my Connect ticket to a 360 ticket?
+        <p>Yes! As long as we still have 360 tickets available, you can upgrade your ticket by paying the difference. Contact our concierge team at <b>concierge@wds.fm</b> and they’ll walk you through the process.</p>
+        ### Not finding the answer to your question?
+        Contact our concierge team at **concierge@wds.fm.** They’ll get back to you at lightning speed!
+ </Block>
+   <div className="clear" />
+</Section>
+<Import id="join-us" />
