aae435b09c4bebf26042b1df72c0984abc8439a3
wkorenek <wekorenek@gmail.com>
2018-07-01 10:10:18 -0700

    Create 2019.js

---
+++ content/pages/2019.js
@@ -0,0 +1,76 @@
+<Section
+headerImage="hero/joinus.jpg"
+headerClip="rl_header"
+headerPosition="top"
+headerSize="thin"
+color="white"
+preload
+>
+<Image src="heading/script-joinUs.png" width="446px" height="196px" className="scriptHeader" align="center" css={{ float: 'none' }} preload/>
+<Block width="600px" css={{ color: 'orange', marginTop: '80px' }}>
+  <h2 style={{ marginBottom: '4px' }}>World Domination Summit 2018</h2><p></p>
+    ## June 27 - July 1, 2019 in Portland, Oregon
+
+</Block>
+<Block cols="6" className="bigger" mobile={{ all:{ padding: '0 50px', marginBottom: '20px' }, phone: { padding: '0 20px' } }}>
+
+<p>Join us again next year for an all-new experience at the <i>penultimate</i> World Domination Summit! We’ll continue to explore how to live a remarkable life and build a better world through community, adventure, and service.</p>
+
+For 2019, we are returning to the beautiful and intimate Newmark Theater, bringing back the WDS Connect experience, and as always, we'll have a few suprises for our time together. All that’s missing is YOU!
+  
+<p>All-access WDS 360 tickets for Alumni cost <b>$597</b> and are sold on a first-come, first-served basis. Get you ticket soon because this early bird special expires at midnight on July 2nd!</p>
+<p><i>(For all ticket policies, please refer to our <a href="https://worlddominationsummit.com/faq" target="_blank">FAQ page</a>)</i></p>
+       
+A 360 ticket holder gets access to it all -- the WDS main-events and the Connect experience. 
+       
+<h2 style={{marginTop: '30px' }}>Sorry we missed you! WDS 360 and Connect tickets are now gone.</h2>
+       See you in 2019? Here's a look at the fun we've got planned for this year. 
+         </Block>
+</Section>
+<Section color="canvas" bound={false}>
+   <Block bleed={false} textAlign="center" margin="0px 0 80px">  
+## Choose Your Adventure: 360 or Connect
+     WDS 360 is the all-access pass. Connect is a social, interactive part of the WDS experience. 
+</Block>
+<ComparisonTable/>
+       </Section>
+       <Section color="sea" bound={false}>
+<Block margin="50px 0 50px 0">
+  ## What's Included in My WDS 360 Ticket
+  <Grid numCols="2" tabletCols="2" block={{ className: 'dropped-button' }} equalize>
+      <Block type="image" src="photos/JoinUs/image-speakers.png">
+      ## Main Stage Speakers
+     Our keynote speakers are bestselling authors, leading activists, and in-demand storytellers. Since our very first year in 2011, the lineup has been inclusive, multicultural, and representative of our whole community.
+    </Block>
+    <Block type="image" src="photos/JoinUs/image-activities.jpg">
+      ## Connect
+      From fun social gatherings to focused discussions, choose from a variety of special WDS hosted adventures and activities throughout the week with the purpose of meeting and connecting with other attendees.
+    </Block>
+    <Block type="image" src="photos/JoinUs/image-meetups.jpg">
+      ## Meetups
+      Every year attendees help lead over 100+ meetups in and around Portland. Share your expertise. Coordinate an adventure. Meet others that share your passions and interests. The best part is you can do all three!
+    </Block>
+    <Block type="image" src="photos/JoinUs/image-people.jpg">
+      ## Incredible People
+      Our incredible adventurers come from over 40+ different countries and all 50 states. At WDS there will be an abundance of opportunities to make new, life-long connections.
+    </Block>
+    <Block type="image" src="photos/JoinUs/image-academies.jpg">
+      ## Academies
+      These half-day workshops feature some of our favorite alumni speakers and select WDS community members that are experts in their fields. As a WDS ticket holder you get first access to the Academy of your choice at a huge discount.
+    </Block>
+     <Block type="image" src="photos/society-hotel.jpg">
+      ## Hotel Discounts
+      Each year we partner with iconic Portland properties to offer our attendees special discounted prices when booking your stay in Portland.  
+    </Block>
+    <Block type="image" src="photos/JoinUs/image-parties.jpg">
+      ## Celebrations
+      There's nothing better than celebrating community. Kick-off WDS and meet new friends at the welcome party and dance the night away with them at the closing celebration.
+    </Block>
+    <Block type="image" src="photos/JoinUs/image-communities.jpg">
+      ## Community
+      Once you attend WDS you'll become an alum and part of a global network of over 10,000 adventurers who will continue to support you and your journey. 
+    </Block>
+  </Grid>
+</Block>
+</Section>
+<Import id="join-us" />
