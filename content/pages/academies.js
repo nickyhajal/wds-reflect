1d050ac43a0ab8dd59bb2bc70449741bd893c33b
Jedd <jedidiah475@gmail.com>
2018-07-06 19:16:54 -0700

    Update academies.js

--- content/pages/academies.js
+++ content/pages/academies.js
@@ -8,7 +8,7 @@
 >
   <Image src="heading/academies.png" width="565px" height="169px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-    ## WDS Academies are back for 2018! 
+    ## WDS Academies
 At a WDS Academy, you might learn how to travel hack your way to a life-changing travel experience, master the art of telling compelling stories, or develop a winning branding strategy for your business. 
 You'll have the opportunity to learn something new to help live your _own_ remarkable life story. It's a great way to lead into the main events of the WDS weekend and keep the conversations going afterward.
   This short video shows more of what it's all about:
@@ -20,16 +20,13 @@ Registration for Academies is separate from WDS itself and is available to both 
 
 - **WDS 360 Attendees:** $29 per Academy
 - **Connect Attendees and General Public:** $59 per Academy
-
-(Please note: if you are attending WDS, [log-in to your WDS account](http://worlddominationsummit.com/login) before making your Academy selection)
                                                                                                                                               
 There is no limit to how many Academies you can register for, but please note that you can only purchase Academies for yourself. No physical ticket or confirmation will be required upon check-in, just a valid photo ID. Academy tickets are capacity controlled and offered on a first-come, first-served basis. All tickets are **_non-refundable_** and **_non-transferrable_**.
 
 If you have any questions or need any assistance, please contact our concierge team at **[concierge@wds.fm](mailto:concierge@wds.fm)**. 
 
+_Sorry we missed you for 2018! Academies for this year have finished but will return for 2019. Please use the following WDS 2018 schedule as reference._ 
+
 <EventList year={18} type="academy" title="WDS 2018 Academies" />
-    <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-We're knocking out the final venue details for these exciting Academies so keep checking back for the most up-to-date information. 
-    </Block>
 </Block></Section>
 <Import id="join-us" />
