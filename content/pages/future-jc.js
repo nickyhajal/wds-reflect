45097a7137d9fb53adde18f131b48a124d9bf408
Jedd <jedidiah475@gmail.com>
2018-07-19 10:34:14 -0700

    Update future-jc.js

--- content/pages/future-jc.js
+++ content/pages/future-jc.js
@@ -16,7 +16,9 @@
     <h2 style={{ fontSize: '32px', textAlign: 'center' }}>
       The Future of WDS
     </h2>
-    I'm humbled and filled with gratitude when I reflect on all of things we've created and shared together since our first gathering in 2011.
+Dear WDS Community,
+
+I'm humbled and filled with gratitude when I reflect on all of things we've created and shared together since our first gathering in 2011.
 
 Over the years our community has grown to over 10,000 adventurers representing all 50 states and over 40+ countries.
 
@@ -45,15 +47,13 @@ First up, if you missed the announcement or just want to see it again, watch thi
   >
 Second, here are some answers to questions we've been asked recently:
 
-**Why is WDS ending?**<br/> 
-Nothing lasts forever. When we started WDS, there wasn't anything like it. Almost every other conference or event was overrun with sponsor logos. Speakers pitched their products from the stage. Swag bags contained junk items that most attendees immediately discarded.
+**Why is WDS ending?**  Nothing lasts forever. When we started WDS, there wasn't anything like it. Almost every other conference or event was overrun with sponsor logos. Speakers pitched their products from the stage. Swag bags contained junk items that most attendees immediately discarded.
 
 We wanted to make something different, and we succeeded. Now, there are numerous conferences and events that contain elements of the WDS "DNA" in different forms.
 
 Lastly, we want to end on a peak, not just go on forever. We want to celebrate what we've made and look forward to something new.
 
-**Why announce the end two years early?**<br/> 
-Some people just come to WDS because it's a fun experience, or to network, and that's fine. But for others (including many of our team and ambassadors), this gathering has become very special.
+**Why announce the end two years early?**  Some people just come to WDS because it's a fun experience, or to network, and that's fine. But for others (including many of our team and ambassadors), this gathering has become very special.
 
 Rather than just say "It's over," we wanted people to be able to prepare for the end, and also to join us in finishing well.
 
@@ -61,14 +61,11 @@ Rather than just say "It's over," we wanted people to be able to prepare for the
   
 <Image src="future/WDS-2018-opening-party.jpg" width="100%" height="541" />
 
-**Is WDS being sold?**<br/>
-Nope. It's not being sold and it's not for sale. For our final two years, we'll continue to operate on the same model we've had from the beginning: all profits go to the WDS Foundation, a 501(c)(3). The Foundation will then continue to operate until all its distributions have been made.
+**Is WDS being sold?**  Nope. It's not being sold and it's not for sale. For our final two years, we'll continue to operate on the same model we've had from the beginning: all profits go to the WDS Foundation, a 501(c)(3). The Foundation will then continue to operate until all its distributions have been made.
 
-**What happens next?**<br/> 
-  We're not sure! Two years is a long time. :) There's no secret plan and we want to focus on putting together a fantastic experience for 2019 and 2020.
+**What happens next?**  We're not sure! Two years is a long time. :) There's no secret plan and we want to focus on putting together a fantastic experience for 2019 and 2020.
 
-**How can I get involved with the final two years?**<br/> 
-  If WDS has meant something to you in the past, please plan to return at least one of the years. As a previous attendee, you know that everyone at the event is a big part of everything. We need you!
+**How can I get involved with the final two years?**  If WDS has meant something to you in the past, please plan to return at least one of the years. As a previous attendee, you know that everyone at the event is a big part of everything. We need you!
  
  <Image src="future/2015-Closing-Party-1.jpg" width="100%" height="541" />
 
@@ -78,7 +75,7 @@ I look forward to celebrating with you in 2019, 2020, and beyond.
 
 To _Community, Adventure,_ and _Service_,
 
-<Image src="future/cg.png" width="100%" height="200" />
+<Image src="future/cg.png" width="25%" height="52" />
 
 Chris Guillebeau
  </Block>
