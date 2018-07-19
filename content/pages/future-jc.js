0885ed3b256b5724e8c00fdaa2118a79fda5ae4e
Jedd <jedidiah475@gmail.com>
2018-07-19 10:20:15 -0700

    Update future-jc.js

--- content/pages/future-jc.js
+++ content/pages/future-jc.js
@@ -24,7 +24,7 @@ We've broken world records, learned to Bollywood dance, faced our fears, found t
     
     {' '}
     
-    <Image src="images/future/WDS-Pins.jpg" width="100%" height="541" />
+    <Image src="future/WDS-Pins.jpg" width="100%" height="541" />
 
    At this year's event, we announced that we'll produce **two more years of our epic annual gathering.** Next year is WDS 2019, and then WDS 2020 will be our 10-year grand finale.
 
@@ -32,7 +32,7 @@ Naturally, not everyone was in attendance at this year's event—so I wanted to 
 
 First up, if you missed the announcement or just want to see it again, watch this short video:
   </Block>
-<Video placeholder="images/photos/WDS-2018-main.jpg" videoId="184868405" cols="8" /> 
+<Video placeholder="photos/WDS-2018-main.jpg" videoId="184868405" cols="8" /> 
    <Block
     cols="8"
     bleed={false}
@@ -45,28 +45,28 @@ First up, if you missed the announcement or just want to see it again, watch thi
   >
 Second, here are some answers to questions we've been asked recently:
 
-**Why is WDS ending?**\ 
+**Why is WDS ending?**&nbsp; 
 Nothing lasts forever. When we started WDS, there wasn't anything like it. Almost every other conference or event was overrun with sponsor logos. Speakers pitched their products from the stage. Swag bags contained junk items that most attendees immediately discarded.
 
 We wanted to make something different, and we succeeded. Now, there are numerous conferences and events that contain elements of the WDS "DNA" in different forms.
 
 Lastly, we want to end on a peak, not just go on forever. We want to celebrate what we've made and look forward to something new.
 
-**Why announce the end two years early?**\ 
+**Why announce the end two years early?**&nbsp; 
 Some people just come to WDS because it's a fun experience, or to network, and that's fine. But for others (including many of our team and ambassadors), this gathering has become very special.
 
 Rather than just say "It's over," we wanted people to be able to prepare for the end, and also to join us in finishing well.
 
-<Image src="images/future/WDS-2015-Waffles-1.jpg" width="100%" height="" />
-<Image src="images/future/WDS-2018-opening-party.jpg" width="100%" height="" />
+<Image src="future/WDS-2015-Waffles-1.jpg" width="100%" height="" />
+<Image src="future/WDS-2018-opening-party.jpg" width="100%" height="" />
 
-**Is WDS being sold?**\ 
+**Is WDS being sold?**&nbsp; 
 Nope. It's not being sold and it's not for sale. For our final two years, we'll continue to operate on the same model we've had from the beginning: all profits go to the WDS Foundation, a 501(c)(3). The Foundation will then continue to operate until all its distributions have been made.
 
-**What happens next?**\  
+**What happens next?**&nbsp;  
   We're not sure! Two years is a long time. :) There's no secret plan and we want to focus on putting together a fantastic experience for 2019 and 2020.
 
-**How can I get involved with the final two years?**\  
+**How can I get involved with the final two years?**&nbsp;  
   If WDS has meant something to you in the past, please plan to return at least one of the years. As a previous attendee, you know that everyone at the event is a big part of everything. We need you!
  
  <Image src="future/2015-Closing-Party-1.jpg" width="100%" height="541" />
@@ -77,7 +77,7 @@ I look forward to celebrating with you in 2019, 2020, and beyond.
 
 To _Community, Adventure,_ and _Service_,
 
-<Image src="images/future/cg.png" width="100%" height="" />
+<Image src="future/cg.png" width="100%" height="" />
 
 Chris Guillebeau
  </Block>
