7d91e94dfe46e3369c8023af5c1f888d26d27a65
Jedd <jedidiah475@gmail.com>
2018-07-19 09:37:36 -0700

    Create future-jc.js

---
+++ content/pages/future-jc.js
@@ -0,0 +1,85 @@
+<Section
+  headerClip="br:0%,-10%;"
+  headerImage="photos/WDS-community.jpg"
+  color="canvas"
+>
+  <Block
+    cols="8"
+    bleed={false}
+    textAlign="left"
+    margin="80px 0 80px"
+    background="white"
+    type="padded"
+    padding="80px 120px"
+    className="bigger"
+  >
+    <h2 style={{ fontSize: '32px', textAlign: 'center' }}>
+      The Future of WDS
+    </h2>
+    I'm humbled and filled with gratitude when I reflect on all of things we've created and shared together since our first gathering in 2011.
+
+Over the years our community has grown to over 10,000 adventurers representing all 50 states and over 40+ countries.
+
+We've broken world records, learned to Bollywood dance, faced our fears, found true connection, challenged conventionality, and supported each other's dreams.
+    
+    {' '}
+    
+    <Image src="photos/2017-WDS-Ambassadors.jpg" width="100%" height="541" />
+
+   At this year's event, we announced that we'll produce **two more years of our epic annual gathering.** Next year is WDS 2019, and then WDS 2020 will be our 10-year grand finale.
+
+Naturally, not everyone was in attendance at this year's event—so I wanted to explain a bit more about this decision to our entire community.
+
+First up, if you missed the announcement or just want to see it again, watch this short video:
+
+  </Block>
+<Video placeholder="photos/academies2.jpg" videoId="121164251" cols="8" /> 
+   <Block
+    cols="8"
+    bleed={false}
+    textAlign="left"
+    margin="80px 0 80px"
+    background="white"
+    type="padded"
+    padding="80px 120px"
+    className="bigger"
+  >
+Second, here are some answers to questions we've been asked recently:
+
+**Why is WDS ending?** 
+Nothing lasts forever. When we started WDS, there wasn't anything like it. Almost every other conference or event was overrun with sponsor logos. Speakers pitched their products from the stage. Swag bags contained junk items that most attendees immediately discarded.
+
+We wanted to make something different, and we succeeded. Now, there are numerous conferences and events that contain elements of the WDS "DNA" in different forms.
+
+Lastly, we want to end on a peak, not just go on forever. We want to celebrate what we've made and look forward to something new.
+
+**Why announce the end two years early?** 
+Some people just come to WDS because it's a fun experience, or to network, and that's fine. But for others (including many of our team and ambassadors), this gathering has become very special.
+
+Rather than just say "It's over," I wanted people to be able to prepare for the end, and also to join us in finishing well.
+
+<Image src="photos/2017-WDS-Ambassadors.jpg" width="100%" height="541" />
+<Image src="photos/2017-WDS-Ambassadors.jpg" width="100%" height="541" />
+
+**Is WDS being sold?** 
+Nope. It's not being sold and it's not for sale. For our final two years, we'll continue to operate on the same model we've had from the beginning: all profits go to the WDS Foundation, a 501(c)(3). The Foundation will then continue to operate until all its distributions have been made.
+
+**What happens next?** 
+We're not sure! Two years is a long time. :) There's no secret plan and we want to focus on putting together a fantastic experience for 2019 and 2020.
+
+**How can I get involved with the final two years?** 
+If WDS has meant something to you in the past, please plan to return at least one of the years. As a previous attendee, you know that everyone at the event is a big part of everything. We need you!
+ 
+ <Image src="photos/2017-WDS-Ambassadors.jpg" width="100%" height="541" />
+
+WDS was never about an event. It has been and will forever be about community. It's been an unforgettable story so far—and I truly believe the best is yet to come.
+
+I look forward to celebrating with you in 2019, 2020, and beyond.
+
+To _Community, Adventure,_ and _Service_,
+
+<Image src="photos/2017-WDS-Ambassadors.jpg" width="100%" height="541" />
+
+Chris Guillebeau
+ </Block>
+</Section>;
