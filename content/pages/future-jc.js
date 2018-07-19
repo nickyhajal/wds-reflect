0252a3393ad35553db6bddd6219ef387b0f8bbcd
Jedd <jedidiah475@gmail.com>
2018-07-19 09:54:24 -0700

    Update future-jc.js

--- content/pages/future-jc.js
+++ content/pages/future-jc.js
@@ -24,16 +24,16 @@ We've broken world records, learned to Bollywood dance, faced our fears, found t
     
     {' '}
     
-    <Image src="photos/2017-WDS-Ambassadors.jpg" width="100%" height="541" />
+    <Image src="images/future/WDS-Pins.jpg" width="100%" height="541" />
 
    At this year's event, we announced that we'll produce **two more years of our epic annual gathering.** Next year is WDS 2019, and then WDS 2020 will be our 10-year grand finale.
 
 Naturally, not everyone was in attendance at this year's event—so I wanted to explain a bit more about this decision to our entire community.
 
 First up, if you missed the announcement or just want to see it again, watch this short video:
-
+<Video placeholder="photos/academies2.jpg" videoId="229733331" cols="8" /> 
   </Block>
-<Video placeholder="photos/academies2.jpg" videoId="121164251" cols="8" /> 
+<Video placeholder="images/future/WDS-2018.jpg" videoId="184868405" cols="8" /> 
    <Block
     cols="8"
     bleed={false}
@@ -46,31 +46,31 @@ First up, if you missed the announcement or just want to see it again, watch thi
   >
 Second, here are some answers to questions we've been asked recently:
 
-**Why is WDS ending?** 
-Nothing lasts forever. When we started WDS, there wasn't anything like it. Almost every other conference or event was overrun with sponsor logos. Speakers pitched their products from the stage. Swag bags contained junk items that most attendees immediately discarded.
+**Why is WDS ending?**  
+  Nothing lasts forever. When we started WDS, there wasn't anything like it. Almost every other conference or event was overrun with sponsor logos. Speakers pitched their products from the stage. Swag bags contained junk items that most attendees immediately discarded.
 
 We wanted to make something different, and we succeeded. Now, there are numerous conferences and events that contain elements of the WDS "DNA" in different forms.
 
 Lastly, we want to end on a peak, not just go on forever. We want to celebrate what we've made and look forward to something new.
 
-**Why announce the end two years early?** 
-Some people just come to WDS because it's a fun experience, or to network, and that's fine. But for others (including many of our team and ambassadors), this gathering has become very special.
+**Why announce the end two years early?**  
+  Some people just come to WDS because it's a fun experience, or to network, and that's fine. But for others (including many of our team and ambassadors), this gathering has become very special.
 
-Rather than just say "It's over," I wanted people to be able to prepare for the end, and also to join us in finishing well.
+Rather than just say "It's over," we wanted people to be able to prepare for the end, and also to join us in finishing well.
 
-<Image src="photos/2017-WDS-Ambassadors.jpg" width="100%" height="541" />
-<Image src="photos/2017-WDS-Ambassadors.jpg" width="100%" height="541" />
+<Image src="images/future/WDS-2015-Waffles-1.jpg" width="100%" height="541" />
+<Image src="images/future/WDS-2018-opening-party.jpg" width="100%" height="541" />
 
-**Is WDS being sold?** 
-Nope. It's not being sold and it's not for sale. For our final two years, we'll continue to operate on the same model we've had from the beginning: all profits go to the WDS Foundation, a 501(c)(3). The Foundation will then continue to operate until all its distributions have been made.
+**Is WDS being sold?**  
+  Nope. It's not being sold and it's not for sale. For our final two years, we'll continue to operate on the same model we've had from the beginning: all profits go to the WDS Foundation, a 501(c)(3). The Foundation will then continue to operate until all its distributions have been made.
 
-**What happens next?** 
-We're not sure! Two years is a long time. :) There's no secret plan and we want to focus on putting together a fantastic experience for 2019 and 2020.
+**What happens next?**  
+  We're not sure! Two years is a long time. :) There's no secret plan and we want to focus on putting together a fantastic experience for 2019 and 2020.
 
-**How can I get involved with the final two years?** 
-If WDS has meant something to you in the past, please plan to return at least one of the years. As a previous attendee, you know that everyone at the event is a big part of everything. We need you!
+**How can I get involved with the final two years?**  
+  If WDS has meant something to you in the past, please plan to return at least one of the years. As a previous attendee, you know that everyone at the event is a big part of everything. We need you!
  
- <Image src="photos/2017-WDS-Ambassadors.jpg" width="100%" height="541" />
+ <Image src="future/2015-Closing-Party-1.jpg" width="100%" height="541" />
 
 WDS was never about an event. It has been and will forever be about community. It's been an unforgettable story so far—and I truly believe the best is yet to come.
 
