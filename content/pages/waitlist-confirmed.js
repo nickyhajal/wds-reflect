ee912bcd4504d853d2cf349d18eb1807912ac90e
Jedd <jedidiah475@gmail.com>
2018-08-16 11:02:36 -0700

    Create waitlist-confirmed.js

---
+++ content/pages/waitlist-confirmed.js
@@ -0,0 +1,16 @@
+
+<Section
+  color="sea"
+  css={{ marginBottom: '-2px' }}
+>
+  <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## Wait List Confirmed!</Block>
+   <p>You're now offically on the WDS 2019 wait list and will be the first to know when tickets go live.</p>
+   <p>See you soon at WDS 2019!</p>  
+     </Block>
+    <Video placeholder="photos/WDS-2018-main.jpg" videoId="280404514" cols="8" /> 
+    <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
+   <p>Have a question? Our concierge team operates year-round so don't hesitate to reach out to them at <Link to="mailto:concierge@wds.fm">concierge@wds.fm.</Link></p>
+  </Block>
+  <Block width="100%" margin="40px 0 0 0" textAlign="center">## [Take me back to WDS site](https://worlddominationsummit.com/)</Block>
+</Section>
