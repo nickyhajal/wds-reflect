d8b47ed1d72959278027ff7eace19f4d7f4a7e73
Jedd <jedidiah475@gmail.com>
2018-08-20 09:41:45 -0700

    Update dj-prashant.js

--- content/variables/speakers/dj-prashant.js
+++ content/variables/speakers/dj-prashant.js
@@ -5,4 +5,8 @@ photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/dj.prashant.thumbn
 video: "https://vimeo.com/285728863",
 quote: "I believe that our community right now really needs to talk. We need to just converse. We need to trust in the goodwill of others, irrespective of their choices.",
 ---
+At WDS 2018, DJ Prashant talked about his journey to find his passion and live his dream. He spoke on how engineering school wasn't the right choice, and time and time again he realized his calling was to entertain and to make music. 
+
+More implicitly, DJ Prashant talked about the power of community - both building it and allowing it to support you in hard times - and the willingness to let things flow. He ended by sharing his dreams for the future, and his hope that people will continue to talk and listen to each other in this political climate. 
+  
 A one-of-a-kind Bollywood entertainer, **DJ Prashant** is a singer, choreographer, MC & DJ with one simple agenda—to make people dance non-stop to an irresistible blend of world’s greatest dance beats. Prashant’s effervescent personality & charisma instantly strikes a chord with audiences of all ages & backgrounds. Through his energetic performances, Prashant has moved masses across both coasts of America, Costa Rica, Panama & India.
