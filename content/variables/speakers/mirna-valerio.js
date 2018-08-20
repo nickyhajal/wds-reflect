c8ece768690f290edff6653db9ddd10a205fc3a0
Jedd <jedidiah475@gmail.com>
2018-08-19 23:06:42 -0700

    Create mirna-valerio.js

---
+++ content/variables/speakers/mirna-valerio.js
@@ -0,0 +1,12 @@
+name: "Mirna Valerio",
+year: "2018",
+headerPhotos: "images/speakers/Mirna-Valerio-WDS-2018-Main-Stage-Speaker.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Mirna.WDS.speaker.thumbnail.png",
+video: "",
+quote: "",
+---
+**Mirna Valerio** is a native of Brooklyn, NY, a former educator, cross-country coach, ultrarunner, obstacle course enthusiast, and author of the recently published memoir, A Beautiful Work in Progress.
+
+Although she began running in high school, she recommitted to the sport after a health scare in 2008. It was then that her love for running and all its attendant benefits were reignited. She soon started her blog Fatgirlrunning, about her experiences as a larger woman in a world of thinner athletes.
+
+Mirna's athletic story was featured in the _WSJ, Runner’s World_, on the _NBC Nightly News_, _CNN_, on the _CW Network_, and in the viral REI-produced documentary short, The Mirnavator. Her writing has been featured in _Women’s Running Magazine, Self Magazine Online, Outside Online,_ and _Runner's World Magazine_. Most recently, she was chosen as a _2018 National Geographic Adventurer of the Year._
