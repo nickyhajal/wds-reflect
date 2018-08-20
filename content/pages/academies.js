c578f7a32cec468ade6ee680743cb99f1a63988d
Jedd <jedidiah475@gmail.com>
2018-08-20 10:15:24 -0700

    Update academies.js

--- content/pages/academies.js
+++ content/pages/academies.js
@@ -15,18 +15,9 @@ You'll have the opportunity to learn something new to help live your _own_ remar
   </Block>
 <Video placeholder="photos/academies2.jpg" videoId="121164251" cols="8" /> 
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-        ## The Details 
-Registration for Academies is separate from WDS itself and is available to both WDS attendees (at a discounted rate) and to the general public.
+        ##  Sorry we missed you for 2018! 
+  
+  _Academies for this year have finished but will return for 2019. Please check back in the spring of 2019 for an all-new scheudle._ 
 
-- **WDS 360 Attendees:** $29 per Academy
-- **Connect Attendees and General Public:** $59 per Academy
-                                                                                                                                              
-There is no limit to how many Academies you can register for, but please note that you can only purchase Academies for yourself. No physical ticket or confirmation will be required upon check-in, just a valid photo ID. Academy tickets are capacity controlled and offered on a first-come, first-served basis. All tickets are **_non-refundable_** and **_non-transferrable_**.
-
-If you have any questions or need any assistance, please contact our concierge team at **[concierge@wds.fm](mailto:concierge@wds.fm)**. 
-
-_Sorry we missed you for 2018! Academies for this year have finished but will return for 2019. Please use the following WDS 2018 schedule as reference._ 
-
-<EventList year={18} type="academy" title="WDS 2018 Academies" />
 </Block></Section>
 <Import id="join-us" />
