758f1d41b0957157436de5630e6da7b922d3c5b2
Jedd <jedidiah475@gmail.com>
2018-08-19 22:48:03 -0700

    Update dj-prashant.js

--- content/variables/speakers/dj-prashant.js
+++ content/variables/speakers/dj-prashant.js
@@ -5,4 +5,4 @@ photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/dj.prashant.thumbn
 video: "https://vimeo.com/285728863",
 quote: "I believe that our community right now really needs to talk. We need to just converse. We need to trust in the goodwill of others, irrespective of their choices.",
 ---
-A one-of-a-kind Bollywood entertainer, ** DJ Prashant** is a singer, choreographer, MC & DJ with one simple agenda—to make people dance non-stop to an irresistible blend of world’s greatest dance beats. Prashant’s effervescent personality & charisma instantly strikes a chord with audiences of all ages & backgrounds. Through his energetic performances, Prashant has moved masses across both coasts of America, Costa Rica, Panama & India.
+A one-of-a-kind Bollywood entertainer, **DJ Prashant** is a singer, choreographer, MC & DJ with one simple agenda—to make people dance non-stop to an irresistible blend of world’s greatest dance beats. Prashant’s effervescent personality & charisma instantly strikes a chord with audiences of all ages & backgrounds. Through his energetic performances, Prashant has moved masses across both coasts of America, Costa Rica, Panama & India.
