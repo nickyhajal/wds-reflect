eb53e5b1fbd2e2decadb02060d2c505cfadb5b25
Jedd <jedidiah475@gmail.com>
2018-08-21 17:08:54 -0700

    Update gary-ware.js

--- content/variables/testimonials/gary-ware.js
+++ content/variables/testimonials/gary-ware.js
@@ -1,4 +1,4 @@
 name: "Gary Ware",
 content: "WDS is a magical experience that I look forward to every year! When you are surrounded by so much creative, positive, and innovative energy you can't help but to leave inspired with a desire to do good in the world.",
 link: "",
-photo: "https://pbs.twimg.com/profile_images/461968679861620737/CX4Nz6WK.jpeg"
+photo: "https://pbs.twimg.com/profile_images/993213074327748613/HGQU9ogo_400x400.jpg"
