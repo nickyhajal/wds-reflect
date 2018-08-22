85df226a3504fe5e3d28c2404c402602f7cda9c8
Jedd <jedidiah475@gmail.com>
2018-08-21 17:13:34 -0700

    Update todd-henry.js

--- content/variables/testimonials/todd-henry.js
+++ content/variables/testimonials/todd-henry.js
@@ -1,4 +1,4 @@
 name: "Todd Henry",
 content: "Powerful things happen when like-minded people get together. The beauty of WDS isn’t the programming, or the venues (which are amazing), or the “networking” like at most conferences. The beauty is that there are several thousand people all gathered in one place, centered on one ideal. The encouragement could be scooped out of the air with a spoon and swallowed whole. Not “Oh – great! You should go do your thing, and best of luck (because honestly, you’ll need it since you’re about to fall flat on your face!)” encouragement, but the kind that is real, trustworthy, and rooted in deep experience.",
 link: "http://www.toddhenry.com/noticing/it-was-beautiful-a-brief-recap-of-wds-2013/",
-photo: "https://goo.gl/89Asr8"
+photo: "https://pbs.twimg.com/profile_images/844989613143982082/i8d4uhDl_400x400.jpg"
