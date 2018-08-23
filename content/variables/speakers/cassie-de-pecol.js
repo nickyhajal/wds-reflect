5dd75f8492e90f85ba9d65b82ef088237969be41
Jedd <jedidiah475@gmail.com>
2018-08-22 22:21:15 -0700

    Update cassie-de-pecol.js

--- content/variables/speakers/cassie-de-pecol.js
+++ content/variables/speakers/cassie-de-pecol.js
@@ -1,10 +1,14 @@
 name: "Cassie De Pecol",
 year: "2018",
-headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/cassie.de.pecol.WDS.2018.Speaker.jpg",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Cassie-De-Pecol-WDS-2018-Main-Stage-Speakers.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/cassie.thumbnail.png",
-video: "",
-quote: "",
+video: "https://vimeo.com/286289258/3d9d2e3468",
+quote: "Step outside yourself and away from the ordinary and experience something that is completely new, out of your comfort zone, and a little bit scary. Then follow your mission through that, use that to create a mission for yourself, and then leave a positive legacy behind.",
 ---
+At the WDS 2015 main stage, Cassie launched **Expedition 196** - her quest to be the first documented woman to travel to all the countries in the world. 
+
+Fast forward to WDS 2018 where Cassie updates the community about her completed epic journey and explores the idea of what makes us human. Drawing upon her extensive travel experiences Cassie explains the need to leave preconceived notions at the door. Her talk also focused on creating a purpose for your life, one that creates a positive legacy.
+  
 **Cassie De Pecol** is the CEO of Expedition 196 LLC and the founder of Her International Inc., a nonprofit organization dedicated to funding female-driven projects that address the United Nations Sustainability Development Goals.  
 
 Cassie holds two Guinness World Records, is recognized in the National Women’s History Museum, and has been invited to travel into space with Virgin Galactic. She is a keynote speaker, brand ambassador, Ironman athlete, author, and content creator who has helped to put sustainable travel on the map.
