f747e28528a1b43cffad45d0c9a5e31a057c4697
Jedd <jedidiah475@gmail.com>
2018-08-23 10:07:36 -0700

    Update mirna-valerio.js

--- content/variables/speakers/mirna-valerio.js
+++ content/variables/speakers/mirna-valerio.js
@@ -2,9 +2,11 @@ name: "Mirna Valerio",
 year: "2018",
 headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Mirna-Valerio-WDS-2018-Main-Stage-Speakers.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Mirna.WDS.speaker.thumbnail.png",
-video: "",
-quote: "",
+video: "https://vimeo.com/286375551/62d1d4c3c3",
+quote: "This is one of my core beliefs: that we allow people to live, to experience, and to express the fullness of their humanity every time and all the time.",
 ---
+Starting off with a beautiful song, Mirna shared with the WDS 2018 audience about stereotypes and embracing multiple identities for herself being a singer, mother, a teacher, an athlete, and a wife. A medical scare became a catalyst for her to start running, battling the limitations and pre conceived notions that people might have about her. And even though her story was picked up nationally, she also recognizes her own biases towards others and realizing how much we all put others in a box and put limits on each other.
+  
 **Mirna Valerio** is a native of Brooklyn, NY, a former educator, cross-country coach, ultrarunner, obstacle course enthusiast, and author of the recently published memoir, A Beautiful Work in Progress.
 
 Although she began running in high school, she recommitted to the sport after a health scare in 2008. It was then that her love for running and all its attendant benefits were reignited. She soon started her blog Fatgirlrunning, about her experiences as a larger woman in a world of thinner athletes.
