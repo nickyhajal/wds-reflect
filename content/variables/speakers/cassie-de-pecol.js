0a22cb7f61c54535084522aacc960b1755366f7b
Jedd <jedidiah475@gmail.com>
2018-08-28 15:03:28 -0700

    Update cassie-de-pecol.js

--- content/variables/speakers/cassie-de-pecol.js
+++ content/variables/speakers/cassie-de-pecol.js
@@ -5,9 +5,9 @@ photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/cassie.thumbnail.p
 video: "https://vimeo.com/286289258/3d9d2e3468",
 quote: "Step outside yourself and away from the ordinary and experience something that is completely new, out of your comfort zone, and a little bit scary. Then follow your mission through that, use that to create a mission for yourself, and then leave a positive legacy behind.",
 ---
-At the WDS 2015 main stage, Cassie launched **Expedition 196** - her quest to be the first documented woman to travel to all the countries in the world. 
+At the WDS 2015 main stage, Cassie launched Expedition 196 - her quest to be the first documented woman to travel to every country in the world.
 
-Fast forward to WDS 2018 where Cassie updates the community about her completed epic journey and explores the idea of what makes us human. Drawing upon her extensive travel experiences Cassie explains the need to leave preconceived notions at the door. Her talk also focused on creating a purpose for your life, one that creates a positive legacy.
+Fast forward to WDS 2018 where Cassie updates the community about her completed epic journey and explores the idea of what makes us human. Drawing upon her extensive travel experiences, Cassie explains the need to leave preconceived notions at the door. Her talk also focused on creating a purpose for your life, one that leaves a positive legacy.
   
 **Cassie De Pecol** is the CEO of Expedition 196 LLC and the founder of Her International Inc., a nonprofit organization dedicated to funding female-driven projects that address the United Nations Sustainability Development Goals.  
 
