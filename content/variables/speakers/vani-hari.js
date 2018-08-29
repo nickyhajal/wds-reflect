9af499c77320386f87bfdba1cf2be47ae06394f0
Jedd <jedidiah475@gmail.com>
2018-08-29 08:15:22 -0700

    Update vani-hari.js

--- content/variables/speakers/vani-hari.js
+++ content/variables/speakers/vani-hari.js
@@ -1,11 +1,11 @@
 name: "Vani Hari",
 year: "2015",
-headerPhotos: "https://farm4.staticflickr.com/3743/9231403243_9fe13f2384_c.jpg",
+headerPhotos: "https://farm1.staticflickr.com/342/19609552682_254ffb8c28_k_d.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Vani+Hari.jpg",
 video: "//player.vimeo.com/video/1347571https95?byline=0&amp;portrait=0&amp;color=adbf27",
 quote: "",
 ---
-**Vani Hari** is a revolutionary food activist, creator of <a href="http://foodbabe.com" target="_blank">FoodBabe.com</a> and author of the #1 bestselling book *The Food Babe Way*, and was named one of the Most Influential
+**Vani Hari** is a revolutionary food activist, creator of **[FoodBabe.com](http://foodbabe.com)** and author of the #1 bestselling book *The Food Babe Way*, and was named one of the Most Influential
 People On The Internet by *Time Magazine*. 
 
 In her work, Hari has influenced how food giants like Kraft, Subway, General Mills, Chipotle, Chick-fil-A, and Starbucks create their products, steering them towards more healthful policies. Vani teaches people how to make the right purchasing decisions at the grocery store, how to live an organic lifestyle, and how to travel healthfully around the world. The success in her writing and investigative work can be seen in the way food companies react to her uncanny ability to find and expose the truth. 
