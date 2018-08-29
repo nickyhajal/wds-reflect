49963a91f201de14976ff3dcd97e8d156f166cc9
Jedd <jedidiah475@gmail.com>
2018-08-29 08:28:25 -0700

    Update jeremy-cowart.js

--- content/variables/speakers/jeremy-cowart.js
+++ content/variables/speakers/jeremy-cowart.js
@@ -1,10 +1,10 @@
 name: "Jeremy Cowart",
 year: "2015",
-headerPhotos: "https://farm4.staticflickr.com/3743/9231403243_9fe13f2384_c.jpg",
+headerPhotos: "https://farm4.staticflickr.com/3795/19482052469_0a7e9a4e43_k_d.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Jeremy+Cowart.jpg",
 video: "//player.vimeo.com/video/134620372?byline=0&amp;portrait=0&amp;color=adbf27",
 quote: "",
 ---
-**Jeremy Cowart**, recently named "the most influential photographer on the internet" by *Huffington Post*, is mostly known for his work as a <a href="http://jeremycowart.com/portfolio/featured/" target="_blank">photographer</a> of celebrities and musicians, but he also spends a lot of time on entrepreneurial ideas and humanitarian projects. 
+**Jeremy Cowart**, recently named "the most influential photographer on the internet" by *Huffington Post*, is mostly known for his work as a [photographer](http://jeremycowart.com/portfolio/featured/) of celebrities and musicians, but he also spends a lot of time on entrepreneurial ideas and humanitarian projects. 
 
 His first iPhone app, OKDOTHIS, launched in 2013. Jeremy has also founded a global photography movement called Help-Portrait that just had it's six-year anniversary. Jeremy lives in Nashville with his wife and two children and soon-to-be two more adopted children from Haiti.
