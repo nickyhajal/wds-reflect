b3c1400555960642ce9b8d1e7f8b12c932c67610
Jedd <jedidiah475@gmail.com>
2018-08-29 08:47:00 -0700

    Update jeremy-cowart.js

--- content/variables/speakers/jeremy-cowart.js
+++ content/variables/speakers/jeremy-cowart.js
@@ -1,6 +1,6 @@
 name: "Jeremy Cowart",
 year: "2015",
-headerPhotos: "https://farm4.staticflickr.com/3799/19673391391_592bada936_k_d.jpg",
+headerPhotos: "https://farm4.staticflickr.com/3799/19673391391_592bada936_k_d.jpg",https://farm4.staticflickr.com/3789/19482077119_49147aede7_k_d.jpg,
 photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Jeremy+Cowart.jpg",
 video: "//player.vimeo.com/video/134620372?byline=0&amp;portrait=0&amp;color=adbf27",
 quote: "",
