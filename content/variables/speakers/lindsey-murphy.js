6766a4c5fcd1dd47256c21cf61ab800cd8347dc3
Jedd <jedidiah475@gmail.com>
2018-09-04 22:46:32 -0700

    Update lindsey-murphy.js

--- content/variables/speakers/lindsey-murphy.js
+++ content/variables/speakers/lindsey-murphy.js
@@ -1,8 +1,12 @@
 name: "Lindsey Murphy",
 year: "2018",
-headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/lindsey.murphey.WDS.2018.Speaker.png",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/lindsey.murphey.WDS.2018.Speaker.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/lindsey.thumbnail.png",
-video: "",
-quote: "",
+video: "https://vimeo.com/288290922",
+quote: "All we have to do is create what we want to see. We can do that together. And guys, I am desperate for that world. I am desperate for you to operate in the fullness of your purpose because we cannot heal without it.",
 ---
-**Lindsey Murphy** stars in and produces _Crazy Aunt Lindsey and The Fab Lab_, an amazing and fresh online web science series for children. Crazy Aunt Lindsey is bringing a fresh, exciting, and needed face to the world of STEM education. She looks at the world in a different way and always has something interesting in her cupboard, on her mind, up her sleeve — Imagination! Creation! Exploration!
+At WDS 2018, Lindsey shared her journey from climbing the corporate latter quickly, breaking away to discover a passion for science projects, and the love of sharing these projects with children. 
+
+With a couple of seasons under her belt and a goal to produce the show full-time, she recently launched her second crowd-funding campaign and pledged to dedicate an episode to WDS if donations from the community reached $1000. Finally, she challenged the community to create the world we want to see by living out our unique purpose. 
+  
+**Lindsey Murphy** stars in and produces _Crazy Aunt Lindsey and [The Fab Lab](https://www.thefablab.com/)_, an amazing and fresh online web science series for children. Crazy Aunt Lindsey is bringing a fresh, exciting, and needed face to the world of STEM education. She looks at the world in a different way and always has something interesting in her cupboard, on her mind, up her sleeve — Imagination! Creation! Exploration!
