1b8e368fe1cdb5a74aa973591f7c29667c63c5e0
Jedd <jedidiah475@gmail.com>
2018-09-06 11:20:15 -0700

    Update ryan-holiday.js

--- content/variables/speakers/ryan-holiday.js
+++ content/variables/speakers/ryan-holiday.js
@@ -5,8 +5,10 @@ photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Ryan.Holiday.WDS.s
 video: "https://vimeo.com/288296913",
 quote: "If we live today as if it is the most important day of our life, then we'll make the right decisions. We will value the quiet moments, we will have the power to say no to things that don't matter.",
 ---
-At WDS 2018, Ryan talked about his compulsion to say yes to opportunities, even though he's happiest at home on his farm with his family, animals, and nature. Over-working has been a big struggle for him. Ryan points out how we give away our time when it's actually one of the most valuable resources we have. He uses our mortality as a reminder to make choices to live a better life and value the quiet moments that truly matter.
-  
+At WDS 2018, Ryan talked about his compulsion to say yes to opportunities, even though he's happiest at home on his farm with his family, animals, and nature. Over-working has been a big struggle for him.
+
+Ryan points out how we give away our time when it's actually one of the most valuable resources we have. He uses our mortality as a reminder to make choices to live a better life and value the quiet moments that truly matter.  
+
 **Ryan Holiday** is a writer and media strategist. When he was 19 years old, he dropped out of college to apprentice under Robert Greene, author of The 48 Laws of Power and went on to become the director of marketing for American Apparel. He has written for the New York Observer, Thought Catalog, Entrepreneur, 99U, Fast Company, The Huffington Post, Medium, Boing Boing, Forbes, Columbia Journalism Review and multiple other outlets.
 
 Ryan is the author of six books, including The Obstacle Is the Way, Ego Is the Enemy, The Daily Stoic and Perennial Seller. Now days Ryan can be found on a ranch outside Austin, Texas where he continues to write and work in between raising cattle, donkeys and goats. You can learn more about Ryan from his blog, [RyanHoliday.net](https://ryanholiday.net/).
