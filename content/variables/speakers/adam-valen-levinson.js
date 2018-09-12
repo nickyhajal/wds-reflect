61d97945cf297d78c9d97a2db92e25e29dac42ca
Jedd <jedidiah475@gmail.com>
2018-09-12 10:35:52 -0700

    Update adam-valen-levinson.js

--- content/variables/speakers/adam-valen-levinson.js
+++ content/variables/speakers/adam-valen-levinson.js
@@ -1,10 +1,14 @@
 name: "Adam Valen Levinson",
 year: "2018",
-headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Adam.Valen.Levinson.WDS.2018.Speaker.jpg",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Adam.Valen.Levinson.WDS.2018.speaker.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Adam.Valenn.Levinson.WDS.speaker.thumbnail.png",
-video: "",
-quote: "",
+video: "https://vimeo.com/286586993/9483a5934c",
+quote: "There are a lot of reasons to be afraid but these are not reasons to live in fear.",
 ---
+At WDS 2018, Adam talked about choosing fear. When you have real freedom, you have to make choices and that's hard. Adam didn't know how to deal with the freedom he was given and had no idea how to make decisions but he really wanted to "do something."
+
+So he traveled to the Middle East during the Arab Spring in search of answers to simple, straight-forward questions. Putting himself in a place that felt dangerous let him avoid the big questions, and he found a purpose in showing the reality of what's actually “safe”.
+  
 **Adam Valen Levinson** is an affiliate of the Middle East Institute, and a Fellow at the Center for Cultural Sociology at Yale University, where he studies senses of humor as a key to cross cultural understanding.  
 
 He has written, filmed, and photographed for _Al Jazeera, The Paris Review, Haaretz, Neue Zürcher Zeitung,_ and _VICE_, and did college stints at the Meccas of real fake news: _The Colbert Report_ and _The Onion_.  All of his stories are true.
