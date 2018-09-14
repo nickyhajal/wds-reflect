e49490c54fcb14ae253adb7c70757fadbe1ceda5
Jedd <jedidiah475@gmail.com>
2018-09-14 09:14:44 -0700

    Update guide.js

--- content/pages/guide.js
+++ content/pages/guide.js
@@ -9,7 +9,7 @@
   <Image src="heading/guide.png" width="421" height="151" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     ## Ready? Set. Go! The Attendee Guide to WDS 2018
-    This summer, you'll join over 1200+ invidividuals from over 30+ countries coming to Portland for our 8th gathering of the remarkable adventurers. This page is a checklist and guide to help you prepare and maximize your WDS experience. If you need any information regarding WDS 2018 - **_you'll find it here!_**      
+    This summer, you'll join over 1200+ invidividuals from over 30+ countries coming to Portland for our 9th gathering of the remarkable adventurers. This page is a checklist and guide to help you prepare and maximize your WDS experience. If you need any information regarding WDS 2018 - **_you'll find it here!_**      
   <Image src="photos/league.of.leaders.banner.jpg" width="100%" height="405" />
       </Block>
   </Section>        
@@ -23,16 +23,16 @@
     preload
   />
    <Block type="padded" cols="6" bleed={false} background="canvas" className="table-list" css={{ padding: '60px 40px', position: 'relative', zIndex:'4' }} clip="top_lr_1.6">
-    ## A Week of WDS: June 26th - July 2nd, 2018
+    ## A Week of WDS: June 25th - July 1st, 2019
 
-    - **Tuesday (06/26)** Small Group Activities and Attendee-Led Meetups
-    - **Wednesday (06/27)** Small Group Activities and Attendee-Led Meetups
-    - **Thursday (06/28)** Academies, Small Group Activities, and Attendee-Led Meetups
-    <li><b className="highlight highlight-2">Friday (06/29)</b> Academies, Adventure, Attendee-Led Meetups, and Opening Party</li>
-    <li><b className="highlight highlight-2">Saturday (06/30)</b> Main Stage Keynotes, Evening Activities</li>
-    <li><b className="highlight highlight-3">Sunday (07/01)</b> Main Stage Keynotes, Closing Party</li>
-    - **Monday (07/02)** Academies and Attendee-Led Meetups
-      <i>Can't come for the whole time? No problem, but you don't want to miss the main events taking place on Friday, Saturday, and Sunday. For the best possible WDS experience we recommend arriving no later than Thursday evening, and leaving no earlier than Monday morning.</i>
+    - **Tuesday (6/25)** Small Group Activities and Attendee-Led Meetups
+    - **Wednesday (6/26)** Academies, Small Group Activities and Attendee-Led Meetups
+    <li><b className="highlight highlight-2">Thursday (6/27)</b> Academies, Activities, and Attendee-Led Meetups, and the Opening Party</li>
+    <li><b className="highlight highlight-2">Friday (6/28)</b>  Academies, Activities, Attendee-Led Meetups, and the Big Adventure</li>
+    <li><b className="highlight highlight-2">Saturday(6/29)</b> Main Stage Keynotes, Attendee-Led Meetups, and Evening Activities</li>
+    <li><b className="highlight highlight-3">Sunday (6/30)</b> Main Stage Keynotes, Attendee-Led Meetups, and the Closing Party</li>
+    - **Monday (7/01)** Academies and Attendee-Led Meetups
+      <i>Can't come for the whole time? No problem, but you don't want to miss the main events taking place on Thursday (evening), Friday, Saturday, and Sunday. For the best possible WDS experience we recommend arriving no later than Thursday afternoon, and leaving no earlier than Monday morning.</i>
   </Block>
   <Image src="photos/jellyfish.png" width="426" height="263" margin="-50px -60px 100px 0" css={{ float: 'right' }} mobile={{ all: { margin: '-50px -15px 100px 0' } }} />
   <div className="clear" />
@@ -40,48 +40,48 @@
     <Section color="white">
 <Block margin="125px 0 50px 0">
     <Block cols="6">
-      ## The Steps to WDS 2018
+      ## Preparing for WDS 2019
      </Block>
       <Grid numCols="3" tabletCols="2" width="968">
       <Block type="image-overlay" anchor="right" src="photos/wds.main.stage.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 1: How WDS Works
         WDS is an unconventional event. How does the week work? What are the events that make up WDS? This should help.       
-          <Button to="https://madmimi.com/p/192d8b/" newWindow={true} styling="orange" fitToText>Learn More</Button> 
+          <Button to="" newWindow={true} styling="orange" fitToText>Coming Soon</Button> 
          </Block>      
         <Block type="image-overlay" anchor="left" src="photos/society-hotel.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 2: Lodging Info
         Every year WDS partners with some of Portland's most iconic hotels to provide exclusive discounts for our attendees.
-       <Button to="https://worlddominationsummit.com/location" newWindow={true} styling="blue" fitToText>Book Now</Button>
+       <Button to="" newWindow={true} styling="orange" fitToText>Coming Soon</Button>
         </Block>      
         <Block type="image-overlay" anchor="right" src="photos/JoinUs/image-academies.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 3: Academies
         Alumni speakers and other special guests share their knowledge and experience in half-day workshops.
-        <Button to="https://worlddominationsummit.com/academies" newWindow={true} styling="blue" fitToText>Register Now</Button>     
+        <Button to="" newWindow={true} styling="orange" fitToText>Coming Soon</Button>     
         </Block>
          <Block type="image-overlay" anchor="left" src="photos/meetups-guide.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 4: Host a Meetup 
         Share your knowledge. Coordinate an activity. Help people connect. Make WDS awesome by proposing a meetup.
-       <Button to="https://worlddominationsummit.com/propose-a-meetup" newWindow={true} styling="blue" fitToText>Propose a Meetup</Button>
-        </Block>      
+        <Button to="" newWindow={true} styling="orange" fitToText>Coming Soon</Button>     
+          </Block>      
        <Block type="image-overlay" anchor="right" src="photos/wds.mobile.app.png" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 5: Mobile App 
         Download our very own mobile app to RSVP for activities, check your personalized schedule, and so much more.
-       <Button to="https://madmimi.com/p/d8ad3c/" newWindow={true} styling="blue" fitToText>Download Now</Button>
+        <Button to="" newWindow={true} styling="orange" fitToText>Coming Soon</Button>     
         </Block>      
         <Block type="image-overlay" anchor="left" src="photos/wds.meetups.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">   
         ## Step #6: RSVP for Activities
         Create your own personalized schedule by RSVPing for Meetups, Activities, and Academies through the mobile app or online. 
-        <Button to="https://worlddominationsummit.com/events" newWindow={true} styling="blue" fitToText>RSVP for Events</Button>     
+         <Button to="" newWindow={true} styling="orange" fitToText>Coming Soon</Button>          
         </Block>
         <Block type="image-overlay" anchor="right" src="photos/wds.packing.list.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 7: Packing List 
         Want to pack like an expert scout and maximize your WDS experience? Want helpful suggestions from WDS alum and the core team? 
-       <Button to="https://madmimi.com/p/d55e5c/" newWindow={true} styling="blue" fitToText>Learn More</Button>
+        <Button to="" newWindow={true} styling="orange" fitToText>Coming Soon</Button>     
         </Block>   
          <Block type="image-overlay" anchor="left" src="photos/wds.checkin.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">   
         ## Step #8: Check-in Info
         You're here - welcome to Portland! Learn what to do when you arrive to Portland (coming soon).  
-        <Button to="https://madmimi.com/p/cce26c/" newWindow={true} styling="blue" fitToText>Learn More</Button>     
+         <Button to="" newWindow={true} styling="orange" fitToText>Coming Soon</Button>          
         </Block>
           <Block type="image-overlay" anchor="right" src="photos/JoinUs/image-parties.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 9: Have an Amazing Experience
