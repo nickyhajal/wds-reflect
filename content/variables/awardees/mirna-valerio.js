6066c0d7a59861403be5febfd29e6989f1fccf34
Jedd <jedidiah475@gmail.com>
2018-09-16 08:58:53 -0700

    Update mirna-valerio.js

--- content/variables/awardees/mirna-valerio.js
+++ content/variables/awardees/mirna-valerio.js
@@ -1,12 +1,10 @@
-name: "Mirna Valerio",
-year: "2018",
-headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Mirna.Valerio.WDS.2018.Speaker.jpg",
-photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Mirna.WDS.speaker.thumbnail.png",
+name: "GC Gallery",
+year: "2017",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/foundation/Jolyn.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/foundation/Jolyn-thumbnail.png",
 video: "",
 quote: "",
 ---
-**Mirna Valerio** is a native of Brooklyn, NY, a former educator, cross-country coach, ultrarunner, obstacle course enthusiast, and author of the recently published memoir, *A Beautiful Work in Progress*.
+Calling all emerging artists, art learners, art lovers, and art leaders!  Have you been thinking of ways to make the art world more inclusive and accessible?  Well, 2017 Voyager Jolyn Gardner has set out to create a digital marketplace that showcases and brings to center, the amazing and underrepresented work of emerging artists of the African Diaspora, a group often marginalized and ignored by the traditional art world.
 
-Although she began running in high school, she recommitted to the sport after a health scare in 2008. It was then that her love for running and all its attendant benefits were reignited. She soon started her blog Fatgirlrunning, about her experiences as a larger woman in a world of thinner athletes. 
-
-Mirna's athletic story was featured in the *WSJ, Runner’s World*, on the *NBC Nightly News*, *CNN*, on the *CW Network*, and in the viral REI-produced documentary short, The Mirnavator. Her writing has been featured in *Women’s Running Magazine*, *Self Magazine Online*, *Outside Online*, and *Runner's World Magazine*. Most recently, she was chosen as a *2018 National Geographic Adventurer of the Year*.  
+With the help of the Foundation, Jolyn will leverage current technology to create a virtual gallery going experience featuring emerging artists in curated exhibitions and art web series' accessible via smart device along with an art web series. Learn more about Jolyn and the gallery at **[jolyngc.com](https://jolyngc.com/about-jolyn-gc/)**.
