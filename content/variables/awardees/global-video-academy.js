28459f5048c67e3ca03adf715d23a8bbb6b31084
Jedd <jedidiah475@gmail.com>
2018-09-18 11:47:04 -0700

    Create global-video-academy.js

---
+++ content/variables/awardees/global-video-academy.js
@@ -0,0 +1,14 @@
+name: "Global Video Academy - Kathleen & Joe",
+year: "2016",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/foundation/Kathleen-and-Joe+Sindorf.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/foundation/benamberlynnslavin.thumbnail.png",
+video: "",
+quote: "",
+---
+There are countless, huge problems that humanitarians and activists around the world are working hard to alleviate. Unfortunately, far too many of these hard-working people are working in a vacuum, isolated from the support of an informed and energized community. At its core, the problem is a failure to effectively communicate what they’re doing. Learning to use simple, effective video storytelling skills could make a tremendous difference in the success of their causes.
+
+Kathleen and Joseph Sindorf are award-winning television professionals, who have worked in more than 75 countries. Kathleen is a university professor, and Joseph is a documentary filmmaker. Their experiences have given them a passion to share their professional knowledge with those who are working hard to make a difference in the world. The Sindorfs have taught people in five countries how to use their smartphones to create short videos to compel viewers to get involved in their work. The WDS “Scholarship for Real Life” will allow them to expand this training to create “Global Video Academy” – an online learning environment to allow people everywhere to learn to use video more effectively.
+
+Part of this training will be developed internationally, including a teaching trip with a group of university students who will be creating digital media stories among Syrian and Iraqi refugees in Amman, Jordan. They will use social media and their new video skills to communicate their experiences back home to their communities.
+
+You can follow the Sindorfs’ adventures as they develop this project. Just go to **[GlobalVideoAcademy.com](http://www.globalvideoacademy.com)**.
