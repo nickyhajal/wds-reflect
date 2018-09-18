467474af033cfa6e9ec59f5b57f6d59b96b66259
Jedd <jedidiah475@gmail.com>
2018-09-18 11:28:11 -0700

    Create self-care-101.js

---
+++ content/variables/awardees/self-care-101.js
@@ -0,0 +1,12 @@
+name: "Self Care 101 for Educators - Michelle & Andrew",
+year: "2017",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/foundation/Andrew-and-Michelle.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/foundation/Andrew-and-Michelle.thumbnail.png",
+video: "",
+quote: "",
+---
+We can all agree that self-care is extremely important, right? And I think we can all agree that educators, with taxing, demanding jobs, have an even greater need for support to ensure they are taken care of. That is what inspired Andrew Nance and Michelle Holdt to create **Self Care 101 for Educators**.
+
+Andrew and Michelle are both very passionate about bringing mindfulness and arts integration to teachers and illustrating how the two connect to creating a life worth living. Through a workshop for public school teachers in underserved communities across the United States, the project will give educators the tools to bring these practices to both their own lives and those of their students.
+      
+The Scholarship for Real Life grant will help jump start the project first year (for the 2017-2018 school year) and will create a model for continued implementation over the next three to five years - with the end goal being to create a National Model and eventually train other educators to bring this work to more schools and districts. 
