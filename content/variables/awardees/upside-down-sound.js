78a4ff61467a11620b2c0258e3b45ace2c7c3243
Jedd <jedidiah475@gmail.com>
2018-09-18 11:33:29 -0700

    Create upside-down-sound.js

---
+++ content/variables/awardees/upside-down-sound.js
@@ -0,0 +1,12 @@
+name: "Upside Down Sound - Lindsay & Leah",
+year: "2017",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/foundation/Lindsay-and-Leah.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/foundation/Jolyn-thumbnail.png",
+video: "",
+quote: "",
+---
+Have you ever wanted to learn something new? Think about how difficult that is. Now imagine you have down syndrome and the extra layer of challenge that presents… Lindsay Stodden’s sister, Leah, has down syndrome, and has been her inspiration for creating **[Upside Down Sound](https://www.upsidedownsound.org/)**, a piano-based app for individuals with down syndrome, to encourage cognitive stimulation, physical activity, and decrease social isolation among this population.
+      
+Charged with the knowledge that there is a growing number of adults with Down syndrome (DS) who, because of their genetic makeup, are highly at-risk for developing Alzheimer’s Disease (AD), likely to experience physical health conditions, and at-risk to develop mental illnesses such as depression due to social isolation.
+    
+Seeing the benefits that have come from Leah learning and playing piano her whole life, the two wanted to extend those benefits to others with down syndrome. Using basic piano lessons as curriculum, UDS will encourage cognitive stimulation, and the app makes learning accessible and fun, as there are no passwords to memorize or barriers to entry, and celebrations at each level of completion. The Scholarship for Real Life will allow UDS to become a reality and Lindsay and Leah are excited for the app to not only support others with down syndrome but other populations for which cognitive stimulation will improve their quality and length of life. Learn more about their story and app here at **[Upside Down Sound](https://www.upsidedownsound.org/)**.
