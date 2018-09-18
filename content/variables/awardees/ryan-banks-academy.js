1b2a1cdd50293cf96614df71bdf8f752c546ae3c
Jedd <jedidiah475@gmail.com>
2018-09-18 11:54:41 -0700

    Create ryan-banks-academy.js

---
+++ content/variables/awardees/ryan-banks-academy.js
@@ -0,0 +1,10 @@
+name: "Ryan Banks Academy - Valerie Groth",
+year: "2015",
+headerPhotos: "Image src="https://s3.amazonaws.com/el-files-wds/images/foundation/Valerie.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/foundation/Valerie.thumbnail.png",
+video: "",
+quote: "",
+---
+School teachers in urban districts face special challenges in promoting their students’ long-term academic success. It is no surprise that so-called “failing” schools are disproportionately situated in urban centers with high rates of poverty and crime. Valerie Groth is a mission to change that for Chicago-area students. In 2014, she founded **Ryan Banks Academy**, a coeducational boarding school that seeks to impact the lives of students through a rigorous college and career preparatory education, a supportive boarding program, and a focus on personal development in a positive environment. The goal of Ryan Banks Academy is to teach students not only the value of education, but also encourage them to focus on cultivating their own unique strengths, interests, and passions so that they may lead a purpose-filled life. Curriculum focuses not only on academic pursuits, but also on developing their relationships, core values, and mindset.
+
+There are many steps in her journey to build the school, and the Scholarship for Real Life grant will be helping get her fundraising efforts of the ground, as well as help support her burgeoning volunteer program. She is currently looking for assistance with grant writing and donor outreach, as well as anyone with a background in curriculum development. If you are interested in contributing to Ryan Banks Academy, you can email [ryanbanksacademy@gmail.com](mailto:ryanbanksacademy@gmail.com) or visit [ryanbanksacademy.org](http://www.ryanbanksacademy.org).
