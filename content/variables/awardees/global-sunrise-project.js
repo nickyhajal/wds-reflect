3690a6d5bd54315b5fcecf642aec229aef3014ae
Jedd <jedidiah475@gmail.com>
2018-09-18 11:43:49 -0700

    Create global-sunrise-project.js

---
+++ content/variables/awardees/global-sunrise-project.js
@@ -0,0 +1,12 @@
+name: "The Global Sunrise Project - Marla & Kasha",
+year: "2016",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/foundation/Global-Sunrise-Project.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/foundation/sunrise-project-marla-kasha.png",
+video: "",
+quote: "",
+---
+Kasha Sequoia Slavner is an 18-year old filmmaker/photographer, author, social entrepreneur, young global leader & peace advocate. As a concerned high school student, disillusioned and outraged by the negativity and powerlessness she felt as a consumer of mainstream media, Kasha was compelled to find an alternative narrative. With her mom Marla’s blessing and mentorship, she launched The Global Sunrise Project, a social impact media hub focused on sharing solution-oriented stories about some of the world’s most pressing issues. Taking a year off school, with the help of community, she sets out on her 16th birthday on an ambitious mission to travel the world for six months with her mom for the purpose of documenting stories of hope, resilience. empowered leadership. With a DSLR camera in hand and no clear itinerary or roadmap, Kasha finds herself intersecting with the lives of people determined to rise above adversity and shares their insights to inspire others to find it within their own hearts to take action, no matter how small, and to live out their purpose and passion in a way that contributes to the betterment of the world.
+
+With the generous help of the WDS Scholarship for Real Life grant, Marla and Kasha will be able to move **[The Global Sunrise Project](https://www.theglobalsunriseproject.com/)** forward in a timely manner, enabling Kasha to finish the post-production of her first feature length documentary film **The Sunrise Storyteller©**, submit it to film festivals, launch a film website and publish the hardcover edition of her book **Reflections of the Sunrise Storyteller - A Journey into the Heart as a Global Citizen©**.
+
+2017 plans include a tour with the film, exhibition & book, as well as accompanying keynote, workshop and development of a community engagement platform. As part of their mandate for social good, between 10-25% of net proceeds are donated back to grassroots organizations. Learn more at [TheGlobalSunriseProject.com](https://www.theglobalsunriseproject.com/).
