520cdfed3d66a5da20617f27bd9cee3d4d734a30
Jedd <jedidiah475@gmail.com>
2018-09-18 11:38:23 -0700

    Create snaptype.js

---
+++ content/variables/awardees/snaptype.js
@@ -0,0 +1,14 @@
+name: "SnapType - Ben & Amberlynn",
+year: "2016",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/foundation/Ben-and-Amberlynn-Slavin.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/foundation/benamberlynnslavin.thumbnail.png",
+video: "",
+quote: "",
+---
+**[SnapType](http://www.snaptypeapp.com/)** breaks down the barriers to education and helps students survive in a school system that wasn’t designed for them. The app enables students to complete any school worksheet on an iPad.
+
+An estimated 1 in 5 people have difficulty expressing themselves through writing. For students, that can lead to falling behind in class, emotional problems at home, low self-esteem, and a sense of hopelessness. When Amberlynn Slavin was doing her fieldwork as an occupational therapy graduate student, she worked with a child who had dysgraphia - a learning disability that makes handwriting incredibly messy.
+
+Together with her husband, Ben Slavin, they created an app to take a picture of a worksheet and then simply type the answers in on the screen. Feedback from the community has been extremely positive! The challenge they face now is that there are 4 times as many Android devices used around the world than there are Apple. To make SnapType work on Android, they have to re-write the app from the ground up in a completely different programming language.
+
+A WDS Foundation scholarship was granted to help fund the development of SnapType for Android. Soon, students around the world will be given the chance to have more independence, self confidence and the opportunity to keep up with their peers! Learn more at **[SnapTypeApp.com](http://www.snaptypeapp.com/).**
