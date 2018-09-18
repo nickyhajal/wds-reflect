0de964271de1289cc2d63e4ef0eb82276fab4e7f
Jedd <jedidiah475@gmail.com>
2018-09-18 11:20:16 -0700

    Create global-gastros.js

---
+++ content/variables/awardees/global-gastros.js
@@ -0,0 +1,10 @@
+name: "Global Gastros",
+year: "2017",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/foundation/Chelsea.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/foundation/Chelsea.thumbnail.png",
+video: "",
+quote: "",
+---
+How much do you know about where your food comes from? And not the ‘Portlandia’-style meeting your chickens, but the history and deeper understanding of where quinoa comes from and why it is a superfood? Travel writer and trained anthropologist, **Chelsea Rae Schmidt’s** vision is to preserve diversity in our world’s food systems through the study and promotion of traditional recipes, indigenous ingredients, and unique culinary traditions. Her project, called Global Gastros, focuses on working with communities in the Nordic region (focused on Sweden and Copenhagen), the Southwest United States (focusing on Native American culinary traditions and recipes), and in the Amazon (spending time in Colombia, Peru, and Brazil) to help bring awareness to the issue of food culture preservation. 
+
+Through scholarship funds, Chelsea will lead workshops in the communities, lead supper clubs and start work toward developing a cookbook to continue to pass on this knowledge of food and culture and plans to share all her research and all the collected recipes in an online, open-source database. Learn more at **[GlobalGastros.com](https://globalgastros.com/)**. 
