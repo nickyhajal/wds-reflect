54a4b930ae4e980eaba843b9507d4d7c5a242e0d
Jedd <jedidiah475@gmail.com>
2018-09-24 20:30:25 -0700

    Update cathy-heller.js

--- content/variables/speakers/cathy-heller.js
+++ content/variables/speakers/cathy-heller.js
@@ -1,10 +1,14 @@
 name: "Cathy Heller",
 year: "2018",
-headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Cathy.Heller.WDS.2018.Speaker.jpg",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Cathy-Heller-WDS-2018-Main-Stage-Speaker.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Cathy.Heller.WDS.2018.Speaker.Thumbnail.png",
-video: "",
-quote: "",
+video: "https://vimeo.com/291621842/c99e0526a4",
+quote: "We compare our behind the scenes mess to other people's highlight reels, and that is a mistake.",
 ---
+At WDS 2018, Cathy reminded the community that everyone is unique and everyone has something to offer. Everyone thinks that they're not enough but you have something that only you can say.
+
+Cathy shared about starting out her podcast, needing to be courageous enough to produce something mediocre until she got better. She learned to tolerate her own inadequacy and be uncomfortable. She challenged the community to try different things and listen to where the world is leading you because it could exceed your wildest dreams.
+  
 **Cathy Heller** is the host of the top rated podcast [Don't Keep Your Day Job](https://www.dontkeepyourdayjob.com/about/). Inspiring thousands each week, she has a way of speaking right to the heart with refreshing sincerity and tremendous insight. 
 
 Since starting her podcast Huffington post wrote a full page story on Cathy's success and her show has been mentioned in the *NY Times* and *Inc* magazines “top 9 podcasts.” Cathy has interviewed creative entrepreneurs like Actress Jenna Fisher, Marketing expert Seth Godin, designer Jonathan Adler, make up artist Bobbi Brown, founder of Jimmy Choo, novelist Emily Giffin, and Gretchen Rubin to name a few.
