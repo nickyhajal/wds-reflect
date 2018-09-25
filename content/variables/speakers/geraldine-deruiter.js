8e52ae3b5985c3e1c51f4dc4b1a0593883bade57
Jedd <jedidiah475@gmail.com>
2018-09-24 20:37:44 -0700

    Update geraldine-deruiter.js

--- content/variables/speakers/geraldine-deruiter.js
+++ content/variables/speakers/geraldine-deruiter.js
@@ -1,10 +1,12 @@
 name: "Geraldine DeRuiter",
 year: "2018",
-headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Geraldine.DeRuiter.WDS.2018.Speaker.jpg",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Geraldine-DeRuiter-WDS-2018-Main-Stage-Speaker.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Geraldine.DeRuiter.WDS.2018.Speaker.Thumbnail.png",
-video: "",
-quote: "",
+video: "https://vimeo.com/289491005/bb0405c297",
+quote: "Hey, the world is burning. We should note it.",
 ---
+At WDS 2018, Geraldine reminded us that online harassment is a real and a very serious thing. The more we acknowledge it, the more we can combat it.
+
 **Geraldine DeRuiter** is a Seattle-based blogger, author, and humorist. She is the voice behind the award-winning [Everywhereist blog](http://www.everywhereist.com/), which *TIME* magazine described as "consistently clever" (Geraldine brings this up all the time, even when it's not relevant to the conversation). 
 
 Her work has appeared in *Refinery 29, The New Yorker's Daily Shouts*, and *The Washington Post*, and one time *The New York Times* described her writing as "dark and hilarious". Her travel memoir, **_All Over The Place: Adventures in Travel, True Love, and Petty Theft_** was published by Public Affairs in 2017. She is currently working on her second book about the nefarious world of social media. You can find her on Twitter: [@everywhereist](https://twitter.com/everywhereist)
