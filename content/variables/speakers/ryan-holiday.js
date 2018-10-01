68226f0a95202a7fbc52d16f775073050a0fe874
wkorenek <wekorenek@gmail.com>
2018-10-01 15:33:37 -0700

    Update ryan-holiday.js

--- content/variables/speakers/ryan-holiday.js
+++ content/variables/speakers/ryan-holiday.js
@@ -9,6 +9,6 @@ At WDS 2018, Ryan talked about his compulsion to say yes to opportunities, even 
 
 Ryan points out how we give away our time when it's actually one of the most valuable resources we have. He uses our mortality as a reminder to make choices to live a better life and value the quiet moments that truly matter.  
 
-**Ryan Holiday** is a writer and media strategist. When he was 19 years old, he dropped out of college to apprentice under Robert Greene, author of The 48 Laws of Power and went on to become the director of marketing for American Apparel. He has written for the New York Observer, Thought Catalog, Entrepreneur, 99U, Fast Company, The Huffington Post, Medium, Boing Boing, Forbes, Columbia Journalism Review and multiple other outlets.
+Ryan Holiday is a writer and media strategist. When he was 19 years old, he dropped out of college to apprentice under Robert Greene, author of The 48 Laws of Power and went on to become the director of marketing for American Apparel. He has written for the New York Observer, Thought Catalog, Entrepreneur, 99U, Fast Company, The Huffington Post, Medium, Boing Boing, Forbes, Columbia Journalism Review and multiple other outlets.
 
 Ryan is the author of six books, including The Obstacle Is the Way, Ego Is the Enemy, The Daily Stoic and Perennial Seller. Now days Ryan can be found on a ranch outside Austin, Texas where he continues to write and work in between raising cattle, donkeys and goats. You can learn more about Ryan from his blog, [RyanHoliday.net](https://ryanholiday.net/).
