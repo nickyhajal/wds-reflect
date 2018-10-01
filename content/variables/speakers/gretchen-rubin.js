e3d6b9da7d954fd85344961b4e055986b0ec2cbb
wkorenek <wekorenek@gmail.com>
2018-10-01 13:31:57 -0700

    Update gretchen-rubin.js

--- content/variables/speakers/gretchen-rubin.js
+++ content/variables/speakers/gretchen-rubin.js
@@ -7,6 +7,6 @@ quote: "",
 ---
 **Gretchen Rubin** is the author of the blockbuster New York Times bestsellers, *Happier at Home* and *The Happiness Project*, accounts of her experiences test-driving the wisdom of the ages, current scientific studies, and lessons from popular culture about how to be happier.
 
-On her popular blog, [The Happiness Project](happiness-project.com), she reports on her daily adventures in pursuit of happiness. Rubin is an enthusiastic proponent of using technology to engage with readers about ideas, and she has a wide, active following on social media. “The Happiness Project” was even an answer on the game-show Jeopardy!
+On her popular blog, [The Happiness Project](https://gretchenrubin.com/), she reports on her daily adventures in pursuit of happiness. Rubin is an enthusiastic proponent of using technology to engage with readers about ideas, and she has a wide, active following on social media. “The Happiness Project” was even an answer on the game-show Jeopardy!
 
 A graduate of Yale and Yale Law School, Rubin started her career in law, and was clerking for Justice Sandra Day O’Connor when she realized she wanted to be a writer. She has written several books, including three novels safely locked in a desk drawer.
