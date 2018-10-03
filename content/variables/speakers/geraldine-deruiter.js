9e6d0c0783cbec9e40c6ccd9d8622214a62db117
Jedd <jedidiah475@gmail.com>
2018-10-03 15:11:25 -0500

    Update geraldine-deruiter.js

--- content/variables/speakers/geraldine-deruiter.js
+++ content/variables/speakers/geraldine-deruiter.js
@@ -5,8 +5,10 @@ photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Geraldine.DeRuiter
 video: "https://vimeo.com/289491005",
 quote: "Hey, the world is burning. We should note it.",
 ---
-At WDS 2018, Geraldine reminded us that online harassment is a real and a very serious thing. The more we acknowledge it, the more we can combat it.
+At WDS 2018, Geraldine began her talk by mentioning a viral blog post she wrote in response to an infamous sexual harassment apology by Mario Batali.
 
-**Geraldine DeRuiter** is a Seattle-based blogger, author, and humorist. She is the voice behind the award-winning [Everywhereist blog](http://www.everywhereist.com/), which *TIME* magazine described as "consistently clever" (Geraldine brings this up all the time, even when it's not relevant to the conversation). 
+She reminded us that online harassment is a real and a very threat to many people. The more we acknowledge it, the more we can combat it. She spoke about taking back the narrative... and ended with a sweet surprise for the community.
+
+  **Geraldine DeRuiter** is a Seattle-based blogger, author, and humorist. She is the voice behind the award-winning [Everywhereist blog](http://www.everywhereist.com/), which *TIME* magazine described as "consistently clever" (Geraldine brings this up all the time, even when it's not relevant to the conversation). 
 
 Her work has appeared in *Refinery 29, The New Yorker's Daily Shouts*, and *The Washington Post*, and one time *The New York Times* described her writing as "dark and hilarious". Her travel memoir, **_All Over The Place: Adventures in Travel, True Love, and Petty Theft_** was published by Public Affairs in 2017. She is currently working on her second book about the nefarious world of social media. You can find her on Twitter: [@everywhereist](https://twitter.com/everywhereist)
