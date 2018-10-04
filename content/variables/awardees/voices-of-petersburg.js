5bbe0dce482bcbc98e47bcc3593833b34a3e2bc6
Jedd <jedidiah475@gmail.com>
2018-10-03 21:01:09 -0500

    Update voices-of-petersburg.js

--- content/variables/awardees/voices-of-petersburg.js
+++ content/variables/awardees/voices-of-petersburg.js
@@ -3,6 +3,7 @@ year: "2015",
 headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/foundation/Emily-Wray.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/foundation/Emily-Wray-thumbnail.png",
 video: "",
+headerPosition: "-150px",
 quote: "",
 ---
 We’ve all experienced moments when we struggling to put into words an experience or emotion. **Voices of Petersburg** is a three-day digital storytelling workshop for clients of Petersburg Mental Health Services in Petersburg, Alaska. Participants choose an experience they want to share and write a script for a digital story to give voice to their struggles and triumphs. The process of writing, choosing supportive media assets, and bringing the story to the screen provides a platform for healing unique to the mental health therapies.
