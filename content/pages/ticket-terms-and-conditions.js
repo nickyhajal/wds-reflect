e0472b3358e8c8a4627c184587eb0cb0d2074a1e
Jedd <jedidiah475@gmail.com>
2018-10-08 07:52:56 -0500

    Create ticket-terms-and-conditions.js

---
+++ content/pages/ticket-terms-and-conditions.js
@@ -0,0 +1,20 @@
+<Section
+  color="sea"
+  css={{ marginBottom: '-2px' }}
+>
+  <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## WDS Tickets Terms and Conditions</Block>
+   
+   Please review all of the following information carefully:
+   * Your WDS ticket provides access to events, meetups, activities, and parties throughout the WDS week, including two days of main stage talks. Some additional activies such as Academies and Supper Clubs will be available for purchase and are exclusive for WDS tickets holders. A WDS ticket also provides access to the online attendee Hub and our event mobile app.
+   * There is no physical ticket. Only a photo-ID is required to check-in at WDS.
+   * WDS tickets are **non-refundable.**  
+   * In the case where a ticket cannot be used, WDS tickets can be transferred until **May 17, 2019**. Please note a **$100 transfer fee** must be successfully paid per ticket to complete the transfer.
+   * Special benefits and perks associated with the _Alumni Early-Bird ticket_ are not transferrable.
+   * Once the transfer deadline has passed, the ticket will still be held for the original purchaser. 
+   * All ticket revenue goes towards the operating cost of WDS with anything left over going to the [WDS Foundation Scholarships for Real Life Program](https://worlddominationsummit.com/foundation).
+   
+   Ready? [Take me back to the purchase page.](https://worlddominationsummit.com/be-there)
+   
+  </Block>
+</Section>
