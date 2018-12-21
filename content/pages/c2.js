9d071fc45702c3122da57a667dbc85fa96ba91ee
Jedd <jedidiah475@gmail.com>
2018-12-21 14:13:50 -0800

    Update c2.js

--- content/pages/c2.js
+++ content/pages/c2.js
@@ -9,7 +9,7 @@
   <Image src="heading/schedule.png" width="421" height="151" className="scriptHeader" preload/>
 <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     ## How the WDS Schedule Works
-    As we've done since year one, the main events (main-stage keynotes and celebrations) take place during the weekend of WDS. Additionally, every attendee will have the ability to add events and activities throughout the entire week using the WDS mobile app to create their own personalized schedule. You decide if you'll join us for the weekend or come for the entire week. Regardless, a [WDS 360 ticket](https://worlddominationsummit.com/join-us) gives you access to it all; the main events plus special discounts, bonuses, and surprises.  
+    As we've done since year one, the main events (main-stage keynotes and celebrations) take place during the weekend of WDS. Additionally, every attendee will have the ability to add events and activities throughout the entire week using the WDS mobile app to create their own personalized schedule. You decide if you'll join us for the weekend or come for the entire week. Regardless, a [WDS ticket](https://worlddominationsummit.com/join-us) gives you access to it all; the main events plus special discounts, bonuses, and surprises.  
         </Block>
  <Image src="photos/audience.png"
     width="426" height="263"
@@ -26,7 +26,7 @@
     <li><b className="highlight highlight-2">Friday (6/28)</b>  Academies, Activities, Attendee-Led Meetups, and the Big Adventure</li>
     <li><b className="highlight highlight-2">Saturday(6/29)</b> Main Stage Keynotes, Attendee-Led Meetups, and Evening Activities</li>
     <li><b className="highlight highlight-3">Sunday (6/30)</b> Main Stage Keynotes, Attendee-Led Meetups, and the Closing Party</li>
-    - **Monday (7/01)** Academies and Attendee-Led Meetups
+    - **Monday (7/01)** Academy and Attendee-Led Meetups
       
       <i>Can't come for the whole time? No problem, but you don't want to miss the main events taking place on Thursday (evening), Friday, Saturday, and Sunday. For the best possible WDS experience we recommend arriving no later than Thursday afternoon, and leaving no earlier than Monday morning.</i>
   </Block>
@@ -42,110 +42,23 @@
 
   </Block>
   <Tabs style="dots">
-    <Tab title="Geraldine">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Geraldine.DeRuiter.jpg" width="904" height="549" margin="0 auto 60px" mobile={{ all: { width: '100%' }}} preload/>
+    <Tab title="Jill">
+      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Jill-Schlesinger-WDS-2019-speaker.jpg" width="904" height="549" margin="0 auto 60px" mobile={{ all: { width: '100%' }}} preload/>
       <Block cols="6" textAlign="left">
-        ## Geraldine DeRuiter
-      **Geraldine DeRuiter** is a Seattle-based blogger, author, and humorist. She is the voice behind the award-winning [Everywhereist blog](http://www.everywhereist.com/), which *TIME* magazine described as "consistently clever" (Geraldine brings this up all the time, even when it's not relevant to the conversation). 
-
-Her work has appeared in *Refinery 29, The New Yorker's Daily Shouts*, and *The Washington Post*, and one time *The New York Times* described her writing as "dark and hilarious". Her travel memoir, **_All Over The Place: Adventures in Travel, True Love, and Petty Theft_** was published by Public Affairs in 2017. She is currently working on her second book about the nefarious world of social media. You can find her on Twitter: [@everywhereist](https://twitter.com/everywhereist)
+        ## Jill Schlesinger
+      **Jill Schlesinger**, CFP®, is the Emmy-nominated and Gracie Award Winning Business Analyst for CBS News. Jill covers the economy, markets, investing and all other things money related on TV, and the “Jill on Money” podcast, radio show and blog. Jill’s radio show recently won the 2018 Gracie Award for Best National Talk Show. Jill’s first book, "The Dumb Things Smart People Do With Their Money", will be available in February 2019 by Ballantine Books.
          </Block>
     </Tab>
-    <Tab title="Ryan">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Ryan.Holiday.WDS.2018.Speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
+    <Tab title="Tania">
+      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Tania-Katan-WDS-2019-speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
       <Block cols="6" textAlign="left">
-        ## Ryan Holiday
+        ## Tania Katan
                                                                                                                                               
-        **Ryan Holiday** is a writer and media strategist. When he was 19 years old, he dropped out of college to apprentice under Robert Greene, author of The _48 Laws of Power_ and went on to become the director of marketing for American Apparel. He has written for the New York Observer, Thought Catalog, Entrepreneur, 99U, Fast Company, The Huffington Post, Medium, Boing Boing, Forbes, Columbia Journalism Review and multiple other outlets.
-
-Ryan is the author of six books, including _The Obstacle Is the Way, Ego Is the Enemy, The Daily Stoic_ and _Perennial Seller_. Now days Ryan can be found on a ranch outside Austin, Texas where he continues to write and work in between raising cattle, donkeys and goats. You can learn more about Ryan from his blog, [RyanHoliday.net](https://ryanholiday.net/).
+       **Tania Katan** is an inspirational speaker, author, creative consultant and co-creator of the #ItWasNeverADress campaign. Tania’s work has been featured in *The New York Times*, *USA Today*, *HuffPost*, *Time*, *Adweek*, *BuzzFeed*, *Mashable*, and *CNN*. 
+                                                                                                                                            
+       Tania’s new book, "Creative Trespassing", (Penguin Random House) launches February 2019. Tania travels the world showing people and organizations how to disrupt the mindsets keeping us stuck and start a creative revolution from our cubicles! For more of Katan’s productive disruptions, [www.taniakatan.com](https://taniakatan.com/)
       </Block>
     </Tab>
- <Tab title="Adam">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Adam.Valen.Levinson.WDS.2018.Speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Adam Valen Levinson
-
-        **Adam Valen Levinson** is an affiliate of the Middle East Institute, and a Fellow at the Center for Cultural Sociology at Yale University, where he studies senses of humor as a key to cross cultural understanding.  
-
-He has written, filmed, and photographed for _Al Jazeera, The Paris Review, Haaretz, Neue Zürcher Zeitung,_ and _VICE_, and did college stints at the Meccas of real fake news: _The Colbert Report_ and _The Onion_.  All of his stories are true.
-
-He usually lives in America. Learn more about Adam and his stories at [adamvalenlevinson.com](https://www.adamvalenlevinson.com/). 
-      </Block>
-    </Tab>  
- <Tab title="Cathy">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Cathy.Heller.jpg" width="904" height="549" mobile={{ all: { width: '100%' }}} margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Cathy Heller
-                                                                                                                                              
-        **Cathy Heller** is the host of the top rated podcast [Don't Keep Your Day Job](https://www.dontkeepyourdayjob.com/about/). Inspiring thousands each week, she has a way of speaking right to the heart with refreshing sincerity and tremendous insight. 
-
-Since starting her podcast Huffington post wrote a full page story on Cathy's success and her show has been mentioned in the *NY Times* and *Inc* magazines “top 9 podcasts”. Cathy has interviewed creative entrepreneurs like Actress Jenna Fisher, Marketing expert Seth Godin, designer Jonathan Adler, make up artist Bobbi Brown, founder of Jimmy Choo, novelist Emily Giffin and Gretchen Rubin to name a few.
-
-When she's not podcasting, she's writing music for film/tv. She lives in Los Angeles with her husband and three daughters. She loves spicy tuna rolls and Larry David. 
-      </Block>
-    </Tab>
-   <Tab title="Mirna">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Mirna.Valerio.WDS.2018.Speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Mirna Valerio
-                                                                                                                                              
-        **Mirna Valerio** is a native of Brooklyn, NY, a former educator, cross-country coach, ultrarunner, obstacle course enthusiast, and author of the recently published memoir, *A Beautiful Work in Progress*.
-
-Although she began running in high school, she recommitted to the sport after a health scare in 2008. It was then that her love for running and all its attendant benefits were reignited. She soon started her blog Fatgirlrunning, about her experiences as a larger woman in a world of thinner athletes. 
-
-Mirna's athletic story was featured in the *WSJ, Runner’s World*, on the *NBC Nightly News*, *CNN*, on the *CW Network*, and in the viral REI-produced documentary short, The Mirnavator. Her writing has been featured in *Women’s Running Magazine*, *Self Magazine Online*, *Outside Online*, and *Runner's World Magazine*. Most recently, she was chosen as a *2018 National Geographic Adventurer of the Year*.
-      </Block>
-    </Tab> 
-    <Tab title="Paula">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Paula.Pant.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Paula Pant
-                                                                                                                                              
-       **Paula Pant** quit her 9-to-5 job, traveled to 35 countries, runs a business from her laptop and built a mini- real estate empire, which provides enough passive income to support her creative lifestyle. She's financially independent and spends her time traveling, writing, podcasting, creating videos, and raising funds for worldwide charity projects.
-
-Today, Paula spends her time traveling, writing, podcasting, creating videos and raising funds for worldwide charities. She’s traveled to more than 35 countries. She’s been featured in hundreds of publications, including *Lifehacker, Mashable, Forbes, Fortune, US News, NPR,* and *Oprah.com*. 
-
-She’s is the host of the award-winning **_Afford Anything_** podcast, which has been downloaded nearly 3 million times. You can find her instigating rebellion on her website and blog, [affordanything.com](https://affordanything.com). 
-      </Block>
-    </Tab>
- <Tab title="Prashant">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/DJ.Prashant.WDS.2018.Speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## DJ Prashant
-
-**DJ Prashant** is one-of-a-kind Bollywood entertainer: a singer, choreographer, MC & DJ with one simple agenda—to make people dance non-stop to an irresistible blend of world’s greatest dance beats. Prashant’s effervescent personality & charisma instantly strikes a chord with audiences of all ages & backgrounds. Through his energetic performances, Prashant has moved masses across both coasts of America, Costa Rica, Panama & India.
-
-      </Block>
-    </Tab>  
-   <Tab title="Kellee">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Kellee.Edwards.WDS.2018.Speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Kellee Edwards
-
-       **Kellee Edwards** had big dreams but never imagined that she would become a Pilot, Scuba Diver, world traveler and the ultimate adrenaline junkie. She began her travels as a child, taking road trips with her parents. This definitely ignited her passion and curiosity for the next step, traveling internationally. 
-
-Exploring over 40 countries thus far, including parts of North and West Africa, Asia, South and Central America, Europe and the UAE, her love for travel has allowed Kellee to also take her enthusiasm for outdoor adventure globally. She is the Host of Mysterious Islands and the co-host of The Trip 2018 on Travel Channel. In addition to Travel Channel she is also the travel expert for FOX 5 San Diego's "Live for Travel" segment, writer and editor for _Pilot Getaways_ magazine and advocate for Women in Adventure Travel. 
-      </Block>
-    </Tab>
-    <Tab title="Cassie">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/cassie.de.pecol.WDS.2018.Speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Cassie De Pecol
-
-**Cassie De Pecol** is the CEO of Expedition 196 LLC and the founder of Her International Inc., a nonprofit organization dedicated to funding female-driven projects that address the United Nations Sustainability Development Goals. Cassie holds two Guinness World Records, is recognized in the National Women’s History Museum, and has been invited to travel into space with Virgin Galactic. She is a keynote speaker, brand ambassador, Ironman athlete, author, and content creator who has helped to put sustainable travel on the map.
-
-      </Block>
-    </Tab> 
-        <Tab title="Lindsey">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/lindsey.murphey.WDS.2018.Speaker.png" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Lindsey Murphy
-
-**Lindsey Murphy** stars in and produces _Crazy Aunt Lindsey and The Fab Lab_, an amazing and fresh online web science series for children. Crazy Aunt Lindsey is bringing a fresh, exciting, and needed face to the world of STEM education. She looks at the world in a different way and always has something interesting in her cupboard, on her mind, up her sleeve — Imagination! Creation! Exploration!
-
-      </Block>
-    </Tab>    
   </Tabs>
   <div class="clear" />
 </Section>  
