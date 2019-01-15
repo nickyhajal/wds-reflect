6c2cdf450b924439a050451ece472a42e83aee2b
Jedd <jedidiah475@gmail.com>
2019-01-15 05:39:06 -0800

    Update waitlist-confirmed.js

--- content/pages/waitlist-confirmed.js
+++ content/pages/waitlist-confirmed.js
@@ -7,7 +7,7 @@
    <p>You're now offically on the WDS 2019 wait list and will be the first to know when tickets go live.</p>
    <p>See you soon at WDS 2019!</p>  
 <p>Have a question? Our concierge team operates year-round so don't hesitate to reach out to them at <Link to="mailto:concierge@wds.fm">concierge@wds.fm.</Link></p>
-      <Video placeholder="photos/WDS-2018-main.jpg" videoId="280404514" cols="8" />    
   </Block>
+      <Video placeholder="photos/WDS-2018-main.jpg" videoId="280404514" cols="8" />    
   <Block width="100%" margin="40px 0 0 0" textAlign="center">## [Take me back to WDS site](https://worlddominationsummit.com/)</Block>
 </Section>
