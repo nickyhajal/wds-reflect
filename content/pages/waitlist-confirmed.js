ebd6626e5f0a4b36a4c43c18af316e766dff18b1
Jedd <jedidiah475@gmail.com>
2019-01-15 22:14:42 -0800

    Update waitlist-confirmed.js

--- content/pages/waitlist-confirmed.js
+++ content/pages/waitlist-confirmed.js
@@ -3,10 +3,10 @@
   css={{ marginBottom: '-2px' }}
 >
   <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
-    <Block width="100%" margin="40px 0 0 0" textAlign="center">## Wait List Confirmed!</Block>
-   <p>You're now offically on the WDS 2019 wait list and will be the first to know when tickets go live.</p>
-   <p>See you soon at WDS 2019!</p>  
-<p>Have a question? Our concierge team operates year-round so don't hesitate to reach out to them at <Link to="mailto:concierge@wds.fm">concierge@wds.fm.</Link></p>
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## You Did It -- Wait List Confirmed!</Block>
+      <p><center>You're now offically on the WDS 2019 wait list and will be the first to know when tickets go live.</center></p>
+   <p><center>See you soon at WDS 2019!</center></p>  
+<p><center>Have a question? Our concierge team operates year-round so don't hesitate to reach out to them at <Link to="mailto:concierge@wds.fm">concierge@wds.fm.</Link></center></p>
   <p><center><i>Checkout highlights from last year's gathering below.</i></center></p>
   </Block>
       <Video placeholder="photos/WDS-2018-main.jpg" videoId="280404514" cols="8" />    
