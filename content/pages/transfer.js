13bfb4d0a57b25182e84e3c3d5d7c5b44fa42bb7
Jedd <jedidiah475@gmail.com>
2019-01-18 16:59:04 -0800

    Update transfer.js

--- content/pages/transfer.js
+++ content/pages/transfer.js
@@ -2,15 +2,15 @@
   color="sea"
   css={{ marginBottom: '-2px' }}
 >
-    <Block width="768px" bleed={true} type="padded" background="white" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
+    <Block width="788px" bleed={true} type="padded" background="white" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
       <Transfer title="So sorry you can't make it for the adventure!">
         <p>Can't make it to the World Domination Summit after all? We're sorry we won't see you there! (there's always next year).</p>
 
-        <p>Transfers and name changes are available for a $100 fee from now until May 15th (6 weeks prior to WDS).</p> 
+        <p>Transfers and name changes are available for a $100 fee from now until May 17th (6 weeks prior to WDS).</p> 
         
-        <p><i>Starting May 16th, 2018, no further transfers or name changes will be possible.</i></p>
+        <p><i>Starting May 18th, 2019, no further transfers or name changes will be possible.</i></p>
 
-        <p>Hope to see you next year!</p>
+        <p>Hope to see you next year for our final year of WDS!</p>
     </Transfer>
   </Block>
 </Section>
