fe22f26296cac3f5a00f6986f38bbd6c7e998748
193countries <chris.guillebeau@gmail.com>
2019-01-22 11:15:39 -0800

    fixed typos

--- content/pages/waitlist-confirmed.js
+++ content/pages/waitlist-confirmed.js
@@ -4,10 +4,10 @@
 >
   <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
     <Block width="100%" margin="40px 0 0 0" textAlign="center">## Your Spot Has Been Saved!</Block>
-      <p>You've officially reconfirmed your spot on the WDS 2019 waitlist and will be the first to know when tickets go on this March. (</p>
+      <p>You've officially reconfirmed your spot on the WDS 2019 waitlist and will be the first to know when tickets go on this March. </p>
    <p>See you soon at WDS 2019!</p>  
-<p>Have a question? Our concierge team operates year-round so don't hesitate to reach out to them by hitting the <b><Link to="https://worlddominationsummit.com/contact-us">bat signal here.</Link></b></p>
-  <p><i>Checkout highlights from last year's gathering below.</i></p>
+<p>Have a question? Our concierge team operates year-round. Don't hesitate to reach out to them by hitting the <b><Link to="https://worlddominationsummit.com/contact-us">bat signal here.</Link></b></p>
+  <p><i>You can also check out a few highlights from last year's gathering below.</i></p>
   </Block>
       <Video placeholder="photos/WDS-2018-main.jpg" videoId="280404514" cols="8" />    
 </Section>
