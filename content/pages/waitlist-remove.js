227a295d59cebef9c0ab1ec3a2a8032ceab77806
Jedd <jedidiah475@gmail.com>
2019-02-08 00:45:26 -0800

    Create waitlist-remove.js

---
+++ content/pages/waitlist-remove.js
@@ -0,0 +1,13 @@
+<Section
+  color="sea"
+  css={{ marginBottom: '-2px' }}
+>
+  <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## Maybe Next Year?!</Block>
+      <p>We've successfully removed you from your spot on the WDS 2019 waitlist.</p>
+   <p>Though we're sad to see you go, hopeflly we'll see you for our final year, WDS 2020!</p>  
+<p>Have a question? Our concierge team operates year-round. Don't hesitate to reach out to them by hitting the <b><Link to="https://worlddominationsummit.com/contact-us">bat signal here.</Link></b></p>
+  <p><i>You can also check out a few highlights from last year's gathering below.</i></p>
+  </Block>
+      <Video placeholder="photos/WDS-2018-main.jpg" videoId="280404514" cols="8" />    
+</Section>
