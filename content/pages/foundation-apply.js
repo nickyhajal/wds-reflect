f551cf3f06bc6f896e782a64f40a55f598bb2e4f
Jedd <jedidiah475@gmail.com>
2019-02-12 23:55:31 -0800

    Update foundation-apply.js

--- content/pages/foundation-apply.js
+++ content/pages/foundation-apply.js
@@ -8,6 +8,6 @@
 >
  <Image src="heading/foundation.png" width="470px" height="165px" className="scriptHeader" preload/>
   <Block cols="6">
-    <iframe height="4600" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/embed/zq9n4iq0s4da50/"><a href="https://worlddominationsummit.wufoo.com/forms/zq9n4iq0s4da50/">Fill out my Wufoo form!</a></iframe>
+    <iframe height="6500" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/embed/zq9n4iq0s4da50/"><a href="https://worlddominationsummit.wufoo.com/forms/zq9n4iq0s4da50/">Fill out my Wufoo form!</a></iframe>
   </Block>
 </Section>
