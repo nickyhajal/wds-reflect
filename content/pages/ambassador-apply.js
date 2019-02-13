b8dc259308d634f9f4ff28fdaff4228d0be1414a
Jedd <jedidiah475@gmail.com>
2019-02-13 00:01:18 -0800

    Create ambassador-apply.js

---
+++ content/pages/ambassador-apply.js
@@ -0,0 +1,13 @@
+<Section
+  headerImage="hero/story.jpg"
+  headerClip="rl_header"
+  headerPosition="center center"
+  headerSize="thin"
+  color="white"
+  preload
+>
+ <Image src="heading/ambassadors.png" width="700px" height="165px" className="scriptHeader" preload/>
+   <Block cols="6">
+    <iframe height="5500" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/embed/k1qb9rtw1jhaxfo/"><a href="https://worlddominationsummit.wufoo.com/forms/zq9n4iq0s4da50/">Fill out my Wufoo form!</a></iframe>
+  </Block>
+</Section>
