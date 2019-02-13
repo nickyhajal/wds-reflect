90bb962e9e92c2778a3ce797f955852e0eff3517
Jedd <jedidiah475@gmail.com>
2019-02-13 00:19:03 -0800

    Update ambassador-apply.js

--- content/pages/ambassador-apply.js
+++ content/pages/ambassador-apply.js
@@ -8,6 +8,6 @@
 >
  <Image src="heading/ambassadors.png" width="700px" height="165px" className="scriptHeader" preload/>
    <Block cols="6">
-    <iframe height="5500" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/embed/k1qb9rtw1jhaxfo/"><a href="https://worlddominationsummit.wufoo.com/forms/zq9n4iq0s4da50/">Fill out my Wufoo form!</a></iframe>
+    <iframe height="4500" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/embed/k1qb9rtw1jhaxfo/"><a href="https://worlddominationsummit.wufoo.com/forms/zq9n4iq0s4da50/">Fill out my Wufoo form!</a></iframe>
   </Block>
 </Section>
