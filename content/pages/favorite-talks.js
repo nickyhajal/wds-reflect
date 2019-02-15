a66da46a551ea0c667f31e375d7cd6c832dac488
Jedd <jedidiah475@gmail.com>
2019-02-14 18:57:00 -0800

    Create favorite-talks.js

---
+++ content/pages/favorite-talks.js
@@ -0,0 +1,34 @@
+<Section
+  headerImage="hero/story.jpg"
+  headerClip="rl_header"
+  headerPosition="top"
+  headerSize="thin"
+  color="white"
+  preload
+>
+  <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+    ## A Look Back at Some of Our Favorite Main-Stage Talks
+    At WDS, the community gathers on Saturday and Sunday at the main-stage—this year in the beautiful Newmark Theatre. We'll have community updates, make special memories together, and always a surprise or two for everyone. 
+    
+    Addtionally, each year we invite several friends of WDS living extraordinary lives and working to build a better world. Our keynote speakers are bestselling authors, leading activists, and in-demand storyteller and they come to WDS to share their stories, experiences, and lessons with our community.
+
+    Here's a look back at some of our favorite keynotes from the last 8 years:
+
+## Brand Montague and Robbie Novak (a.k.a Kid President)
+  <Video placeholder="foundation/2018-Voyagers.jpg" videoId="1349689953" width="710" height="430"/>
+  
+## Gretchen Rubin 
+  <Video placeholder="foundation/2018-Voyagers.jpg" videoId="70277456" width="710" height="430"/>
+  
+## Zach Anner 
+  <Video placeholder="foundation/2018-Voyagers.jpg" videoId="183042075" width="710" height="430"/>  
+  
+## Jadah Sellner 
+  <Video placeholder="foundation/2018-Voyagers.jpg" videoId="102690002" width="710" height="430"/>
+  
+## Jeremy Cowart 
+  <Video placeholder="foundation/2018-Voyagers.jpg" videoId="134620372" width="710" height="430"/>
+  
+  </Block>
+</Section>
+<Import id="join-us" />
