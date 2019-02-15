1e60147be0330ac81ee8f0c98f0bcce02b9eb6a1
Jedd <jedidiah475@gmail.com>
2019-02-15 02:05:23 -0800

    Create 8years.js

---
+++ content/pages/8years.js
@@ -0,0 +1,57 @@
+<Section
+  headerImage="hero/story.jpg"
+  headerClip="rl_header"
+  headerPosition="top"
+  headerSize="thin"
+  color="white"
+  preload
+>
+  <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+    ## 8 remarkable years of WDS
+    They say time flies when you're having fun and we've had a LOT of fun. Here's a look back at 8 incredible years of WDS.
+    
+## 2011
+  <Video placeholder="https://farm6.staticflickr.com/5022/5807057956_3cf3b0e7db_b_d.jpg" videoId="28062040" width="710" height="430"/>
+  
+    
+--
+## 2012 
+  <Video placeholder="https://farm8.staticflickr.com/7249/7523486638_61fe99d45f_b_d.jpg" videoId="50576518" width="710" height="430"/>
+  
+    
+--    
+## 2013 
+  <Video placeholder="https://farm8.staticflickr.com/7347/9239547017_da455d0098_b_d.jpg" videoId="75330183" width="710" height="430"/>  
+ 
+    
+--    
+## 2014 
+  <Video placeholder="https://farm4.staticflickr.com/3872/14637915291_9f765f22f3_k_d.jpg" videoId="100830702" width="710" height="430"/>
+  
+    
+--    
+## 2015
+  <Video placeholder="https://farm1.staticflickr.com/460/19058731803_5f1223556d_k_d.jpg" videoId="151533965" width="710" height="430"/>
+ 
+    
+--    
+## 2016
+  <Video placeholder="https://farm9.staticflickr.com/8889/28673994110_2d7ed5d5a1_k_d.jpg" videoId="182869666" width="710" height="430"/>
+ 
+    
+--   
+## 2017
+  <Video placeholder="https://farm4.staticflickr.com/3789/19482077119_49147aede7_k_d.jpg" videoId="229733331" width="710" height="430"/>
+ 
+    
+--
+## 2018
+  <Video placeholder="https://farm2.staticflickr.com/1761/41327849830_ba4411b3e8_k_d.jpg" videoId="280404514" width="710" height="430"/>
+ 
+    
+--   
+#### Looking for more videos?! 
+  Check-out the **[WDS YouTube page](https://www.youtube.com/channel/UCOdRqojggRLnyTGhim15-og)** for more amazing keynotes. 
+  </Block>
+</Section>
+<Import id="join-us" />
