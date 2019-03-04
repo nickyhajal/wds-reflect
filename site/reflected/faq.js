ada9b196182a776ebb67a1d17fe8589c6e71d732
Nicky Hajal <nhajal@gmail.com>
2019-03-04 03:11:25 -0600

    update card

--- site/reflected/faq.js
+++ site/reflected/faq.js
@@ -53,22 +53,23 @@ import YearReviews from '../../site/components/YearReviews';
    
    ### How can I get a WDS ticket?{'\n'}
 
-    <p>Tickets for WDS are only offered a couple of times a year, and they go quickly. There is no fast-track or secret process to get a ticket; they are all offered on a first-come, first-served basis. Once tickets are gone, they're gone. We do not hold any tickets back for higher-priced sales later, nor do we sell tickets at the door.</p>
+   <p><b>Tickets for WDS 2019 are on sale now!</b> Register before they sell out here: <Link to="https://worlddominationsummit.com/join-us">WDS 2019 Tickets.</Link></p>
 
-<p><b>Tickets for WDS 2019 will go on sale again soon!</b> To be the first to be notified when tickets go on sale next, join the official waitlist: <Link to="http://madmimi.com/signups/aea67a30548f4ac1a00b9e5b1f15efa3/join">WDS 2019 Waitlist.</Link></p>
+    <p><i>Tickets for WDS are only offered a couple of times a year, and they go quickly. There is no fast-track or secret process to get a ticket; they are all offered on a first-come, first-served basis. Once tickets are gone, they're gone. We do not hold any tickets back for higher-priced sales later, nor do we sell tickets at the door.</i></p>
 
-  ### Do you offer a payment plan for tickets?
+
+  ### Do you offer a payment plan for tickets?{'\n'}
 
 <p>Yes! For the next sale (starting March 4th), the first initial payment is <b>$95</b>, followed by <b>three payments of $204</b> for a total of <b>$707</b>. <b>All payments are non-refundable</b>, and you can pay for up to 3 tickets using a payment plan. To review the full terms and conditions of the WDS payment plan <Link to="https://worlddominationsummit.com/payment-plan-terms-and-conditions">click here.</Link></p>
 
-### I've purchased a WDS ticket, what do I do next? 
+### I've purchased a WDS ticket, what do I do next? {'\n'}
 
 <p>Keep checking your inbox. All communication about the event and specific actions needed from you to prepare you for the event is sent via email. If you are not receiving WDS 2019 emails, please contact our concierge team at <Link to="mailto:concierge@wds.fm">concierge@wds.fm</Link> and they will be able to assist you.</p>
 
-### What is the refund/cancellation policy for WDS tickets?
+### What is the refund/cancellation policy for WDS tickets?{'\n'}
     <p>All WDS tickets are <b>non-refundable.</b></p> 
 
-    ### Can I transfer a WDS ticket to someone else?
+    ### Can I transfer a WDS ticket to someone else?{'\n'}
 
 <p>Once a ticket has been officially "assigned"* to someone that ticket may be transferred to someone else for a $100 fee until May 17th (6 weeks prior to WDS). Starting <b>May 18th, 2019</b>, no further transfers or name changes will be possible. <i>Please note: Special benefits and perks associated with an original ticket are not transferrable.</i></p>
     
@@ -76,7 +77,7 @@ import YearReviews from '../../site/components/YearReviews';
 
  <p><i>*All WDS tickets are assigned to a specific person using a unique name and email -- this is required for attendance. This process takes place when a ticket is initially purchased (or by a specific date for early-bird ticket purchasers). Transferring a ticket and assigning a ticket are two different processes. Only assigned tickets can be transferred.</i></p>
        
-    ### Can I nominate myself or someone I represent to be a speaker?
+    ### Can I nominate myself or someone I represent to be a speaker?{'\n'}
 
    <p>We're sure that you or your client would be great but we do not accept nominations for speakers. Everyone who attends WDS is a big part of everything. Current year speakers are nominated by alumni speakers from previous years.</p>
 
