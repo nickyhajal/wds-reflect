3c2794af1dcfe420e4b4fd8fadd8ed8b29692c7d
Nicky Hajal <nhajal@gmail.com>
2019-03-07 20:28:58 -0600

    Update credit card exp dates

--- site/containers/Cart/Cart.js
+++ site/containers/Cart/Cart.js
@@ -129,6 +129,9 @@ class Cart extends React.Component {
       { value: '2023', label: '2023' },
       { value: '2024', label: '2024' },
       { value: '2025', label: '2025' },
+      { value: '2026', label: '2026' },
+      { value: '2027', label: '2027' },
+      { value: '2028', label: '2028' },
     ];
   }
 
