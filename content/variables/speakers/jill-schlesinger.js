9c909f82565cd98215eba348867164488631bfa9
Jedd <jedidiah475@gmail.com>
2019-03-28 18:05:15 -0500

    Create jill-schlesinger.js

---
+++ content/variables/speakers/jill-schlesinger.js
@@ -0,0 +1,11 @@
+name: "Jill Schlesinger",
+year: "2019",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Jill-Schlesinger-WDS-2019-speaker.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Jill-Schlesinger-thumbnail.png",
+video: "",
+headerPosition: "-100px",
+quote: "",
+---
+**Jill Schlesinger** CFP®, is the Emmy-nominated and Gracie Award Winning Business Analyst for CBS News. She covers the economy, markets, investing and anything else with a dollar sign on TV, on the “Jill on Money” podcast, radio (including her nationally syndicated show, “Jill on Money”, which won the 2018 Gracie Award for Best National Talk Show), the web and her blog, "Jill on Money."  
+
+Jill also won a 2018 Personal Finance Reporting Award from the Radio Television Digital News Association (RTDNA)/National Endowment for Financial Education (NEFE). Jill’s first book, *The Dumb Things Smart People Do With Their Money*, was recently published this past February (2019) by Ballantine Books.
