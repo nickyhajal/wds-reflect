139a0ea564244d8feb9465b5e80a2337dcbe7942
Jedd <jedidiah475@gmail.com>
2019-03-28 20:06:37 -0500

    Update chris-guillebeau.js

--- content/variables/speakers/chris-guillebeau.js
+++ content/variables/speakers/chris-guillebeau.js
@@ -1,16 +1,16 @@
 name: "Chris Guillebeau",
 year: "2019",
-headerPhotos: "https://farm1.staticflickr.com/309/19635556795_066b76fc74_k.jpg",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/chris.guillebeau.wds.png",
 photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Chris+Guillebeau.jpg",
 video: "",
 quote: "",
 ---
-**Chris Guillebeau** is a New York Times bestselling author and modern-day explorer.
+**Chris Guillebeau** is a New York Times bestselling author and modern-day explorer.  
 
-During a lifetime of self-employment that included a four-year commitment as a volunteer executive in West Africa, he visited every country in the world (193 in total) before his 35th birthday. Since then he has modeled the proven definition of an entrepreneur: “Someone who will work 24 hours a day for themselves to avoid working one hour a day for someone else.”
+During a lifetime of self-employment that included a four-year commitment as a volunteer executive in West Africa, he visited every country in the world (193 in total) before his 35th birthday. Since then he has modeled the proven definition of an entrepreneur: “Someone who will work 24 hours a day for themselves to avoid working one hour a day for someone else.”  
 
-Chris’s first book, The Art of Non-Conformity, was translated into more than twenty languages. His second book, The $100 Startup, was a New York Times and Wall Street Journal bestseller, selling more than 500,000 copies worldwide. His third book, The Happiness of Pursuit, was published in September 2014 and was also a New York Times bestseller. His fourth book, Born for This, will help you find the work you were meant to do. His newest book, Side Hustle, will help you create a new source of income in 27 days.
+Chris’s first book, The Art of Non-Conformity, was translated into more than twenty languages. His second book, The $100 Startup, was a New York Times and Wall Street Journal bestseller, selling more than 500,000 copies worldwide. His third book, The Happiness of Pursuit, was published in September 2014 and was also a New York Times bestseller. His fourth book, Born for This, will help you find the work you were meant to do. His newest book, Side Hustle, will help you create a new source of income in 27 days.  
 
-His daily podcast, Side Hustle School, is downloaded more than 2 million times a month.
+His daily podcast, Side Hustle School, is downloaded more than 2 million times a month.  
 
 Every summer in Portland, Oregon, Chris hosts the World Domination Summit, a gathering of creative, remarkable people with thousands in attendance. Chris is also the founder of Pioneer Nation, Unconventional Guides, the Travel Hacking Cartel, and numerous other projects.
