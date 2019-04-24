83998df6cd6c23b4c76227c076b42a0fc59755cf
Jedd <jedidiah475@gmail.com>
2019-04-24 12:24:11 -0700

    Create humble-the-poet.js

---
+++ content/variables/speakers/humble-the-poet.js
@@ -0,0 +1,13 @@
+name: "Humble the Poet",
+year: "2019",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Humble-The-Poet-WDS-2019-Speaker.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Humble-The-Poet-thumbnail.png",
+video: "",
+headerPosition: "-100px",
+quote: "",
+---
+**Kanwer Singh AKA Humble the Poet** is a Toronto-bred MC/spoken word artist with an aura that embodies the diversity and resiliency of one of the world's most unique cities. 
+
+With tattoos, beard, head wrap and a silly smile, Humble commands attention. He stimulates audiences with ideas that challenge conventional wisdom and go against the grain, with dynamic live sets that shake conventions and minds at the same time.  
+
+Humble's latest EP titled Righteous/Ratchet features the first single H.A.I.R, a celebration of women of all shapes, sizes, hair and walks of life. His self directed video for H.A.I.R has amassed almost 2 million views since its release. Humble's first book *Unlearn: 101 Simple Truths For A Better Life* was published through Indigo Press.
