03dd1b70b8e45667bea351541a5dfb79108bf0fb
Jedd <jedidiah475@gmail.com>
2019-04-24 12:27:43 -0700

    Create trystan-reese.js

---
+++ content/variables/speakers/trystan-reese.js
@@ -0,0 +1,11 @@
+name: "Trystan Reese",
+year: "2019",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Trystan-Angel-Reese-WDS-2019-Speaker.png",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Trystan-Reese-thumbnail.png",
+video: "",
+headerPosition: "-100px",
+quote: "",
+---
+**Trystan Reese** is perhaps best known as “the pregnant man” of internet and television fame. But he would prefer to be known as the adoptive father of Hailey and Riley, gestational father of Leo, all-around awesome trans community member, and partner to the amazing Biff Chaplow. 
+
+He lives in Portland, OR and works as the Director of Family Formation at Family Equality Council. He can be found on Facebook and Instagram as **@biffandi** or at [www.biffandi.com](https://www.biffandi.com/).
