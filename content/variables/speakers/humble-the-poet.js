9432539b0e64279bc4459330f3095cc4ab8b26c2
Jedd <jedidiah475@gmail.com>
2019-04-24 16:28:27 -0700

    Update humble-the-poet.js

--- content/variables/speakers/humble-the-poet.js
+++ content/variables/speakers/humble-the-poet.js
@@ -10,4 +10,4 @@ quote: "",
 
 With tattoos, beard, head wrap and a silly smile, Humble commands attention. He stimulates audiences with ideas that challenge conventional wisdom and go against the grain, with dynamic live sets that shake conventions and minds at the same time.  
 
-Humble's latest EP titled Righteous/Ratchet features the first single H.A.I.R, a celebration of women of all shapes, sizes, hair and walks of life. His self directed video for H.A.I.R has amassed almost 2 million views since its release. Humble's first book *Unlearn: 101 Simple Truths For A Better Life* was published through Indigo Press.
+Humble's latest EP titled Righteous/Ratchet features the first single H.A.I.R, a celebration of women of all shapes, sizes, hair and walks of life. His self directed video for H.A.I.R has amassed almost 2 million views since its release. Humble's first book Unlearn: 101 Simple Truths For A Better Life was published through Indigo Press. Learn more about Humble the Poet on his website, **[humblethepoet.com](https://www.humblethepoet.com/)**.
