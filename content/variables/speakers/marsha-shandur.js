7c01242c56e2095d90a7b3c9abb2dbbba0f344f2
Jedd <jedidiah475@gmail.com>
2019-05-23 10:20:19 -0700

    Create marsha-shandur.js

---
+++ content/variables/speakers/marsha-shandur.js
@@ -0,0 +1,11 @@
+name: "Marsha Shandur",
+year: "2019",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Marsha-Shandur-WDS-2019-Speaker.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/MarshaShandur-Thumbnail.png",
+video: "",
+headerPosition: "-100px",
+quote: "",
+---
+**Marsha Shandur** understands that telling stories can change the world. She has coached hundreds of storytellers across the globe, drawing their personal stories out and taking them from confusing mess to compelling piece.
+
+As the founder of **[YesYesMarsha.com](http://www.yesyesmarsha.com/), she shows entrepreneurs and organizations how to make an INSTANT emotional connection with their dream clients and collaborators, through storytelling on stage and screen, and effective (and FUN!) networking. She has been featured on Forbes, the BBC, Mashable and The Muse.
