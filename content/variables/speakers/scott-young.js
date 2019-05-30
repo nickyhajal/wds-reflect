d379e54afba0c3e85d5ecb8c081b3e928a94300e
Jedd <jedidiah475@gmail.com>
2019-05-30 13:45:16 -0700

    Create scott-young.js

---
+++ content/variables/speakers/scott-young.js
@@ -0,0 +1,11 @@
+name: "Scott Young",
+year: "2019",
+headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Scott-Young.WDS-2019-Speaker.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Scott-Young.thumbnail.png",
+video: "",
+headerPosition: "-100px",
+quote: "",
+---
+**Scott Young** is a writer, programmer, traveler and avid reader of interesting things. For the last ten years he’s been experimenting to find out how to learn and think better. Beyond writing, entrepre- neurship and life philosophy, his interests include programming, travel, cooking and teaching himself anything he can. 
+
+Scott’s new book, _Ultralearning: Master Hard Skills, Outsmart the Competition, and Accelerate Your Career_ will be released later this summer. 
