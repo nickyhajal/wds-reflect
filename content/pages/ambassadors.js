e1c10e104138ff4a495c4d0609223df2a125d564
Jedd <jedidiah475@gmail.com>
2019-05-29 18:03:49 -0700

    Update ambassadors.js

--- content/pages/ambassadors.js
+++ content/pages/ambassadors.js
@@ -31,52 +31,10 @@ Ambassadors are our all-volunteer team of amazing humans who help make WDS happe
 - And much more! 
   
 -- 
+###  We’ve Closed Ambassador Applications for 2019
+
+If you’re still interested in joining as an Ambassador this year (or for 2020!) please reach out to Tina at **concierge@wds.fm** and we’ll get everything straightened out!
   
-### Who can apply? 
-We welcome applications from anyone who meets the following criteria: 
-
-- You’re committed to helping our attendees have the BEST experience possible 
-- You live in or near Portland, OR (or you’re very familiar with the city)
-- You’re willing to share **20 hours** of your time between **Monday, June 24th and Monday, July 1st** _(please note: the majority of Ambassador shifts are Thursday-Sunday)_
-- You’re available on the evening of **Monday, June 24th** for Ambassador orientation 
-- You’re not a paying WDS attendee 
-
-  <Image src="photos/2018-WDS-Ambassadors.jpg" width="100%" height="541" />
-       </Block>
-      <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-
-### What do I get in return? 
-Fame, fortune, glory! Ok, maybe not all those things. But you do get the satisfaction of helping others, along with some more tangible perks in exchange for being a part of this epic experience, including: 
-- An all access pass to WDS - any time you’re not volunteering as an Ambassador you’re free to experience WDS like any other attendee. This includes attending our opening and closing parties, seeing our mainstage talks, participating or hosting meetups, and just about all other aspects of the event. 
-- An exclusive Ambassador t-shirt and all the WDS swag
-- Free food! Well, technically a couple gift cards to help cover your lunch. 
--- 
-
-### Tell me more about this 20 hour commitment… 
-  
-This year we’re trying something new! We’ll be releasing 85% of our Ambassador roles to the Ambassador team at a pre-announced day and time (likely between 3-4 weeks out from the event). From there, it will be first-come, first-served to pick your shifts. You can choose any shifts you want, as long as they add up to at least 20 hours. This gives YOU more freedom to choose when you want to volunteer and what you’d like to be doing. You can even coordinate to volunteer alongside a family member or friend (though we’ll encourage you to meet new people over the week too). 
-
-**If at all possible, we suggest you hold **all-day Thursday, June 27 until Sunday, June 30** on your calendar now so you have lots of options to choose from. 
-
-If you’re not able to volunteer 20 hours but still want to help, we’re not able to offer a WDS pass in return, but we’d love to chat more! 
-  
-### I have more questions/would like more information!
-Email Tina at **[concierge@wds.fm](mailto:concierge@wds.fm)** and we’ll get everything straightened out! 
-   </Block>
-</Section>  
-   
-<Section
-  color="white"
->  
- <Block type="padded" background="blue">
-    <Image src="icon/circle-map-green.png" width="101" height="78"  align="center" margin="0 0 40px 0" />
-        <Block cols="5">
-      ## Okay, ready to apply? Let’s do this! 
-      Click the button below to get started.
-      
-      _Please note: We recommended completing the application on a computer, as some users have reported trouble submitting the form from their phones._
-           <Button to="https://worlddominationsummit.com/ambassador-apply" newWindow={true} styling="sea" fitToText>WDS 2019 Ambassador Application</Button> 
-    </Block>
-  </Block>
-
+</Block>
 </Section>
+
