7bf5310f8e2f383fbc9f3b3f4abf208e6f7b1596
Jedd <jedidiah475@gmail.com>
2019-06-04 15:04:19 -0700

    Create picnic-purchase-confirmed.js

---
+++ content/pages/picnic-purchase-confirmed.js
@@ -0,0 +1,13 @@
+<Section
+  color="blue"
+  css={{ marginBottom: '-2px' }}
+>
+  <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## Aw Yeah, Your Bento Order is Confirmed!</Block>
+    <Block width="100%" margin="0 0 40px 0"><Image src="https://media.giphy.com/media/bTzFnjHPuVvva/giphy.gif" width="100%" height="400" /></Block>
+   <p>You're all set! Just come to the picnic and present your photo ID at the check-in to collect your meal.</p>  
+   <p>Have any questions? Please contact our concierge team at <Link to="mailto:concierge@wds.fm">concierge@wds.fm</Link> and they'll be able to assist you.</p>
+   <p>Want to order another meal? Click on the link below.</p> 
+  </Block>
+<Block width="100%" margin="40px 0 0 0" textAlign="center">## [Picnic Bento Preorder page](https://worlddominationsummit.com/picnic-preorder)</Block>
+</Section>
