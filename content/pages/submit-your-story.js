789a44a9b46e83bfbe44b96adba91aa8bf5dd3d7
Jedd <jedidiah475@gmail.com>
2019-06-17 21:35:51 -0700

    Update submit-your-story.js

--- content/pages/submit-your-story.js
+++ content/pages/submit-your-story.js
@@ -21,7 +21,9 @@
     <h2 style={{ fontSize: '32px', textAlign: 'center' }}>
       Submit Your Story
     </h2>
-     <p>Do you have a remarkable, personal story that you'd like to share on the WDS main stage? We want to learn more. <b>Fill out the following form by Sunday, June 23rd, 5pm PST/8pm EST.</b> Late submissions will not be considered.</p>
+     <p>Do you have a remarkable, personal story that you'd like to share on the WDS main stage? We want to learn more.</p> 
+      <p><b>Fill out the following form by Sunday, June 23rd, 5pm PST/8pm EST.</b></p> 
+        <p>Late submissions will not be considered.</p>
      <Block cols="6">
     <iframe height="1750" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/forms/s1wmwbs81n6u528/"><a href="https://worlddominationsummit.wufoo.com/forms/s1wmwbs81n6u528/">Submit your remarkable story!</a></iframe>
   </Block>
