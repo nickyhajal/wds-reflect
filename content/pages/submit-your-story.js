737a5f4d1e4410d0a26a9a96e9fd2a3f23cce731
Jedd <jedidiah475@gmail.com>
2019-06-19 12:29:06 -0700

    Update submit-your-story.js

--- content/pages/submit-your-story.js
+++ content/pages/submit-your-story.js
@@ -22,6 +22,20 @@
       Submit Your Story
     </h2>
      <p>Do you have a remarkable, personal story that you'd like to share on the WDS main stage? We want to learn more.</p> 
+
+<h3>Your Chance to Propose an Attendee Story</h3>
+<p>Community, Adventure and Service are the three core values of WDS—and an Attendee Stories session is your chance to experience all three. The adventure of sharing your story on stage is an awesome way to serve and be a part of this community.</p>
+
+<p>Here are a few ideas of the kind of stories we're looking for:</p>
+<ul>
+<li>A time in which you needed to trust</li>
+<li>A moment that altered the trajectory of your life</li>
+<li>A quick lesson or life principle you think would help others</li>
+<li>A way in which you were inspired or influenced by someone else, or something that happened</li>
+<li>A story that is vulnerable and reflective</li>
+<li>A request that you think the WDS community can help you with</li>
+<li>A person you'd like to publicly thank</li>
+</ul>
       <p><b>Fill out the following form by Sunday, June 23rd, 5pm PST/8pm EST.</b></p> 
         <p>Late submissions will not be considered. For the best user experience we suggest completing this form with a computer or tablet.</p>
      <Block cols="6">
