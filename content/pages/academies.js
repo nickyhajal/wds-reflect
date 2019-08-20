c00cb5f2a953fb533075eb4f13ba95eb72aa6ec4
Jedd <jedidiah475@gmail.com>
2019-08-20 16:15:34 -0700

    Update academies.js

--- content/pages/academies.js
+++ content/pages/academies.js
@@ -8,7 +8,7 @@
 >
   <Image src="v2/headings/academies.png" width="565px" height="169px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-    ## WDS Academies will back for 2020 with an all-star lineup! 
+    ## WDS Academies will be back for 2020 with an all-star lineup! 
 At a WDS Academy, you might learn how to develop a fulfilling writing practice, build or business through Youtube, or what it's like to build and live in a tiny house. 
 You'll have the opportunity to learn something new to help live your _own_ remarkable life story. It's a great way to lead into the main events of the WDS weekend and keep the conversations going afterward.
   This short video shows more of what it's all about:
