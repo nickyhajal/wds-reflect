f055a09beb9a7f4756676ab948257ed6d7899f0e
Jedd <jedidiah475@gmail.com>
2019-08-21 21:12:53 -0700

    Update humble-the-poet.js

--- content/variables/speakers/humble-the-poet.js
+++ content/variables/speakers/humble-the-poet.js
@@ -8,8 +8,8 @@ quote: "When you assign blame you give power. Our problems are not our fault, bu
 ---
 **Kanwer Singh AKA Humble the Poet** is a Toronto-bred MC/spoken word artist with an aura that embodies the diversity and resiliency of one of the world's most unique cities. 
 
-<p>At WDS 2019, Kanwer Singh (a.k.a. Humble the Poet) shared his origin story.  He followed his father's advice: to get a good education, became an elementary school teacher but at the same time, fell in love with spoken word art and performance.</p>
+At WDS 2019, Kanwer Singh (a.k.a. Humble the Poet) shared his origin story.  He followed his father's advice: to get a good education, became an elementary school teacher but at the same time, fell in love with spoken word art and performance.
 
-<p>On the promise of a potential record deal, he quit his job and went all in to performing. But things didn't go as planned, leaving him $80,000 in debt and with no record deal. He channeled his struggles and pain into vulnerability and honesty which led to a successful book but also new revelations about life.</p> 
+On the promise of a potential record deal, he quit his job and went all in to performing. But things didn't go as planned, leaving him $80,000 in debt and with no record deal. He channeled his struggles and pain into vulnerability and honesty which led to a successful book but also new revelations about life.
 
-<p>Now he focuses on "seva", or service -- the idea of using your pain and your experiences to help others. Pain isn't a badge of honor or something to compare with others, but instead something to use to help others, and add beauty and value to the world.</p>
+Now he focuses on "seva", or service -- the idea of using your pain and your experiences to help others. Pain isn't a badge of honor or something to compare with others, but instead something to use to help others, and add beauty and value to the world.
