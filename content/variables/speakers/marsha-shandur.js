a1cd8573898659651edebab9e43015b48643e3dd
Jedd <jedidiah475@gmail.com>
2019-08-22 21:16:27 -0700

    Update marsha-shandur.js

--- content/variables/speakers/marsha-shandur.js
+++ content/variables/speakers/marsha-shandur.js
@@ -8,4 +8,4 @@ quote: "",
 ---
 **Marsha Shandur** understands that telling stories can change the world. She has coached hundreds of storytellers across the globe, drawing their personal stories out and taking them from confusing mess to compelling piece.
 
-As the founder of **[YesYesMarsha.com](http://www.yesyesmarsha.com/), she shows entrepreneurs and organizations how to make an INSTANT emotional connection with their dream clients and collaborators, through storytelling on stage and screen, and effective (and FUN!) networking. She has been featured on Forbes, the BBC, Mashable and The Muse.
+As the founder of **[YesYesMarsha.com](http://www.yesyesmarsha.com/)**, she shows entrepreneurs and organizations how to make an INSTANT emotional connection with their dream clients and collaborators, through storytelling on stage and screen, and effective (and FUN!) networking. She has been featured on Forbes, the BBC, Mashable and The Muse.
