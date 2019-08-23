ab93cb0606c10d66ff5ebd1f3347de44888263e7
Jedd <jedidiah475@gmail.com>
2019-08-22 21:12:17 -0700

    Update marsha-shandur.js

--- content/variables/speakers/marsha-shandur.js
+++ content/variables/speakers/marsha-shandur.js
@@ -1,6 +1,6 @@
 name: "Marsha Shandur",
 year: "2019",
-headerPhotos: "https://s3.amazonaws.com/el-files-wds/images/speakers/Marsha-Shandur-WDS-2019-Speaker.jpg",
+headerPhotos: "https://el-files-wds.s3.amazonaws.com/images/speakers/Marsha-WDS2019-Mainstage.jpg, https://live.staticflickr.com/65535/48183710627_1c007899ce_k_d.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/MarshaShandur-Thumbnail.png",
 video: "",
 headerPosition: "-100px",
