fec1552465d1cace267070b15191d8f835c2105f
Jedd <jedidiah475@gmail.com>
2019-08-22 21:37:14 -0700

    Update nate-staniforth.js

--- content/variables/speakers/nate-staniforth.js
+++ content/variables/speakers/nate-staniforth.js
@@ -3,7 +3,7 @@ year: "2019",
 headerPhotos: "https://el-files-wds.s3.amazonaws.com/images/speakers/Nate-WDS2019-Mainstage.jpg, https://live.staticflickr.com/65535/48183709901_2b4cf9c39f_k_d.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Nate-Staniforth-thumbnail.png",
 video: "",
-headerPosition: "-200px",
+headerPosition: "-20px",
 quote: "",
 ---
 **Nate Staniforth** Magician, writer, traveler, and host of the Discovery Channel’s international hit TV series Breaking Magic. Nate abandons the ubiquitous style-without-substance bravado so often associated with magic and appeals instead to the imagination and intellect of his audience. The journey is wild, visceral and immediate, and like all great art, encourages us to open our minds and hearts, and see the world in new ways.
