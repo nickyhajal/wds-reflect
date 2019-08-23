c988958ba58f113c02c340c1f963b94b678c7cc4
Jedd <jedidiah475@gmail.com>
2019-08-22 21:21:25 -0700

    Create nadya-okamoto.js

---
+++ content/variables/speakers/nadya-okamoto.js
@@ -0,0 +1,11 @@
+name: "Nadya Okamoto",
+year: "2019",
+headerPhotos: "https://el-files-wds.s3.amazonaws.com/images/speakers/Nadya-WDS2019-Mainstage.jpg, https://live.staticflickr.com/65535/48183728457_919dadf5ac_k_d.jpg",
+photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Nadya-Okamoto-thumbnail.png",
+video: "",
+headerPosition: "-20px",
+quote: "",
+---
+**Nadya Okamoto** grew up in Portland, OR, and is a 21-year-old Harvard student on a leave of absence. She is the Founder and Executive Director of [PERIOD](https://period.org), an organization she founded at the age of 16. PERIOD is now the largest youth-run NGO in women’s health, and one of the fastest growing ones here in the United States. Since 2014 they have addressed over 500,000 periods and registered over 300 campus chapters.
+
+In 2017, Nadya ran for office in Cambridge, MA. While she did not win, her campaign team made historic waves in mobilizing young people on the ground and at polls. Nadya recently published her debut book, Period Power: A Manifesto for the Menstrual Movement, with publisher Simon & Schuster, which made the Kirkus Reviews list for Best Young Adult Nonfiction of 2018. Most recently, Nadya has become the Chief Brand Officer of JUV Consulting, a Generation Z marketing agency based in NYC.
