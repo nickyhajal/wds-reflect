3999b78f2e6b628c33add8b8ed8d12ab023df5fc
Jedd <jedidiah475@gmail.com>
2019-08-28 12:38:39 -0700

    Update james-victore.js

--- content/variables/speakers/james-victore.js
+++ content/variables/speakers/james-victore.js
@@ -2,13 +2,13 @@ name: "James Victore",
 year: "2019",
 headerPhotos: "https://el-files-wds.s3.amazonaws.com/images/speakers/James-WDS2019-Mainstage.jpg, https://live.staticflickr.com/65535/48183754262_265459d268_k_d.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/James-Victore-thumbnail.png",
-video: "",
+video: "https://vimeo.com/349982821",
 headerPosition: "-20px",
-quote: "",
+quote: "You have no friends. You have no enemies. You only have teachers.",
 ---
 **James Victore** is a graphic designer, art educator and dynamic speaker on creativity. He lectures and teaches regularly around the globe, inspiring people to illuminate their individual gifts in order to achieve personal greatness.
 Described as “part Darth Vader, part Yoda,” James is widely known for his timely wisdom and impassioned views about design and it’s place in the world. 
 
-James’ work has been exhibited at the Museum of Modern Art in New York and are in the permanent collections of the Palais du Louvre, Paris, the Library of Congress, Washington, DC, the Design Museum in Zurich and the Stedelijk Museum, Amsterdam. His clients include Moet & Chandon, Aveda, *Esquire* and *TIME Magazines*, Yohji Yamamoto, Bobbi Brown cosmetics, *The New York Times*, and The School of Visual Arts.
+Though known as a graphic designer, James is also, but reluctantly, a teacher. But as a teacher, he's not here to tell anyone anything. Using his colorful personality, James reminded the WDS 2020 audience to ‘Feck’ rules that constrain ourselves from doing the things we are meant to. 
 
-James Victore’s new book, **“Feck Perfuction: Dangerous Ideas on the Business of Creativity”** was published in 2019.
+He shared an important story and realization that in life, we only have teachers. That we can learn from anything. But we also need to choose and do the painful work that’s necessary.
