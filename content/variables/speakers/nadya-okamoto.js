a98aa3ab91ccb6cd668f4f5bf4f35db3c24729a3
Jedd <jedidiah475@gmail.com>
2019-08-28 12:09:17 -0700

    Update nadya-okamoto.js

--- content/variables/speakers/nadya-okamoto.js
+++ content/variables/speakers/nadya-okamoto.js
@@ -2,10 +2,14 @@ name: "Nadya Okamoto",
 year: "2019",
 headerPhotos: "https://el-files-wds.s3.amazonaws.com/images/speakers/Nadya-WDS2019-Mainstage.jpg, https://live.staticflickr.com/65535/48183728457_919dadf5ac_k_d.jpg",
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Nadya-Okamoto-thumbnail.png",
-video: "",
+video: "https://vimeo.com/349994408/c6db58c088",
 headerPosition: "-20px",
-quote: "",
+quote: "Generation Z doesn’t want a seat at the table, they want to flip the damn table.",
 ---
 **Nadya Okamoto** grew up in Portland, OR, and is a 21-year-old Harvard student on a leave of absence. She is the Founder and Executive Director of [PERIOD](https://period.org), an organization she founded at the age of 16. PERIOD is now the largest youth-run NGO in women’s health, and one of the fastest growing ones here in the United States. Since 2014 they have addressed over 500,000 periods and registered over 300 campus chapters.
 
 In 2017, Nadya ran for office in Cambridge, MA. While she did not win, her campaign team made historic waves in mobilizing young people on the ground and at polls. Nadya recently published her debut book, Period Power: A Manifesto for the Menstrual Movement, with publisher Simon & Schuster, which made the Kirkus Reviews list for Best Young Adult Nonfiction of 2018. Most recently, Nadya has become the Chief Brand Officer of JUV Consulting, a Generation Z marketing agency based in NYC.
+
+At WDS 2020, Nadya brought to the community’s attention about ‘Period Poverty’, the #1 reason why girls in developing countries miss school. But it’s also a major issue here in the US. So she did what any typical 16 year old would founded a non-profit and now globally recognized organization to end period poverty and period stigma.  
+
+Though the initial goal was to help get menstrual products to those that badly need it, Nadya discovered a bigger battle to help change the stigma and uncomfortableness around periods in general. She challenged the community to engage in these important conversations and also to remember that her generation, Z, is more than capable of making a difference in our world.
