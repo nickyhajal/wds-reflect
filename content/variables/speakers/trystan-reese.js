adac0b53e855a977341b8e54594323d89a4a17c8
Jedd <jedidiah475@gmail.com>
2019-09-03 22:33:37 -0700

    Update trystan-reese.js

--- content/variables/speakers/trystan-reese.js
+++ content/variables/speakers/trystan-reese.js
@@ -1,11 +1,11 @@
-name: "Trystan Reese",
+name: "Trystan Angel Reese",
 year: "2019",
 headerPhotos: "https://el-files-wds.s3.amazonaws.com/images/speakers/Trystan-Reese-WDS2019-Mainstage.jpg, https://live.staticflickr.com/65535/48183655831_397f1b8471_k_d.jpg",
 photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Trystan-Reese-thumbnail.png",
-video: "",
+video: "https://vimeo.com/350004518",
 headerPosition: "-20px",
-quote: "",
+quote: "I learned that being vulnerable and opening yourself up makes you powerful and gives other people permission to open up to you as well.",
 ---
-**Trystan Reese** is perhaps best known as “the pregnant man” of internet and television fame. But he would prefer to be known as the adoptive father of Hailey and Riley, gestational father of Leo, all-around awesome trans community member, and partner to the amazing Biff Chaplow. 
+**Trystan Reese** is perhaps best known as “the pregnant man” of internet and television fame. But he would prefer to be known as the adoptive father of Hailey and Riley, gestational father of Leo, all-around awesome trans community member, and partner to the amazing Biff Chaplow. He lives in Portland, OR and works as the Director of Family Formation at Family Equality Council. He can be found on Facebook and Instagram as **@biffandi** or at [www.biffandi.com](https://www.biffandi.com/).
 
-He lives in Portland, OR and works as the Director of Family Formation at Family Equality Council. He can be found on Facebook and Instagram as **@biffandi** or at [www.biffandi.com](https://www.biffandi.com/).
+Trystan reminded the WDS 2020 audience that stories are important in helping people reframe the way we think about something. This is especially true in situations where we combat close-minded beliefs. As a trans pregnant man, he was aware that he had a unique story, but he wasn’t sure if wanted to publicly share it. He was then inspired (and ashamed) that the most marginalized members of his community were courageously bearing the burden of educating our society regarding trans issues. He recognized his own privileges and wanted to make sure another story was being told from the trans community. Not just stories of pain, but those of power, and pride, resilience, love, and family. 
