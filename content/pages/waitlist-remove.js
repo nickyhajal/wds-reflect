af9cd647b6d6f7056cd07ae4bf2202c870d13038
Jedd <jedidiah475@gmail.com>
2019-09-16 15:01:02 -0700

    Update waitlist-remove.js

--- content/pages/waitlist-remove.js
+++ content/pages/waitlist-remove.js
@@ -3,11 +3,9 @@
   css={{ marginBottom: '-2px' }}
 >
   <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
-    <Block width="100%" margin="40px 0 0 0" textAlign="center">## "Don't stop believin"...</Block>
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## Don't stop believin...</Block>
       <p>Per your request, we've removed you from the WDS 2020 waitlist.</p>
-   <p>Though we're sad to see you go, hopefully we'll see you at some future point.</p>  
+   <p>Though we're sad to see you go, we wish you well adventurer.</p>  
 <p>Have a question? Our concierge team operates year-round. Don't hesitate to reach out to them by hitting the <b><Link to="https://worlddominationsummit.com/contact-us">bat signal here.</Link></b></p>
-  <p><i>Before you go, check out highlights from last year's gathering.</i></p>
-  </Block>
-      <Video placeholder="https://el-files-wds.s3.amazonaws.com/images/WDS-2019-recap-video.jpg" videoId="350010002" cols="8" />    
+  </Block>   
 </Section>
