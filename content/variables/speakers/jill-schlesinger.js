b2e50647ca6ede2ddd556cd5b47b921708858a5c
Jedd <jedidiah475@gmail.com>
2019-09-19 17:52:08 -0700

    Update jill-schlesinger.js

--- content/variables/speakers/jill-schlesinger.js
+++ content/variables/speakers/jill-schlesinger.js
@@ -8,6 +8,6 @@ quote: "Fear is the enemy of understanding. I implore you to ask questions.",
 ---
 **Jill Schlesinger** CFP®, is the Emmy-nominated and Gracie Award Winning Business Analyst for CBS News. She covers the economy, markets, investing and anything else with a dollar sign on TV, on the “Jill on Money” podcast, radio (including her nationally syndicated show, “Jill on Money”, which won the 2018 Gracie Award for Best National Talk Show), the web and her blog, "Jill on Money."  
 
-Jill believes that there are two main emotions that effect our relationship with money — greed and fear. She reminded the WDS 2020 audience that we have the power to control and take action even when we feel these emotions. We don’t have to make dumb decisions when it comes to money.
+Jill believes that there are two main emotions that effect our relationship with money — greed and fear. She reminded the WDS 2019 audience that we have the power to control and take action even when we feel these emotions. We don’t have to make dumb decisions when it comes to money.
 
 With humor and real-life examples, Jill tackles subjects like college debt, renting vs. buying a home, saving for retirement, and creating an emergency fund.  If we plan and take action today, we create the freedom to live the lives we want to.
