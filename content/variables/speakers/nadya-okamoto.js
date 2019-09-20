ae7433e178d260c2be9404d44821652c88133645
Jedd <jedidiah475@gmail.com>
2019-09-19 17:52:36 -0700

    Update nadya-okamoto.js

--- content/variables/speakers/nadya-okamoto.js
+++ content/variables/speakers/nadya-okamoto.js
@@ -8,6 +8,6 @@ quote: "Generation Z doesn’t want a seat at the table, they want to flip the d
 ---
 **Nadya Okamoto** grew up in Portland, OR, and is a 21-year-old Harvard student on a leave of absence. She is the Founder and Executive Director of [PERIOD](https://period.org), an organization she founded at the age of 16. PERIOD is now the largest youth-run NGO in women’s health, and one of the fastest growing ones here in the United States. Since 2014 they have addressed over 500,000 periods and registered over 300 campus chapters.
 
-At WDS 2020, Nadya brought to the community’s attention about ‘Period Poverty,’ the #1 reason why girls in developing countries miss school. But it’s also a major issue in the U.S. So she did what any typical 16 year old would and started a non-profit and now globally recognized organization to end period stigma. 
+At WDS 2019, Nadya brought to the community’s attention about ‘Period Poverty,’ the #1 reason why girls in developing countries miss school. But it’s also a major issue in the U.S. So she did what any typical 16 year old would and started a non-profit and now globally recognized organization to end period stigma. 
 
 Though the initial goal was to help get menstrual products to those that badly need it, Nadya discovered a bigger battle to help change the stigma and uncomfortableness around periods in general. She challenged the community to engage in these important conversations and also to remember that her generation is more than capable of making a difference in our world.
