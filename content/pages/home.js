1903e342b609f3747042293856146d323e4766c3
Nicky Hajal <nhajal@gmail.com>
2019-09-26 15:56:02 -0500

    update home

--- content/pages/home.js
+++ content/pages/home.js
@@ -1,100 +1,137 @@
 <Section
-  headerClip="br:0%,-6%;"
-  headerImage="v2/hero/futurehero.png"
+  headerVideo="350010002"
+  headerImage="hero/balls.jpg"
+  headerClip="lr_header"
   color="canvas"
 >
-  <Block
-    cols="8"
-    bleed={false}
-    textAlign="left"
-    margin="80px 0 80px"
-    background="white"
-    type="padded"
-    padding="80px 120px"
-    className="bigger"
-    mobile={{tablet: {padding: '50px 40px'}, phone: {padding: '30px 20px'}}}
-  >
-    <h2 style={{ fontSize: '32px', textAlign: 'center' }}>
-      The Future of WDS
-    </h2>
-Dear WDS Community,
-
-I'm humbled and filled with gratitude when I reflect on all of things we've created and shared together since our first gathering in 2011.
-
-Over the years, our community has grown to **over 10,000 adventurers representing all 50 states and over 40+ countries**.
-
-We've broken world records, learned to Bollywood dance, faced our fears, found true connection, challenged conventionality, and supported each other's dreams.
-    
-    {' '}
-    
-    <Image src="future/WDS-Pins.jpg" width="100%" height="541" /><br/>
-
-   At WDS 2018, we announced that we'll produce **two more years of our epic annual gathering.** We just completed 2019 our peultimate, better-than-ultimate year. Next year, WDS 2020 will be our final year. Naturally, not everyone was in attendance at this year's event—so I wanted to explain a bit more about this decision to our entire community.
-
-First up, if you missed this past year's event (WDS 2019) check out this quick highlights video:
-  </Block>
-<Video placeholder="https://el-files-wds.s3.amazonaws.com/images/WDS-2019-recap-video.jpg" videoId="350010002" cols="8" /> 
-   <Block
-    cols="8"
-    bleed={false}
-    textAlign="left"
-    margin="80px 0 80px"
-    background="white"
-    type="padded"
-    padding="80px 120px"
-    className="bigger"
-    mobile={{tablet: {padding: '50px 40px'}, phone: {padding: '30px 20px'}}}
-  >
-Second, here are some answers to questions we've been asked recently:
-
-**Why is WDS ending?**
-
-Nothing lasts forever. When we started WDS, there wasn't anything like it. Almost every other conference or event was overrun with sponsor logos. Speakers pitched their products from the stage. Swag bags contained junk items that most attendees immediately discarded.
-
-We wanted to make something different, and we succeeded. Now, there are numerous conferences and events that contain elements of the WDS "DNA" in different forms.
-
-Lastly, we want to end on a peak, not just go on forever. We want to celebrate what we've made and look forward to something new.
-
-**Why announce the end two years early?**  
-  
-Some people just come to WDS because it's a fun experience, or to network, and that's fine. But for others (including many of our team and ambassadors), this gathering has become very special.
-
-Rather than just say "It's over," we wanted people to be able to prepare for the end, and also to join us in finishing well.
-
-<Image src="future/WDS-2015-Waffles-1.jpg" width="100%" height="541" />
-  
-<Image src="future/WDS-2018-opening-party.jpg" width="100%" height="541" /><br/>
-  
-**Is WDS being sold?**
-
-Nope. It's not being sold and it's not for sale. For our final year, we'll continue to operate on the same model we've had from the beginning — focusing on the attendee experience while being a non-sponsored, non-commerical event.
-
-**What happens next?**  
-  
-We're not sure! Two years is a long time. :) There's no secret plan and we want to focus on putting together a fantastic experience for 2020.
-
-**How can I get involved with the final year?**  
-  
-If WDS has meant something to you in the past, *please plan to return for one final celebration*. As a previous attendee, you know that everyone at the event is a big part of everything. We need you!
-  
-And if you've stumbled on WDS but have yet to attend... now's the time! Join us to help create and celebrate our final year.  
- 
- <Image src="future/2015-Closing-Party-1.jpg" width="100%" height="541" /><br/>
-  
-WDS was never just about an event. It has been and will forever be about community. It's been an unforgettable story so far—and I truly believe the best is yet to come.
-
-I look forward to celebrating with you next year, and beyond.
-
-To _Community, Adventure,_ and _Service_,
-
-<Image src="future/cg.png" width="15%" height="52" />
-
-Chris Guillebeau
-
-P.S. WDS 2020 tickets will go **on sale soon**, join the wait list using the button or form below.
-
-<Button to="/join-us" fitToText>Join the WDS 2020 Wait List!</Button> 
-  
+  <Image src="v2/heading/weekend.png" width="819px" height="217px" align="center" mobile={{ all: { width: '580px', backgroundSize: '100% auto' }, phone: { width: '320px', height: '97px' } }}/>
+  <Block cols="6" bleed={false} align="center" textAlign="center" margin="50px 0 20px" mobile={{ all:{ width: '550px' }, phone: {width: '100%'} }}>
+    #### A full week of activities and an unforgettable main-stage weekend in Portland, Oregon. Learn from unconventional thinkers, acquire powerful new skills, and go away with an all-new community of friends and supporters.
+    <Button to="/join-us" style={{ fontSize: '22px', padding: '18px 100px', marginTop: '80px', marginBottom: '40px' }} fitToText>Join Us!</Button>
   </Block>
 </Section>
+<Section color="white" css={{ padding: '90px 0 230px 0' }} mobile={{ phone: { padding: '20px 0 230px 0' } }}>
+  <Block width="600px" css={{ color: 'orange' }}>
+    <h2 style={{ marginBottom: '4px' }}>World Domination Summit 2020 (final year!)</h2>
+    ## June 23 - June 29, 2020 
+    ## in Portland, Oregon
+  </Block>
+  <Block cols="6" className="bigger" mobile={{ all:{ padding: '0 50px', marginBottom: '20px' }, phone: { padding: '0 20px' } }}>
+
+    Since 2011, WDS has brought together more than 10,000 people from more than forty countries. As adventurers from all walks of life, our mission is to create a remarkable community that helps each person pursue a big dream. 
+
+    With discussions and activities on topics like how to live a good life, travel hacking your way around the globe, and writing your first book, it’s an experience like no other.
+    
+    For 2020 we'll celebrate 10 years of Community, Adventure, and Service featuring an all-star speaker/Academy lineup, tons of opportunities to connect with fellow attendees and as always, fun, unforgettable surprises. 
+
+    _All that’s missing is YOU!_
+
+  </Block>
+</Section>
+<Section color="green" css={{ padding: '90px 0 230px 0' }} mobile={{ phone: { padding: '20px 0 130px 0' } }}>
+  <Grid>
+    <Block
+      type="padded"
+      width="550"
+      height="240"
+      anchor="right"
+      mobile={{
+        all: { position: 'relative', zIndex: '6', top: '-85px', width: '450px', right: '20px' },
+        phone: { width: '100%', margin: '-220px -20px 30px -12px' }
+      }}
+      background="blue"
+      feed="twitter"
+      refresh="12"
+      margin="-235px 0 0 0"
+    >
+      I started my day breaking a world record and ended it enjoying time with some of the coolest people on the planet. Nothing better. #WDS2015
+    </Block>
+  </Grid>
+  <div className="clear"></div>
+  <Image src="photos/people.jpg" width="426" height="263" margin="-120px 0 -80px -50px" mobile={ { all: {position: 'relative', zIndex: '-1' }, phone: {width: '100%', margin: '-120px 0 -80px -20px'}} }/>
+  <Block
+    type="padded"
+    cols="6"
+    bleed={false}
+    background="canvas"
+    className="has-checklist"
+    css={{ padding: '60px 40px' }}
+    mobile={{ phone: {
+      padding: '60px 25px 60px 10px'
+    }}}
+    clip="top_lr_1.6"
+  >
+    ## A WDS Ticket Includes:
+
+    - All access pass to main stage keynotes on Saturday & Sunday
+    - Access to your choice of more than 150+ themed meetups all over the city (or host your own!)
+    - Entrance to the Opening & Closing Parties
+    - Ability to connect with attendees and RSVP to your choice of activities through the WDS mobile app
+    - Discounted registration for any of the half-day Academies taking place througout the week
+    - Discounted and priority hotel registration, including the chance to stay at the WDS Hotel™
+    - A special community that will support you for life
+  </Block>
+</Section>
+<Testimonials />
+<Section color="white" clip="tr:0,2%;" margin="-70px 0 0 0" mobile={{ all: {margin: '-120px 0 0 0' } }}>
+  <Block cols="6" css={{ marginBottom: '96px' }} className="bigger" css={{ paddingTop: '80px' }} mobile={{ all:{ padding: '80px 50px 0'}, phone: { padding: '40px 20px 0' }}}>
+    ## Take the Tour
+
+    Since 2011, WDS has brought together more than 10,000 remarkable people from all 50 states and more than 40 countries. Our mission is to explore community, to undertake an adventure, and to be of service to all those around us.
+  </Block>
+  <div>
+    <Block type="image" src="photos/schedule.jpg">
+      ## The Schedule
+
+      With literally hundreds of activities taking place during WDS week, you won't be bored. (But we'll also have suggestions, so you won't be overwhelmed.)
+      <Button to="/schedule">See the Details</Button>
+    </Block>
+    <Block type="image" src="photos/happening.jpg">
+      ## Our Story
+
+    See some of our favorite highlights and unforgettable memories we've created as a community over the past 9 years.
+      <Button to="/story">See the Highlights</Button>
+    </Block>
+    <Block type="image" src="photos/academies.jpg">
+      ## Academies
+
+      Intensive, half-day sessions on focused topics. Learn from alumni speakers and other community leaders to help you level-up your life. 
+      <Button to="/academies">Learn More</Button>
+    </Block>
+    <Block type="image" src="photos/foundation.jpg">
+      ## WDS Foundation
+      
+Profits from WDS go to our "Scholarships for Real Life" foundation, a 501(c)(3) charity that provides microgrants in support of bold projects. <Button to="/foundation">Learn More</Button>
+    </Block>
+  </div>
+  <div className="clear" />
+  <Block>## And lots more!</Block>
+  <Tabs>
+    <Tab title="Activities" color="blue" layout="imageLeft">
+      <Image src="photos/tab-activities.png" width="348px" height="324px" anchor="left" mobile={{ phone: { width: '100%', height: '180px', marginBottom: '16px' } }}/>
+      Community, Adventure, and Service: for one week you’ll be surrounded by like-minded people interested in pursuing big dreams and building a better world.  
+      When you spend WDS with us in Portland, you’ll help us take over the city. With main-stage keynotes, hundreds of attendee-led meetups, dozens of workshops known as “Academies,” adventure activities, two big parties, and hundreds of new friends high-fiving you on street corners, it’s a truly immersive and awesome experience.
+      Everyone who comes to WDS is a big part of everything that happens. Our attendees make the community strong and independent with no corporate sponsorships.
+    </Tab>
+    <Tab title="Attendee Meetups" color="sea" layout="imageLeft">
+      <Image src="photos/tab-meetups.png" width="348px" height="324px" anchor="left" mobile={{ phone: { width: '100%', height: '180px', marginBottom: '16px' } }}/>
+      Attendee-led meetups have been a staple of WDS since our first year. In 2020, they're back and better than ever!
+      Each day, there will be time set aside for attendees to host these informal meetups, and on any given day, you can go from a meetup for star gazing to a meetup for creative entrepreneurs—and then over to a meetup for a group of people interested in the Tiny House Movement, where a fight will likely break out over who owns the fewest things.
+      Meetups aren't led by WDS team members—they're led by your fellow attendees! Our attendee-only app will guide you to your choice of meetups happening all over town from morning to night.    
+      And if you have your own audience, you can host a meetup too. (Our dedicated attendee web app will tell you where each meetup is being held.)
+    </Tab>
+    <Tab title="Special Events" color="green" layout="imageLeft">
+      <Image src="photos/tab-events.png" width="348px" height="324px" anchor="left" mobile={{ phone: { width: '100%', height: '180px', marginBottom: '16px' } }}/>
+      WDS is a week-long gathering bringing together some of the most remarkable people from around the world. This means we'll have new activities and mini-events to create even more incredible memories together.
+      From an Opening Party that gives you a chance to meet the team, speakers, and other attendees, all the way through to an epic Closing Party, we’ve crafted a wonderfully immersive experience. 
+      Join your fellow attendees at your choice of extracurricular activities around town—from film screenings and river cruises, to world record attempts and more!
+    </Tab>
+    <Tab title="Speakers" color="orange" layout="imageLeft">
+      <Image src="photos/tab-speakers.png" width="348px" height="324px" anchor="left" mobile={{ phone: { width: '100%', height: '180px', marginBottom: '16px' } }}/>
+      Part of what makes WDS so great is our weekend of main stage events. Our keynote speakers are bestselling authors, leading activists, and in-demand storytellers. Since our very first year in 2011, the lineup has been inclusive, multicultural, and representative of our whole community. 
+      Over the weekend, each speaker will share a short message that combines personal stories with practical ideas on our main stage, but most speakers stick around for the entire weekend and are available for informal conversation.  Sunday afternoon—before we all head to the afterparty—we've set aside a space for our speakers to sign books and meet readers. Print books will be available for sale throughout the weekend, and if you're digital-only, you can stop by to say hi to your favorite author!
+    </Tab>
+  </Tabs>
+</Section>
 <Import id="join-us" />
+<Import id="tell-a-friend" />
