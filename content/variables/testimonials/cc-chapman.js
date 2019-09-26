4f8ae8b034a9f7e91c90d5c049c7696b9f822895
Jedd <jedidiah475@gmail.com>
2019-09-26 12:32:01 -0700

    Update cc-chapman.js

--- content/variables/testimonials/cc-chapman.js
+++ content/variables/testimonials/cc-chapman.js
@@ -3,4 +3,4 @@ content: "Events are started to make money. The World Domination Summit was star
 link: "http://www.cc-chapman.com/2012/the-people-and-magic-of-world-domination-summit-2012/",
 avatarPosition: 'right top',
 bgPosition: 'right top',
-photo: "http://www.cc-chapman.com/wp-content/uploads/2015/07/CC-Chapman-Cubelean.jpg"
+photo: "https://pbs.twimg.com/profile_images/1010234762164760576/H74e0Xiv_400x400.jpg"
