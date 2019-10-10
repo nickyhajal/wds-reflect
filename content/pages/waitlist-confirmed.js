76f6a8e2cc88491fad72ab2846350e30d1516825
Jedd <jedidiah475@gmail.com>
2019-10-10 21:53:40 +0200

    Update waitlist-confirmed.js

--- content/pages/waitlist-confirmed.js
+++ content/pages/waitlist-confirmed.js
@@ -4,8 +4,8 @@
 >
   <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
     <Block width="100%" margin="40px 0 0 0" textAlign="center">## Your Spot Has Been Saved!</Block>
-      <p>You've officially confirmed your spot on the WDS 2020 waitlist and will be the first to know when tickets go on sale next. </p>
-   <p>See you soon next summer!</p>  
+      <p>Thanks for letting us know your intention to stay on the waitlist in the hopes for a ticket to WDS 2020, our final year. </p>
+   <p>Stay tuned for more information!</p>  
 <p>Have a question? Our concierge team operates year-round. Don't hesitate to reach out to them by hitting the <b><Link to="https://worlddominationsummit.com/contact-us">bat signal here.</Link></b></p>
   <p><i>You can also check out a few highlights from last year's gathering below.</i></p>
   </Block>
