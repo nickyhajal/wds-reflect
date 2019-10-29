06330ad38575049d94d1668b33acf4a0a4c5b45f
Jedd <jedidiah475@gmail.com>
2019-10-29 07:49:48 +0100

    Update test123.js

--- content/pages/test123.js
+++ content/pages/test123.js
@@ -6,16 +6,14 @@
   color="white"
   preload
 >
-  <Image src="v2/headings/academies.png" width="565px" height="169px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-    ## WDS Academies are back for 2019! 
-At a WDS Academy, you might learn how to develop a fulfilling writing practice, build or business through Youtube, or what it's like to build and live in a tiny house. 
-You'll have the opportunity to learn something new to help live your _own_ remarkable life story. It's a great way to lead into the main events of the WDS weekend and keep the conversations going afterward.
-  This short video shows more of what it's all about:
+    ## Help Us Plan WDS 2020 
+As we dream big for the final WDS, help us with our planning by providing your input. Keep checking back for different ways you can assist.
+
+    ## Current Ask: Where Are You Staying in Portland? 
   </Block>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdfNxu7O1SYn85akHPe0CGczG2ypv39UUsyvmJi27uUBJ-vrw/viewform?embedded=true" width="100%" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 </Block>
-<EventList year={19} type="academy" title="WDS 2019 Academies" />
 </Section>
 <Import id="join-us" />
