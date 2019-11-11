4b46ef1ef4e2be36ccdd6dc2051f68d7c0f653eb
Jedd <jedidiah475@gmail.com>
2019-11-11 14:07:55 +0100

    Update test123.js

--- content/pages/test123.js
+++ content/pages/test123.js
@@ -10,20 +10,20 @@
     ## Help Us Plan WDS 2020 
 As we dream big for the final WDS, help us with our planning by providing your input. Keep checking back for different ways you can assist.
 
-    #### Current Ask: WDS Pop-up RV Campground
+    #### Current Ask: WDS Pop-up RV Campground Feedback
     We know that there are several full and partial RVers in our community and we've been playing with the idea of creating our own site for this community—a WDS Pop-up RV Campground in downtown Portland (RV, tent, van, Airstream rental, etc)!
 
-From speaking with RVers who've attended WDS we've heard: 
-
-1) there's not a lot of good options or nearby campgrounds. 
-2) it would be great to be in the same place with other WDS attendees 
+From speaking with RVers who've attended WDS we've heard, "there aren't a lot of good options or nearby campgrounds" and "it would be great to be in the same place with other WDS attendees." 
 
 As you can imagine, this would be a HUGE feat to accomplish (challenge accepted) but we wanted to see if enough people would be interested in the idea. 
 
 Similar to any campground you would be paying a nightly fee that would help to cover the cost of security and other logistics including fun WDS touches. Because this is very much in the "idea phase" we don't have additional details. 
 
 The important thing to remember that this would be urban (in downtown Portland or really close to it) and it would just be for registered WDS attendees (how fun is that!). 
-  </Block>
+
+P.S. If you're wondering, "what about the WDS Hotel?" (an entire hotel just for attendees). We're looking into that as well.
+
+</Block>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdfNxu7O1SYn85akHPe0CGczG2ypv39UUsyvmJi27uUBJ-vrw/viewform?embedded=true" width="100%" height="1100" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 </Block>
