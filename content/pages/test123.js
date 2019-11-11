eadc0721c9b39596f7942784bdbd80186d85385a
Jedd <jedidiah475@gmail.com>
2019-11-11 10:26:04 +0100

    Update test123.js

--- content/pages/test123.js
+++ content/pages/test123.js
@@ -10,7 +10,7 @@
     ## Help Us Plan WDS 2020 
 As we dream big for the final WDS, help us with our planning by providing your input. Keep checking back for different ways you can assist.
 
-    #### Current Ask: Where Will You Stay in Portland for WDS? 
+    #### Current Ask: Your Feedback for a Big Idea
   </Block>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdfNxu7O1SYn85akHPe0CGczG2ypv39UUsyvmJi27uUBJ-vrw/viewform?embedded=true" width="100%" height="1100" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
