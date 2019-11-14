997fcdfb855355a93d83c15350043ba73492ab75
193countries <chris.guillebeau@gmail.com>
2019-11-14 12:13:07 +1100

    CG copyedits 2

--- content/pages/feedback.js
+++ content/pages/feedback.js
@@ -11,17 +11,17 @@
 As we dream big for the final WDS, we'd love to get your input. Keep checking back for different ways you can assist!
 
     #### FOR NOW: WDS Pop-up RV Campground Feedback
-    We know that there are numerous "RVers" in our community, and we've been playing with the idea of creating our own dedicated site for this community—a WDS Pop-up RV Campground in downtown Portland (RV, tent, van, Airstream rental, etc)!
+    We know that there are numerous "RVers" in our community, and we've been playing with the idea of creating our own dedicated site for this community—a WDS Pop-up RV Campground in downtown Portland (and not just for RVs, but also tents, vans, Airstream rentals, and more)!
 
 From speaking with RVers who've attended WDS, we've heard that there aren't a lot of good options or nearby campgrounds—and of course, we want to avoid having everyone scattered near and far. 
 
-As you can imagine, setting up a large-scale community campground would be a HUGE project. Fortunately we like challenges... but before we took this on, we wanted to see if enough people would be interested in the idea. 
+As you can imagine, setting up a large-scale community campground would be a HUGE project. Fortunately, we like challenges ... but before we take this on, we want to see if enough people would be interested in the idea. 
 
 Similar to any campground, anyone who stays there would pay a nightly fee to cover the cost of security and other logistics. Because this is very much in the "idea phase" we don't have additional details just yet. 
 
-The important thing to remember that this would be <em>centrally located</em> (in downtown Portland or really close to it) and it would just be for registered WDS attendees. What do you think? 
+The important thing to remember that this would be centrally located (in downtown Portland or really close to it) and it would just be for registered WDS attendees. 
 
-P.S. If you're wondering, "What about the WDS Hotel?"—an entire hotel just for attendees—rest assured we're looking into that as well.
+Oh, and if you're wondering, "What about the WDS Hotel?"—an entire hotel just for attendees—rest assured we're looking into that as well.
 
 </Block>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
