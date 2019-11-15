4f2871d7a881ca53e11f31e470cd6e94a6633a76
193countries <chris.guillebeau@gmail.com>
2019-11-15 14:38:16 -0800

    CG copyedits

--- content/pages/feedback-received.js
+++ content/pages/feedback-received.js
@@ -5,7 +5,7 @@
   <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
     <Block width="100%" margin="40px 0 0 0" textAlign="center">## Feedback Received!</Block>
       <p>Thanks for helping us plan WDS 2020 - You're awesome! </p>
-   <p>Our team will lookover all the feedback and let you know if this idea will become a reality or not.</p>  
-<p>Have a question? Our concierge team operates year-round. Don't hesitate to reach out to them by hitting the <b><Link to="https://worlddominationsummit.com/contact-us">bat signal here.</Link></b></p>
+   <p>Our team will consider all the feedback and let you know if this idea will become a reality.</p>  
+<p>Have a question? Our concierge team operates year-round. Don't hesitate to reach out to them by <b><Link to="https://worlddominationsummit.com/contact-us">hitting the bat signal.</Link></b></p>
   </Block>
 </Section>
