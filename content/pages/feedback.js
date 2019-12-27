123cb962394a4c8cf40210e7351b2f4e66c56df3
Jedd <jedidiah475@gmail.com>
2019-12-27 13:37:09 -1000

    Update feedback.js

--- content/pages/feedback.js
+++ content/pages/feedback.js
@@ -10,21 +10,9 @@
     ## Help Us Plan WDS 2020 
 As we dream big for the final WDS, we'd love to get your input. Keep checking back for different ways you can assist!
 
-    #### FOR NOW: WDS Pop-up RV Campground Feedback
-    We know that there are numerous "RVers" in our community, and we've been playing with the idea of creating our own dedicated site for this community—a WDS Pop-up RV Campground in downtown Portland (and not just for RVs, but also tents, vans, Airstream rentals, and more)!
-
-From speaking with RVers who've attended WDS, we've heard that there aren't a lot of good options or nearby campgrounds—and of course, we want to avoid having everyone scattered near and far. 
-
-As you can imagine, setting up a large-scale community campground would be a HUGE project. Fortunately, we like challenges ... but before we take this on, we want to see if enough people would be interested in the idea. 
-
-Similar to any campground, anyone who stays there would pay a nightly fee to cover the cost of security and other important setup and logistics. Unlike a regular campground, this would be a full-on experience and magical transformation of an urban space so take that into consideration when thinking of a nightly fee. Because this is very much in the "idea phase" we don't have additional details just yet. 
-
-The important thing to remember that this would be centrally located (in downtown Portland or really close to it) and it would just be for registered WDS attendees. 
-
-Oh, and if you're wondering, "What about the WDS Hotel?"—an entire hotel just for attendees—rest assured we're looking into that as well.
+    #### Stay Tuned!
+    We'll need your help again soon so please look for the next email announcement. Thanks!
 
 </Block>
-  <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-<iframe height="800" allowTransparency="true" frameborder="0" scrolling="no" style={{ width:'100%', border: 'none'}}  src="https://worlddominationsummit.wufoo.com/embed/m1ddm1nu0711t3x/"><a href="https://worlddominationsummit.wufoo.com/forms/m1ddm1nu0711t3x/">Fill out my Wufoo form!</a></iframe></Block>
 </Section>
 <Import id="join-us" />
