1eb7c203ae361030d2b9bbbb185cd2a8ce8a7046
Jedd <jedidiah475@gmail.com>
2020-01-03 22:25:42 -1000

    Update location.js

--- content/pages/location.js
+++ content/pages/location.js
@@ -84,11 +84,11 @@
 
 </Block>
   <Block cols="8" bleed={false} margin="0 0 80px">
-    <Image src="photos/Duniway-Lobby-WDS-Hotel-Partner.jpg" width="100%" height="350" />
+    <Image src="images/photos/Hotel-Zags-Lobby.jpg" width="100%" height="350" />
   </Block>
   
  <Grid margin="60px 0 -20px 0">
-<Block type="image" src="photos/Partner-Hotels-Location-WDS-2019.png">
+<Block type="image" src="photos/Partner-Hotels-Location-WDS-2020.png">
       ## Perfectly Located
       Each partner hotel is within walking distance or a quick ride on public transit to WDS events and activities and are accessible to the airport by light rail. You have three great options to choose from based on value, ammenities, and location. Just make sure to reserve your room ASAP.   
       
@@ -98,34 +98,34 @@
        
       <Button to="https://worlddominationsummit.com/contact-us" newWindow={true} >Contact the Concierge Team</Button> 
     </Block>
- <Block type="image" src="https://el-files-wds.s3.amazonaws.com/images/photos/paramount-hotel-wds-partner.jpg">
+<Block type="image" src="photos/Hoxton-roomy-view.jpg">
+      ## The WDS Hotel™
+      We've taken over an entire boutique hotel in Portland's Old Town Chinatown. The WDS Hotel™ features a variety of fun room types and social areas including a coffee shop/co-working area and a beautiful rooftop area to take in breathaking Portland views. Don't forget the entire hotel is reserved just for WDS attendees!  
+      
+      ~Limited availability and based on a 4-night package. Use the link below for details.~
+        
+     <p>0.8 Mi From the Main Stage <Link to="https://goo.gl/maps/yAmfYNzFDgRgDk7y7">(Google Directions)</Link></p> 
+        
+    <Button to="" newWindow={true} >Coming Soon!</Button> 
+</Block>
+  <Block type="image" src="images/photos/Hotel-Zags-King.jpg">
+      ## Hotel Zags
+      (Formerly the Hotel Modera) The newly renovated Hotel Zags is a fun and delightful boutique hotel located by the WDS main-stage featuring complimentary wifi, access to a gear shed (bikes, ukuleles, cameras and more), and the popular Nel Centro restaurant. It also comes with passes to 24-Hour Fitness.
+      
+      Use the button below or call **(877)484-1084** and ask for the **"World Domination Summit 2020 Group Rate".**
+      
+        <p>0.3 Mi From the Main Stage <Link to="https://goo.gl/maps/CjCe1a3Tfc8K4YB99">(Google Directions)</Link></p> 
+  <Button to="https://aonc.co/2tstT4q" newWindow={true} >Coming Soon</Button> 
+</Block>
+<Block type="image" src="https://el-files-wds.s3.amazonaws.com/images/photos/paramount-hotel-wds-partner.jpg">
       ## Paramount Hotel 
       The Paramount Hotel is a casual, intimate boutique hotel boasting oversized guestrooms and genuine hospitality. Each guestroom and suite features relaxing décor, spacious marble finished bathrooms and modern amenities. The Paramount provides easy access to the very best the city has to offer.
       
-      Use the button below or call **(503)223-9900**. Make sure to reference WDS 2019 for the special group rate.
+      Use the button below or call **(503)223-9900**. Make sure to reference **WDS20** for the special group rate.
       <p>0.1 Mi From the Main Stage <Link to="https://goo.gl/maps/VwXGqjMYw9B2">(Google Directions)</Link></p> 
         
-     <Button to="https://www.portlandparamount.com/" newWindow={true} >The Discount Period Has Ended</Button> 
+     <Button to="https://aonc.co/2ZNALWa" newWindow={true} >Coming Soon!</Button> 
  </Block>
-  <Block type="image" src="photos/Duniway-portland-WDS-partner.jpg">
-      ## Duniway Hotel
-      This modern boutique hotel offers spacious rooms and suites, complimentary wifi (for WDS attendees), 65-inch TVs, and unique eclectic decor. Workout and/or swim onsite in the heated indoor waveless lap pool, or relax on the 11th floor outdoor patio with friends. Attention travel hackers, did we mention it's a Hilton?  
-      
-      At the top of the booking page, look for **"more options"**, then **"add special rate code"**. Enter **WDS** into the **Group Code**. 
-      
-        <p>0.2 Mi From the Main Stage <Link to="https://goo.gl/maps/YYvbGjiuiMN2">(Google Directions)</Link></p> 
-  <Button to="http://www3.hilton.com/en/hotels/oregon/the-duniway-portland-a-hilton-hotel-PDXTPHH/index.html" newWindow={true} >The Discount Period Has Ended</Button> 
-</Block>
- <Block type="image" src="photos/Hoxton-roomy-view.jpg">
-      ## The Hoxton Hotel
-      A new boutique hotel near Portland's Old Town Chinatown. The Hoxton features a variety of room types, cleverly designed with mid-century touches, free breakfast bag, and complimentary wifi. Don't forget to explore the property's fun and cozy communal spaces including a rootop taqueria and bar, a cafe co-working area, onsite restaurant, and basement bar.
-      
-      ~Call **(503)770-0500** and make sure to reference **WDS 2019** for the special group rate.~
-        
-     <p>0.7 Mi From the Main Stage <Link to="https://goo.gl/maps/CQtH6AnvrR12">(Google Directions)</Link></p> 
-        
-    <Button to="https://thehoxton.com/oregon/portland/hotels" newWindow={true} >The Discount Period Has Ended</Button> 
-</Block>
   </Grid>
   
     <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
