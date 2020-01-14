c581e2c12770ca1b1b73048b273127bb3cbbe202
Jedd <jedidiah475@gmail.com>
2020-01-14 13:57:18 -0800

    Update hotel.js

--- content/pages/hotel.js
+++ content/pages/hotel.js
@@ -137,7 +137,7 @@
       </Block>
       <Grid numCols="3" tabletCols="2" width="968">
         <Block type="image-overlay" anchor="right" src="wds-hotel/suite-room1.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
-          ## Private Suite & Bathroom
+          ## Private Suite & Bathroom (SOLD OUT)
          Spread out with a bit more space for yourself and your own private bathroom in one of our private suites. The private suite is a refuge for those who want a more traditional hotel room with king size bed, more space, more views, and the privacy of your own bath. **(12 available suites)** 
          **4-Night Package: $947** (Max occupancy: 2 persons)
         </Block>
@@ -147,7 +147,7 @@
         **4-Night Package: $747** (max occupancy: 2 persons)
         </Block>
         <Block type="image-overlay" anchor="right" src="wds-hotel/Bunk-room1.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
-          ## Bunk Bed & Shared Bathroom
+          ## Bunk Bed & Shared Bathroom (SOLD OUT)
           <p>Get the most bed for your buck in one of our hostel-style bunks in the bunk room. With the custom-built triple-tall bunks, you’ll get both community and privacy. Each bunk has a storage locker, charging station, reading light, and privacy curtain. Full-size luxury mattresses and quiet hours ensure you’ll get solid night’s sleep. Plus, you’ll get the opportunity to make new friends or grow closer with old ones, as you share time together at the community table or kitchenette area. <i>Please Note: Upper bunks require climbing a ladder.</i> <b>(23 available bunks)</b> </p>  
           **4-Night Package: $347** (1 bed per person)
           </Block>
