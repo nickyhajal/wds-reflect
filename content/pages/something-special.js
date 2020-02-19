f8a440c37dbe294cf47ae1e0bf2472b68e2147aa
Jedd <jedidiah475@gmail.com>
2020-02-19 08:24:50 -1000

    Update something-special.js

--- content/pages/something-special.js
+++ content/pages/something-special.js
@@ -1,13 +1,13 @@
 <Section
-  headerImage="hero/balls.jpg"
+  headerImage="v2/hero/story.png"
   color="blue"
 >
   <Block cols="8" bleed={false} margin="0 0 80px">
-      ## Help Us Create Something Special
+      ## Help Us Make Something Special
 
     <Block cols="6" bleed={false} textAlign="left" margin="10px 0 80px" background="canvas" type="padded">
       <h3 style={{lineHeight: '140%', marginBottom: '18px'}}>
-        This year we want to do something a bit different, but we need your participation!
+        We have an idea for WDS 2020, but we need your help!
       </h3>
       <div style={{fontSize: '16px', marginBottom: '12px', lineHeight: '140%'}}>
         Enter a word or short phrase you want to be reminded of each day<br/>(max: 11 characters).
