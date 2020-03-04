427009406a4404d9cf2033730654173196542b72
Jedd <jedidiah475@gmail.com>
2020-03-04 09:42:17 -0800

    Update schedule.js

--- content/pages/schedule.js
+++ content/pages/schedule.js
@@ -40,141 +40,44 @@ Click on the button for a step-by-step guide on how to prepare and maximize your
 </Block>
 </Section>
   <Section color="canvas">
-  <Block>## Speakers and Special Guests For WDS 2019</Block>
+  <Block>## Speakers and Special Guests For WDS 2020</Block>
   <Block cols="6" className="bigger">
     Every year we're excited to invite several friends of WDS living extraordinary lives and working to build a better world. Our keynote speakers are bestselling authors, leading activists, and in-demand storyteller and they come to WDS to share their stories, experiences, and lessons with our community. 
     
-    Check out this superhero lineup from WDS 2019:
+    Check out this superhero lineup for WDS 2020:
   </Block>
   <Tabs style="simple" margin="80px 0 0">
-    <Tab title="Jill">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Jill-Schlesinger-WDS-2019-speaker.jpg" width="904" height="549" margin="0 auto 60px" mobile={{ all: { width: '100%' }}} preload/>
+    <Tab title="Arlan">
+      <Image src="https://el-files-wds.s3.amazonaws.com/images/speakers/Arlan-Hamilton.WDS.2020.speaker.png" width="904" height="549" margin="0 auto 60px" mobile={{ all: { width: '100%' }}} preload/>
       <Block cols="6" textAlign="left">
-        ## Jill Schlesinger
-      **Jill Schlesinger** CFP®, is the Emmy-nominated and Gracie Award Winning Business Analyst for CBS News. Jill covers the economy, markets, investing and all other things money related on TV, and the “Jill on Money” podcast, radio show and blog. 
-      
-      Jill’s radio show won the 2018 Gracie Award for Best National Talk Show. Jill’s first book, *"The Dumb Things Smart People Do With Their Money,"* was recently released.
+        ## Arlan Hamilton
+      **Arlan Hamilton** built a venture capital fund from the ground up, while homeless. She is the Founder and Managing Partner of **[Backstage Capital](https://backstagecapital.com/)**, a fund that is dedicated to minimizing funding disparities in tech by investing in high-potential founders who are people of color, women, and/or LGBT. 
+
+Started from scratch in 2015, Backstage has now raised more than $7 million and invested in more than 130 startup companies led by underestimated founders. In 2018 Arlan co-founded Backstage Studio which launched four accelerator programs for underestimated founders in Detroit, Los Angeles, Philadelphia, and London.
+
 </Block>
     </Tab>
- <Tab title="James">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/James-Victore-WDS-2019-Speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
+ <Tab title="Colin">
+      <Image src="https://el-files-wds.s3.amazonaws.com/images/speakers/ColinOBrady.WDS.2020.speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
       <Block cols="6" textAlign="left">
-        ## James Victore
-        **James Victore** is a graphic designer, art educator and dynamic speaker on creativity. He lectures and teaches regularly around the globe, inspiring people to illuminate their individual gifts in order to achieve personal greatness.
+        ## Colin O'Brady
+        **Colin O’Brady** is a world record holding explorer and one of the world’s best endurance athletes. Colin is regarded as a foremost expert on mindset. 
 
-James’ work has been exhibited at the Museum of Modern Art in New York and are in the permanent collections of the Palais du Louvre, Paris, the Library of Congress, Washington, DC, the Design Museum in Zurich and the Stedelijk Museum, Amsterdam. 
+He isn’t your typical athlete despite his unmatched athletic accomplishments - a world first solo crossing of Antarctica, a world first row across Drake Passage, summiting Mt. Everest, and setting three prestigious mountaineering world records - and he has done it all after overcoming a devastating accident to prove that anything is possible.
+
       </Block>
     </Tab>  
- <Tab title="Laura">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Laura-Vanderkam-WDS-2019-Speaker.jpg" width="904" height="549" mobile={{ all: { width: '100%' }}} margin="0 auto 60px" preload/>
+ <Tab title="Paloma">
+      <Image src="https://el-files-wds.s3.amazonaws.com/images/speakers/PalomaMedina.WDS.2020.speaker.png" width="904" height="549" mobile={{ all: { width: '100%' }}} margin="0 auto 60px" preload/>
       <Block cols="6" textAlign="left">
-        ## Laura Vanderkam
+        ## Paloma Medina
                                                                                                                                               
-        **Laura Vanderkam** is the author of several time management and productivity books, including *Off the Clock*, *I Know How She Does It*, *What the Most Successful People Do Before Breakfast*, and *168 Hours*. Her work has appeared in publications including the *New York Times, the Wall Street Journal, Fast Company,* and *Fortune*.
+        **Paloma Medina** is a performance coach, speaker, and founder of **[11:11 Supply](https://www.1111supply.com/)**, a retail startup that specializes in beautiful work tools designed to help you feel more productive, happy and balanced in your everyday life. 
 
-She is the co-host, with Sarah Hart-Unger, of the podcast Best of Both Worlds. She lives outside Philadelphia with her husband and four children, and blogs at [LauraVanderkam.com](LauraVanderkam.com).
+Her background includes working with tech companies, homeless healthcare clinics, as well as individual leaders and CEOs. She was born in Guadalajara, Mexico and has made a home since on the West Coast since she was 8 years old.
+
           </Block>
     </Tab>
-   <Tab title="Nate">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Nate-Staniforth-WDS-2019-Speaker.png" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Nate Staniforth                                                                                                                            
-        **Nate Staniforth** is a magician, writer, traveler, and host of the Discovery Channel’s international hit TV series Breaking Magic. Nate abandons the ubiquitous style-without-substance bravado so often associated with magic and appeals instead to the imagination and intellect of his audience. The journey is wild, visceral and immediate, and like all great art, encourages us to open our minds and hearts, and see the world in new ways.
-
-For over a decade, Nate has toured the US college circuit as one of the busiest working magicians in the country. He’s given a TED Talk, lectured at the world-famous Oxford Union, and in 2018 the Harry Potter-famed Bloomsbury Publishing released Nate’s memoir, *Here is Real Magic*.
-      </Block>
-    </Tab> 
-   <Tab title="Humble">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Humble-The-Poet-WDS-2019-Speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Humble the Poet                                                                                                                           
-        **Kanwer Singh AKA Humble the Poet** is a Toronto-bred MC/spoken word artist with an aura that embodies the diversity and resiliency of one of the world's most unique cities. 
-
-With tattoos, beard, head wrap and a silly smile, Humble commands attention. He stimulates audiences with ideas that challenge conventional wisdom and go against the grain, with dynamic live sets that shake conventions and minds at the same time.  
-
-Humble's latest EP titled Righteous/Ratchet features the first single H.A.I.R, a celebration of women of all shapes, sizes, hair and walks of life. His self directed video for H.A.I.R has amassed almost 2 million views since its release. Humble's first book Unlearn: 101 Simple Truths For A Better Life was published through Indigo Press. Learn more about Humble the Poet on his website, **[humblethepoet.com](https://www.humblethepoet.com/)**.
-      </Block>
-    </Tab>
-<Tab title="Tania">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Tania-Katan-WDS-2019-speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Tania Katan
-                                                                                                                                              
-        **Tania Katan** is an inspirational speaker, author, creative consultant and co-creator of the #ItWasNeverADress campaign. Tania’s work has been featured in _The New York Times, USA Today, HuffPost, Time, Adweek, BuzzFeed, Mashable,_ and _CNN._
-
-Tania travels the world showing people and organizations how to disrupt the mindsets keeping us stuck and start a creative revolution from their cubicles.
-
-For more of Tania’s productive disruptions visit [taniakatan.com](www.taniakatan.com).
-      </Block>
-    </Tab>
-<Tab title="Trystan">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Trystan-Reese-WDS-Speaker.png" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Trystan Reese                                                                                                                            
-        **Trystan Reese** is perhaps best known as “the pregnant man” of internet and television fame. But he would prefer to be known as the adoptive father of Hailey and Riley, gestational father of Leo, all-around awesome trans community member, and partner to the amazing Biff Chaplow. 
-
-He lives in Portland, OR and works as the Director of Family Formation at Family Equality Council. He can be found on Facebook and Instagram as **@biffandi** or at [www.biffandi.com](https://www.biffandi.com/).
-      </Block>
-    </Tab> 
-<Tab title="Marsha">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Marsha-Shandur-WDS-2019-Speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Marsha Shandur                                                                                                                           
-        **Marsha Shandur** understands that telling stories can change the world. She has coached hundreds of storytellers across the globe, drawing their personal stories out and taking them from confusing mess to compelling piece.
-
-As the founder of **[YesYesMarsha.com](http://www.yesyesmarsha.com/), she shows entrepreneurs and organizations how to make an INSTANT emotional connection with their dream clients and collaborators, through storytelling on stage and screen, and effective (and FUN!) networking. She has been featured on Forbes, the BBC, Mashable and The Muse.
-
-      </Block>
-    </Tab>
-<Tab title="Scott">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Scott-Young.WDS-2019-Speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Scott Young                                                                                                                         
-        **Scott Young** is a writer, programmer, traveler and avid reader of interesting things. For the last ten years he’s been experimenting to find out how to learn and think better. Beyond writing, entrepre- neurship and life philosophy, his interests include programming, travel, cooking and teaching himself anything he can. 
-
-Scott’s new book, _Ultralearning: Master Hard Skills, Outsmart the Competition, and Accelerate Your Career_ will be released later this summer.
-      </Block>
-    </Tab>
-    <Tab title="Nadya">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Nadya-Okamoto-WDS-2019-Speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Nadya Okamoto
-                                                                                                                                              
-       **Nadya Okamoto** grew up in Portland, OR, and is a 21-year-old Harvard student on a leave of absence. She is the Founder and Executive Director of PERIOD ([period.org](https://period.org)), an organization she founded at the age of 16. PERIOD is now the largest youth-run NGO in women’s health, and one of the fastest growing ones here in the United States. Since 2014 they have addressed over 500,000 periods and registered over 300 campus chapters.
-      
-      In 2017, Nadya ran for office in Cambridge, MA. While she did not win, her campaign team made historic waves in mobilizing young people on the ground and at polls. Nadya recently published her debut book, *Period Power: A Manifesto for the Menstrual Movement*, with publisher Simon & Schuster, which made the Kirkus Reviews list for Best Young Adult Nonfiction of 2018. Most recently, Nadya has become the Chief Brand Officer of JUV Consulting, a Generation Z marketing agency based in NYC. 
-          </Block>
-    </Tab>
-     <Tab title="Paloma">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Paloma+Medina+WDS+Academy-wide.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Paloma Medina (Featured Academy Leader)
-                                                                                                                                              
-       **Paloma Medina** is a performance coach, trainer, and owner of [11:11 Supply](https://www.1111supply.com/), a store that specializes in beautiful work tools designed to help you feel more productive, happy and balanced in your everyday life. In addition to offering practical tools and frameworks, Paloma's workshops provide audiences with a solid foundation of neuroscience and psychology on how to improve our work and personal lives. 
-       
-       Her workshop topics range from equity and inclusion to the science of goals and stress. She has worked with tech companies such as Etsy and Digital Ocean, nonprofit organizations, homeless healthcare clinics, as well as individual leaders and CEOs. She was born in Guadalajara, Mexico but has made a home on the West Coast since she was 8 years old. 
-          </Block>
-    </Tab>
- <Tab title="Matthew">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/foundation/Matthew-Goins-WDS-Puzzle-Huddle.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Matthew Goins
-
-**Matthew Goins** wanted his children to see themselves in the toys they played with. Like many children, Matthew’s kids love jigsaw puzzles but he and his wife struggled with the lack of representative images in commercially-produced products. 
-
-They began creating their own puzzle designs featuring African American characters in positive images. Their company, **[Puzzle Huddle](http://www.puzzlehuddle.com)**, develops and produces a line of culturally inclusive images manufactured as puzzles. 
-      </Block>
-    </Tab>
-     <Tab title="Rick">
-      <Image src="https://s3.amazonaws.com/el-files-wds/images/speakers/Rick-Clemons.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
-      <Block cols="6" textAlign="left">
-        ## Rick Clemons
-
-**Rick Clemons** is a culture disruptor (shakes up the status quo), closet buster (shatters silly beliefs), and a bold move expert (like Superman without the muscles and cape). 
-
-His superpower is guiding closet dwellers - which is every human - to break free of fear, make bold moves, and live life without apology.
-      </Block>
-    </Tab>   
   </Tabs>
   <div class="clear" />
 </Section>  
@@ -183,7 +86,7 @@ His superpower is guiding closet dwellers - which is every human - to break free
   color="white"
 >
     
-  <SpeakerList title="2019 Speakers" year="2019" />
+  <SpeakerList title="2020 Speakers" year="2020" />
   <SpeakerList title="Alumni Speakers" year="alumni" />
   <Grid margin="60px 0 -20px 0">
     <Block type="image" src="photos/Portland-Oregon.png" label="Portlandia">
