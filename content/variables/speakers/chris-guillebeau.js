a3bf1d7db131f34157259ee06dff03b559eec4a8
Jedd <jedidiah475@gmail.com>
2020-03-04 09:54:23 -0800

    Update chris-guillebeau.js

--- content/variables/speakers/chris-guillebeau.js
+++ content/variables/speakers/chris-guillebeau.js
@@ -7,10 +7,8 @@ quote: "",
 ---
 **Chris Guillebeau** is a New York Times bestselling author and modern-day explorer.  
 
-During a lifetime of self-employment that included a four-year commitment as a volunteer executive in West Africa, he visited every country in the world (193 in total) before his 35th birthday. Since then he has modeled the proven definition of an entrepreneur: “Someone who will work 24 hours a day for themselves to avoid working one hour a day for someone else.”  
+<p>During a lifetime of self-employment that included a four-year commitment as a volunteer executive in West Africa, he visited every country in the world (193 in total) before his 35th birthday. Since then he has modeled the proven definition of an entrepreneur: “Someone who will work 24 hours a day for themselves to avoid working one hour a day for someone else.”</p> 
 
-Chris’s first book, The Art of Non-Conformity, was translated into more than twenty languages. His second book, The $100 Startup, was a New York Times and Wall Street Journal bestseller, selling more than 500,000 copies worldwide. His third book, The Happiness of Pursuit, was published in September 2014 and was also a New York Times bestseller. His fourth book, Born for This, will help you find the work you were meant to do. His newest book, Side Hustle, will help you create a new source of income in 27 days.  
+<p>Chris’s first book, The Art of Non-Conformity, was translated into more than twenty languages. His second book, The $100 Startup, was a New York Times and Wall Street Journal bestseller, selling more than 500,000 copies worldwide. His third book, The Happiness of Pursuit, was published in September 2014 and was also a New York Times bestseller. His newest book, The Money Tree, is a story about the power you have to create your own financial destiny.</p> 
 
-His daily podcast, Side Hustle School, is downloaded more than 2 million times a month.  
-
-Every summer in Portland, Oregon, Chris hosts the World Domination Summit, a gathering of creative, remarkable people with thousands in attendance. Chris is also the founder of Pioneer Nation, Unconventional Guides, the Travel Hacking Cartel, and numerous other projects.
+<p>His daily podcast, Side Hustle School, is downloaded more than 2 million times a month.</p>  
