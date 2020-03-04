813253ec00d8862de76f8c7f463773483986d67a
Jedd <jedidiah475@gmail.com>
2020-03-03 20:38:11 -0800

    Create paloma-medina.js

---
+++ content/variables/speakers/paloma-medina.js
@@ -0,0 +1,11 @@
+name: "Paloma Medina",
+year: "2020",
+headerPhotos: "https://el-files-wds.s3.amazonaws.com/images/speakers/PalomaMedina.WDS.2020.speaker.png",
+photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/PalomaMedina-Thumbnail.png",
+video: "",
+headerPosition: "-20px",
+quote: "",
+---
+**Paloma Medina** is a performance coach, speaker, and founder of 11:11 Supply, a retail startup that specializes in beautiful work tools designed to help you feel more productive, happy and balanced in your everyday life. 
+
+Her background includes working with tech companies, homeless healthcare clinics, as well as individual leaders and CEOs. She was born in Guadalajara, Mexico and has made a home since on the West Coast since she was 8 years old.
