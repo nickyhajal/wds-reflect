0d93206e9348387f890631fec5a843c24f6f1ffd
Jedd <jedidiah475@gmail.com>
2020-03-03 20:55:08 -0800

    Update and rename testhotel.js to spkr20.js

---
+++ content/pages/spkr20.js
@@ -0,0 +1,112 @@
+<Section
+  headerImage="v2/hero/schedule.png"
+  headerClip="rl_header"
+  headerPosition="top"
+  headerSize="thin"
+  color="white"
+  preload
+>
+  <Image src="v2/headings/schedule.png" width="421" height="151" className="scriptHeader" preload/>
+<Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+    ## How the WDS Schedule Works
+    As we've done since year one, the main events (main-stage keynotes and celebrations) take place during the weekend of WDS. Additionally, every attendee will have the ability to add events and activities throughout the entire week using the WDS mobile app to create their own personalized schedule. 
+
+You decide if you'll join us for the weekend or come for the entire week. Regardless, a WDS ticket gives you access to it all: main events, special discounts, bonuses, and surprises.  
+        </Block>
+ <Image src="photos/audience.png"
+    width="426" height="263"
+    margin="0 0 -100px -50px"
+    mobile={{ phone: { margin: '0 0 -100px -20px', width: '100%' } }}
+    preload
+  />
+  <Block type="padded" cols="6" bleed={false} background="canvas" className="table-list" css={{ padding: '60px 40px', position: 'relative', zIndex:'4' }} clip="top_rl_1.6">
+    ## A Week of WDS: June 23rd - June 29th, 2020
+
+    - **Tuesday (6/23)** Activities at WDS HQ and Attendee-Led Meetups
+    - **Wednesday (6/24)** Academies, Activities at WDS HQ and Attendee-Led Meetups
+    <ul><li><b className="highlight highlight-2">Thursday (6/25)</b> Academies, Activities, and Attendee-Led Meetups, and Opening Party</li></ul>
+    <ul><li><b className="highlight highlight-2">Friday (6/26)</b>  Academies, Activities, Attendee-Led Meetups, World Record Attempt</li></ul>
+    <ul><li><b className="highlight highlight-2">Saturday(6/27)</b> Main Stage Keynotes, Attendee-Led Meetups, and Evening Activities</li></ul>
+    <ul><li><b className="highlight highlight-3">Sunday (6/28)</b> Main Stage Keynotes, Attendee-Led Meetups, and the Closing Party</li></ul>
+    - **Monday (6/29)** Attendee-Led Meetups & Final Farewells
+      <div className="note">Can't come for the whole time? No problem, but you don't want to miss the main events taking place on Thursday (evening), Friday, Saturday, and Sunday. For the best possible WDS experience we recommend arriving no later than Thursday afternoon, and leaving no earlier than Monday morning.</div>
+  </Block>
+<Image src="photos/jellyfish.png" width="426" height="263" margin="-50px -60px 100px 0" css={{ float: 'right' }} mobile={{ all: { margin: '-50px -15px 100px 0' } }} />
+  <div className="clear" />
+<Block cols="6" bleed={false} textAlign="center" margin="80px 0 80px">
+## Registered for WDS 2020? Start here!
+Click on the button for a step-by-step guide on how to prepare and maximize your WDS experience.
+<Button to="https://worlddominationsummit.com/guide" style={{ fontSize: '22px', padding: '18px 100px', marginTop: '80px', marginBottom: '40px' }} fitToText>WDS 2020 Guide</Button>
+</Block>
+</Section>
+  <Section color="canvas">
+  <Block>## Speakers and Special Guests For WDS 2020</Block>
+  <Block cols="6" className="bigger">
+    Every year we're excited to invite several friends of WDS living extraordinary lives and working to build a better world. Our keynote speakers are bestselling authors, leading activists, and in-demand storyteller and they come to WDS to share their stories, experiences, and lessons with our community. 
+    
+    Check out this superhero lineup for WDS 2020:
+  </Block>
+  <Tabs style="simple" margin="80px 0 0">
+    <Tab title="Arlan">
+      <Image src="https://el-files-wds.s3.amazonaws.com/images/speakers/Arlan-Hamilton.WDS.2020.speaker.png" width="904" height="549" margin="0 auto 60px" mobile={{ all: { width: '100%' }}} preload/>
+      <Block cols="6" textAlign="left">
+        ## Arlan Hamilton
+      **Arlan Hamilton** built a venture capital fund from the ground up, while homeless. She is the Founder and Managing Partner of **[Backstage Capital](https://backstagecapital.com/)**, a fund that is dedicated to minimizing funding disparities in tech by investing in high-potential founders who are people of color, women, and/or LGBT. 
+
+Started from scratch in 2015, Backstage has now raised more than $7 million and invested in more than 130 startup companies led by underestimated founders. In 2018 Arlan co-founded Backstage Studio which launched four accelerator programs for underestimated founders in Detroit, Los Angeles, Philadelphia, and London.
+
+</Block>
+    </Tab>
+ <Tab title="Colin">
+      <Image src="https://el-files-wds.s3.amazonaws.com/images/speakers/ColinOBrady.WDS.2020.speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
+      <Block cols="6" textAlign="left">
+        ## Colin O'Brady
+        **Colin O’Brady** is a world record holding explorer and one of the world’s best endurance athletes. Colin is regarded as a foremost expert on mindset. 
+
+He isn’t your typical athlete despite his unmatched athletic accomplishments - a world first solo crossing of Antarctica, a world first row across Drake Passage, summiting Mt. Everest, and setting three prestigious mountaineering world records - and he has done it all after overcoming a devastating accident to prove that anything is possible.
+
+      </Block>
+    </Tab>  
+ <Tab title="Paloma">
+      <Image src="https://el-files-wds.s3.amazonaws.com/images/speakers/PalomaMedina.WDS.2020.speaker.png" width="904" height="549" mobile={{ all: { width: '100%' }}} margin="0 auto 60px" preload/>
+      <Block cols="6" textAlign="left">
+        ## Paloma Medina
+                                                                                                                                              
+        **Paloma Medina** is a performance coach, speaker, and founder of 11:11 Supply, a retail startup that specializes in beautiful work tools designed to help you feel more productive, happy and balanced in your everyday life. 
+
+Her background includes working with tech companies, homeless healthcare clinics, as well as individual leaders and CEOs. She was born in Guadalajara, Mexico and has made a home since on the West Coast since she was 8 years old.
+
+          </Block>
+    </Tab>
+  </Tabs>
+  <div class="clear" />
+</Section>  
+   
+<Section
+  color="white"
+>
+    
+  <SpeakerList title="2020 Speakers" year="2020" />
+  <SpeakerList title="Alumni Speakers" year="alumni" />
+  <Grid margin="60px 0 -20px 0">
+    <Block type="image" src="photos/Portland-Oregon.png" label="Portlandia">
+      ## Portland
+      Our home base since year one. Learn more about our beloved city and confirm whether or not all the fun things you've heard is true (they probably are).
+      <Button to="/location">Learn More</Button>
+    </Block>
+    <Block type="image" src="photos/JoinUs/image-academies.jpg" label="Coming Soon">
+      ## Academies
+      These half-day workshops feature some of our favorite alumni speakers and select WDS community members that are experts in their fields.
+      <Button to="/academies">Learn More</Button>
+    </Block>
+  </Grid>
+  <Block type="padded" background="bgry">
+    <Image src="v2/icon/outline-bed.png" width="100" height="100"  align="center" margin="0 0 40px 0" />
+        <Block cols="5">
+      ## How Can We Help? 
+      Have questions about WDS? Trying to find the best donut in Portland? Looking for a high-five? Let our concierge team know how we can help make your WDS experience great.
+           <Button to="https://worlddominationsummit.com/contact-us" newWindow={true} styling="orange" fitToText>Contact Us</Button> 
+    </Block>
+  </Block>
+</Section>
+<Import id="join-us" />
