35812bf3225ec36abdb7ffded4a9caa7cd7656e9
Jedd <jedidiah475@gmail.com>
2020-03-04 09:56:56 -0800

    Update chris-guillebeau.js

--- content/variables/speakers/chris-guillebeau.js
+++ content/variables/speakers/chris-guillebeau.js
@@ -5,10 +5,10 @@ photo: "https://s3.amazonaws.com/el-files-wds/images/photos/Chris+Guillebeau.jpg
 video: "",
 quote: "",
 ---
-**Chris Guillebeau** is a New York Times bestselling author and modern-day explorer.  
+**Chris Guillebeau** is a New York Times bestselling author and modern-day explorer.
 
-<p>During a lifetime of self-employment that included a four-year commitment as a volunteer executive in West Africa, he visited every country in the world (193 in total) before his 35th birthday. Since then he has modeled the proven definition of an entrepreneur: “Someone who will work 24 hours a day for themselves to avoid working one hour a day for someone else.”</p> 
+During a lifetime of self-employment that included a four-year commitment as a volunteer executive in West Africa, he visited every country in the world (193 in total) before his 35th birthday. Since then he has modeled the proven definition of an entrepreneur: “Someone who will work 24 hours a day for themselves to avoid working one hour a day for someone else.”
 
-<p>Chris’s first book, The Art of Non-Conformity, was translated into more than twenty languages. His second book, The $100 Startup, was a New York Times and Wall Street Journal bestseller, selling more than 500,000 copies worldwide. His third book, The Happiness of Pursuit, was published in September 2014 and was also a New York Times bestseller. His newest book, The Money Tree, is a story about the power you have to create your own financial destiny.</p> 
+Chris’s first book, The Art of Non-Conformity, was translated into more than twenty languages. His second book, The $100 Startup, was a New York Times and Wall Street Journal bestseller, selling more than 500,000 copies worldwide. His third book, The Happiness of Pursuit, was published in September 2014 and was also a New York Times bestseller. His newest book, The Money Tree, is a story about the power you have to create your own financial destiny.
 
-<p>His daily podcast, Side Hustle School, is downloaded more than 2 million times a month.</p>  
+His daily podcast, Side Hustle School, is downloaded more than 2 million times a month.
