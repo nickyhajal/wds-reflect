5104b5b19d33b3a7e413c480805aa730f9c05284
Jedd <jedidiah475@gmail.com>
2020-03-03 20:16:49 -0800

    Create arlan-hamilton.js

---
+++ content/variables/speakers/arlan-hamilton.js
@@ -0,0 +1,11 @@
+name: "Arlan Hamilton",
+year: "2020",
+headerPhotos: "https://el-files-wds.s3.amazonaws.com/images/speakers/Arlan-Hamilton.WDS.2020.speaker.jpg",
+photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Arlan-Hamilton-Thumbnail.png",
+video: "",
+headerPosition: "-20px",
+quote: "",
+---
+**Arlan Hamilton** built a venture capital fund from the ground up, while homeless. She is the Founder and Managing Partner of **[Backstage Capital](https://backstagecapital.com/)**, a fund that is dedicated to minimizing funding disparities in tech by investing in high-potential founders who are people of color, women, and/or LGBT. 
+
+Started from scratch in 2015, Backstage has now raised more than $7 million and invested in more than 130 startup companies led by underestimated founders. In 2018 Arlan co-founded Backstage Studio which launched four accelerator programs for underestimated founders in Detroit, Los Angeles, Philadelphia, and London.
