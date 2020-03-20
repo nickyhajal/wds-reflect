db5538929664e60b6b9e4b08d8f5e1692c6cc157
Jedd <jedidiah475@gmail.com>
2020-03-19 21:23:01 -0500

    Create z031920.js

---
+++ content/pages/z031920.js
@@ -0,0 +1,25 @@
+<Section
+  headerImage="v2/hero/local.png"
+  headerClip="rl_header"
+  headerPosition="top"
+  headerSize="thin"
+  color="white"
+  preload
+>
+  <Image src="v2/headings/community.png" width="565px" height="169px" className="scriptHeader" preload/>
+  <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+    ## WDS Community Call #1 
+For everyone who _was_ able to join (our apologies again), thank you so much! I wasn't sure if people would want to hang out online ... but it seems like many of us do. :) 
+
+<p>Accordingly, we're going to plan another one of these calls soon, and perhaps some kind of structure for those who would like to connect by video call on a more regular basis.</p>  
+
+<p>Until then, take care of yourself over there ... we need you!</p>
+
+<p>Yours in sanitization,</p>
+
+<p>Chris Guillebeau</p>
+
+  </Block>
+<Video placeholder="photos/academies2.jpg" videoId="121164251" cols="8" /> 
+</Section>
+<Import id="join-us" />
