031d54315326377dcd9237141f9bbd571cf3603a
Jedd <jedidiah475@gmail.com>
2020-03-20 06:55:08 -0500

    Update z031920.js

--- content/pages/z031920.js
+++ content/pages/z031920.js
@@ -17,7 +17,7 @@ I wasn't sure if people would want to hang out online ... but it seems like many
 
 <Video placeholder="https://el-files-wds.s3.amazonaws.com/images/photos/checkin1-31920.png" videoautoplay="true" videoId="399025561" width="710" height="430"/>
  <p>
-    Here's a PDF with all of the text from the chatroom. Fair warning, it's long!: <a href="https://el-files-wds.s3.amazonaws.com/docs/communitycallcheckin1.pdf" target="_blank">Community Call Chat Room Text (PDF)</a> 
+    Here's a PDF with all of the text from the group chat: <a href="https://el-files-wds.s3.amazonaws.com/docs/communitycallcheckin1.pdf" target="_blank">Community Call Chat Room Text (PDF)</a> 
       </p>
   </Block>
 </Section>
