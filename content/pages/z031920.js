f8dc928d4e5e4b8a57b2efe38d50d5810b27da9d
Jedd <jedidiah475@gmail.com>
2020-03-19 21:59:50 -0500

    Update z031920.js

--- content/pages/z031920.js
+++ content/pages/z031920.js
@@ -9,15 +9,12 @@
   <Image src="https://el-files-wds.s3.amazonaws.com/images/heading/community.png" width="565px" height="169px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
     ## WDS Community Call #1 
-For everyone who _was_ able to join (our apologies again), thank you so much! I wasn't sure if people would want to hang out online ... but it seems like many of us do. :) 
+I wasn't sure if people would want to hang out online ... but it seems like many of us do. :) 
 
 <p>Accordingly, we're going to plan another one of these calls soon, and perhaps some kind of structure for those who would like to connect by video call on a more regular basis.</p>  
 
 <p>Until then, take care of yourself over there ... we need you!</p>
 
-<p>Yours in sanitization,</p>
-
-<p>Chris Guillebeau</p>
 <Video placeholder="https://el-files-wds.s3.amazonaws.com/images/photos/checkin1-31920.png" videoautoplay="true" videoId="399025561" width="710" height="430"/>
 </Block>
 </Section>
