2b821c9559f42ee76acce296ee8018c23414eac5
Jedd <jedidiah475@gmail.com>
2020-03-31 14:47:48 -0700

    Create announcement.js

---
+++ content/pages/announcement.js
@@ -0,0 +1,49 @@
+<Section
+  headerImage="v2/hero/local.png"
+  headerClip="rl_header"
+  headerPosition="top"
+  headerSize="thin"
+  color="white"
+  preload
+>
+  <Image src="https://cascade.madmimi.com/promotion_images/1478/1610/original/2014-WDS-question.jpg?1585590463" width="565px" height="169px" className="scriptHeader" preload/>
+  <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+    <Video placeholder="https://el-files-wds.s3.amazonaws.com/images/photos/checkin1-31920.png" videoautoplay="true" videoId="402725641" width="710" height="430"/>
+    
+    Dear WDS Community,  
+    
+    Greetings from WDS HQ! I wanted to bring you this important update as soon as we had the information.  
+    
+    As we continue to monitor the global situation, we've come to the decision to **postpone WDS 2020 until the summer of 2021**. This wasn't an easy decision, but we feel it's the right one given the ever-changing situation and uncertainty.  
+    
+    We want to make sure that when we gather again to celebrate (and we will gather again), we'll do so under the best possible circumstances. As I’m sure you’re aware, more and more it’s looking like this summer is not the best time.  
+    
+    ## Important: WDS is Not Canceled. 
+    
+    We remain committed to hosting a final WDS. This event is important to all of us, and the entire core team is dedicated to seeing it through. Before this happened, we were working on some big plans for the event … and those plans will still take place, just not right away.  
+    
+    ## New Dates for WDS 2021  
+    
+    We've confirmed the following dates for next year's WDS:  
+    
+    - **WDS Week:** Tuesday, June 22 to Sunday, June 27  
+    - **Main Events:** Thursday, June 24 to Sunday, June 27 
+    
+    <Image src="https://cascade.madmimi.com/promotion_images/1474/7121/original/WDS-community-cheer.jpg?1585434097" width="565px" height="169px" className="scriptHeader" preload/> 
+    
+    ## What's Next?  
+    
+    We realize that you might have questions that we don't have answers for _right now_. I wanted to make sure that everyone knew about this important decision ASAP, since I know it affects your schedule and plans.  
+    
+    Our team is working to reconfigure a lot of logistics and plans. We’ll email updates as they become available, and we’ll also update this page (you might want to bookmark it). Please know that we'll be working as quickly as possible, but a lot of things are beyond our control at the moment.  
+    
+    ### Hang in There!  
+    
+    Thank you for your understanding during these challenging times. The WDS community is mighty—we will make it through this!  
+    
+    Wiping down counters,  
+    
+    Chris Guillebeau
+  </Block>
+</Section>
+<Import id="join-us" />
