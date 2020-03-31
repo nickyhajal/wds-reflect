a32eeb404f371f45d02845dd9eaec5a07f1b587b
Jedd <jedidiah475@gmail.com>
2020-03-31 15:12:14 -0700

    Update announcement.js

--- content/pages/announcement.js
+++ content/pages/announcement.js
@@ -11,21 +11,23 @@
     
     Dear WDS Community,  
     
-    Greetings from WDS HQ! I wanted to bring you this important update as soon as we had the information.  
+    Greetings from WDS HQ! I wanted to bring you this important update as soon as we had the information.   
     
-    As we continue to monitor the global situation, we've come to the decision to **postpone WDS 2020 until the summer of 2021**. This wasn't an easy decision, but we feel it's the right one given the ever-changing situation and uncertainty.  
+    As our team has continued to monitor the global pandemic we all know as COVID-19, we've come to the decision to **postpone the final WDS until the summer of 2021**. This wasn't an easy decision, but we also don't have much of an alternative given the ever-changing situation.  
     
-    We want to make sure that when we gather again to celebrate (and we will gather again), we'll do so under the best possible circumstances. As I’m sure you’re aware, more and more it’s looking like this summer is not the best time. Watch the video below to learn more about the thought process behind the decision.  
+    We want to make sure that when we gather again to celebrate (and we _will_ gather again), we'll do so under the best possible circumstances. Watch the video below to learn more about the thought process behind the decision.  
 
 <Video placeholder="https://el-files-wds.s3.amazonaws.com/images/photos/checkin1-31920.png" videoautoplay="true" videoId="402725641" width="710" height="430"/>
 
     ## Important: WDS is Not Canceled. 
     
-    We remain committed to hosting a final WDS. This event is important to all of us, and the entire core team is dedicated to seeing it through. Before this happened, we were working on some big plans for the event … and those plans will still take place, just not right away.  
+    We remain committed to hosting a final WDS. This event is important to all of us, and the entire core team is dedicated to seeing it through.  
     
-    ## New Dates for WDS 2021  
+    Before this happened, we were working on some big plans for the event … and those plans will still take place, just not right away.  
     
-    We've confirmed the following dates for next year's WDS:  
+    ## New WDS Dates for 2021  
+    
+    We've already confirmed the following dates for next year:  
     
     - **WDS Week:** Tuesday, June 22 to Sunday, June 27  
     - **Main Events:** Thursday, June 24 to Sunday, June 27 
@@ -34,10 +36,12 @@
     
     ## What's Next?  
     
-    We realize that you might have questions that we don't have answers for _right now_. I wanted to make sure that everyone knew about this important decision ASAP, since I know it affects your schedule and plans.  
+    I wanted to make sure that everyone knew about this important decision ASAP, since I know it affects your schedule and plans.  
     
-    Our team is working to reconfigure a lot of logistics and plans. We’ll email updates as they become available, and we’ll also update this page (you might want to bookmark it). Please know that we'll be working as quickly as possible, but a lot of things are beyond our control at the moment.  
+    Our team is working to reconfigure a lot of logistics and plans. We’ll email updates as they become available so keep checking your inbox.  
     
+    We’ll also **update this page** (you might want to bookmark it). Please know that we'll be working as quickly as possible, but a lot of things are beyond our control at the moment.  
+                                                       
     ### Hang in There!  
     
     Thank you for your understanding during these challenging times. The WDS community is mighty—we will make it through this!  
