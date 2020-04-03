c313e2049bfe57fdc93c40ea65897f001de5f170
Jedd <jedidiah475@gmail.com>
2020-04-02 21:20:46 -0700

    Update transfer.js

--- content/pages/transfer.js
+++ content/pages/transfer.js
@@ -4,13 +4,14 @@
 >
     <Block width="788px" bleed={true} type="padded" background="white" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
       <Transfer title="So sorry you can't make it for the adventure!">
-        <p>Can't make it to the World Domination Summit after all? We're sorry we won't see you there! (there's always next year).</p>
+        <p>Can't make it to the World Domination Summit after all? We're sorry we won't see you there!</p>
 
-        <p>Transfers and name changes are available for a $100 fee from now until May 17th (6 weeks prior to WDS).</p> 
+        <p>Transfers and name changes can be made from now until December 31, 2020 for free.</p> 
         
-        <p><i>Starting May 18th, 2019, no further transfers or name changes will be possible.</i></p>
+        <p>Starting January 1, 2021, a $100 transfer fee will be required per transfer.</p>
 
-        <p>Hope to see you next year for our final year of WDS!</p>
+        <p><b>The last day to transfer a WDS ticket is May 12, 2021.<b> No further transfers or name changes will be possible after this date.</i></p>
+
     </Transfer>
   </Block>
 </Section>
