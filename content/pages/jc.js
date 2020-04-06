208e88cfc4fcd5e9a402ca83dbce81aa1f96e2e5
Jedd <jedidiah475@gmail.com>
2020-04-06 08:40:53 -0700

    Update and rename z031920.js to jc.js

---
+++ content/pages/jc.js
@@ -0,0 +1,59 @@
+<Section
+  color="sea"
+  css={{ marginBottom: '-2px' }}
+>
+  <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## FAQs</Block>
+
+    ### What is WDS?
+
+    The World Domination Summit (WDS) is a gathering of creative, interesting, and remarkable people from all over the world. Every summer, more than one thousand people travel to Portland, Oregon for an immersive experience in life, work, and travel.  
+    
+    The almost week-long event consists of three primary activities: main-stage events with keynote speakers that are attended by the entire group, workshops that focus on specific topics, and lots of meetups, parties, and informal engagement. Despite the large number of people who attend, WDS is introvert-friendly, and everyone who participates is a big part of the event.  
+    
+    WDS was founded by **[Chris Guillebeau](http://chrisguillebeau.com)**, explorer of the world and _New York Times_ bestselling author of _The $100 Startup_.
+
+   ### When is the next WDS? 
+      
+   **Due to COVID-19, WDS for 2020 has been postponed until June 2021**. For more information go to our **[Postponement page](https://worlddominationsummit.com/postponed)**.  
+   
+   ### How can I get a WDS ticket?  
+   
+   **Tickets for WDS are still sold out!** If you're still looking for a ticket, please contact our concierge team at **[concierge@wds.fm](mailto:concierge@wds.fm)** to learn what options might exist to attend WDS.  
+
+  ### Do you offer a payment plan for tickets?  
+  
+Yes! The first initial payment is **$95**, followed by **three payments of $204** for a total of **$707**. **All payments are non-refundable**, and you can pay for up to three tickets using a payment plan. To review the full terms and conditions of the WDS payment plan **[click here](https://worlddominationsummit.com/payment-plan-terms-and-conditions)**.  
+                                                                                                                              
+### I've purchased a WDS ticket, what do I do next?  
+
+Keep checking your inbox. All communication about the event and specific actions needed from you to prepare you for the event is sent via email. If you are not receiving WDS 2020 emails, please contact our concierge team at **[concierge@wds.fm](mailto:concierge@wds.fm)** and they will be able to assist you.
+
+### What is the refund/cancellation policy for WDS tickets?  
+
+All WDS tickets are **non-refundable.**
+
+    ### Can I transfer a WDS ticket to someone else?  
+
+  If you are no longer planning to attend, **the $100 transfer fee has been waived from now until the end of this year (December 31, 2020)**. Note that this waiver applies to the first transfer of a ticket—if another ticketholder wants to transfer their ticket after receiving it from you, they'll need to pay the fee.  
+
+Contact our Concierge team at **[concierge@wds.fm](mailto:concierge@wds.fm)** and provide the name, email, and mailing address of the person you’re transferring your ticket to. Starting January 1, 2021, a $100 transfer fee will be required to complete the process. **The last day to complete all transfers is May 12, 2021** (40 days prior to WDS).  
+       
+    ### Can I nominate myself or someone I represent to be a speaker?  
+    
+    We're sure that you or your client would be great! ... but we do not accept nominations for speakers. Everyone who attends WDS is a big part of everything. Current year speakers are nominated by alumni speakers from previous years.
+
+    ### Can I sponsor WDS or donate something for your attendees?  
+    
+    WDS is a sponsor-free, non-commercial gathering. We do not accept advertising or sponsored products.
+
+    ### Can I be a volunteer?  
+    
+    Every year we recruit a number of volunteer "Ambassadors" to serve our attendees during WDS. To apply for this role, you must be a champion in helping attendees have an amazing experience, have extensive knowledge of Portland, and be available during the entire weekend for WDS main-events. Special consideration is given to those that are available for the entire week of WDS. Applications are typically available 3-4 months prior to WDS. Email our team at **[concierge@wds.fm](mailto:concierge@wds.fm)** to get notified when applications become available. 
+   
+   ### Still have additional questions?  
+   
+   Contact our year-round concierge team at **[concierge@wds.fm](mailto:concierge@wds.fm)** and they'll be able to assist you. 
+  </Block>
+</Section>
+<Import id="join-us" />
