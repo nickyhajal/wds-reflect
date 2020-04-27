63ab76b99dd501608a72d54fbf23177b586ece44
Jedidiah <jedidiah475@gmail.com>
2020-04-27 10:00:18 -0700

    Update academies.js

--- content/pages/academies.js
+++ content/pages/academies.js
@@ -8,7 +8,7 @@
 >
   <Image src="v2/headings/academies.png" width="565px" height="169px" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-    ## WDS Academies will be back for 2020 with an all-star lineup! 
+    ## WDS Academies will be back with an all-star lineup! 
 At a WDS Academy, you might learn how to develop a fulfilling writing practice, build or business through Youtube, or what it's like to build and live in a tiny house. 
 You'll have the opportunity to learn something new to help live your _own_ remarkable life story. It's a great way to lead into the main events of the WDS weekend and keep the conversations going afterward.
   This short video shows more of what it's all about:
@@ -27,9 +27,9 @@ There is no limit to how many Academies you can register for, but please note th
 
 If you have any questions or need any assistance, please contact our concierge team at **[concierge@wds.fm](mailto:concierge@wds.fm)**. 
 
-### Sorry We Missed You For 2019!
+### Sorry We Missed You!
 
-_Academies for this year have concluded but will return for 2020 (our final year). Please use the following WDS 2019 Academies schedule as reference._ 
+_Academies will return for our final year. Please use the following WDS 2019 Academies schedule as reference of the type of content you might experience._ 
 
 </Block>
 <EventList year={19} type="academy" title="WDS 2019 Academies" />
