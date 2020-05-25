4928fab0107c3660357b526bdb655d3dfda3bf84
Jedidiah <jedidiah475@gmail.com>
2020-05-25 13:15:23 -0700

    Create wdsday-confirm.js

---
+++ content/pages/wdsday-confirm.js
@@ -0,0 +1,11 @@
+<Section
+  color="sea"
+  css={{ marginBottom: '-2px' }}
+>
+  <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## You're on the WDS Day List!</Block>
+      <p>Thanks for letting us know your intention to join us for WDS Day. </p>
+   <p>Stay tuned for more information!</p>  
+<p>Have a question? Our concierge team operates year-round. Don't hesitate to reach out to them by hitting the <b><Link to="https://worlddominationsummit.com/contact-us">bat signal here.</Link></b></p>
+  </Block>
+</Section>
