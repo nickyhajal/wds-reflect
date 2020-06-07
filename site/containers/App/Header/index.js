f4e77128c8b9a470b212919523d4540d56799d53
Jedidiah <jedidiah475@gmail.com>
2020-06-06 21:41:45 -0700

    Update index.js

--- site/containers/App/Header/index.js
+++ site/containers/App/Header/index.js
@@ -116,7 +116,7 @@ const desktopHeader = (ticketClick, scrollTo, auth, subNav) =>
       </nav>
       {auth.me ? '' : <TicketStub onClick={ticketClick} />}
     </div>
-    <div styleName="subNav" className="covid">Update: WDS has been postponed due to COVID-19<Link to="/postponed">More Info</Link></div>
+    <div styleName="subNav" className="covid">Black Lives Matter: Our Commitment to Action, Not Just Words<Link to="/blm">Learn More</Link></div>
     {renderSubNav(subNav, scrollTo, auth.me)}
     {renderAlert(subNav, scrollTo, auth.me)}
   </header>;
