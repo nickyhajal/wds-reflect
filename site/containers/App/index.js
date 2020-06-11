7db1ea749d56bd8ececa688b47de8842ee331713
Jedidiah <jedidiah475@gmail.com>
2020-06-10 20:41:16 -0700

    Update index.js

--- site/containers/App/index.js
+++ site/containers/App/index.js
@@ -160,6 +160,7 @@ export class App extends Component {
           <div styleName="links">
             <Link to="/faq">FAQ</Link>
             <Link to="/story">Our Story</Link>
+            <Link to="/postponed">Postponed</Link>
             <Link to="/contact-us">Contact Us</Link>
           </div>
           <div styleName="sub">© World Domination Summit 2011-2020</div>
