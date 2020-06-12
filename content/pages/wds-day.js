c2c41c9b056c8692682d051f80bb7e087babe677
Nicky Hajal <nhajal@gmail.com>
2020-06-12 12:32:19 -0500

    wds-day

--- content/pages/wds-day.js
+++ content/pages/wds-day.js
@@ -1,94 +1,74 @@
-;<Section
-	headerClip="br:0%,-10%;"
-	headerImage="photos/WDS-community.jpg"
-	color="canvas"
+<Section
+  headerClip="br:0%,-10%;"
+  headerImage="photos/WDS-community.jpg"
+  color="canvas"
 >
-	<Block
-		cols="8"
-		bleed={false}
-		textAlign="left"
-		margin="80px 0 80px"
-		background="white"
-		type="padded"
-		padding="80px 120px"
-		className="bigger"
-		mobile={{tablet: {padding: '50px 40px'}, phone: {padding: '30px 20px'}}}
-	>
-		<h2 style={{fontSize: '32px', textAlign: 'center'}}>
-			Join Us for WDS Day - June 27, 2020
-		</h2>
-		Greetings from WDS HQ! I hope you're staying safe during these
-		unprecendented times. This is the time of year when the team usually
-		kick things into a higher gear to prepare for everyone's arrival in
-		Portland. This time, though, we've been confined to a series of virtual
-		meetings as a team, and Zoom check-ins with our 2020 attendees. In case
-		you missed it, we decided a while back that WDS 2020 (our intended
-		finale) will now take place one year later, in the summer of 2021.
-		Though we're sad that we won't be meeting for WDS this year, we're
-		excited to announce the start of something new that will outlive WDS
-		itself... **WDS Day!**{' '}
-		<Image
-			src="https://el-files-wds.s3.amazonaws.com/images/photos/collage-wds.jpg"
-			width="100%"
-			height="541"
-		/>
-		<br /> <h2>What is WDS Day</h2>
-		Every year on the last Saturday of June, members of the community will
-		commit to doing an activity that honors the WDS core values of
-		**Community**, **Adventure**, and **Service**. This could mean
-		volunteering at a local food pantry, getting your first tattoo, going on
-		that hike you've always wanted to, learning a new recipe, checking in
-		with an old friend (or making a new one), etc... We'll then share those
-		activities online through social media using #WDSlocal. Prior to the
-		pandemic, we were already having discussions as a community about how
-		WDS can live on (independent of the main event) after our final year. We
-		know that the best part about WDS is the people and not the event
-		itself! Additional examples ideas include (especially with our current
-		global situation in mind): - Taking part in a protest - Connect with a
-		long lost friend - Doing something to thank essential workers - Read a
-		book or take a workshop on anti-rascism - Hosting an online dance party
-		- Learning to cook/bake something new - Create and send a care package -
-		Going on a hike or exploring a neighborhood (if it's safe to do so) ##
-		Virtual Attendee-Led Meetups And because of our current global
-		situation, we're putting a call out for Virtual Attendee-led Meetups.
-		This would be a great opportunity for us to talk about current issues
-		such as Black Lives Matter, and what we'll do as a community to foster
-		real change. Meetups will take place from 10:00am Saturday (26th)
-		through all-day Sunday (27th). Here's two important links to for those
-		interested in hosting a meetup: **[--> Virtual Meetup Guide for WDS
-		Day](https://el-files-wds.s3.amazonaws.com/docs/WDS-Virtual-Meetup-Guide-2020.pdf)**
-		**[--> Propose a Meetup
-		Form](https://worlddominationsummit.com/propose-a-meetup)**
-	</Block>
-	<Video
-		placeholder="photos/WDS-2018-main.jpg"
-		videoId="282373496"
-		cols="8"
-	/>
-	<Block cols="8" bleed={false} textAlign="left" margin="80px 0 80px" background="white" type="padded" padding="80px 120px" className="bigger" mobile={{tablet: {padding: '50px 40px'}, phone: {padding: '30px 20px'}}} >
-		**What is the Schedule/Plan for This Year's WDS Day?** First, save the
-		date for this year's WDS Day: **Saturday, June 27, 2020** We'll start
-		this year's WDS Day by coming together and meeting online as a community
-		(through Zoom) **starting at 9:00am PT**. From there the community will
-		take part in individual activities that reflect the WDS core values and
-		participate in Attendee-led, virtual meetups. We'll then **finish the
-		day together at 6:00pm PT**. **How Do I Paricipate in WDS Day?** WDS Day
-		is open to the entire WDS community. To make sure you get all the event
-		information and updates, please sign up with your email at **[this
-		link](http://madmimi.com/signups/37b1184441c943a0a93ac294552aa549/join)**.
-		**Please note: a WDS Account is required to RSVP for meetups.** All
-		information about the event will be communicated via email and also
-		posted on this page. If you have any questions about WDS Day, don't
-		hesitate to reach out to our team by emailing us at concierge@wds.fm.
-		<Image
-			src="https://el-files-wds.s3.amazonaws.com/images/photos/ukulelemeetup.jpg"
-			width="100%"
-			height="541"
-		/>
-		<Image
-			src="https://el-files-wds.s3.amazonaws.com/images/photos/bollywoodmeetup.jpg"
-			width="100%"
-			height="541"
-		/>
-	</Block>
+  <Block
+    cols="8"
+    bleed={false}
+    textAlign="left"
+    margin="80px 0 80px"
+    background="white"
+    type="padded"
+    padding="80px 120px"
+    className="bigger"
+    mobile={{tablet: {padding: '50px 40px'}, phone: {padding: '30px 20px'}}}
+  >
+    <h2 style={{ fontSize: '32px', textAlign: 'center' }}>
+      Join Us for WDS Day - June 27, 2020
+    </h2>
+Greetings from WDS HQ! I hope you're staying safe during these unprecendented times.
+
+This is the time of year when the team usually kick things into a higher gear to prepare for everyone's arrival in Portland. This time, though, we've been confined to a series of virtual meetings as a team, and Zoom check-ins with our 2020 attendees.
+
+In case you missed it, we decided a while back that WDS 2020 (our intended finale) will now take place one year later, in the summer of 2021.
+
+Though we're sad that we won't be meeting for WDS this year, we're excited to announce the start of something new that will outlive WDS itself... **WDS Day!**
+        {' '}
+    <Image src="https://el-files-wds.s3.amazonaws.com/images/photos/collage-wds.jpg" width="100%" height="541" /><br/>
+    {' '}
+<h2>What is WDS Day</h2>   
+
+Every year on the last Saturday of June, members of the community will commit to doing an activity that honors the WDS core values of **Community**, **Adventure**, and **Service**.
+
+This could mean volunteering at a local food pantry, getting your first tattoo, going on that hike you've always wanted to, learning a new recipe, checking in with an old friend (or making a new one), etc... We'll then share those activities online through social media using #WDSlocal.
+
+Prior to the pandemic, we were already having discussions as a community about how WDS can live on (independent of the main event) after our final year. We know that the best part about WDS is the people and not the event itself!
+
+Additional examples ideas include (especially with our current global situation in mind):
+- Taking part in a protest
+- Connect with a long lost friend
+- Doing something to thank essential workers
+- Read a book or take a workshop on anti-rascism
+- Hosting an online dance party
+- Learning to cook/bake something new
+- Create and send a care package
+- Going on a hike or exploring a neighborhood (if it's safe to do so)
+
+## Virtual Attendee-Led Meetups                                               
+And because of our current global situation, we're putting a call out for Virtual Attendee-led Meetups. This would be a great opportunity for us to talk about current issues such as Black Lives Matter, and what we'll do as a community to foster real change. Meetups will take place from 10:00am Saturday (26th) through all-day Sunday (27th). Here's two important links to for those interested in hosting a meetup:
+
+**[--> Virtual Meetup Guide for WDS Day](https://el-files-wds.s3.amazonaws.com/docs/WDS-Virtual-Meetup-Guide-2020.pdf)**
+**[--> Propose a Meetup Form](https://worlddominationsummit.com/propose-a-meetup)**
+                                            
+  </Block>
+<Video placeholder="photos/WDS-2018-main.jpg" videoId="282373496" cols="8" /> 
+   <Block cols="8" bleed={false} textAlign="left" margin="80px 0 80px" background="white" type="padded" padding="80px 120px" className="bigger" mobile={{tablet: {padding: '50px 40px'}, phone: {padding: '30px 20px'}}} >
+**What is the Schedule/Plan for This Year's WDS Day?** 
+First, save the date for this year's WDS Day: **Saturday, June 27, 2020** 
+                                         We'll start this year's WDS Day by coming together and meeting online as a community (through Zoom) **starting at 9:00am PT**.  
+From there the community will take part in individual activities that reflect the WDS core values and participate in Attendee-led, virtual meetups. We'll then **finish the day together at 6:00pm PT**.
+
+**How Do I Paricipate in WDS Day?**  
+WDS Day is open to the entire WDS community. To make sure you get all the event information and updates, please sign up with your email at **[this link](http://madmimi.com/signups/37b1184441c943a0a93ac294552aa549/join)**. 
+  
+**Please note: a WDS Account is required to RSVP for meetups.** All information about the event will be communicated via email and also posted on this page.  
+
+If you have any questions about WDS Day, don't hesitate to reach out to our team by emailing us at concierge@wds.fm.
+
+<Image src="https://el-files-wds.s3.amazonaws.com/images/photos/ukulelemeetup.jpg" width="100%" height="541" />
+  
+<Image src="https://el-files-wds.s3.amazonaws.com/images/photos/bollywoodmeetup.jpg" width="100%" height="541" />
+
+ </Block>
 </Section>
