6987e5096e0aacd02bb768f0235ed2b4c55b8d41
Jedidiah <jedidiah475@gmail.com>
2021-03-17 22:00:07 -0700

    Update guide.js

--- content/pages/guide.js
+++ content/pages/guide.js
@@ -8,7 +8,7 @@
 >
   <Image src="v2/headings/guide.png" width="421" height="151" className="scriptHeader" preload/>
   <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-    ## Ready? Set. Go! The Attendee Guide to WDS 2020
+    ## Ready? Set. Go! The Attendee Guide to WDS X
     This summer, you'll join over 1000+ invidividuals from over 30+ countries coming to Portland for our final WDS gathering of remarkable adventurers. This is a checklist and guide to help you prepare and maximize your WDS experience. If you need any information regarding WDS 2020 - **_you'll find it here!_**      
   <Image src="photos/league.of.leaders.banner.jpg" width="100%" height="405" />
       </Block>
@@ -25,13 +25,12 @@
    <Block type="padded" cols="6" bleed={false} background="canvas" className="table-list" css={{ padding: '60px 40px', position: 'relative', zIndex:'4' }} clip="top_lr_1.6">
     ## A Week of WDS: June 23rd - June 29th, 2020
 
-    - **Tuesday (6/23)** Activities at WDS HQ and Attendee-Led Meetups
-    - **Wednesday (6/24)** Academies, Activities at WDS HQ and Attendee-Led Meetups
-    <ul><li><b className="highlight highlight-2">Thursday (6/25)</b> Academies, Activities, and Attendee-Led Meetups, and Opening Party</li></ul>
-    <ul><li><b className="highlight highlight-2">Friday (6/26)</b>  Academies, Activities, Attendee-Led Meetups, World Record Attempt</li></ul>
-    <ul><li><b className="highlight highlight-2">Saturday(6/27)</b> Main Stage Keynotes, Attendee-Led Meetups, and Evening Activities</li></ul>
-    <ul><li><b className="highlight highlight-3">Sunday (6/28)</b> Main Stage Keynotes, Attendee-Led Meetups, and the Closing Party</li></ul>
-    - **Monday (6/29)** Attendee-Led Meetups & Final Farewells
+    - **Tuesday (6/21)** Activities at WDS HQ and Attendee-Led Meetups
+    - **Wednesday (6/22)** Academies, Activities at WDS HQ and Attendee-Led Meetups
+    <ul><li><b className="highlight highlight-2">Thursday (6/23)</b> Academies, Activities, and Attendee-Led Meetups, and Opening Party</li></ul>
+    <ul><li><b className="highlight highlight-2">Friday (6/24)</b>  Academies, Activities, Attendee-Led Meetups, World Record Attempt</li></ul>
+    <ul><li><b className="highlight highlight-2">Saturday(6/25)</b> Main Stage Keynotes, Attendee-Led Meetups, and Evening Activities</li></ul>
+    <ul><li><b className="highlight highlight-3">Sunday (6/26)</b> Main Stage Keynotes, Attendee-Led Meetups, and the Closing Party</li></ul>
       <i>Can't come for the whole time? No problem, but you don't want to miss the main events taking place on Thursday (evening), Friday, Saturday, and Sunday. For the best possible WDS experience we recommend arriving no later than Thursday afternoon, and leaving no earlier than Monday morning.</i>
   </Block>
   <Image src="photos/jellyfish.png" width="426" height="263" margin="-50px -60px 100px 0" css={{ float: 'right' }} mobile={{ all: { margin: '-50px -15px 100px 0' } }} />
@@ -40,58 +39,58 @@
     <Section color="white">
 <Block margin="125px 0 50px 0">
     <Block cols="6">
-      ## Preparing for WDS 2020
+      ## Preparing for WDS X
      </Block>
       <Grid numCols="3" tabletCols="2" width="968">
       <Block type="image-overlay" anchor="right" src="photos/wds.main.stage.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 1: Learn How WDS Works
         WDS is an unconventional event. How does the week work? What are the events that make up WDS? This should help.       
-          <Button to="https://worlddominationsummit.com/wds-how-it-works" newWindow={true} styling="orange" fitToText>Learn More</Button> 
+          <Button to="" newWindow={true} styling="orange" fitToText>Coming Soon</Button> 
          </Block>
        <Block type="image-overlay" anchor="left" src="https://el-files-wds.s3.amazonaws.com/images/WDS-high-five-chain.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 2: Plan Your Arrival to Portland
         As you make your travel plans, use this helpful guide to determine when to arrive to (and depart from) Portland.
-       <Button to="https://worlddominationsummit.com/when-to-arrive-in-portland-for-wds-2020" newWindow={true} styling="orange" fitToText>Learn More</Button>
+       <Button to="" newWindow={true} styling="orange" fitToText>Coming Soon</Button>
         </Block>            
         <Block type="image-overlay" anchor="right" src="https://el-files-wds.s3.amazonaws.com/images/photos/Hotel-Zags-King.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 3: Book Lodging
         Every year WDS partners with some of Portland's most iconic hotels to provide exclusive discounts for our attendees.
-       <Button to="https://worlddominationsummit.com/location" newWindow={true} styling="orange" fitToText>Book Now!</Button>
+       <Button to="" newWindow={true} styling="orange" fitToText>Coming Soon</Button>
         </Block>
          <Block type="image-overlay" anchor="left" src="https://el-files-wds.s3.amazonaws.com/images/World-Record-Attempt.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 4: Help Set a World Record
         It's back! We're going to band together to break another world record and we'll need your help!
-        <Button to="" newWindow={true} styling="orange" fitToText>Check back around March/April 2020</Button>     
+        <Button to="" newWindow={true} styling="orange" fitToText>Check back around March/April 2022</Button>     
         </Block>
         <Block type="image-overlay" anchor="right" src="photos/JoinUs/image-academies.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 5: Register for Academies
         Alumni speakers and other special guests share their knowledge and experience in half-day workshops.
-        <Button to="" newWindow={true} styling="orange" fitToText>Check back around April 2020</Button>     
+        <Button to="" newWindow={true} styling="orange" fitToText>Check back around April 2022</Button>     
         </Block>
          <Block type="image-overlay" anchor="left" src="photos/meetups-guide.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 6: Host a Meetup 
         Share your knowledge. Coordinate an activity. Help people connect. Make WDS awesome by proposing a meetup.
-        <Button to="" newWindow={true} styling="orange" fitToText>Check back around April 2020</Button>     
+        <Button to="" newWindow={true} styling="orange" fitToText>Check back around April 2022</Button>     
           </Block>      
        <Block type="image-overlay" anchor="right" src="photos/wds.mobile.app.png" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 7: Download the Mobile App 
         Download our very own mobile app to RSVP for activities, check your personalized schedule, and so much more.
-        <Button to="" newWindow={true} styling="orange" fitToText>Check back around April 2020</Button>     
+        <Button to="" newWindow={true} styling="orange" fitToText>Check back around April 2022</Button>     
         </Block>      
         <Block type="image-overlay" anchor="left" src="https://s3.amazonaws.com/el-files-wds/images/2015-Waffles-1.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">   
         ## Step 8: RSVP for Activities
         Create your own personalized schedule by RSVPing for Meetups, Activities, and Academies through the mobile app or online. 
-         <Button to="" newWindow={true} styling="orange" fitToText>Check back around April 2020</Button>          
+         <Button to="" newWindow={true} styling="orange" fitToText>Check back around April 2022</Button>          
         </Block>
         <Block type="image-overlay" anchor="right" src="photos/wds.packing.list.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 9: Pack for WDS 
         Want to pack like an expert scout and maximize your WDS experience? Want helpful suggestions from WDS alum and the core team? 
-        <Button to="" newWindow={true} styling="orange" fitToText>Check back around May 2020</Button>     
+        <Button to="" newWindow={true} styling="orange" fitToText>Check back around May 2022</Button>     
         </Block>   
          <Block type="image-overlay" anchor="left" src="photos/wds.checkin.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">   
         ## Step 10: Check-in at WDS
         You're here - welcome to Portland! Learn what to do when you arrive to Portland.  
-         <Button to="" newWindow={true} styling="orange" fitToText>Check back around May 2020</Button>          
+         <Button to="" newWindow={true} styling="orange" fitToText>Check back around May 2022</Button>          
         </Block>
           <Block type="image-overlay" anchor="right" src="photos/JoinUs/image-parties.jpg" imageClip="tr:0,+10%;" clip="tr:0,+10%;">
         ## Step 11: Have an Amazing Experience
@@ -108,4 +107,3 @@
     </Block>
   </Block> 
 </Section>
-<Import id="join-us" />
