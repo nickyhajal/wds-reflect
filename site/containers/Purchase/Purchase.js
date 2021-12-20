92f65681d5f9759798fd140168bebe4659515dd0
Jedidiah <jedidiah475@gmail.com>
2021-12-20 08:44:51 -0800

    Update Purchase.js

--- site/containers/Purchase/Purchase.js
+++ site/containers/Purchase/Purchase.js
@@ -139,7 +139,7 @@ export class Purchase extends Component {
           />
           <Block align="center" textAlign="center">
             <Image
-              src="v2/logo2020.png"
+              src="https://el-files-wds.s3.amazonaws.com/images/v2/WDS-X-logo-orange.png"
               width="123"
               height="26"
               fit="contain"
@@ -170,7 +170,7 @@ export class Purchase extends Component {
       <Section color="white" styleName="shell" className="fillScreen">
         <Block align="center" textAlign="center">
           <Image
-            src="v2/logo2020.png"
+            src="https://el-files-wds.s3.amazonaws.com/images/v2/WDS-X-logo-orange.png"
             width="123"
             height="26"
             fit="contain"
