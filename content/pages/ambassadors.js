b23d382a2b7365d3e3c2532c3e0401ff6145f9ca
Jedidiah <jedidiah475@gmail.com>
2022-01-10 20:29:30 -0800

    Update ambassadors.js

--- content/pages/ambassadors.js
+++ content/pages/ambassadors.js
@@ -15,10 +15,10 @@
   />
  <Block cols="6" css={{ padding: '20px 0 20px' }} textAlign="left">
     ## Be part of the magic of WDS!
-    We're searching for 80 amazing people who would like to help support our FINAL World Domination Summit!
+    We are searching for 80 amazing people who would like to help support our FINAL World Domination Summit (WDS X)! We already have an amazing roster of folks who previously applied, but we have room for a small handful of additional volunteers. If you’re interested, read on and apply below! 
  
    ### What is an Ambassador? 
-Ambassadors are our all-volunteer team of amazing humans who help make WDS happen each year. Ambassadors do a variety of tasks that make WDS run smoothly including: 
+Ambassadors are our all-volunteer team of amazing humans who help make WDS happen each year. Ambassadors do a variety of tasks that make WDS run smoothly including:
 - Running registration
 - Handing out swag
 - Creating an amazing experience at our opening and closing parties 
@@ -33,9 +33,9 @@ Ambassadors are our all-volunteer team of amazing humans who help make WDS happe
 We welcome applications from anyone who meets the following criteria: 
 - You’re committed to helping our attendees have the BEST experience possible 
 - You live in or near Portland, OR (or you’re very familiar with the city)
-- You’re willing to share **20 hours** of your time between **Monday, June 22nd and Sunday, June 28th** (please note: the majority of Ambassador shifts are Thursday-Sunday)
-- You’re available on the evening of **Monday, June 22nd** for Ambassador orientation 
-- You’re not a 2020 WDS attendee 
+- You’re willing to share **20 hours** of your time between **Monday, June 20th and Sunday, June 26th** (please note: the majority of Ambassador shifts are Thursday-Sunday)
+- You’re available on the evening of **Monday, June 20th** for Ambassador orientation 
+- You’re not a WDS X attendee 
      </Block>
 
 <Block cols="6" css={{ padding: '20px 0 20px' }} textAlign="left">
@@ -52,19 +52,20 @@ Fame, fortune, glory! Ok, maybe not all those things. But you do get the satisfa
 
  <Block cols="6" css={{ padding: '20px 0 20px' }} textAlign="left">
 ### Tell me more about this 20 hour commitment…
-This is the second year we’re giving this a try. We’ll be releasing a majority of our Ambassador roles to the Ambassador team at a pre-announced day and time (likely between 3-4 weeks out from the event). From there, it will be first-come, first-served to pick your shifts. You can choose any shifts you want, as long as they add up to at least 20 hours. This gives YOU more freedom to choose when you want to volunteer and what you’d like to be doing. You can even coordinate to volunteer alongside a family member or friend (though we encourage you to meet new people over the week too). We’ll send the shift schedule out ahead of time, so you can look at what options might be best for you. 
+Once we get close to the big event we’ll release a majority of our Ambassador roles to the team at a pre-announced day and time. From there, it will be first-come, first-served to pick your shifts. You can choose any shifts you want, as long as they add up to at least 20 hours. This gives YOU more freedom to choose when you want to volunteer and what you’d like to be doing. You can even coordinate to volunteer alongside a family member or friend (though we encourage you to meet new people over the week too). We’ll send the shift schedule out ahead of time, so you can look at what options might be best for you. 
 
-If at all possible, **we suggest you hold all day Thursday, 6/25 - Sunday, 6/28** on your calendar now so you have lots of options to choose from (and if you can hold the whole week, even better). 
+If at all possible, we suggest you block off all day Thursday, 6/23 through Sunday, 6/26 on your calendar now so you have lots of options to choose from (and if you can hold the whole week, even better). That allows you to be available for a variety of shifts, but also to make sure you can enjoy the rest of the event when you’re not volunteering. 
 
-To be very clear, if you are unable to sign up for the shifts you want and/or you are unable to sign up for shifts totaling 20 hours we will not be able to give you a 2020 all access pass. 
+To be very clear, if you are unable to sign up for shifts totaling 20 hours we will not be able to give you a WDS X all access pass. If you have concerns about this please let us know ASAP and we’ll do our best to get it sorted. 
 
-If you’re not able to volunteer 20 hours but still want to help, we’re not able to offer a WDS pass in return, but we’d love to chat more about what might be possible! 
+If you’re not able to volunteer 20 hours but still want to help, we’re not able to offer a WDS pass in return, but we’d love to chat more about what might be possible!
+
 
 ### Is anything different because this is the last year? 
-Possibly. We have had extensive interest from volunteers, and we anticipate that we may not be able to accept anyone. Being a previous Ambassador will help your chances, but we cannot guarantee that all previous Ambassadors will be able to return again.
+Yes -- we have had extensive interest from volunteers and already have a significant pool of Ambassadors who have been approved. We anticipate that we will not be able to accept everyone who applies. Note: Being a previous Ambassador will help your chances, but we cannot guarantee that all previous Ambassadors will be able to return again.
 
 ### When should I apply by? 
-Ambassador applications are now closed. Thanks for your interest!. 
+To ensure the best chance of being selected, please apply by February 1, 2022. We cannot guarantee that any applications after this date will be accepted, but if there’s space we’ll do our best!
 
 ### Anything happening ahead of the event? 
 Each year we host an Ambassador Meet and Greet to reconnect with old friends and to meet new ones. It also gives us a chance to share a ‘sneak peek’ of what’s to come. This year’s Meet and Greet will be held in early May, exact date and time TBD. 
@@ -72,16 +73,20 @@ Each year we host an Ambassador Meet and Greet to reconnect with old friends and
 ### I have more questions or would like more information!
 Email Tina at concierge@wds.fm and we’ll get everything straightened out! 
 
+Okay, ready to apply? Let’s do this:!
+
+
 </Block>
 
  <Block type="padded" background="blue">
     <Image src="icon/circle-map-green.png" width="101" height="78"  align="center" margin="0 0 40px 0" />
         <Block cols="5">
-      ## Sorry we missed you! 
-      Thanks so much for your interest in volunteering for WDS 2020. However, as of March 15, Ambassador applications are closed. 
       
-      If you have any questions, please reach out to us at concierge@wds.fm.
-           <Button to="" newWindow={true} styling="sea" fitToText>Application Are No Longer Available</Button> 
+          ## Sorry we missed you! 
+              
+<Button to="https://worlddominationsummit.com/ambassador-application" newWindow={true} styling="sea" fitToText>Apply Now!</Button> 
+
+We’ll be in touch to confirm receipt of your application within about a week of receiving it, and again with next steps a couple weeks after the February 1st deadline.
     </Block>
   </Block>
 
