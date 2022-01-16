888d0ca41fdeb6aa92e6e5fba924bb9f28d6710a
Jedidiah <jedidiah475@gmail.com>
2022-01-16 12:43:20 -0800

    Update transfer.js

--- content/pages/transfer.js
+++ content/pages/transfer.js
@@ -6,9 +6,9 @@
       <Transfer title="So sorry you can't make it for the adventure!">
         <p>Can't make it to the World Domination Summit after all? We're sorry we won't see you there!</p>
 
-        <p>Transfers and name changes can be made from now until December 31, 2021 for free.</p> 
+        <p>Transfers and name changes can be made from now until February 28, 2022 for free.</p> 
         
-        <p>Starting January 1, 2022, a $100 transfer fee will be required per transfer.</p>
+        <p>Starting March 1, 2022, a $100 transfer fee will be required per transfer.</p>
 
         <p><b>The last day to transfer a WDS ticket is May 12, 2022.</b> No further transfers or name changes will be possible after this date.</p>
           
