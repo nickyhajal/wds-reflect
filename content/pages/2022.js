57a55dbdb2977dae39660ef1d8aa763852441992
Jedidiah <jedidiah475@gmail.com>
2022-01-18 10:08:30 -0800

    Create 2022.js

---
+++ content/pages/2022.js
@@ -0,0 +1,99 @@
+  
+<Section
+  headerClip="br:0%,-10%;"
+  headerImage="photos/WDS-community.jpg"
+  color="canvas"
+>
+  <Block
+    cols="8"
+    bleed={false}
+    textAlign="left"
+    margin="80px 0 80px"
+    background="white"
+    type="padded"
+    padding="80px 120px"
+    className="bigger"
+    mobile={{tablet: {padding: '50px 40px'}, phone: {padding: '30px 20px'}}}
+  >
+    <h2 style={{ fontSize: '32px', textAlign: 'center' }}>
+      WDS Returns In June (Really!)
+    </h2>
+    
+    Dear WDS Community,  
+    
+   **WDS X _WILL_ be held this summer!**
+
+After 2020's forced stoppage and 2021's uncertainty, we're very happy to confirm that WDS X will take place this summer from June 21st-26th.
+
+Once again: it's definitely happening! Plan on it.
+
+## Dates for WDS X
+
+- **WDS Week:** Tuesday, June 21 through Sunday, June 26
+- **Main Events:** Thursday, June 24 to Sunday, June 26  
+
+  <hr></hr>
+  
+  ## We're looking forward to...
+    
+    We'll be back in many of our favorite venues, including the Newmark Theater, Pioneer Courthouse Square, and many more.
+
+Many of the speakers we originally confirmed for WDS 2020 have re-committed for WDS X, and we'll have a full lineup published by March.
+
+Small meetups will be more important than ever. We're working on ways for people to connect before, during, and after the event.
+
+You can also expect an all-new set of WDS Academies, at least two themed parties, and a few surprises big and small. In fact, I’ll mention one of them right now: we're even going to set a new World Record!
+
+All of these things and more are fully in the works for June 2022.  
+      
+      <Image src="https://cascade.madmimi.com/promotion_images/2296/0905/original/WDS-Memories.png?1615329072" width="100%" height="533px" /> 
+      
+    ## FAQs About WDS Postponement    
+
+    ##### Where will WDS take place?  
+    
+    WDS will remain in Portland, Oregon and in many of the same venues we were planning on using. The city of Portland has always been the home of WDS and will continue to be for the 2022 event.
+    
+    ##### I have a ticket for WDS 2020, am I still registered for WDS X?  
+    
+    YES, your original ticket has already been rolled over to next year's event, **which we're now calling WDS X** to honor our 10th and final year. No further action is needed.
+
+    ##### Want to come and need a ticket?  
+    
+    Since it's been a long time, we've heard from lots of people in the alumni network who didn't have a ticket to WDS 2020 and now want to attend. With that in mind, we're going to offer a small number of tickets in a public sale, most likely in February or March.
+
+Unlike the 2020 tickets that we'll honor, these tickets will be a bit more expensive to reflect our own higher costs. We hope that having a limited ticket sale will be a way to accommodate demand for those who missed out before.
+
+We'll share details and timing for the sale at least two weeks in advance through this newsletter. Join the waitlist at the bottom of this page or contact our Concierge team at **[concierge@wds.fm](mailto:concierge@wds.fm)**. 
+    
+    ##### If I won't be able to attend next year, can I transfer my ticket?  
+    
+    YES, and we are making this process more flexible. If you are no longer planning to attend, **the $100 transfer fee has been waived until February 28, 2022.**. 
+    
+    Note that this waiver applies to the first transfer of a ticket—if another ticketholder wants to transfer their ticket after receiving it from you, they'll need to pay the fee.  
+
+    For the ticket terms and conditions, please review [this link](https://worlddominationsummit.com/ticket-terms-and-conditions)
+                                                       
+    ##### How do I transfer my ticket?  
+    
+    Contact our Concierge team at **[concierge@wds.fm](mailto:concierge@wds.fm)** and provide the name, email, and mailing address of the person you’re transferring your ticket to. Starting March 1, 2022, a $100 transfer fee will be required to complete the process. **The last day to complete all transfers is May 12, 2022** (40 days prior to WDS).  
+                            
+    ## I have more questions...  
+    
+    Contact our year-round concierge team at **[concierge@wds.fm](mailto:concierge@wds.fm)** and they'll do their best to assist you.
+                            
+   <Image src="https://cascade.madmimi.com/promotion_images/8271/3043/original/2016-heros-journey-2.jpg?1551616247" width="100%" height="533px" /> 
+   ## See you next year!
+   
+   The last two years have been unlike anything any of us have ever experienced. Like you, I never expected we'd all have to rearrange our lives for so long.
+
+Now that we're closing in on a new season, I'm very much looking forward to getting together once more!
+
+The best is yet to come.
+    
+    Yours in slightly delayed world domination,  
+    
+    Chris Guillebeau
+  </Block>
+</Section>
+<Import id="wait-list" />
