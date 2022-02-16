7a5cb85ff9770f7d008a4c29b425d5e8257b5a33
Jedidiah <jedidiah475@gmail.com>
2022-02-16 08:16:53 -0800

    Update payment-plan-terms-and-conditions.js

--- content/pages/payment-plan-terms-and-conditions.js
+++ content/pages/payment-plan-terms-and-conditions.js
@@ -6,8 +6,8 @@
     <Block width="100%" margin="40px 0 0 0" textColor="black" textAlign="center">## The Fine Print: WDS Payment Plan Terms</Block>
    
    Please review all of the following information carefully:
-   * The WDS payment plan is divided into 4-monthly payments for a **total of $707**.
-   * The initial payment is $95, followed by 3-payments of $204.
+   * The WDS payment plan is divided into 4-monthly payments for a **total of $997**.
+   * The initial payment is $97, followed by 3-payments of $300.
    * Payments are made via credit or debit card and charged automatically each month after the initial payment.
    * All payments are **non-refundable.**
    * Once a plan has been completed, that WDS ticket is eligible to be transferred (if needed) until **May 12, 2022. **Please note** Due to COVID, transfer fees have been waived until December 31, 2021.
