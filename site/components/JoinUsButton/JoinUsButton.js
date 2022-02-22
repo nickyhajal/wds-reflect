7c9097ecde403e5ff860ea48d192e2506dd43d4c
Nicky Hajal <nhajal@gmail.com>
2022-02-21 23:50:32 -0500

    join button

--- site/components/JoinUsButton/JoinUsButton.js
+++ site/components/JoinUsButton/JoinUsButton.js
@@ -4,8 +4,8 @@ import styles from './JoinUsButton.css';
 import Link from '../Link/Link';
 
 const JoinUsButton = () => {
-  const to = window.location.pathname.indexOf('join-us') > -1 ? '/secret-purchase' : '/join-us';
-  // const to = window.location.pathname.indexOf('join-us') > -1 ? '/be-there' : '/join-us';
+  // const to = window.location.pathname.indexOf('join-us') > -1 ? '/secret-purchase' : '/join-us';
+  const to = window.location.pathname.indexOf('join-us') > -1 ? '/be-there' : '/join-us';
   return (
     <Link styleName="button" to={to} className="joinusbuttonticket" />
   );
