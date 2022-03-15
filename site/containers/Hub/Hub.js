69d4813552d76ce0d615de3b7d2c279a4f80896c
Jedidiah <jedidiah475@gmail.com>
2022-03-15 08:10:26 -0700

    Update Hub.js

--- site/containers/Hub/Hub.js
+++ site/containers/Hub/Hub.js
@@ -45,7 +45,7 @@ export class Hub extends Component {
         <Section headerSize="thin" headerClip="br:0%,-10%;" headerImage="hero/green.jpg" color="canvas">
           <div style={{ display: 'flex', flexDirection: 'row' }}>
             <div style={{ width: '280px', background: 'transparent', marginRight: '20px', marginTop: '-35px' }}>
-              <SideBtn style={{ backgroundImage: 'url(https://s3.amazonaws.com/el-files-wds/images/photos/when-to-arrive-wds-2019.png)'}} to="/when-to-arrive-in-portland-for-wds-2020" />
+              <SideBtn style={{ backgroundImage: 'url(https://s3.amazonaws.com/el-files-wds/images/photos/when-to-arrive-wds-2019.png)'}} to="/when-to-arrive-in-portland-for-wds-x-2022" />
               <SideBtn style={{ backgroundImage: 'url(https://files.wds.fm/images/how-wds-works.png)'}} to="/wds-how-it-works" />
               <SideBtn style={{ backgroundImage: 'url(https://s3.amazonaws.com/el-files-wds/images/wds-portlandia.png)'}} to="/a-wds-guide-of-portlandia" />
             </div>
