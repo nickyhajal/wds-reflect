b527842e39bcf0726bad1a99ec3d1e902742fa30
Jedidiah <jedidiah475@gmail.com>
2022-03-16 06:07:01 -0700

    Update waitlist-remove.js

--- content/pages/waitlist-remove.js
+++ content/pages/waitlist-remove.js
@@ -3,9 +3,9 @@
   css={{ marginBottom: '-2px' }}
 >
   <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
-    <Block width="100%" margin="40px 0 0 0" textAlign="center">## Don't stop believin...</Block>
-      <p><center>Per your request, we've removed you from the WDS 2020 waitlist.</center></p>
-   <p><center>Though we're sad to see you go, we wish you well adventurer.</center></p>  
-<p><center>Have a question? Our concierge team operates year-round. Don't hesitate to reach out to them by hitting the <b><Link to="https://worlddominationsummit.com/contact-us">bat signal here.</Link></b></center></p>
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## We wish you well adventurer...</Block>
+      <p><center>Per your request, we've removed you from the WDS X waitlist.</center></p>
+   <p><center>Though we're sad to see you go, we wish you well.</center></p>  
+<p><center>Was this a mistake? Our concierge team operates year-round. Don't hesitate to reach out to them by hitting the <b><Link to="https://worlddominationsummit.com/contact-us">bat signal here.</Link></b></center></p>
   </Block>   
 </Section>
