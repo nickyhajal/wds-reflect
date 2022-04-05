7af34d5fff167245b0e0b4d4eda96b7421e4cc40
Jedidiah <jedidiah475@gmail.com>
2022-04-05 10:02:41 -0700

    Update trex-confirmed.js

--- content/pages/trex-confirmed.js
+++ content/pages/trex-confirmed.js
@@ -4,7 +4,7 @@
 >
   <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
     <Block width="100%" margin="40px 0 0 0" textAlign="center">## Aw Yeah, Your T-Rex Registration is Confirmed!</Block>
-    <Block width="100%" margin="0 0 40px 0"><Image src="https://giphy.com/media/l3mZ8KXl5HjOgiOZ2/giphy.gif" width="100%" height="200px" /></Block>
+    <Block width="100%" margin="0 0 40px 0"><Image src="https://giphy.com/media/l3mZ8KXl5HjOgiOZ2/giphy.gif" width="100%" height="300px" /></Block>
    <p>You're all set! Just come to the T-Rex Takeover gathering point at <b>Pioneer Courthouse Square on Friday, June 24 at 8am</b> and present your photo ID at the check-in to collect your suit.</p>  
    <p>As we get closer to the event, we'll send more information and details so stay tuned!</p>
    <p>Have any questions? Please contact our concierge team at <Link to="mailto:concierge@wds.fm">concierge@wds.fm</Link> and they'll be able to assist you.</p>
