00256559b21f8ec466af1a0b2cdafafd8ee94ec0
Jedidiah <jedidiah475@gmail.com>
2022-04-05 08:48:55 -0700

    Create trex-confirmed.js

---
+++ content/pages/trex-confirmed.js
@@ -0,0 +1,15 @@
+<Section
+  color="blue"
+  css={{ marginBottom: '-2px' }}
+>
+  <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## Aw Yeah, Your T-Rex Registration is Confirmed!</Block>
+    <Block width="100%" margin="0 0 40px 0"><Image src="https://giphy.com/media/l3mZ8KXl5HjOgiOZ2/giphy.gif" width="100%" height="400" /></Block>
+   <p>You're all set! Just come to the T-rex Takeover gathering point at Pioneer Courthouse Square on Friday, June 24 at 8am and present your photo ID at the check-in to collect your suit.</p>  
+   <p>As we get closer to the event, we'll send more information and details so stay tuned!</p>
+   <p>Have any questions? Please contact our concierge team at <Link to="mailto:concierge@wds.fm">concierge@wds.fm</Link> and they'll be able to assist you.</p>
+   <p>Want to register an additional WDS attendee? Click on the link below.</p> 
+
+   ### [--> T-Rex Takeover registration page](https://worlddominationsummit.com/trex)
+  </Block>
+</Section>
