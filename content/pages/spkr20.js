d76485a5bb079774fb6a3d964a5ae544814e679a
Jedidiah <jedidiah475@gmail.com>
2022-04-18 23:02:07 -0700

    Update spkr20.js

--- content/pages/spkr20.js
+++ content/pages/spkr20.js
@@ -6,6 +6,39 @@
   color="white"
   preload
 >
+      <Image src="v2/headings/schedule.png" width="421" height="151" className="scriptHeader" preload/>
+<Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+    ## How the WDS Schedule Works
+    As we've done since year one, the main events (main-stage keynotes and celebrations) take place during the weekend of WDS. Additionally, every attendee will have the ability to add events and activities throughout the entire week using the WDS mobile app to create their own personalized schedule. 
+
+You decide if you'll join us for the weekend or come for the entire week. Regardless, a WDS ticket gives you access to it all: main events, special discounts, bonuses, and surprises.  
+        </Block>
+ <Image src="photos/audience.png"
+    width="426" height="263"
+    margin="0 0 -100px -50px"
+    mobile={{ phone: { margin: '0 0 -100px -20px', width: '100%' } }}
+    preload
+  />
+  <Block type="padded" cols="6" bleed={false} background="canvas" className="table-list" css={{ padding: '60px 40px', position: 'relative', zIndex:'4' }} clip="top_rl_1.6">
+    ## A Week of WDS: June 23rd - June 29th, 2020
+
+    - **Tuesday (6/23)** Activities at WDS HQ and Attendee-Led Meetups
+    - **Wednesday (6/24)** Academies, Activities at WDS HQ and Attendee-Led Meetups
+    <ul><li><b className="highlight highlight-2">Thursday (6/25)</b> Academies, Activities, and Attendee-Led Meetups, and Opening Party</li></ul>
+    <ul><li><b className="highlight highlight-2">Friday (6/26)</b>  Academies, Activities, Attendee-Led Meetups, World Record Attempt</li></ul>
+    <ul><li><b className="highlight highlight-2">Saturday(6/27)</b> Main Stage Keynotes, Attendee-Led Meetups, and Evening Activities</li></ul>
+    <ul><li><b className="highlight highlight-3">Sunday (6/28)</b> Main Stage Keynotes, Attendee-Led Meetups, and the Closing Party</li></ul>
+    - **Monday (6/29)** Attendee-Led Meetups & Final Farewells
+      <div className="note">Can't come for the whole time? No problem, but you don't want to miss the main events taking place on Thursday (evening), Friday, Saturday, and Sunday. For the best possible WDS experience we recommend arriving no later than Thursday afternoon, and leaving no earlier than Monday morning.</div>
+  </Block>
+<Image src="photos/jellyfish.png" width="426" height="263" margin="-50px -60px 100px 0" css={{ float: 'right' }} mobile={{ all: { margin: '-50px -15px 100px 0' } }} />
+  <div className="clear" />
+<Block cols="6" bleed={false} textAlign="center" margin="80px 0 80px">
+## Registered for WDS 2020? Start here!
+Click on the button for a step-by-step guide on how to prepare and maximize your WDS experience.
+<Button to="https://worlddominationsummit.com/guide" style={{ fontSize: '22px', padding: '18px 100px', marginTop: '80px', marginBottom: '40px' }} fitToText>WDS 2020 Guide</Button>
+</Block>
+</Section>
   <Section color="canvas">
   <Block>## Speakers and Special Guests For WDS X</Block>
   <Block cols="6" className="bigger">
@@ -45,7 +78,6 @@ Ozan has been called a “true original” by Adam Grant and dubbed a “superhe
 
           </Block>
     </Tab>
-  </Tabs>
 <Tab title="Samira">
       <Image src="https://el-files-wds.s3.amazonaws.com/images/speakers/Samira-Rajabi-WDS-Main-Stage-Speaker.jpeg" width="904" height="549" mobile={{ all: { width: '100%' }}} margin="0 auto 60px" preload/>
       <Block cols="6" textAlign="left">
