d36d7925bb0b9160b0121469efa86f6223c1f320
Jedidiah <jedidiah475@gmail.com>
2022-04-18 22:54:19 -0700

    Update spkr20.js

--- content/pages/spkr20.js
+++ content/pages/spkr20.js
@@ -6,75 +6,52 @@
   color="white"
   preload
 >
-  <Image src="v2/headings/schedule.png" width="421" height="151" className="scriptHeader" preload/>
-<Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
-    ## How the WDS Schedule Works
-    As we've done since year one, the main events (main-stage keynotes and celebrations) take place during the weekend of WDS. Additionally, every attendee will have the ability to add events and activities throughout the entire week using the WDS mobile app to create their own personalized schedule. 
-
-You decide if you'll join us for the weekend or come for the entire week. Regardless, a WDS ticket gives you access to it all: main events, special discounts, bonuses, and surprises.  
-        </Block>
- <Image src="photos/audience.png"
-    width="426" height="263"
-    margin="0 0 -100px -50px"
-    mobile={{ phone: { margin: '0 0 -100px -20px', width: '100%' } }}
-    preload
-  />
-  <Block type="padded" cols="6" bleed={false} background="canvas" className="table-list" css={{ padding: '60px 40px', position: 'relative', zIndex:'4' }} clip="top_rl_1.6">
-    ## A Week of WDS: June 23rd - June 29th, 2020
-
-    - **Tuesday (6/23)** Activities at WDS HQ and Attendee-Led Meetups
-    - **Wednesday (6/24)** Academies, Activities at WDS HQ and Attendee-Led Meetups
-    <ul><li><b className="highlight highlight-2">Thursday (6/25)</b> Academies, Activities, and Attendee-Led Meetups, and Opening Party</li></ul>
-    <ul><li><b className="highlight highlight-2">Friday (6/26)</b>  Academies, Activities, Attendee-Led Meetups, World Record Attempt</li></ul>
-    <ul><li><b className="highlight highlight-2">Saturday(6/27)</b> Main Stage Keynotes, Attendee-Led Meetups, and Evening Activities</li></ul>
-    <ul><li><b className="highlight highlight-3">Sunday (6/28)</b> Main Stage Keynotes, Attendee-Led Meetups, and the Closing Party</li></ul>
-    - **Monday (6/29)** Attendee-Led Meetups & Final Farewells
-      <div className="note">Can't come for the whole time? No problem, but you don't want to miss the main events taking place on Thursday (evening), Friday, Saturday, and Sunday. For the best possible WDS experience we recommend arriving no later than Thursday afternoon, and leaving no earlier than Monday morning.</div>
-  </Block>
-<Image src="photos/jellyfish.png" width="426" height="263" margin="-50px -60px 100px 0" css={{ float: 'right' }} mobile={{ all: { margin: '-50px -15px 100px 0' } }} />
-  <div className="clear" />
-<Block cols="6" bleed={false} textAlign="center" margin="80px 0 80px">
-## Registered for WDS 2020? Start here!
-Click on the button for a step-by-step guide on how to prepare and maximize your WDS experience.
-<Button to="https://worlddominationsummit.com/guide" style={{ fontSize: '22px', padding: '18px 100px', marginTop: '80px', marginBottom: '40px' }} fitToText>WDS 2020 Guide</Button>
-</Block>
-</Section>
   <Section color="canvas">
-  <Block>## Speakers and Special Guests For WDS 2020</Block>
+  <Block>## Speakers and Special Guests For WDS X</Block>
   <Block cols="6" className="bigger">
     Every year we're excited to invite several friends of WDS living extraordinary lives and working to build a better world. Our keynote speakers are bestselling authors, leading activists, and in-demand storyteller and they come to WDS to share their stories, experiences, and lessons with our community. 
     
-    Check out this superhero lineup for WDS 2020:
+    Check out this superhero lineup for the WDS main stage:
   </Block>
   <Tabs style="simple" margin="80px 0 0">
-    <Tab title="Arlan">
-      <Image src="https://el-files-wds.s3.amazonaws.com/images/speakers/Arlan-Hamilton.WDS.2020.speaker.png" width="904" height="549" margin="0 auto 60px" mobile={{ all: { width: '100%' }}} preload/>
+    <Tab title="Tiago">
+      <Image src="https://el-files-wds.s3.amazonaws.com/images/speakers/Tiago-Forte-WDS-Main-Stage-Speaker.jpg" width="904" height="549" margin="0 auto 60px" mobile={{ all: { width: '100%' }}} preload/>
       <Block cols="6" textAlign="left">
-        ## Arlan Hamilton
-      **Arlan Hamilton** built a venture capital fund from the ground up, while homeless. She is the Founder and Managing Partner of **[Backstage Capital](https://backstagecapital.com/)**, a fund that is dedicated to minimizing funding disparities in tech by investing in high-potential founders who are people of color, women, and/or LGBT. 
-
-Started from scratch in 2015, Backstage has now raised more than $7 million and invested in more than 130 startup companies led by underestimated founders. In 2018 Arlan co-founded Backstage Studio which launched four accelerator programs for underestimated founders in Detroit, Los Angeles, Philadelphia, and London.
+        ## Tiago Forte
+        
+       What started as a quest for **Tiago Forte** to deal with a chronic illness turned into a life-long mission to help people harness the full potential of their ideas and turn them into reality. Tiago has spent more than 10 years researching and personally experimenting with a new way of organizing our digital lives and using software to improve our productivity as creative professionals.
 
 </Block>
     </Tab>
- <Tab title="Colin">
-      <Image src="https://el-files-wds.s3.amazonaws.com/images/speakers/ColinOBrady.WDS.2020.speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
+ <Tab title="Emily">
+      <Image src="https://el-files-wds.s3.amazonaws.com/images/speakers/Emily-Rudow-WDS-Main-Stage-Speaker.jpg" mobile={{ all: { width: '100%' }}} width="904" height="549" margin="0 auto 60px" preload/>
       <Block cols="6" textAlign="left">
-        ## Colin O'Brady
-        **Colin O’Brady** is a world record holding explorer and one of the world’s best endurance athletes. Colin is regarded as a foremost expert on mindset. 
+        ## Emily Rudow
+        **Emily Rudow** is a digital marketer, an entrepreneur, and a fitness writer. She holds a bachelor’s degree in business administration from Wilfrid Laurier University. Emily is also an avid long-distance runner. In 2017, she broke a world record by running 74 consecutive half-marathon distances (21.1 kilometers/13.1 miles) while raising over $10,000 for the Canadian Cancer Society. Since then, she hasn’t stopped running. To date, she has run an average of 10 kilometers every day for over four years.
 
-He isn’t your typical athlete despite his unmatched athletic accomplishments - a world first solo crossing of Antarctica, a world first row across Drake Passage, summiting Mt. Everest, and setting three prestigious mountaineering world records - and he has done it all after overcoming a devastating accident to prove that anything is possible.
+Her new book, Find Your Stride, is available now at all major retailers.
 
       </Block>
     </Tab>  
- <Tab title="Paloma">
-      <Image src="https://el-files-wds.s3.amazonaws.com/images/speakers/PalomaMedina.WDS.2020.speaker.png" width="904" height="549" mobile={{ all: { width: '100%' }}} margin="0 auto 60px" preload/>
+ <Tab title="Ozan">
+      <Image src="https://el-files-wds.s3.amazonaws.com/images/speakers/Ozan-Varol-WDS-Main-Stage-Speaker.jpg" width="904" height="549" mobile={{ all: { width: '100%' }}} margin="0 auto 60px" preload/>
       <Block cols="6" textAlign="left">
-        ## Paloma Medina
+        ## Ozan Varol
                                                                                                                                               
-        **Paloma Medina** is a performance coach, speaker, and founder of 11:11 Supply, a retail startup that specializes in beautiful work tools designed to help you feel more productive, happy and balanced in your everyday life. 
+**Ozan Varol** is a rocket scientist turned award-winning professor and bestselling author of Think Like a Rocket Scientist: Simple Strategies You Can Use to Make Giant Leaps in Work and Life. The book has been translated to over 20 foreign languages and has sold over 100,000 copies worldwide. 
 
-Her background includes working with tech companies, homeless healthcare clinics, as well as individual leaders and CEOs. She was born in Guadalajara, Mexico and has made a home since on the West Coast since she was 8 years old.
+Ozan has been called a “true original” by Adam Grant and dubbed a “superhero” by Dan Pink. His work has been described as “must read” by Susan Cain and featured in the Wall Street Journal, Time, BBC, CNN, Fast Company, Washington Post, and more.
+
+
+          </Block>
+    </Tab>
+  </Tabs>
+<Tab title="Samira">
+      <Image src="https://el-files-wds.s3.amazonaws.com/images/speakers/Samira-Rajabi-WDS-Main-Stage-Speaker.jpeg" width="904" height="549" mobile={{ all: { width: '100%' }}} margin="0 auto 60px" preload/>
+      <Block cols="6" textAlign="left">
+        ## Samira Rajabi
+                                                                                                                                              
+**Samira Rajabi, Ph.D.**, is currently an Assistant Professor of Media Studies at the University of Colorado. Samira is also a freelance culture writer and qualitative research consultant specializing in research design. Her most recent book, All of my friends live in my computer: Tactical Media, Trauma and Meaning Making is available now.  Her research and writing center around how we make sense of trauma using the platform affordances of digital technologies and social media.  She is currently working on her next book, a field guide to help everyday people cope with ambiguous grief through digital media. Previously she was a Postdoctoral Fellow at the Center for Advanced Research in Global Communication at the Annenberg School for Communication at the University of Pennsylvania.
 
           </Block>
     </Tab>
