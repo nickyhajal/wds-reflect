b48dc3a60aae6e5b4aa0fa3d22194a6d87e00266
Jedidiah <jedidiah475@gmail.com>
2022-06-08 13:00:07 -0700

    Create story-received.js

---
+++ content/pages/story-received.js
@@ -0,0 +1,13 @@
+<Section
+  color="blue"
+  css={{ marginBottom: '-2px' }}
+>
+  <Block width="768px" bleed={true} type="padded" background="gray" textAlign="left" margin="60px 0 80px" clip="tr:0,1%;">
+    <Block width="100%" margin="40px 0 0 0" textAlign="center">## You're awesome. Thank you for sharing with us your story with us!</Block>
+  
+   <p>Our team will review all of the submissions and do our best to feature as many of them as possible whether on the main stage or in other ways. If selected we'll reach out directly with next steps.</p>  
+   <p>Don't stop believing,</p>
+   <p>-WDS Team</p>
+
+  </Block>
+</Section>
