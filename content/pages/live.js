b32bb5e04d3533e58b1f2a28f68a001b817c8f05
Jedidiah <jedidiah475@gmail.com>
2022-06-21 07:26:53 -0700

    Update live.js

--- content/pages/live.js
+++ content/pages/live.js
@@ -3,9 +3,16 @@
   headerClip="rl_header"
   headerPosition="top"
   headerSize="thin"
-  color="sea"
+  color="white"
   preload
 >
+      <Block cols="6" bleed={false} textAlign="left" margin="80px 0 80px">
+    ## WDS X Livestream
+    Wherever you are right now, we're glad you're here for the main stage livestream. The following schedule is when we hope to start and end sessions. As you know, due to the nature of live events, the timing may not be exact.      
+
+      </Block>
+<Video placeholder="photos/academies2.jpg" videoId="121164251" cols="8" /> 
+  </Section>   
   <Image src="photos/audience.png"
     width="426" height="263"
     margin="0 0 -100px -50px"
@@ -13,16 +20,13 @@
     preload
   />
    <Block type="padded" cols="6" bleed={false} background="canvas" className="table-list" css={{ padding: '60px 40px', position: 'relative', zIndex:'4' }} clip="top_lr_1.6">
-    ## WDS: June 21 - June 26, 2022
-
-    - **Tuesday (6/21)** WDS HQ and Attendee-Led Meetups
-    - **Wednesday (6/22)** Academies, WDS HQ and Attendee-Led Meetups
-    - **Thursday (6/23)** Academies, WDS HQ, and Attendee-Led Meetups
-    <ul><li><b className="highlight highlight-2">Friday (6/24)</b>  Academies, WDS HQ, Attendee-Led Meetups, World Record Attempt, and Opening Party</li></ul>
-    <ul><li><b className="highlight highlight-2">Saturday(6/25)</b> Main Stage Keynotes and Attendee-Led Meetups</li></ul>
-    <ul><li><b className="highlight highlight-3">Sunday (6/26)</b> Main Stage Keynotes, Attendee-Led Meetups, and the Closing Party</li></ul>
-      <i>Can't come for the whole time? No problem, but you don't want to miss the main events taking place on Thursday (evening), Friday, Saturday, and Sunday. For the best possible WDS experience we recommend arriving no later than Thursday afternoon, and leaving no earlier than Monday morning.</i>
+    ## WDS X: Livestream Schedule (in Pacific Time)
+    - **Saturday Morning, 9:00am - 12:30pm** Main Stage Session 1 (with midway break)
+    - **Saturday Afternoon** 4:30pm - 6:30pm** Main Stage Session 2
+    <ul><li><b className="highlight highlight-2">Sunday Morning, 9:00am - 12:30pm** Main Stage Session 3 (with midway break)</li></ul>
+    <ul><li><b className="highlight highlight-2">Sunday Afternoon, 4:30pm - 6:30pm** Main Stage Session 4</li></ul>
+      
   </Block>
-  <Image src="photos/jellyfish.png" width="426" height="263" margin="-50px -60px 100px 0" css={{ float: 'right' }} mobile={{ all: { margin: '-50px -15px 100px 0' } }} />
+  <Image src="photos/academies.jpg" width="426" height="263" margin="-50px -60px 100px 0" css={{ float: 'right' }} mobile={{ all: { margin: '-50px -15px 100px 0' } }} />
   <div className="clear" />
     </Section> 
