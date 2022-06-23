3b61d4455e63106ffbda94b482aaa48bd748fa0b
Jedidiah <jedidiah475@gmail.com>
2022-06-23 15:23:13 -0700

    Update live.js

--- content/pages/live.js
+++ content/pages/live.js
@@ -12,12 +12,7 @@
 
       </Block>
 <Video placeholder="photos/academies2.jpg" videoId="121164251" cols="8" />  
-  <Image src="photos/audience.png"
-    width="426" height="263"
-    margin="0 0 -100px -50px"
-    mobile={{ phone: { margin: '0 0 -100px -20px', width: '100%' } }}
-    preload
-  />
+
    <Block type="padded" cols="6" bleed={false} background="canvas" className="table-list" css={{ padding: '60px 40px', position: 'relative', zIndex:'4' }} clip="top_lr_1.6">
     ## WDS X: Livestream Schedule (in Pacific Time)
     - **Saturday Morning, 9:00am - 12:30pm** Main Stage Session 1 (with midway break)
@@ -26,6 +21,4 @@
     <ul><li><b className="highlight highlight-2">Sunday Afternoon, 4:30pm - 6:30pm</b> Main Stage Session 4</li></ul>
       
   </Block>
-  <Image src="photos/academies.jpg" width="426" height="263" margin="-50px -60px 100px 0" css={{ float: 'right' }} mobile={{ all: { margin: '-50px -15px 100px 0' } }} />
-  <div className="clear" />
     </Section> 
