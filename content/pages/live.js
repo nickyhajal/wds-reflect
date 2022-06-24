cf004a64c260b0d975d1ec732218ce8d68cd8d30
Jedidiah <jedidiah475@gmail.com>
2022-06-23 20:46:48 -0700

    Update live.js

--- content/pages/live.js
+++ content/pages/live.js
@@ -17,8 +17,8 @@
     ## WDS X: Livestream Schedule (in Pacific Time)
     - **Saturday Morning, 9:00am - 12:30pm** Main Stage Session 1 (with midway break)
     - **Saturday Afternoon** 4:30pm - 6:30pm** Main Stage Session 2
-    <ul><li><b className="highlight highlight-2">Sunday Morning, 9:00am - 12:30pm</b> Main Stage Session 3 (with midway break)</li></ul>
-    <ul><li><b className="highlight highlight-2">Sunday Afternoon, 4:30pm - 6:30pm</b> Main Stage Session 4</li></ul>
+    - **Sunday Morning** 9:00am - 12:30pm** Main Stage Session 3 (with midway break)
+     - **Sunday Afternoon** 4:30pm - 6:30pm** Main Stage Session 4
       
   </Block>
     </Section> 
