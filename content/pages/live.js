8fe3802a3b5d37957332feca92a1edee57fe61d5
Jedidiah <jedidiah475@gmail.com>
2022-06-23 19:56:02 -0700

    Update live.js

--- content/pages/live.js
+++ content/pages/live.js
@@ -11,7 +11,7 @@
     Wherever you are right now, we're glad you're here for the main stage livestream. The following schedule is when we hope to start and end sessions. As you know, due to the nature of live events, the timing may not be exact.      
 
       </Block>
-<Video placeholder="photos/academies2.jpg" videoId="121164251" cols="8" />  
+<Video placeholder="photos/academies2.jpg" videoId="" cols="8" />  
 
    <Block type="padded" cols="6" bleed={false} background="canvas" className="table-list" css={{ padding: '60px 40px', position: 'relative', zIndex:'4' }} clip="top_lr_1.6">
     ## WDS X: Livestream Schedule (in Pacific Time)
