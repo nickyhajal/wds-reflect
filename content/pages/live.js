514cf2713643d05030fd23e6377a4f841b3623ee
Nicky Hajal <nhajal@gmail.com>
2022-06-25 09:41:53 -0700

    fullscreen

--- content/pages/live.js
+++ content/pages/live.js
@@ -13,7 +13,7 @@
     Announcement (6/24): Please note the image/video below will not be ready until tomorrow's first main stage session. Thanks. 
 
       </Block>
-      <div style={{padding:'56.25% 0 0 0', position:'relative'}}><iframe src="https://player.vimeo.com/video/723990931?h=d1c9d2dc77&color=ffffff&title=0&byline=0&portrait=0" style={{position:'absolute', top:'0', left:'0', width:'100%', height:'100%'}} frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+      <div style={{padding:'56.25% 0 0 0', position:'relative'}}><iframe src="https://player.vimeo.com/video/723990931?h=d1c9d2dc77&color=ffffff&title=0&byline=0&portrait=0" style={{position:'absolute', top:'0', left:'0', width:'100%', height:'100%'}} frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullscreen="true"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
    <Block type="padded" cols="6" bleed={false} background="canvas" className="table-list" css={{ padding: '60px 40px', position: 'relative', zIndex:'4' }} clip="top_lr_1.6">
     ## WDS X: Livestream Schedule (in Pacific Time)
     - **Saturday Morning: 9:00am - 12:30pm** Main Stage Session 1 (with midway break)
