9453c33e7ebc28bcba1b1ced5c0bc8e282efc2f9
Jedidiah <jedidiah475@gmail.com>
2022-07-18 22:54:03 -0700

    Create emily-rudow.js

---
+++ content/variables/speakers/emily-rudow.js
@@ -0,0 +1,11 @@
+name: "Emily Rudow",
+year: "2022",
+headerPhotos: "https://live.staticflickr.com/65535/52186024810_1059aa8d5f_k_d.jpg, https://live.staticflickr.com/65535/52185544481_ece1df2bcb_k_d.jpg",
+photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Emily-Rudow.png",
+video: "https://youtu.be/VwdsK69N05I",
+headerPosition: "-70px",
+quote: "",
+---
+**Emily Rudow** is a digital marketer, an entrepreneur, fitness writer, and an avid long-distance runner. In 2017, she broke a world record by running 74 consecutive half-marathon distances (21.1 kilometers/13.1 miles) while raising over $10,000 for the Canadian Cancer Society. Since then, she hasn’t stopped running. To date, she has run an average of 10 kilometers every day for over four years.  
+
+Emily’s debut book _Find Your Stride: A Personalized Path to Sustainable Nutrition and Training_ is available now.
