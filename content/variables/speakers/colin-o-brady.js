c890c412b76385839eebe93fa24d3b0126dbea39
Jedidiah <jedidiah475@gmail.com>
2022-07-18 22:26:48 -0700

    Update colin-o-brady.js

--- content/variables/speakers/colin-o-brady.js
+++ content/variables/speakers/colin-o-brady.js
@@ -3,7 +3,7 @@ year: "2022",
 headerPhotos: "https://live.staticflickr.com/65535/52185788534_0a9e579f97_k_d.jpg, https://live.staticflickr.com/65535/52184509152_fefa11d38d_k_d.jpg",
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/ColinOBrady.WDS.speaker.png",
 video: "",
-headerPosition: "-10px",
+headerPosition: "-50px",
 quote: "",
 ---
 **Colin O’Brady** is a world record holding explorer and one of the world’s best endurance athletes. Colin is regarded as a foremost expert on mindset.  
