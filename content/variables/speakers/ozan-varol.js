0d94d92344b37df76cda9e7b3395df5b86273171
Jedidiah <jedidiah475@gmail.com>
2022-07-18 23:06:02 -0700

    Create ozan-varol.js

---
+++ content/variables/speakers/ozan-varol.js
@@ -0,0 +1,11 @@
+name: "Ozan Varol",
+year: "X",
+headerPhotos: "https://live.staticflickr.com/65535/52185335066_e0c4c5d940_k_d.jpg, https://live.staticflickr.com/65535/52185819150_7a91f99630_k_d.jpg",
+photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Ozan-Varol-WDS-Main-Stage-Speaker.png",
+video: "https://youtu.be/cOsIvqAnd20",
+headerPosition: "-30px",
+quote: "",
+---
+**Ozan Varol** is a rocket scientist turned award-winning professor and bestselling author of _Think Like a Rocket Scientist: Simple Strategies You Can Use to Make Giant Leaps in Work and Life_. The book has been translated to over 20 foreign languages and has sold over 100,000 copies worldwide.  
+
+Ozan has been called a “true original” by Adam Grant and dubbed a “superhero” by Dan Pink. His work has been described as “must read” by Susan Cain and featured in the _Wall Street Journal_, _Time_, _BBC_, _CNN_, _Fast Company_, _Washington Post_, and more.
