a0301eba12f252ffd36703af00de9d82918f3c13
Jedidiah <jedidiah475@gmail.com>
2022-07-18 23:16:20 -0700

    Create tiago-forte.js

---
+++ content/variables/speakers/tiago-forte.js
@@ -0,0 +1,13 @@
+name: "Tiago Forte",
+year: "WDS X (2022)",
+headerPhotos: "https://live.staticflickr.com/65535/52186024050_fc7a82a3e4_k_d.jpg, https://live.staticflickr.com/65535/52186024130_c2b7e21fb5_k_d.jpg",
+photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Tiago-Forte-WDS-Main-Stage-Speaker.png",
+video: "https://youtu.be/XlAqrS-fSAI",
+headerPosition: "-90px",
+quote: "",
+---
+**Tiago Forte** is one of the world’s foremost experts on productivity. He has taught more than 20,000 people worldwide through his programs and writes and speaks on how technology can help knowledge workers revolutionize their personal effectiveness.    
+
+What started as a quest to deal with a chronic illness turned into a life-long mission to help people harness the full potential of their ideas and turn them into reality.  
+
+His work has been featured in media outlets like _The New York Times_, _Lifehacker_, _The Atlantic_, _Thrive Global_, and _Quartz. 
