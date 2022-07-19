07d2288d7cd06c9c9f3e5c0817a2696051541bd7
Jedidiah <jedidiah475@gmail.com>
2022-07-18 23:37:43 -0700

    Update jonathan-fields-x.js

--- content/variables/speakers/jonathan-fields-x.js
+++ content/variables/speakers/jonathan-fields-x.js
@@ -3,6 +3,6 @@ year: "WDS X (2022)",
 headerPhotos: "https://live.staticflickr.com/65535/52186151185_51518fccc9_k_d.jpg, https://live.staticflickr.com/65535/52185673848_b923b3cfda_k_d.jpg",
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/jonathan-fields-2.png",
 video: "https://youtu.be/I3eaQk6ez10",
-quote:  "A good life is not a place at which you arrive, it is a lens through which you see and create your world.",
+quote: "A good life is not a place at which you arrive, it is a lens through which you see and create your world.",
 ---
 **Jonathan Fields** is on a quest to inspire possibility. A New York City dad, husband, entrepreneur and author, he founded mission-driven media and education venture, Good Life Project®, where he and his team lead a global community in the quest to live more meaningful, connected and vital lives, produce a top-rated podcast with millions of downloads, and offer a growing catalog of events, trainings and courses.
