81b256a9673bf6405c42a8b72b5750078dcc58e2
Jedidiah <jedidiah475@gmail.com>
2022-07-18 22:44:33 -0700

    Update karla-starr.js

--- content/variables/speakers/karla-starr.js
+++ content/variables/speakers/karla-starr.js
@@ -3,9 +3,9 @@ year: "2022",
 headerPhotos: "https://live.staticflickr.com/65535/52185676633_0ab2bd8c3c_k_d.jpg, https://live.staticflickr.com/65535/52186153835_2d3ab6ce52_k_d.jpg",
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Karla-Starr-WDS.speaker.png",
 video: "https://youtu.be/ow1bZvVWAsc",
-headerPosition: "-10px",
+headerPosition: "-40px",
 quote: "",
 ---
 **Karla Starr** is a writer and Underdog Advocate. In addition to being a columnist for _Medium_, Karla also writes the newsletter, _The Starr Report_ on _Substack_.  
-        Karla has appeared on _NPR_ and _CBS Sunday Morning_, and has written for _The Atlantic_, _Slate_, _Popular Science_, and the _Guardian_.  
-        Her most recent book, _Making Numbers Count: The Art and Science of Communicating Numbers_, with Chip Heath (_Switch, Made to Stick_) has been met with critical acclaim from _The Wall Street Journal_, _Kirkus Reviews_, and _Adam Grant_.  
+Karla has appeared on _NPR_ and _CBS Sunday Morning_, and has written for _The Atlantic_, _Slate_, _Popular Science_, and the _Guardian_.  
+Her most recent book, _Making Numbers Count: The Art and Science of Communicating Numbers_, with Chip Heath (_Switch, Made to Stick_) has been met with critical acclaim from _The Wall Street Journal_, _Kirkus Reviews_, and _Adam Grant_.  
