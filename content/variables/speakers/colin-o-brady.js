7c39b3ee7884eae595ee33e5e30d372a9d5bd096
Jedidiah <jedidiah475@gmail.com>
2022-07-18 22:21:36 -0700

    Create colin-o-brady.js

---
+++ content/variables/speakers/colin-o-brady.js
@@ -0,0 +1,10 @@
+name: "Colin O’Brady",
+year: "2022",
+headerPhotos: "https://live.staticflickr.com/65535/52185788534_0a9e579f97_k_d.jpg, https://live.staticflickr.com/65535/52184509152_fefa11d38d_k_d.jpg",
+photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/ColinOBrady.WDS.speaker.png",
+video: "",
+headerPosition: "-10px",
+quote: "",
+---
+**Colin O’Brady** is a world record holding explorer and one of the world’s best endurance athletes. Colin is regarded as a foremost expert on mindset.  
+ He isn’t your typical athlete despite his unmatched athletic accomplishments - a world first solo crossing of Antarctica, a world first row across Drake Passage, summiting Mt. Everest, and setting three prestigious mountaineering world records - and he has done it all after overcoming a devastating accident to prove that anything is possible.  
