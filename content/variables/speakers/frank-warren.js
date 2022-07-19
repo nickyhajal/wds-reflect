0cc1fa47d6b1d3599227b0ab71964cc99f10a511
Jedidiah <jedidiah475@gmail.com>
2022-07-18 23:01:15 -0700

    Create frank-warren.js

---
+++ content/variables/speakers/frank-warren.js
@@ -0,0 +1,11 @@
+name: "Frank Warren",
+year: "X",
+headerPhotos: "https://live.staticflickr.com/65535/52186013320_400e09aea9_k_d.jpg, https://live.staticflickr.com/65535/52185537551_5b108057fd_k_d.jpg",
+photo: "https://s3.amazonaws.com/el-files-wds/images/speakers/Humble-The-Poet-thumbnail.png",
+video: "https://www.youtube.com/watch?v=Bx50BUOwp-8",
+headerPosition: "-10px",
+quote: "",
+---
+**Frank Warren** is the creator of The PostSecret Project, over one million highly personal postcards mailed anonymously from around the world. They display soulful secrets we never voice—from shocking to romantic, from tragic to laugh-out-loud funny.  
+
+They have been published in six best-selling books and on the award-winning PostSecret blog. Frank’s multimedia live events reveal the stories behind the secrets, what he has learned as “America’s most trusted stranger,” and how sharing our stories deepens relationships and transforms lives.
