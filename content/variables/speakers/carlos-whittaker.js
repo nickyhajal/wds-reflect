e337f24bd30ebb08a3ce05e87393a8b8b410b937
Jedidiah <jedidiah475@gmail.com>
2022-07-18 22:48:39 -0700

    Create carlos-whittaker.js

---
+++ content/variables/speakers/carlos-whittaker.js
@@ -0,0 +1,10 @@
+name: "Carlos Whittaker",
+year: "2022",
+headerPhotos: "https://live.staticflickr.com/65535/52185770014_acd73377d6_k_d.jpg, https://live.staticflickr.com/65535/52185769684_284352e9b8_k_d.jpg",
+photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Carlos-Whittaker.png",
+video: "https://youtu.be/nqTUar3B1bc",
+headerPosition: "-60px",
+quote: "",
+---
+**Carlos Whittaker** is a self-proclaimed “Hope Dealer” who spends the majority of his time telling stories on stages and phone screens across the world that lead people towards action.  
+He’s a Peoples Choice Award winner and the author of three books: _Moment Maker_, _Kill The Spider_, & _Enter Wild_ which all lead people towards freedom in some way, shape, or form. Carlos is the host of the popular podcast “Human Hope w/ Carlos Whittaker.”
