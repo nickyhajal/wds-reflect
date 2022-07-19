9df583623b392a1140bb2130559f42fd358276ba
Jedidiah <jedidiah475@gmail.com>
2022-07-18 22:43:32 -0700

    Create gary-ware.js

---
+++ content/variables/speakers/gary-ware.js
@@ -0,0 +1,11 @@
+name: "Gary Ware",
+year: "2022",
+headerPhotos: "https://live.staticflickr.com/65535/52173960937_1a70698a1d_k_d.jpg, https://live.staticflickr.com/65535/52174987113_5709cb038c_k_d.jpg",
+photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Gary-Ware-WDS-Speaker.png",
+video: "https://youtu.be/4rqz5PqaSHY",
+headerPosition: "-10px",
+quote: "",
+---
+**Gary Ware** is a corporate facilitator, certified coach, and Creative Catalyst. He has over 14 years of experience holding leadership positions in the corporate world and nearly a decade as a performer in improv theatre.  
+        After experiencing burnout, he realized that what was missing was play, leading him to discover his passion for facilitating.  
+        Through his company Breakthrough Play, Gary uses the power of applied improvisation and other playful methods to assist people in unlocking creativity, confidence, and better communication and was recently featured as one of the Top 100 HR influencers of 2021 by Engagedly.  
