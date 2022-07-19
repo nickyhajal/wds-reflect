96b95f87f7af153cbeab9a92db1370bd504ff3b3
Jedidiah <jedidiah475@gmail.com>
2022-07-18 23:21:29 -0700

    Create laura-pena.js

---
+++ content/variables/speakers/laura-pena.js
@@ -0,0 +1,9 @@
+name: "Laura Peña",
+year: "WDS X (2022)",
+headerPhotos: "https://live.staticflickr.com/65535/52184318732_dd0bd94a42_k_d.jpg, https://live.staticflickr.com/65535/52185339101_05b2a6eb51_k_d.jpg",
+photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Laura_Pena_1_lowres.png",
+video: "https://youtu.be/szcOMGia-mg",
+headerPosition: "-80px",
+quote: "",
+---
+Laura Peña is a Dominican-American speaker, filmmaker, storyteller, entrepreneur, motion designer, traveler, and mentor to young girls. She is the founder of She Is the Universe, an award-winning Global Movement and 501(c) 3 non-profit for girl's empowerment. For three years Laura has traveled the world collecting on film the stories of teenage girls.
