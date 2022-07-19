ee769e0c29266c8c3d407dc9e23ca0a19c267e77
Jedidiah <jedidiah475@gmail.com>
2022-07-18 23:16:53 -0700

    Update samira-rajabi.js

--- content/variables/speakers/samira-rajabi.js
+++ content/variables/speakers/samira-rajabi.js
@@ -3,7 +3,7 @@ year: "WDS X (2022)",
 headerPhotos: "https://live.staticflickr.com/65535/52175464900_a58672cfc6_k_d.jpg, https://live.staticflickr.com/65535/52174973128_70c529a925_k_d.jpg",
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Samira-Rajabi-WDS-Main-Stage-Speaker.png",
 video: "https://youtu.be/bqXhuXNLF88",
-headerPosition: "-10px",
+headerPosition: "-80px",
 quote: "",
 ---
 **Samira Rajabi, Ph.D.**, is currently an Assistant Professor of Media Studies at the University of Colorado. Samira is also a freelance culture writer and qualitative research consultant specializing in research design.  
