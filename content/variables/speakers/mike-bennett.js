89ac94614a918561ed998539ea9993918c2fed69
Jedidiah <jedidiah475@gmail.com>
2022-07-18 23:25:50 -0700

    Create mike-bennett.js

---
+++ content/variables/speakers/mike-bennett.js
@@ -0,0 +1,12 @@
+name: "Mike Bennett",
+year: "WDS X (2022)",
+headerPhotos: "https://live.staticflickr.com/65535/52175391919_4719171d0b_k_d.jpg, https://live.staticflickr.com/65535/52186009940_c7f21140c8_k_d.jpg",
+photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Mike-Bennet-WDS.speaker.png",
+video: "https://youtu.be/EY9ucPB5-Qk",
+headerPosition: "-30px",
+quote: "",
+---
+**Mike Bennett** moved from Central Pennsylvania to Portland, Oregon in the winter of 2016. He studied Art Education, Computer Design, Illustration and Animation at Shippensburg University of Pennsylvania and proceeded to chase his dream of illustration and character design immediately after graduating in 2012. The move to Portland was an incredibly easy choice because of his passion for the outdoors and wanting to live in a community with an appreciation for art.  
+
+Mike will be presenting Dinolandia in downtown Portland this summer. It’s an immersive walk-through two-story museum that features more than 50 species of dinosaurs, ranging from the chicken-sized Compsognathus to the massive floor-to-ceiling T. Rex! 
+
