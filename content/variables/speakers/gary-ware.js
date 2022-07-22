ada8bc9155c39279b960c13f9da5d0c4f97b19de
Jedidiah <jedidiah475@gmail.com>
2022-07-22 15:28:47 -0700

    Update gary-ware.js

--- content/variables/speakers/gary-ware.js
+++ content/variables/speakers/gary-ware.js
@@ -3,7 +3,7 @@ year: "WDS X (2022)",
 headerPhotos: "https://live.staticflickr.com/65535/52173960937_1a70698a1d_k_d.jpg, https://live.staticflickr.com/65535/52174987113_5709cb038c_k_d.jpg",
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Gary-Ware-WDS-Speaker.png",
 video: "https://youtu.be/4rqz5PqaSHY",
-headerPosition: "-100px",
+headerPosition: "-170px",
 quote: "",
 ---
 **Gary Ware** is a corporate facilitator, certified coach, and Creative Catalyst. He has over 14 years of experience holding leadership positions in the corporate world and nearly a decade as a performer in improv theatre.  
