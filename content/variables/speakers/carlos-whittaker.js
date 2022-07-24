d12785dbf1a46a8766723a87ffd817331a73d8e8
Jedidiah <jedidiah475@gmail.com>
2022-07-24 11:49:34 -0700

    Update carlos-whittaker.js

--- content/variables/speakers/carlos-whittaker.js
+++ content/variables/speakers/carlos-whittaker.js
@@ -4,7 +4,9 @@ headerPhotos: "https://live.staticflickr.com/65535/52185770014_acd73377d6_k_d.jp
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Carlos-Whittaker.png",
 video: "https://youtu.be/nqTUar3B1bc",
 headerPosition: "-60px",
-quote: "",
+quote: "Stop showing just the highlights. Start showing your mistakes, showing your authenticity, and you’re going to see that people will be attracted to who you are.",
 ---
-**Carlos Whittaker** is a self-proclaimed “Hope Dealer” who spends the majority of his time telling stories on stages and phone screens across the world that lead people towards action.  
+At WDS X, Carlos shared that we have forgotten “how to human” and shares ideas for how to realign ourselves through authenticity, slowing down, and truly seeing each other.  
+  
+  **Carlos Whittaker** is a self-proclaimed “Hope Dealer” who spends the majority of his time telling stories on stages and phone screens across the world that lead people towards action.  
 He’s a Peoples Choice Award winner and the author of three books: _Moment Maker_, _Kill The Spider_, & _Enter Wild_ which all lead people towards freedom in some way, shape, or form. Carlos is the host of the popular podcast “Human Hope w/ Carlos Whittaker.”
