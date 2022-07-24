f6ce7507c6788f26edbe43f025880870b75faff1
Jedidiah <jedidiah475@gmail.com>
2022-07-24 11:37:14 -0700

    Update samira-rajabi.js

--- content/variables/speakers/samira-rajabi.js
+++ content/variables/speakers/samira-rajabi.js
@@ -4,8 +4,10 @@ headerPhotos: "https://live.staticflickr.com/65535/52175464900_a58672cfc6_k_d.jp
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Samira-Rajabi-WDS-Main-Stage-Speaker.png",
 video: "https://youtu.be/bqXhuXNLF88",
 headerPosition: "-130px",
-quote: "",
+quote: "We have to really mourn, we have to honor where we’ve been.",
 ---
+During her keynote at WDS X, Samira uses her experience with a serious brain tumor to reflect on our losses over recent years and celebrate our ability to turn pain into purpose. She shared about unfinished resilience and the journey of moving forward even through grief.  
+  
 **Samira Rajabi, Ph.D.**, is currently an Assistant Professor of Media Studies at the University of Colorado. Samira is also a freelance culture writer and qualitative research consultant specializing in research design.  
 
 Her most recent book, _All of My Friends Live in my Computer: Tactical Media, Trauma and Meaning Making_ is available now. 
