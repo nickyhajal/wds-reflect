748cf7ac8aa07218e0226e4e67d560430c68310c
Jedidiah <jedidiah475@gmail.com>
2022-07-24 12:03:49 -0700

    Update frank-warren.js

--- content/variables/speakers/frank-warren.js
+++ content/variables/speakers/frank-warren.js
@@ -4,8 +4,10 @@ headerPhotos: "https://live.staticflickr.com/65535/52186013320_400e09aea9_k_d.jp
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Frank-Warren.png",
 video: "https://youtu.be/b-1Vmxhp8Vo",
 headerPosition: "-10px",
-quote: "",
+quote: "Sometimes when we think we’re keeping a secret, that secret’s actually keeping us.",
 ---
-**Frank Warren** is the creator of The PostSecret Project, over one million highly personal postcards mailed anonymously from around the world. They display soulful secrets we never voice—from shocking to romantic, from tragic to laugh-out-loud funny.  
+At WDS X, Frank shared secrets from his PostSecret Project and from the WDS community with touching, meaningful, and hilarious take-aways. He reminded the WDS community that secrets we often keep and make us feel isolated are actually things that can connect us with others. 
+  
+  **Frank Warren** is the creator of The PostSecret Project, over one million highly personal postcards mailed anonymously from around the world. They display soulful secrets we never voice—from shocking to romantic, from tragic to laugh-out-loud funny.  
 
 They have been published in six best-selling books and on the award-winning PostSecret blog. Frank’s multimedia live events reveal the stories behind the secrets, what he has learned as “America’s most trusted stranger,” and how sharing our stories deepens relationships and transforms lives.
