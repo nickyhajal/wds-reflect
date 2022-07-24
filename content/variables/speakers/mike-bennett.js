7844ced19a29ae6a02d43ac41d78587695c42b01
Jedidiah <jedidiah475@gmail.com>
2022-07-24 12:23:16 -0700

    Update mike-bennett.js

--- content/variables/speakers/mike-bennett.js
+++ content/variables/speakers/mike-bennett.js
@@ -4,7 +4,7 @@ headerPhotos: "https://live.staticflickr.com/65535/52175391919_4719171d0b_k_d.jp
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Mike-Bennet-WDS.speaker.png",
 video: "https://youtu.be/EY9ucPB5-Qk",
 headerPosition: "-30px",
-quote: "",
+quote: "Social media made me the artist I am today by teaching me what I didn’t want to do.",
 ---
 **Mike Bennett** moved from Central Pennsylvania to Portland, Oregon in the winter of 2016. He studied Art Education, Computer Design, Illustration and Animation at Shippensburg University of Pennsylvania and proceeded to chase his dream of illustration and character design immediately after graduating in 2012. The move to Portland was an incredibly easy choice because of his passion for the outdoors and wanting to live in a community with an appreciation for art.  
 
