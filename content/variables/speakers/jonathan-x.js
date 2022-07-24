d649c369f9760410fb38df14df73e2dc8fda4122
Jedidiah <jedidiah475@gmail.com>
2022-07-24 12:09:27 -0700

    Update jonathan-x.js

--- content/variables/speakers/jonathan-x.js
+++ content/variables/speakers/jonathan-x.js
@@ -3,6 +3,8 @@ year: "WDS X (2022)",
 headerPhotos: "https://live.staticflickr.com/65535/52186151185_51518fccc9_k_d.jpg, https://live.staticflickr.com/65535/52185673848_b923b3cfda_k_d.jpg",
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/jonathan-fields-2.png",
 video: "https://youtu.be/I3eaQk6ez10",
-quote: "A good life is not a place at which you arrive, it is a lens through which you see and create your world.",
+quote: “Acknowledging the fact that we are not promised tomorrow is at once terrifying and simultaneously incredibly empowering.”,
 ---
-**Jonathan Fields** is on a quest to inspire possibility. A New York City dad, husband, entrepreneur and author, he founded mission-driven media and education venture, Good Life Project®, where he and his team lead a global community in the quest to live more meaningful, connected and vital lives, produce a top-rated podcast with millions of downloads, and offer a growing catalog of events, trainings and courses.
+Having been with WDS from the very beginning, there was no one more fitting than Jonathan to help the WDS community reflect on 10 years and look forward to what might be next.  
+  
+  **Jonathan Fields** is on a quest to inspire possibility. A New York City dad, husband, entrepreneur and author, he founded mission-driven media and education venture, Good Life Project®, where he and his team lead a global community in the quest to live more meaningful, connected and vital lives, produce a top-rated podcast with millions of downloads, and offer a growing catalog of events, trainings and courses.
