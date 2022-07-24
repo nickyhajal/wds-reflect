fb023fecb340bea14d98e9668f54e3b0ff0d6523
Jedidiah <jedidiah475@gmail.com>
2022-07-24 11:27:48 -0700

    Update ozan-varol.js

--- content/variables/speakers/ozan-varol.js
+++ content/variables/speakers/ozan-varol.js
@@ -4,8 +4,10 @@ headerPhotos: "https://live.staticflickr.com/65535/52185335066_e0c4c5d940_k_d.jp
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Ozan-Varol-WDS-Main-Stage-Speaker.png",
 video: "https://youtu.be/cOsIvqAnd20",
 headerPosition: "-30px",
-quote: "",
+quote: "Reclaiming that wisdom and awakening your genius is a key to making a dent in the universe.",
 ---
 **Ozan Varol** is a rocket scientist turned award-winning professor and bestselling author of _Think Like a Rocket Scientist: Simple Strategies You Can Use to Make Giant Leaps in Work and Life_. The book has been translated to over 20 foreign languages and has sold over 100,000 copies worldwide.  
 
-Ozan has been called a “true original” by Adam Grant and dubbed a “superhero” by Dan Pink. His work has been described as “must read” by Susan Cain and featured in the _Wall Street Journal_, _Time_, _BBC_, _CNN_, _Fast Company_, _Washington Post_, and more.
+Ozan has been called a “true original” by Adam Grant and dubbed a “superhero” by Dan Pink. His work has been described as “must read” by Susan Cain and featured in the _Wall Street Journal_, _Time_, _BBC_, _CNN_, _Fast Company_, _Washington Post_, and more.  
+
+At WDS X, Ozan shared how to awaken your genius, carve your own path as a leader and creator, escape your false imprison loops, and how to act from our imagination instead of our programming.
