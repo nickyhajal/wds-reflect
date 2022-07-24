1387f8e8cc8b59479628c27870ec8947ddf04133
Jedidiah <jedidiah475@gmail.com>
2022-07-24 12:20:34 -0700

    Update karla-starr.js

--- content/variables/speakers/karla-starr.js
+++ content/variables/speakers/karla-starr.js
@@ -4,8 +4,10 @@ headerPhotos: "https://live.staticflickr.com/65535/52185676633_0ab2bd8c3c_k_d.jp
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Karla-Starr-WDS.speaker.png",
 video: "https://youtu.be/ow1bZvVWAsc",
 headerPosition: "-40px",
-quote: "",
+quote: "We know what is difficult to us. We don’t realize all the things that have made our journey easier.",
 ---
-**Karla Starr** is a writer and Underdog Advocate. In addition to being a columnist for _Medium_, Karla also writes the newsletter, _The Starr Report_ on _Substack_.  
+At WDS X, Karla talked about the Self Help Industrial Complex – where privileged experts who have been at the top of their game are removed from the reality of their readers. She shared that we’re drawn to simple answers and yet “there is no one neat trick to become your best self.” People mistake data for the whole truth, but there are always complex factors to take into consideration.  
+  
+  **Karla Starr** is a writer and Underdog Advocate. In addition to being a columnist for _Medium_, Karla also writes the newsletter, _The Starr Report_ on _Substack_.  
 Karla has appeared on _NPR_ and _CBS Sunday Morning_, and has written for _The Atlantic_, _Slate_, _Popular Science_, and the _Guardian_.  
 Her most recent book, _Making Numbers Count: The Art and Science of Communicating Numbers_, with Chip Heath (_Switch, Made to Stick_) has been met with critical acclaim from _The Wall Street Journal_, _Kirkus Reviews_, and _Adam Grant_.  
