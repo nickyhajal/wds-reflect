163885af98bee8df05d011bb7f7b2335d8d3baff
Jedidiah <jedidiah475@gmail.com>
2022-07-24 12:11:26 -0700

    Update gary-ware.js

--- content/variables/speakers/gary-ware.js
+++ content/variables/speakers/gary-ware.js
@@ -4,9 +4,11 @@ headerPhotos: "https://live.staticflickr.com/65535/52173960937_1a70698a1d_k_d.jp
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Gary-Ware-WDS-Speaker.png",
 video: "https://youtu.be/4rqz5PqaSHY",
 headerPosition: "-170px",
-quote: "",
+quote: "Remember who you were before the world got its hands on you.",
 ---
-**Gary Ware** is a corporate facilitator, certified coach, and Creative Catalyst. He has over 14 years of experience holding leadership positions in the corporate world and nearly a decade as a performer in improv theatre.  
+Gary encouraged the WDS community to shift our thinking and unlock dormant playfulness in order to live out remarkable lives. The world doesn’t have to be a proving ground, it can be a playground of possibilities.  
+  
+  **Gary Ware** is a corporate facilitator, certified coach, and Creative Catalyst. He has over 14 years of experience holding leadership positions in the corporate world and nearly a decade as a performer in improv theatre.  
 After experiencing burnout, he realized that what was missing was play, leading him to discover his passion for facilitating.  
 
 Through his company Breakthrough Play, Gary uses the power of applied improvisation and other playful methods to assist people in unlocking creativity, confidence, and better communication and was recently featured as one of the Top 100 HR influencers of 2021 by Engagedly.  
