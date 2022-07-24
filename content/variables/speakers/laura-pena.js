cf92e8f18706b491c706f9747cc6957e6bff949b
Jedidiah <jedidiah475@gmail.com>
2022-07-24 12:27:05 -0700

    Update laura-pena.js

--- content/variables/speakers/laura-pena.js
+++ content/variables/speakers/laura-pena.js
@@ -4,6 +4,8 @@ headerPhotos: "https://live.staticflickr.com/65535/52184318732_dd0bd94a42_k_d.jp
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Laura_Pena_1_lowres.png",
 video: "https://youtu.be/szcOMGia-mg",
 headerPosition: "-80px",
-quote: "",
+quote: "I see so many incredible women around me who don’t see their own brilliance. I know this is not by chance.",
 ---
+As a past recipient of the WDS Foundation, Scholarships For Real Life, Laura shares the journey of her project – She Is the Universe. This media project with 111 girls around the world unveils their struggles and dreams and what they need to move forward. As Laura seeks “how to go from me to we”, the project has become so much more.  
+
 Laura Peña is a Dominican-American speaker, filmmaker, storyteller, entrepreneur, motion designer, traveler, and mentor to young girls. She is the founder of She Is the Universe, an award-winning Global Movement and 501(c) 3 non-profit for girl's empowerment. For three years Laura has traveled the world collecting on film the stories of teenage girls.
