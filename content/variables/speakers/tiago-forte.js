0dba09f0fd40f48ad3adca9ecb1b64dce3e02a35
Jedidiah <jedidiah475@gmail.com>
2022-07-24 11:39:48 -0700

    Update tiago-forte.js

--- content/variables/speakers/tiago-forte.js
+++ content/variables/speakers/tiago-forte.js
@@ -4,8 +4,10 @@ headerPhotos: "https://live.staticflickr.com/65535/52186024050_fc7a82a3e4_k_d.jp
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Tiago-Forte-WDS-Main-Stage-Speaker.png",
 video: "https://youtu.be/XlAqrS-fSAI",
 headerPosition: "-90px",
-quote: "",
+quote: "We’re in technology adolescence.",
 ---
+During his WDS X keynote, Tiago addresses the complexity of managing modern life in comparison to the capacity of a human brain. He suggests that technology and note-taking has potential to be an extension of ourselves, like a prosthetic device.
+  
 **Tiago Forte** is one of the world’s foremost experts on productivity. He has taught more than 20,000 people worldwide through his programs and writes and speaks on how technology can help knowledge workers revolutionize their personal effectiveness.    
 
 What started as a quest to deal with a chronic illness turned into a life-long mission to help people harness the full potential of their ideas and turn them into reality.  
