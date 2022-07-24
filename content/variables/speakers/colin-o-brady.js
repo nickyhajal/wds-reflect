2d51525a43ab64cb6ec55c94cd820ae380964880
Jedidiah <jedidiah475@gmail.com>
2022-07-24 12:16:07 -0700

    Update colin-o-brady.js

--- content/variables/speakers/colin-o-brady.js
+++ content/variables/speakers/colin-o-brady.js
@@ -4,8 +4,10 @@ headerPhotos: "https://live.staticflickr.com/65535/52185788534_0a9e579f97_k_d.jp
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/ColinOBrady.WDS.speaker.png",
 video: "https://youtu.be/eQstdeiD74E",
 headerPosition: "-80px",
-quote: "",
+quote: "We all have reservoirs of untapped potential to achieve extraordinary things, but it starts with our minds. It starts with pushing back on limiting beliefs.",
 ---
-**Colin O’Brady** is a world record holding explorer and one of the world’s best endurance athletes. Colin is regarded as a foremost expert on mindset.  
+During his WDS X keynote, Colin encourages the WDS community to embark on adventures and overcome limiting beliefs as he shared the story of his epic world-record-breaking expedition.  
+  
+  **Colin O’Brady** is a world record holding explorer and one of the world’s best endurance athletes. Colin is regarded as a foremost expert on mindset.  
 
 He isn’t your typical athlete despite his unmatched athletic accomplishments - a world first solo crossing of Antarctica, a world first row across Drake Passage, summiting Mt. Everest, and setting three prestigious mountaineering world records - and he has done it all after overcoming a devastating accident to prove that anything is possible.  
