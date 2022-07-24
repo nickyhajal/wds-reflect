0513273fb3be95c42fc326116f9f3e192ee3fa00
Jedidiah <jedidiah475@gmail.com>
2022-07-24 11:54:01 -0700

    Update emily-rudow.js

--- content/variables/speakers/emily-rudow.js
+++ content/variables/speakers/emily-rudow.js
@@ -4,8 +4,10 @@ headerPhotos: "https://live.staticflickr.com/65535/52186024810_1059aa8d5f_k_d.jp
 photo: "https://el-files-wds.s3.amazonaws.com/images/speakers/Emily-Rudow.png",
 video: "https://youtu.be/VwdsK69N05I",
 headerPosition: "-70px",
-quote: "",
+quote: "I was the author of my torment and the author of success. I had to find my stride.",
 ---
-**Emily Rudow** is a digital marketer, an entrepreneur, fitness writer, and an avid long-distance runner. In 2017, she broke a world record by running 74 consecutive half-marathon distances (21.1 kilometers/13.1 miles) while raising over $10,000 for the Canadian Cancer Society. Since then, she hasn’t stopped running. To date, she has run an average of 10 kilometers every day for over four years.  
+At WDS X, Emily shared about working through self-esteem issues, anxiety, and self-empowerment. She reflected on her personal journey of discovering her own path toward sustainable fitness.  
 
-Emily’s debut book _Find Your Stride: A Personalized Path to Sustainable Nutrition and Training_ is available now.
+  **Emily Rudow** is a digital marketer, an entrepreneur, fitness writer, and an avid long-distance runner. In 2017, she broke a world record by running 74 consecutive half-marathon distances (21.1 kilometers/13.1 miles) while raising over $10,000 for the Canadian Cancer Society. Since then, she hasn’t stopped running. To date, she has run an average of 10 kilometers every day for over four years.  
+  
+  Emily’s debut book _Find Your Stride: A Personalized Path to Sustainable Nutrition and Training_ is available now.
